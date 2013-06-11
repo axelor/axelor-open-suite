b043e9cbdf47b78944a48a0bb0d097f184a69e5b
gdu-axelor <g.dubaux@axelor.com>
2013-06-11 13:10:37 +0200

    Changed fileReceived Select in integer

---
+++ src/main/java/com/axelor/apps/organisation/db/IExpense.java
@@ -0,0 +1,22 @@
+package com.axelor.apps.organisation.db;
+
+/**
+ * Interface of Expense package. Enum all static variable of packages.
+ * 
+ * @author nguyen
+ * 
+ */
+public interface IExpense {
+
+
+	/**
+     * Static select in ExpenseLine
+     */
+
+    // STATE
+
+	static final int YES = 1;
+	static final int NO = 2;
+
+	
+}
