e2be375459a7cee81dc0f59a961e1ec6f3a2380a
gdu-axelor <g.dubaux@axelor.com>
2013-06-17 16:46:39 +0200

    baseUnitConversionService

--- src/main/java/com/axelor/apps/base/service/UnitConversionService.java
+++ src/main/java/com/axelor/apps/base/service/UnitConversionService.java
@@ -6,11 +6,14 @@ import java.util.List;
 
 import com.axelor.apps.base.db.Unit;
 import com.axelor.apps.base.db.UnitConversion;
+import com.axelor.exception.AxelorException;
 
 public class UnitConversionService {
 
 	/**
-	 * Obtenir le coefficient entre deux unités dans une liste de conversion.
+	 * Obtenir le coefficient entre deux unités dans une liste de conversion. Si l'unité de départ et l'unité
+	 * d'arrivée ne se trouve pas dans la liste alors on inverse l'unité de départ avec l'unité d'arrivée.
+	 * Si il n'y a toujours pas de résultat alors on déclenche une exception.
 	 * 
 	 * @param unitConversionList
 	 * 				La liste des unités de conversion.
@@ -22,16 +25,23 @@ public class UnitConversionService {
 	 * 				L'unité d'arrivée.
 	 * 
 	 * @return Le coefficient de conversion.
+	 * @throws AxelorException Les unités demandés ne se trouvent pas dans la liste de conversion
 	 */
-	public BigDecimal getCoefficient(List<UnitConversion> unitConversionList, Unit startUnit, Unit endUnit) {
-
+	public BigDecimal getCoefficient(List<UnitConversion> unitConversionList, Unit startUnit, Unit endUnit) throws AxelorException {
+		/* Looking for the start unit and the end unit in the unitConversionList to get the coefficient */
 		for (UnitConversion unitConversion : unitConversionList){
 			
 			if (unitConversion.getStartUnit().equals(startUnit) && unitConversion.getEndUnit().equals(endUnit)) { return unitConversion.getCoef(); }
+			
 		}
-		
-		return BigDecimal.ONE;
-		
+		/* The endUnit become the start unit and the startUnit become the end unit */
+		for (UnitConversion unitConversion : unitConversionList){
+			
+			if (unitConversion.getStartUnit().equals(endUnit) && unitConversion.getEndUnit().equals(startUnit)) { return BigDecimal.ONE.divide(unitConversion.getCoef(), 6, RoundingMode.HALF_EVEN); }
+			
+		}
+		/* If there is no startUnit and endUnit in the UnitConversion list so we throw an exception */
+		throw new AxelorException();
 	}
 	
 	/**
@@ -50,12 +60,19 @@ public class UnitConversionService {
 	 * 				La valeur à convertir.
 	 * 
 	 * @return Le coefficient de conversion.
+	 * @throws AxelorException Les unités demandés ne se trouvent pas dans la liste de conversion
 	 */
-	public BigDecimal convert(List<UnitConversion> unitConversionList, Unit startUnit, Unit endUnit, BigDecimal value) {
+	public BigDecimal convert(List<UnitConversion> unitConversionList, Unit startUnit, Unit endUnit, BigDecimal value) throws AxelorException {
 		 
-		if (startUnit == null || endUnit == null || startUnit.equals(endUnit)) { return value; }
-		else { return value.multiply(this.getCoefficient(unitConversionList, startUnit, endUnit)).setScale(6, RoundingMode.HALF_EVEN); }
-		
+		if (startUnit == null || endUnit == null)
+			throw new AxelorException();
+			
+		if (startUnit.equals(endUnit))
+			return value; 
+		else { 		
+			BigDecimal coefficient = this.getCoefficient(unitConversionList, startUnit, endUnit);		
+			
+			return value.multiply(coefficient).setScale(6, RoundingMode.HALF_EVEN);		
+		}	
 	}
-
 }
