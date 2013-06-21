867d7039337f9d63999104e48f5f1e3e5a3c4c5c
gdu-axelor <g.dubaux@axelor.com>
2013-06-21 19:14:00 +0530

    Modified ProductCatelog Report design as well controller

--- src/main/groovy/com/axelor/apps/base/web/ProductControllerSimple.groovy
+++ src/main/groovy/com/axelor/apps/base/web/ProductControllerSimple.groovy
@@ -4,6 +4,7 @@ import com.axelor.apps.AxelorSettings
 import com.axelor.apps.account.db.Invoice
 import com.axelor.apps.base.db.IAdministration;
 import com.axelor.apps.base.db.Product
+import com.axelor.apps.base.service.administration.GeneralService
 import com.axelor.apps.base.service.administration.SequenceService;
 import com.axelor.exception.AxelorException
 import com.axelor.apps.tool.net.URLService
@@ -18,7 +19,8 @@ import com.google.inject.Inject;
 
 @Slf4j
 class ProductControllerSimple {
-	
+	@Inject
+	private GeneralService generalService
 	
 	/**
 	 * Fonction appeler par le bouton imprimer
@@ -30,13 +32,14 @@ class ProductControllerSimple {
 	def void printProductCatelog(ActionRequest request, ActionResponse response) {
 
 		Product product = request.context as Product
-
+		AxelorSettings axelorSettings = AxelorSettings.get()
+		
 		StringBuilder url = new StringBuilder()
 		User user = request.context.get("__user__")
 		
-		def currentYear = "2013"
-		AxelorSettings axelorSettings = AxelorSettings.get()
+		def currentYear = generalService.getTodayDateTime().getYear()
 		String productIds = ""
+		
 		def lstSelectedPartner = request.context.get("_ids")
 		lstSelectedPartner.each {
 			productIds+= it+","
@@ -44,7 +47,7 @@ class ProductControllerSimple {
 		if(!productIds.equals("")){
 			productIds = "&ProductIds="+productIds.substring(0, productIds.size()-1)	
 		}
-		print("Context.."+request.context)
+		
 		url.append("${axelorSettings.get('axelor.report.engine', '')}/frameset?__report=report/ProductCatalog_PGQL.rptdesign&__format=pdf${productIds}&UserId=${user.id}&CurrYear=${currentYear}&__locale=fr_FR${axelorSettings.get('axelor.report.engine.datasource')}")
 		log.debug("URL : {}", url)
 		String urlNotExist = URLService.notExist(url.toString())
