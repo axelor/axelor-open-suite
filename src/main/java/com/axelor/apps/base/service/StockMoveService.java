25acf546676b7db405443310ea68c332b083ffd3
gdu-axelor <g.dubaux@axelor.com>
2013-07-04 12:04:07 +0200

    baseStockMove

---
+++ src/main/java/com/axelor/apps/base/service/StockMoveService.java
@@ -0,0 +1,65 @@
+package com.axelor.apps.base.service;
+
+import com.axelor.apps.base.db.Address;
+import com.axelor.apps.base.db.Company;
+import com.axelor.apps.base.db.IProduct;
+import com.axelor.apps.base.db.Partner;
+import com.axelor.apps.base.db.Product;
+import com.axelor.apps.base.service.administration.GeneralService;
+import com.axelor.apps.supplychain.db.IStockMove;
+import com.axelor.apps.supplychain.db.StockMove;
+import com.axelor.apps.supplychain.db.StockMoveLine;
+
+import com.google.inject.persist.Transactional;
+
+public class StockMoveService {
+
+	/**
+	 * Méthode générique permettant de créer un StockMove.
+	 * @param toAddress l'adresse destination
+	 * @param company la société
+	 * @param clientPartner le tier client
+	 * @param refSequence la séquence du StockMove
+	 * @return l'objet StockMove
+	 */
+	@Transactional
+	public StockMove createStocksMoves(Address toAddress, Company company, Partner clientPartner, String refSequence) {
+
+		StockMove stockMove = new StockMove();
+
+		stockMove.setStockMoveSeq(refSequence);
+		stockMove.setName(refSequence);
+		stockMove.setToAddress(toAddress);
+		stockMove.setCompany(company);
+		stockMove.setStatusSelect(IStockMove.CONFIRMED);
+		stockMove.setRealDate(GeneralService.getTodayDate());
+		stockMove.setEstimatedDate(GeneralService.getTodayDate());
+		stockMove.setPartner(clientPartner);
+
+		stockMove.save();
+		return stockMove;
+	}
+
+	/**
+	 * Méthode générique permettant de créer un StockMoveLine.
+	 * @param product le produit
+	 * @param quantity la quantité
+	 * @param parent le StockMove parent
+	 * @return l'objet StockMoveLine
+	 */
+	@Transactional
+	public StockMoveLine createStocksMovesLines(Product product, int quantity, StockMove parent) {
+
+		if(product != null && product.getApplicationTypeSelect() == IProduct.PRODUCT_TYPE && product.getProductTypeSelect().equals(IProduct.STOCKABLE)) {
+			
+			StockMoveLine stockMoveLine = new StockMoveLine();
+			stockMoveLine.setStockMove(parent);
+			stockMoveLine.setProduct(product);
+			stockMoveLine.setQty(quantity);
+
+			stockMoveLine.save();
+			return stockMoveLine;
+		}
+		return null;
+	}
+}
