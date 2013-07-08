d49d859c5c8058d1fbe5f2974291a28710c2b341
gdu-axelor <g.dubaux@axelor.com>
2013-07-08 13:44:26 +0200

    initialised module

---
+++ src/main/groovy/com/axelor/apps/account-organisation/web/TaskInvoiceController.groovy
@@ -0,0 +1,17 @@
+package com.axelor.apps.account-organisation.web
+
+import groovy.util.logging.Slf4j
+import com.axelor.apps.account-organisation.service.TaskInvoiceService
+import com.axelor.exception.service.TraceBackService
+import com.axelor.rpc.ActionRequest
+import com.axelor.rpc.ActionResponse
+import com.google.inject.Inject
+
+@Slf4j
+class TaskInvoiceController {
+	
+	@Inject
+	private TaskInvoiceService tis
+	
+	
+}
---
+++ src/main/java/com/axelor/apps/account-organisation/service/TaskInvoiceService.java
@@ -0,0 +1,15 @@
+package com.axelor.apps.account-organisation.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class TaskInvoiceService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(TaskInvoiceService.class);
+	
+	
+		
+	
+}
