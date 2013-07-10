311d7fc3dad199fa655c04f71274a8d82cadc281
gdu-axelor <g.dubaux@axelor.com>
2013-07-10 18:13:19 +0200

    Create Inventory From Location

--- src/main/groovy/com/axelor/apps/supplychain/web/LocationController.groovy
+++ src/main/groovy/com/axelor/apps/supplychain/web/LocationController.groovy
@@ -1,13 +1,32 @@
 package com.axelor.apps.supplychain.web
 
+import com.axelor.apps.base.db.ProductCategory
+import com.axelor.apps.base.db.ProductFamily
+import com.axelor.apps.base.db.IAdministration;
+import com.axelor.apps.base.service.administration.SequenceService;
+import com.axelor.apps.supplychain.db.Inventory
 import com.axelor.apps.supplychain.db.Location
-import com.axelor.exception.AxelorException;
-import com.axelor.rpc.ActionRequest;
-import com.axelor.rpc.ActionResponse;
-import com.google.inject.persist.Transactional;
+import com.axelor.apps.supplychain.db.LocationLine
+import com.axelor.apps.supplychain.service.LocationService
+import com.axelor.exception.AxelorException
+import com.axelor.exception.service.TraceBackService
+import com.axelor.exception.db.IException
+import com.axelor.rpc.ActionRequest
+import com.axelor.rpc.ActionResponse
+import com.axelor.rpc.Context
+import com.google.common.collect.Lists;
+import com.google.inject.Inject;
+
+import org.joda.time.LocalDate
 
 
 class LocationController {
+	
+	@Inject
+	LocationService locationService
+	
+	@Inject
+	SequenceService sequenceService;
 
 	void checkIsDefaultLocation(ActionRequest request, ActionResponse response){
 		
@@ -22,5 +41,40 @@ class LocationController {
 				response.values = ["isDefaultLocation" : false]
 			}
 		}
+	}
+	
+	void createInventory(ActionRequest request, ActionResponse response) {
+		Context context = request.context
+		LocalDate date = new LocalDate(context.inventoryDate)
+		String description = request.context.get("description")
+		int locationId = context._location.id
+		Location location = Location.find(locationId)
+		boolean excludeOutOfStock = context.excludeOutOfStock
+		boolean includeObsolete = context.includeObsolete
+		
+		
+		if (location == null)
+			throw new AxelorException("Emplacement invalide",
+							IException.CONFIGURATION_ERROR);
+		
+		ProductFamily productFamily = null
+		if (context.productFamily)
+			productFamily = ProductFamily.find(context.productFamily.id)
+			
+		ProductCategory productCategory = null
+		if (context.productCategory)
+			productCategory = ProductCategory.find(context.productCategory.id)
+			
+		def ref = sequenceService.getSequence(IAdministration.INVENTORY, location.getCompany(),false)
+		if (ref == null || ref.isEmpty())
+			throw new AxelorException("Aucune séquence configurée pour les inventaires pour la société "+location.getCompany().getName(),
+							IException.CONFIGURATION_ERROR);
+		
+		Inventory inventory = locationService.createInventory(ref, date, description, location, excludeOutOfStock,
+										includeObsolete, productFamily, productCategory)
+		
+		response.values = [
+			"inventoryId" : inventory.id
+		]
 	}
 }
