4d1c311c6cce43dcdbfdc23cf243e883fcbfd315
gdu-axelor <g.dubaux@axelor.com>
2013-07-26 13:34:37 +0200

    Added ModuleTest to run test.

---
+++ src/test/java/com/axelor/apps/base/test/TestModule.java
@@ -0,0 +1,20 @@
+package com.axelor.apps.base.test;
+
+import net.sf.ehcache.CacheManager;
+
+import com.axelor.auth.AuthModule;
+import com.axelor.db.JpaModule;
+import com.google.inject.AbstractModule;
+
+public class TestModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		// shutdown the cache manager if running : Breaking the test
+		if (CacheManager.ALL_CACHE_MANAGERS.size() > 0) {
+			CacheManager.getInstance().shutdown();
+		}
+        install(new JpaModule("testUnit", true, true));
+        install(new AuthModule.Simple());
+	}
+}
