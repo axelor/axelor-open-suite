c1633ad2f2160f8eafb6fb4961d54f46bfb5c45d
gdu-axelor <g.dubaux@axelor.com>
2013-07-26 10:55:25 +0200

    Added TemplateMaker class to generate template. Added Tests on it.

--- src/main/java/com/axelor/tool/template/TemplateMaker.java
+++ src/main/java/com/axelor/tool/template/TemplateMaker.java
@@ -1,50 +1,58 @@
 package com.axelor.tool.template;
 
-import groovy.lang.Binding;
-import groovy.lang.MissingPropertyException;
-import groovy.xml.XmlUtil;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 
-import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.joda.time.LocalTime;
+import org.stringtemplate.v4.AttributeRenderer;
+import org.stringtemplate.v4.DateRenderer;
+import org.stringtemplate.v4.Interpreter;
+import org.stringtemplate.v4.ModelAdaptor;
+import org.stringtemplate.v4.NumberRenderer;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.StringRenderer;
+import org.stringtemplate.v4.misc.ObjectModelAdaptor;
 
+import com.axelor.auth.AuthUtils;
 import com.axelor.db.Model;
 import com.axelor.db.mapper.Mapper;
 import com.axelor.db.mapper.Property;
-import com.axelor.meta.TemplateHelper;
 import com.axelor.meta.db.MetaSelectItem;
 import com.axelor.rpc.Resource;
 import com.beust.jcommander.internal.Maps;
 import com.google.common.base.Charsets;
-import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
-import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 import com.google.common.io.Files;
 
 public class TemplateMaker {
 	
-	private Class<?> entity;
-	private Binding binding;
 	private Map<String, Object> context;
 	private Map<String, Object> localContext;
 	
 	private String template;
+	private STGroup stGroup;
+	private Locale locale;
 	
-	public TemplateMaker(String template) {
-		this.setTemplate(template);
-	}
-	
-	public TemplateMaker(File template) throws FileNotFoundException {
-		this.setTemplate(template);
+	public TemplateMaker(Locale locale, char delimiterStartChar, char delimiterStopChar) {
+		this.locale = locale;
+		this.stGroup = new STGroup(delimiterStartChar,delimiterStopChar);
+		//Custom renderer
+		this.stGroup.registerModelAdaptor(Model.class, new ModelFormatRenderer());
+		this.stGroup.registerRenderer(LocalDate.class, new LocalDateRenderer());
+		this.stGroup.registerRenderer(LocalDateTime.class, new LocalDateTimeRenderer());
+		this.stGroup.registerRenderer(LocalTime.class, new LocalTimeRenderer());
+		//Default renderer provide by ST
+		this.stGroup.registerRenderer(String.class, new StringRenderer());
+		this.stGroup.registerRenderer(Number.class, new NumberRenderer());
+		this.stGroup.registerRenderer(Date.class, new DateRenderer());
 	}
 	
 	public void setContext(Model model) {
@@ -61,20 +69,17 @@ public class TemplateMaker {
 	
 	public void setContext(Model model, Map<String, Object> map, String nameInContext) {
 		Preconditions.checkNotNull(model);
-		
-		this.entity = getBeanClass(model);
 		this.context = makeContext(nameInContext, model, map);
 	}
 	
 	private Map<String, Object> makeContext(String nameInContext, Model model, Map<String, Object> map) {
 		Map<String, Object> _map = Maps.newHashMap();
-		Map<String, Object> modelMap = Resource.toMap(model);
 		
 		if(nameInContext != null) {
-			_map.put(nameInContext, modelMap);
+			_map.put(nameInContext, model);
 		}
 		else {
-			_map.putAll(modelMap);
+			_map.putAll(Resource.toMap(model));
 		}
 		
 		if(map != null) {
@@ -84,22 +89,11 @@ public class TemplateMaker {
 		return _map;
 	}
 	
-	private void setTemplate(String text) {
-		text = text.replaceAll("\\{\\{\\s*(\\w+)(\\?)?\\.([^}]*?)\\s*\\|\\s*text\\s*\\}}", "\\${__fmt__.text($1, '$3')}");
-		text = text.replaceAll("\\{\\{\\s*([^}]*?)\\s*\\|\\s*text\\s*\\}}", "\\${__fmt__.text('$1')}");
-		text = text.replaceAll("\\{\\{\\s*([^}]*?)\\s*\\|\\s*e\\s*\\}}", "\\${($1) ?: ''}");
-		
-		if (text.trim().startsWith("<?xml ")) {
-			text = text.replaceAll("\\{\\{(.*?)\\}}", "\\${__fmt__.escape($1)}");
-		}
-		else {
-			text = text.replaceAll("\\{\\{(.*?)\\}}", "\\${$1}");
-		}
-		
-		template = text;
+	public void setTemplate(String text) {
+		this.template = text;
 	}
 	
-	private void setTemplate(File file) throws FileNotFoundException {
+	public void setTemplate(File file) throws FileNotFoundException {
 		if (!file.isFile()) {
 			throw new FileNotFoundException("No such template: " + file.getName());
 		}
@@ -137,119 +131,93 @@ public class TemplateMaker {
 			throw new IllegalArgumentException("Templating can not be empty");
 		}
 		
+		ST st = new ST(stGroup, template);
+		
 		Map<String, Object> _map = Maps.newHashMap();
 		if(localContext != null && !localContext.isEmpty()) {
 			_map.putAll(localContext);
 		}
 		_map.putAll(context);
 		
-		binding = new Binding(_map) {
-			
-			@Override
-			public Object getVariable(String name) {
-				try {
-					return super.getVariable(name);
-				} catch (MissingPropertyException e) {
-					if ("__date__".equals(name))
-						return new LocalDate();
-					if ("__time__".equals(name))
-						return new LocalDateTime();
-					if ("__datetime__".equals(name))
-						return new DateTime();
-				}
-				return null;
-			}
-		};
+		//Internal context
+		_map.put("__user__", AuthUtils.getUser());
+		_map.put("__date__", new LocalDate());
+		_map.put("__time__", new LocalTime());
+		_map.put("__datetime__", new LocalDateTime());
 		
-		binding.setProperty("__fmt__", new FormatHelper());
-		return _make();
+		for (String key : _map.keySet()) {
+			st.add(key, _map.get(key));
+		}
+		
+		return _make(st);
 	}
 	
-	private String _make() {
-		return TemplateHelper.make(template, binding);
+	private String _make(ST st) {
+		return st.render(locale);
 	}
 	
-	class FormatHelper {
+	class ModelFormatRenderer implements ModelAdaptor {
 		
-		private final Logger log = LoggerFactory.getLogger(FormatHelper.class);
-		
-		public Object escape(Object value) {
-			if (value == null) {
-				return "";
-			}
-			return XmlUtil.escapeXml(value.toString());
+		private Property getProperty(Class<?> beanClass, String name) {
+			return Mapper.of(beanClass).getProperty(name);
 		}
 		
-		public String text(String expr) {
-			return getSelectTitle(entity, expr, binding.getProperty(expr));
-		}
-		
-		public String text(Object bean, String expr) {
-			if (bean == null) {
+		private String getSelectionValue(Property prop, Object o, Object value) {
+			if(value == null) {
 				return "";
-			}
-			expr = expr.replaceAll("\\?", "");
-			return getSelectTitle(bean.getClass(), expr, getValue(bean, expr));
-		}
-
-		private String getSelectTitle(Class<?> klass, String expr, Object value) {
-			if (value == null) {
-				return "";
-			}
-			Property property = this.getProperty(klass, expr);
-			if (property == null || property.getSelection() == null) {
-				return value == null ? "" : value.toString();
 			}
 			MetaSelectItem item = MetaSelectItem
 					.all()
 					.filter("self.select.name = ?1 AND self.value = ?2",
-							property.getSelection(), value).fetchOne();
+							prop.getSelection(), value).fetchOne();
+			
 			if (item != null) {
 				return item.getTitle();
 			}
 			return value == null ? "" : value.toString();
 		}
-		
-		private Property getProperty(Class<?> beanClass, String name) {
-			Iterator<String> iter = Splitter.on(".").split(name).iterator();
-			Property p = Mapper.of(beanClass).getProperty(iter.next());
-			while(iter.hasNext() && p != null) {
-				p = Mapper.of(p.getTarget()).getProperty(iter.next());
+
+		@Override
+		public Object getProperty(Interpreter interp, ST self, Object o, Object property, String propertyName) {
+			Property prop = this.getProperty(o.getClass(), (String) property);
+			ModelAdaptor adap = self.groupThatCreatedThisInstance.getModelAdaptor(ObjectModelAdaptor.class);
+			
+			if (prop == null || Strings.isNullOrEmpty(prop.getSelection())) {
+				return adap.getProperty(interp, self, o, property, propertyName);
 			}
-			return p;
-		}
-		
-		@SuppressWarnings("all")
-		private Object getValue(Object bean, String expr) {
-			if (bean == null) return null;
-			Iterator<String> iter = Splitter.on(".").split(expr).iterator();
-			Object obj = null;
-			if (bean instanceof Map) {
-				obj = ((Map) bean).get(iter.next());
-			} else {
-				obj = Mapper.of(bean.getClass()).get(bean, iter.next());
-			}
-			if(iter.hasNext() && obj != null) {
-				return getValue(obj, Joiner.on(".").join(iter));
-			}
-			return obj;
-		}
 
-		public void info(String text,  Object... params) {
-			log.info(text, params);
-		}
-
-		public void debug(String text,  Object... params) {
-			log.debug(text, params);
-		}
-
-		public void error(String text,  Object... params) {
-			log.error(text, params);
-		}
-
-		public void trace(String text,  Object... params) {
-			log.trace(text, params);
+			Object value = adap.getProperty(interp, self, o, property, propertyName);
+			return getSelectionValue(prop, o, value);
 		}
 	}
+	
+	class LocalDateRenderer implements AttributeRenderer {
 
+		@Override
+		public String toString(Object o, String formatString, Locale locale) {
+	        if ( formatString==null ) return o.toString();
+	        LocalDate ld = (LocalDate) o;
+	        return ld.toString(formatString);
+		}
+	}
+	
+	class LocalDateTimeRenderer implements AttributeRenderer {
+
+		@Override
+		public String toString(Object o, String formatString, Locale locale) {
+	        if ( formatString==null ) return o.toString();
+	        LocalDateTime ld = (LocalDateTime) o;
+	        return ld.toString(formatString);
+		}
+	}
+	
+	class LocalTimeRenderer implements AttributeRenderer {
+
+		@Override
+		public String toString(Object o, String formatString, Locale locale) {
+	        if ( formatString==null ) return o.toString();
+	        LocalTime ld = (LocalTime) o;
+	        return ld.toString(formatString);
+		}
+	}
 }
