b934474337a2dfe9f09d5f1d544a5cdddf998978
gdu-axelor <g.dubaux@axelor.com>
2013-07-29 12:14:19 +0200

    organisationProjectAffair

---
+++ src/main/java/com/axelor/apps/organisation/service/ProjectService.java
@@ -0,0 +1,36 @@
+package com.axelor.apps.organisation.service;
+
+import com.axelor.apps.organisation.db.Project;
+import com.axelor.apps.organisation.db.Task;
+import com.google.inject.persist.Transactional;
+
+public class ProjectService {
+
+	@Transactional
+	public void createDefaultTask(Project project) {
+
+		if(project.getAffairName() != null && !project.getAffairName().isEmpty()) {
+			Task findTask = Task.all().filter("name = ?", project.getAffairName()).fetchOne();
+			if(findTask == null) {
+				Task task = new Task();
+
+				task.setName(project.getAffairName());
+				task.save();
+			}
+		}
+	}
+
+	@Transactional
+	public void createPreSalesTask(Project project) {
+
+		if(project.getAffairName() != null && !project.getAffairName().isEmpty()) {
+			Task findTask = Task.all().filter("name = ?", "Avant vente "+project.getAffairName()).fetchOne();
+			if(findTask == null) {
+				Task task = new Task();
+
+				task.setName("Avant vente "+project.getAffairName());
+				task.save();
+			}
+		}
+	}
+}
