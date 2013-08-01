e01f2610dda80a384499207d30f9a86cb41b0f83
gdu-axelor <g.dubaux@axelor.com>
2013-08-01 11:45:18 +0200

    organisationProjectController

---
+++ src/main/java/com/axelor/apps/organisation/web/ProjectController.java
@@ -0,0 +1,33 @@
+package com.axelor.apps.organisation.web;
+
+import com.axelor.apps.organisation.db.Project;
+import com.axelor.apps.organisation.service.ProjectService;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.google.inject.Inject;
+
+public class ProjectController {
+
+	@Inject
+	ProjectService projectService;
+	
+	public void createDefaultTask(ActionRequest request, ActionResponse response) {
+		
+		Project project = request.getContext().asType(Project.class);
+		
+		if(project != null) {
+			
+			projectService.createDefaultTask(project);
+		}
+	}
+	
+	public void createPreSalesTask(ActionRequest request, ActionResponse response) {
+		
+		Project affair = request.getContext().asType(Project.class);
+		
+		if(affair != null) {
+			
+			projectService.createPreSalesTask(affair);
+		}
+	}
+}
