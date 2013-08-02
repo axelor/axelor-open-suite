999f3b688a5275eab3816df704d162c4f8533599
gdu-axelor <g.dubaux@axelor.com>
2013-08-02 17:51:30 +0200

    accountInvoiceLineController

---
+++ src/main/java/com/axelor/apps/account/web/InvoiceLineController.java
@@ -0,0 +1,75 @@
+package com.axelor.apps.account.web;
+
+import java.math.BigDecimal;
+
+import com.axelor.apps.account.db.Invoice;
+import com.axelor.apps.account.db.InvoiceLine;
+import com.axelor.apps.account.service.invoice.InvoiceLineService;
+import com.axelor.apps.account.service.invoice.generator.line.InvoiceLineManagement;
+import com.axelor.exception.AxelorException;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.google.inject.Inject;
+
+public class InvoiceLineController {
+
+	@Inject
+	private InvoiceLineService invoiceLineService;
+	
+	public void compute(ActionRequest request, ActionResponse response) throws AxelorException {
+
+		InvoiceLine invoiceLine = request.getContext().asType(InvoiceLine.class);
+		BigDecimal exTaxTotal = BigDecimal.ZERO;
+		BigDecimal accountingExTaxTotal = BigDecimal.ZERO;
+		
+//		try{
+		
+			if(invoiceLine.getPrice() != null && invoiceLine.getQty() != null) {
+				
+				exTaxTotal = InvoiceLineManagement.computeAmount(invoiceLine.getQty(), invoiceLine.getPrice());
+			}
+			
+			if(exTaxTotal != null) {
+				
+				Invoice invoice = invoiceLine.getInvoice();
+				
+				if(invoice == null) {
+					invoice = request.getContext().getParentContext().asType(Invoice.class);
+				}
+				
+				if(invoice != null) {
+					accountingExTaxTotal = invoiceLineService.getAccountingExTaxTotal(exTaxTotal, invoice);
+				}
+			}
+			response.setValue("exTaxTotal", exTaxTotal);
+			response.setValue("accountingExTaxTotal", accountingExTaxTotal);
+//		}
+//		catch(Exception e)  {
+//			response.flash = e
+//		}
+	}
+	
+	public void getProductInformation(ActionRequest request, ActionResponse response) throws AxelorException {
+		
+		InvoiceLine invoiceLine = request.getContext().asType(InvoiceLine.class);
+		
+		Invoice invoice = invoiceLine.getInvoice();
+		
+		if(invoice == null)  {
+			invoice = request.getContext().getParentContext().asType(Invoice.class);
+		}
+			
+		if(invoice != null && invoiceLine.getProduct() != null)  {
+			
+//			try  {
+				boolean isPurchase = invoiceLineService.isPurchase(invoice);
+				response.setValue("vatLine", invoiceLineService.getVatLine(invoice, invoiceLine, isPurchase));
+				response.setValue("price", invoiceLineService.getUnitPrice(invoice, invoiceLine, isPurchase));
+				response.setValue("productName", invoiceLine.getProduct().getName());
+//			}
+//			catch(Exception e)  {
+//				response.flash = e
+//			}
+		}
+	}
+}
