4e68cc161e64239333e01f0f0eb4ca4708cf9d1e
gdu-axelor <g.dubaux@axelor.com>
2013-08-02 12:38:24 +0200

    accountMoveController

---
+++ src/main/java/com/axelor/apps/account/web/MoveController.java
@@ -0,0 +1,53 @@
+package com.axelor.apps.account.web;
+
+import com.axelor.apps.account.db.Move;
+import com.axelor.apps.account.service.MoveService;
+import com.axelor.apps.account.service.PeriodService;
+import com.axelor.exception.service.TraceBackService;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+
+public class MoveController {
+
+//	@Inject
+//	private MoveService ms
+	
+//	@Inject
+//	private PeriodService ps
+	
+	@Inject
+	private Injector injector;
+	
+	public void validate(ActionRequest request, ActionResponse response) {
+
+		Move move = request.getContext().asType(Move.class);
+		move = Move.find(move.getId());
+		
+		MoveService ms = injector.getInstance(MoveService.class);
+		
+		try {
+			ms.validate(move);
+			response.setReload(true);
+		}
+		catch (Exception e){ TraceBackService.trace(response, e); }
+	}
+	
+	public void getPeriod(ActionRequest request, ActionResponse response) {
+		
+		Move move = request.getContext().asType(Move.class);
+	
+		try {
+			if(move.getDate() != null && move.getCompany() != null) {
+				
+				PeriodService ps = injector.getInstance(PeriodService.class);
+				response.setValue("period", ps.rightPeriod(move.getDate(), move.getCompany()));				
+			}
+			else {
+				response.setValue("period", null);
+			}
+		}
+		catch (Exception e){ TraceBackService.trace(response, e); }
+	}
+}
