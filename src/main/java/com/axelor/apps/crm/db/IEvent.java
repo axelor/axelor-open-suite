0f69fc569f20cc9c5aca7926b646a35c800cb982
gdu-axelor <g.dubaux@axelor.com>
2013-08-07 15:05:13 +0200

    Added select in IEvent.

--- src/main/java/com/axelor/apps/crm/db/IEvent.java
+++ src/main/java/com/axelor/apps/crm/db/IEvent.java
@@ -12,12 +12,21 @@ public interface IEvent {
 	/**
 	 * Static event type select
 	 */
-
 	static final int CALL = 1;
 	static final int MEETING = 2;
 	static final int TASK = 3;
 	static final int HOLIDAY = 4;
 	static final int TICKET = 5;
-
-
+	
+	/**
+	 * Static event call status select
+	 */
+	static final int CALL_INCOMING_STATUS = 1;
+	static final int CALL_OUTGOING_STATUS = 2;
+	
+	/**
+	 * Static event related to select
+	 */
+	static final String RELATED_TO_PARTNER = "com.axelor.apps.base.db.Partner";
+	static final String RELATED_TO_LEAD = "com.axelor.apps.crm.db.Lead";
 }
