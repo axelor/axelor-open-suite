b1f5be2d967887c122c07ad1bccff9be5ce95404
gdu-axelor <g.dubaux@axelor.com>
2013-09-05 20:55:22 +0200

    added message creation

---
+++ src/main/java/com/axelor/apps/message/db/IMessage.java
@@ -0,0 +1,72 @@
+package com.axelor.apps.message.db;
+/**
+ * Copyright (c) 2012-2013 Axelor. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Common Public
+ * Attribution License Version 1.0 (the “License”); you may not use
+ * this file except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://license.axelor.com/.
+ *
+ * The License is based on the Mozilla Public License Version 1.1 but
+ * Sections 14 and 15 have been added to cover use of software over a
+ * computer network and provide for limited attribution for the
+ * Original Developer. In addition, Exhibit A has been modified to be
+ * consistent with Exhibit B.
+ *
+ * Software distributed under the License is distributed on an “AS IS”
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is part of "Axelor Business Suite", developed by
+ * Axelor exclusively.
+ *
+ * The Original Developer is the Initial Developer. The Initial Developer of
+ * the Original Code is Axelor.
+ *
+ * All portions of the code written by Axelor are
+ * Copyright (c) 2012-2013 Axelor. All Rights Reserved.
+ */
+
+
+/**
+ * Interface of Event object. Enum all static variable of object.
+ * 
+ * @author dubaux
+ * 
+ */
+public interface IMessage {
+
+
+	/**
+	 * Static status select
+	 */
+	static final int STATUS_DRAFT = 1;
+	static final int STATUS_SENT = 2;
+	static final int STATUS_CANCELED = 3;
+	
+	
+	/**
+	 * Static status select
+	 */
+	static final int TYPE_RECEIVED = 1;
+	static final int TYPE_SENT = 2;
+	
+	
+	/**
+	 * Static related to select
+	 */
+	static final String RELATED_TO_PARTNER = "com.axelor.apps.base.db.Partner";
+	static final String RELATED_TO_LEAD = "com.axelor.apps.crm.db.Lead";
+	static final String RELATED_TO_OPPORTUNITY = "com.axelor.apps.crm.db.Opportunity";
+	static final String RELATED_TO_PRODUCT = "com.axelor.apps.base.db.Product";
+	static final String RELATED_TO_EVENT = "com.axelor.apps.crm.db.Event";
+	static final String RELATED_TO_SALESORDER = "com.axelor.apps.supplychain.db.SalesOrder";
+	static final String RELATED_TO_PROJECT = "com.axelor.apps.organisation.db.Project";
+	static final String RELATED_TO_TASK = "com.axelor.apps.organisation.db.Task";
+	
+	
+
+}
---
+++ src/main/java/com/axelor/apps/message/service/MessageService.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2012-2013 Axelor. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Common Public
+ * Attribution License Version 1.0 (the “License”); you may not use
+ * this file except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://license.axelor.com/.
+ *
+ * The License is based on the Mozilla Public License Version 1.1 but
+ * Sections 14 and 15 have been added to cover use of software over a
+ * computer network and provide for limited attribution for the
+ * Original Developer. In addition, Exhibit A has been modified to be
+ * consistent with Exhibit B.
+ *
+ * Software distributed under the License is distributed on an “AS IS”
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is part of "Axelor Business Suite", developed by
+ * Axelor exclusively.
+ *
+ * The Original Developer is the Initial Developer. The Initial Developer of
+ * the Original Code is Axelor.
+ *
+ * All portions of the code written by Axelor are
+ * Copyright (c) 2012-2013 Axelor. All Rights Reserved.
+ */
+package com.axelor.apps.message.service;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.axelor.apps.base.db.UserInfo;
+import com.axelor.apps.base.service.administration.GeneralService;
+import com.axelor.apps.base.service.user.UserInfoService;
+import com.axelor.apps.crm.db.Event;
+import com.axelor.apps.message.db.EmailAddress;
+import com.axelor.apps.message.db.IMessage;
+import com.axelor.apps.message.db.Message;
+import com.google.inject.Inject;
+import com.google.inject.persist.Transactional;
+
+public class MessageService {
+
+	private static final Logger LOG = LoggerFactory.getLogger(MessageService.class);
+	
+	private DateTime todayTime;
+
+	@Inject
+	public MessageService(UserInfoService userInfoService) {
+
+		this.todayTime = GeneralService.getTodayDateTime();
+	}
+	
+	@Transactional
+	public Message createMessage(Event event)  {
+		return this.createMessage(
+				event.getDescription(), 
+				null, 
+				event.getUserInfo(), 
+				IMessage.RELATED_TO_EVENT, 
+				event.getId().intValue(), 
+				event.getRelatedToSelect(), 
+				event.getRelatedToSelectId(), 
+				todayTime.toLocalDateTime(), 
+				event.getResponsibleUserInfo(), 
+				false, 
+				IMessage.STATUS_SENT, 
+				"Remind : "+event.getSubject(), 
+				IMessage.TYPE_RECEIVED)
+				.save();
+	}	
+	
+	
+	public Message createMessage(String content, EmailAddress fromEmailAddress, UserInfo recipientUserInfo, String relatedTo1Select, int relatedTo1SelectId,
+			String relatedTo2Select, int relatedTo2SelectId, LocalDateTime sendedDate, UserInfo senderUserInfo, boolean sentByEmail, int statusSelect, 
+			String subject, int typeSelect)  {
+		Message message = new Message();
+		message.setContent(content);
+		message.setFromEmailAddress(fromEmailAddress);
+		message.setRecipientUserInfo(recipientUserInfo);
+		message.setRelatedTo1Select(relatedTo1Select);
+		message.setRelatedTo1SelectId(relatedTo1SelectId);
+		message.setRelatedTo2Select(relatedTo2Select);
+		message.setRelatedTo2SelectId(relatedTo2SelectId);
+		message.setSendedDateT(sendedDate);
+		message.setSenderUserInfo(senderUserInfo);
+		message.setSentByEmail(sentByEmail);
+		message.setStatusSelect(statusSelect);
+		message.setSubject(subject);
+		message.setTypeSelect(typeSelect);
+		
+		return message;
+	}	
+	
+}
