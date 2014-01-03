7c28b1dadfca75d9e5e6542c5c3ef0bda469ba81
gdu-axelor <g.dubaux@axelor.com>
2014-01-03 11:55:12 +0100

    Update license:format

--- run-import.sh
+++ run-import.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2012-2013 Axelor. All Rights Reserved.
+# Copyright (c) 2012-2014 Axelor. All Rights Reserved.
 #
 # The contents of this file are subject to the Common Public
 # Attribution License Version 1.0 (the “License”); you may not use
@@ -27,7 +27,7 @@
 # the Original Code is Axelor.
 #
 # All portions of the code written by Axelor are
-# Copyright (c) 2012-2013 Axelor. All Rights Reserved.
+# Copyright (c) 2012-2014 Axelor. All Rights Reserved.
 #
 
 
