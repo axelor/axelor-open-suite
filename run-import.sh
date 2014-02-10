6ebf11c087f9f8171778f76d45c4086d6005305f
gdu-axelor <g.dubaux@axelor.com>
2014-02-10 12:00:03 +0100

    Updated license

--- run-import.sh
+++ run-import.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2012-2014 Axelor. All Rights Reserved.
 #
 # The contents of this file are subject to the Common Public
-# Attribution License Version 1.0 (the “License”); you may not use
+# Attribution License Version 1.0 (the "License"); you may not use
 # this file except in compliance with the License. You may obtain a
 # copy of the License at:
 #
@@ -15,7 +15,7 @@
 # Original Developer. In addition, Exhibit A has been modified to be
 # consistent with Exhibit B.
 #
-# Software distributed under the License is distributed on an “AS IS”
+# Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 # the License for the specific language governing rights and limitations
 # under the License.
