9a7190424728b878a229e4c0eccf4d9245f9cdef
gdu-axelor <g.dubaux@axelor.com>
2014-04-24 12:31:52 +0530

    Added a button Print in Lead list and form, changed lead PDF.

--- src/main/java/com/axelor/apps/crm/web/EventController.java
+++ src/main/java/com/axelor/apps/crm/web/EventController.java
@@ -39,6 +39,8 @@ import org.joda.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.axelor.apps.AxelorSettings;
+import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.IAdministration;
 import com.axelor.apps.base.service.AddressService;
 import com.axelor.apps.base.service.administration.SequenceService;
@@ -47,11 +49,17 @@ import com.axelor.apps.crm.db.Event;
 import com.axelor.apps.crm.db.IEvent;
 import com.axelor.apps.crm.db.Lead;
 import com.axelor.apps.crm.service.EventService;
+import com.axelor.apps.supplychain.db.Location;
+import com.axelor.apps.tool.net.URLService;
+import com.axelor.auth.AuthUtils;
+import com.axelor.auth.db.User;
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.db.IException;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import com.google.common.base.Strings;
 import com.google.inject.Inject;
+
 
 public class EventController {
 
@@ -239,4 +247,78 @@ public class EventController {
 		response.setReload(true);
 		
 	}
+	
+	/**
+	 * Fonction appeler par le bouton imprimer
+	 *
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public void print(ActionRequest request, ActionResponse response) {
+
+
+		Lead lead = request.getContext().asType(Lead.class );
+		String leadIds = "";
+
+		@SuppressWarnings("unchecked")
+		List<Integer> lstSelectedleads = (List<Integer>) request.getContext().get("_ids");
+		if(lstSelectedleads != null){
+			for(Integer it : lstSelectedleads) {
+				leadIds+= it.toString()+",";
+			}
+		}	
+			
+		if(!leadIds.equals("")){
+			leadIds = "&LeadId="+leadIds.substring(0, leadIds.length()-1);	
+			lead = Lead.find(new Long(lstSelectedleads.get(0)));
+		}else if(lead.getId() != null){
+			leadIds = "&LeadId="+lead.getId();			
+		}
+		
+		if(!leadIds.equals("")){
+			StringBuilder url = new StringBuilder();			
+			AxelorSettings axelorSettings = AxelorSettings.get();
+			
+			User user = AuthUtils.getUser();
+			String language = "en";
+			try {
+			
+				if(user != null && !Strings.isNullOrEmpty(user.getLanguage()))  {
+					language = user.getLanguage();
+				}
+				else if (lead.getPartner().getLanguageSelect()!= null){
+					language = lead.getPartner().getLanguageSelect()!= null? lead.getPartner().getLanguageSelect():"en";
+				} 
+			}catch(Exception e){}
+			
+			url.append(axelorSettings.get("axelor.report.engine", "")+"/frameset?__report=report/Lead.rptdesign&__format=pdf&Locale="+language+leadIds+"&__locale=fr_FR"+axelorSettings.get("axelor.report.engine.datasource"));
+
+			LOG.debug("URL : {}", url);
+			
+			String urlNotExist = URLService.notExist(url.toString());
+			if (urlNotExist == null){
+				LOG.debug("Impression de l'O.F.  "+lead.getFullName()+" : "+url.toString());
+				
+				String title = " ";
+				if(lead.getFirstName() != null)  {
+					title += lstSelectedleads == null ? "Lead "+lead.getFirstName():"Leads";
+				}
+				
+				Map<String,Object> mapView = new HashMap<String,Object>();
+				mapView.put("title", title);
+				mapView.put("resource", url);
+				mapView.put("viewType", "html");
+				response.setView(mapView);	
+					
+			}
+			else {
+				response.setFlash(urlNotExist);
+			}
+		}else{
+			response.setFlash("Please select the Lead(s) to print.");
+		}	
+	}
+	
+	
 }
