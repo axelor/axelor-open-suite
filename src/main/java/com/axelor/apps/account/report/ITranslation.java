0cc8550beccc4657686fa3345fa67118a3b981e4
gdu-axelor <g.dubaux@axelor.com>
2014-05-21 19:12:48 +0530

    Static translation strings for reports added

---
+++ src/main/java/com/axelor/apps/account/report/ITranslation.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2012-2014 Axelor. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Common Public
+ * Attribution License Version 1.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://license.axelor.com/.
+ *
+ * The License is based on the Mozilla Public License Version 1.1 but
+ * Sections 14 and 15 have been added to cover use of software over a
+ * computer network and provide for limited attribution for the
+ * Original Developer. In addition, Exhibit A has been modified to be
+ * consistent with Exhibit B.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is part of "Axelor Business Suite", developed by
+ * Axelor exclusively.
+ *
+ * The Original Developer is the Initial Developer. The Initial Developer of
+ * the Original Code is Axelor.
+ *
+ * All portions of the code written by Axelor are
+ * Copyright (c) 2012-2014 Axelor. All Rights Reserved.
+ */
+package com.axelor.apps.account.report;
+
+public interface ITranslation {
+
+	
+	public static final String INVOICE_DATE = /*$$(*/ "Invoice.date"; /*)*/
+	public static final String INVOICE_PAYMENT_TERMS = /*$$(*/ "Invoice.paymentTerms"; /*)*/
+	public static final String INVOICE_DUE_DATE = /*$$(*/ "Invoice.dueDate"; /*)*/
+	public static final String INVOICE_DOCUMENT_REF = /*$$(*/ "Invoice.documentRef"; /*)*/
+	public static final String INVOICE_SUPPLIER = /*$$(*/ "Invoice.supplier"; /*)*/
+	public static final String INVOICE_PRODUCT_DESCRIPTION = /*$$(*/ "Invoice.productDescription"; /*)*/
+	public static final String INVOICE_TAX = /*$$(*/ "Invoice.tax"; /*)*/
+	public static final String INVOICE_QTY_UNIT = /*$$(*/ "Invoice.qtyUnit"; /*)*/
+	public static final String INVOICE_UNIT_PRICE = /*$$(*/ "Invoice.unitPrice"; /*)*/
+	public static final String INVOICE_PRICE_EXCL_TAX = /*$$(*/ "Invoice.priceExclTax"; /*)*/
+	public static final String INVOICE_BASE = /*$$(*/ "Invoice.base"; /*)*/
+	public static final String INVOICE_TAX_AMOUNT = /*$$(*/ "Invoice.taxAmount"; /*)*/
+	public static final String INVOICE_TOTAL_EXCL_TAX = /*$$(*/ "Invoice.totalExclTax"; /*)*/
+	public static final String INVOICE_TOTAL_TAX = /*$$(*/ "Invoice.totalTax"; /*)*/
+	public static final String INVOICE_TOTAL_INCL_TAX = /*$$(*/ "Invoice.totalInclTax"; /*)*/
+	public static final String INVOICE_NOTE = /*$$(*/ "Invoice.note"; /*)*/
+	public static final String INVOICE_CHEQUE = /*$$(*/ "Invoice.cheque"; /*)*/
+	public static final String INVOICE_BANK_DETAILS = /*$$(*/ "Invoice.bankDetails"; /*)*/
+	public static final String INVOICE_BANKING_INFO = /*$$(*/ "Invoice.bankingInfo"; /*)*/
+	public static final String INVOICE_CLIENT_REF = /*$$(*/ "Invoice.clientRef"; /*)*/
+	public static final String INVOICE_SUPPLY_REF = /*$$(*/ "Invoice.supplyRef"; /*)*/
+	public static final String INVOICE_PURCHASE_INVOICE_NO = /*$$(*/ "Invoice.purchaseInvoiceNo"; /*)*/
+	public static final String INVOICE_PURCHASE_REFUND_NO = /*$$(*/ "Invoice.purchaseRefundNo"; /*)*/
+	public static final String INVOICE_INVOICE_NO = /*$$(*/ "Invoice.invoiceNo"; /*)*/
+	public static final String INVOICE_REFUND_NO = /*$$(*/ "Invoice.refundNo"; /*)*/
+	
+	
+	
+	
+}
