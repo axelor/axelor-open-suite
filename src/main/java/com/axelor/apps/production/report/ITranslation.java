d17e25764c2fa9be9873a754138678830cfc7c27
gdu-axelor <g.dubaux@axelor.com>
2014-05-21 19:17:10 +0530

    Static translation strings for reports added

---
+++ src/main/java/com/axelor/apps/production/report/ITranslation.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2012-2014 Axelor. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Common Public
+ * Attribution License Version 1.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://license.axelor.com/.
+ *
+ * The License is based on the Mozilla Public License Version 1.1 but
+ * Sections 14 and 15 have been added to cover use of software over a
+ * computer network and provide for limited attribution for the
+ * Original Developer. In addition, Exhibit A has been modified to be
+ * consistent with Exhibit B.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is part of "Axelor Business Suite", developed by
+ * Axelor exclusively.
+ *
+ * The Original Developer is the Initial Developer. The Initial Developer of
+ * the Original Code is Axelor.
+ *
+ * All portions of the code written by Axelor are
+ * Copyright (c) 2012-2014 Axelor. All Rights Reserved.
+ */
+package com.axelor.apps.production.report;
+
+public interface ITranslation {
+
+	public static final String OPERATION_ORDER_TITLE = /*$$(*/ "OperationOrder.title"; /*)*/
+	public static final String OPERATION_ORDER_OPERATION_NAME = /*$$(*/ "OperationOrder.operationName"; /*)*/
+	public static final String OPERATION_ORDER_RELATED_TO_PRODUCT = /*$$(*/ "OperationOrder.relatedToProduct"; /*)*/
+	public static final String OPERATION_ORDER_ASSOCIATED_BOM = /*$$(*/ "OperationOrder.associatedBOM"; /*)*/
+	public static final String OPERATION_ORDER_DATES = /*$$(*/ "OperationOrder.dates"; /*)*/
+	public static final String OPERATION_ORDER_PLANNED_START_DATE = /*$$(*/ "OperationOrder.plannedStartDate"; /*)*/
+	public static final String OPERATION_ORDER_PLANNED_END_DATE = /*$$(*/ "OperationOrder.plannedEndDate"; /*)*/
+	public static final String OPERATION_ORDER_REAL_START_DATE = /*$$(*/ "OperationOrder.realStartDate"; /*)*/
+	public static final String OPERATION_ORDER_REAL_END_DATE = /*$$(*/ "OperationOrder.realEndDate"; /*)*/
+	public static final String OPERATION_ORDER_OPERATION_REQUIREMENT = /*$$(*/ "OperationOrder.operationRequirement"; /*)*/
+	public static final String OPERATION_ORDER_MACHINE = /*$$(*/ "OperationOrder.machine"; /*)*/
+	public static final String OPERATION_ORDER_CYCLE_DURATION = /*$$(*/ "OperationOrder.cycleDuration"; /*)*/
+	public static final String OPERATION_ORDER_START_TIME = /*$$(*/ "OperationOrder.startTime"; /*)*/
+	public static final String OPERATION_ORDER_STOP_TIME = /*$$(*/ "OperationOrder.stopTime"; /*)*/
+	public static final String OPERATION_ORDER_DELAY_BTW_CYCLE = /*$$(*/ "OperationOrder.delayBtwCycle"; /*)*/
+	public static final String OPERATION_ORDER_OPERATION_DETAILS = /*$$(*/ "OperationOrder.operationDetails"; /*)*/
+	public static final String OPERATION_ORDER_PRODUCT_PREPARE = /*$$(*/ "OperationOrder.productToPrepare"; /*)*/
+	public static final String OPERATION_ORDER_PRODUCT_NAME = /*$$(*/ "OperationOrder.productName"; /*)*/
+	public static final String OPERATION_ORDER_QTY = /*$$(*/ "OperationOrder.qty"; /*)*/
+	public static final String OPERATION_ORDER_TRACKING_NUMBER = /*$$(*/ "OperationOrder.trackingNumber"; /*)*/
+	public static final String OPERATION_ORDER_NUM = /*$$(*/ "OperationOrder.num"; /*)*/
+	
+	public static final String MANUF_ORDER_TITLE = /*$$(*/ "ManufOrder.title"; /*)*/
+	public static final String MANUF_ORDER_PLANNED_START_DATE = /*$$(*/ "ManufOrder.plannedStartDate"; /*)*/
+	public static final String MANUF_ORDER_PRODUCT = /*$$(*/ "ManufOrder.product"; /*)*/
+	public static final String MANUF_ORDER_QUANTITY = /*$$(*/ "ManufOrder.quantity"; /*)*/
+	public static final String MANUF_ORDER_ORIGIN = /*$$(*/ "ManufOrder.origin"; /*)*/
+	public static final String MANUF_ORDER_BILL_OF_MATERIAL = /*$$(*/ "ManufOrder.billOfMaterial"; /*)*/
+	public static final String MANUF_ORDER_PRODUCTION_PROCESS = /*$$(*/ "ManufOrder.productionProcess"; /*)*/
+	public static final String MANUF_ORDER_OPERATIONS = /*$$(*/ "ManufOrder.operations"; /*)*/
+	public static final String MANUF_ORDER_FORECAST = /*$$(*/ "ManufOrder.forecast"; /*)*/
+	public static final String MANUF_ORDER_EFFECTIVE = /*$$(*/ "ManufOrder.effective"; /*)*/
+	public static final String MANUF_ORDER_NUM = /*$$(*/ "ManufOrder.num"; /*)*/
+	public static final String MANUF_ORDER_MACHINE = /*$$(*/ "ManufOrder.machine"; /*)*/
+	public static final String MANUF_ORDER_OPERATION = /*$$(*/ "ManufOrder.operation"; /*)*/
+	public static final String MANUF_ORDER_START_DATE = /*$$(*/ "ManufOrder.startDate"; /*)*/
+	public static final String MANUF_ORDER_END_DATE = /*$$(*/ "ManufOrder.endDate"; /*)*/
+	public static final String MANUF_ORDER_STATUS = /*$$(*/ "ManufOrder.status"; /*)*/
+	public static final String MANUF_ORDER_PRODUCTS_TO_PREPARE = /*$$(*/ "ManufOrder.productsToPrepare"; /*)*/
+	public static final String MANUF_ORDER_PRODUCT_CODE = /*$$(*/ "ManufOrder.productCode"; /*)*/
+	public static final String MANUF_ORDER_PRODUCT_NAME = /*$$(*/ "ManufOrder.productName"; /*)*/
+	public static final String MANUF_ORDER_TRACKING_NUMBER = /*$$(*/ "ManufOrder.trackingNumber"; /*)*/
+}
