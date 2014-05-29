86685e8dc86d3bb7ea1bdaff4cf3c694f8458309
gdu-axelor <g.dubaux@axelor.com>
2014-05-29 18:39:40 +0530

    Update Translation Data.

---
+++ src/main/java/com/axelor/apps/crm/db/report/ITranslation.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2012-2014 Axelor. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Common Public
+ * Attribution License Version 1.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://license.axelor.com/.
+ *
+ * The License is based on the Mozilla Public License Version 1.1 but
+ * Sections 14 and 15 have been added to cover use of software over a
+ * computer network and provide for limited attribution for the
+ * Original Developer. In addition, Exhibit A has been modified to be
+ * consistent with Exhibit B.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is part of "Axelor Business Suite", developed by
+ * Axelor exclusively.
+ *
+ * The Original Developer is the Initial Developer. The Initial Developer of
+ * the Original Code is Axelor.
+ *
+ * All portions of the code written by Axelor are
+ * Copyright (c) 2012-2014 Axelor. All Rights Reserved.
+ */
+package com.axelor.apps.crm.db.report;
+
+public interface ITranslation {
+
+
+	public static final String LEAD_HEADER = /*$$(*/ "Lead.header"; /*)*/
+	public static final String LEAD_NAME = /*$$(*/ "Lead.name"; /*)*/
+	public static final String LEAD_TITLE = /*$$(*/ "Lead.title"; /*)*/
+	public static final String LEAD_EMAIL = /*$$(*/ "	Lead.email"; /*)*/
+	public static final String LEAD_PHONE = /*$$(*/ "Lead.phone"; /*)*/
+	public static final String LEAD_FAX = /*$$(*/ "Lead.fax"; /*)*/
+	public static final String LEAD_LEAD_OWNER = /*$$(*/ "Lead.lead_owner"; /*)*/
+	public static final String LEAD_COMPANY = /*$$(*/ "Lead.company"; /*)*/
+	public static final String LEAD_INDUSTRY = /*$$(*/ "Lead.industry"; /*)*/
+	public static final String LEAD_SOURCE = /*$$(*/ "Lead.source"; /*)*/
+	public static final String LEAD_STATUS = /*$$(*/ "Lead.status"; /*)*/
+	public static final String LEAD_ADDRESS_INFORMATION = /*$$(*/ "Lead.address_information"; /*)*/
+	public static final String LEAD_PRIMARY_ADDRESS = /*$$(*/ "Lead.primary_address"; /*)*/
+	public static final String LEAD_OTHER_ADDRESS = /*$$(*/ "Lead.other_address"; /*)*/
+
+}
