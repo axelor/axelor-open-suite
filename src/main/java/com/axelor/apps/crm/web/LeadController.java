b4f80f66ab621f8af40eaec3712b39367e176478
gdu-axelor <g.dubaux@axelor.com>
2014-06-17 19:37:08 +0530

    check duplicate feature generalized and moved to general

--- src/main/java/com/axelor/apps/crm/web/LeadController.java
+++ src/main/java/com/axelor/apps/crm/web/LeadController.java
@@ -18,14 +18,9 @@
 package com.axelor.apps.crm.web;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import javax.persistence.Query;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,16 +33,8 @@ import com.axelor.apps.crm.db.report.IReport;
 import com.axelor.apps.tool.net.URLService;
 import com.axelor.auth.AuthUtils;
 import com.axelor.auth.db.User;
-import com.axelor.db.JPA;
-import com.axelor.db.Model;
-import com.axelor.meta.db.MetaField;
-import com.axelor.meta.db.MetaModel;
-import com.axelor.meta.schema.actions.ActionView;
-import com.axelor.meta.schema.views.Field;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
-import com.google.common.base.CaseFormat;
-import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 import com.google.inject.Inject;
 
@@ -154,61 +141,5 @@ public class LeadController {
 		mapView.put("viewType", "html");		
 		response.setView(mapView);
 	}	
-	
-	public Set<MetaField> setFields(String model) throws IOException {
-		LOG.debug("Model: {}",model);
-		Set<MetaField> fieldSet = new HashSet<MetaField>();
-		List<String> fields = new ArrayList<String>();
-		for(MetaField field :MetaField.all_().filter("metaModel.fullName = ?1 AND (relationship = null OR relationship = 'ManyToOne')",model).fetch()){
-			fieldSet.add(field);
-			fields.add(field.getName());
-		}
-			
-		LOG.debug("Fields set: {}",fields);
-		return fieldSet;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public void showDuplicate(ActionRequest request, ActionResponse response){
-		String object = (String) request.getContext().get("object");
-		LOG.debug("Duplicate record model: {}",object);
-		MetaModel model =  MetaModel.all_().filter("fullName = ?1", object).fetchOne();
-		if(model == null){
-			response.setFlash("No meta model");
-			return;
-		}
-		String table = model.getTableName();
-		LOG.debug("Duplicate record table: {}",table);
-		List<String> fieldList = new ArrayList<String>();
-		List<String> joinList = new ArrayList<String>();
-		for(HashMap<String,Object> field:(List<HashMap<String,Object>>) request.getContext().get("fieldsSet")){
-			if((Boolean)field.get("selected")){
-				String name = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, (String)field.get("name"));
-				fieldList.add(name);
-				joinList.add(String.format("model.%s=dmodel.%s",name,name));
-			}
-		}
-		if(fieldList.isEmpty()){
-			response.setFlash("Please select key fields to check duplicate");
-			return;
-		}
-		String fields =  Joiner.on(",").join(fieldList);
-		LOG.debug("Duplicate record fieldList: {}",fields);
-		LOG.debug("Duplicate record joinList: {}",joinList);
-		Query query = JPA.em().createNativeQuery(String.format("SELECT id FROM %s model join (SELECT %s FROM %s GROUP BY %s HAVING COUNT(*) > 1) dmodel on (%s)", table,fields,table,fields,Joiner.on(" AND ").join(joinList)));
-		List<String> ids = new ArrayList<String>();
-		for(Object id : query.getResultList())
-			ids.add(id.toString());
-		if(ids.isEmpty())
-			response.setFlash("No duplicate records found");
-		else
-			response.setView(ActionView
-					  .define("Duplicate records")
-					  .model(object)
-					  .add("grid", "lead-grid")
-					  .add("form", "lead-form")
-					  .domain("self.id in ("+Joiner.on(",").join(ids)+")")
-					  .map());
-	}
 	
 }
