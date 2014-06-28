6e8317925a4fe2d7d066aed2b0d89e3c0f80e574
gdu-axelor <g.dubaux@axelor.com>
2014-06-28 18:57:58 +0530

    Move template added

---
+++ src/main/java/com/axelor/apps/account/web/MoveTemplateController.java
@@ -0,0 +1,86 @@
+/**
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2012-2014 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.axelor.apps.account.web;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.axelor.apps.account.db.Move;
+import com.axelor.apps.account.db.MoveTemplate;
+import com.axelor.apps.account.db.MoveTemplateLine;
+import com.axelor.apps.account.service.MoveTemplateService;
+import com.axelor.apps.account.service.invoice.InvoiceService;
+import com.axelor.meta.schema.actions.ActionView;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.google.common.base.Joiner;
+import com.google.inject.Inject;
+
+public class MoveTemplateController {
+	private static final Logger LOG = LoggerFactory.getLogger(MoveTemplateController.class);
+	
+	@Inject
+	MoveTemplateService mts;
+	
+	public void checkValidity(ActionRequest request, ActionResponse response){
+		MoveTemplate moveTemplate = request.getContext().asType(MoveTemplate.class);
+		moveTemplate = MoveTemplate.find(moveTemplate.getId());
+		BigDecimal creditPercent = BigDecimal.ZERO;
+		BigDecimal debitPercent = BigDecimal.ZERO;
+		for(MoveTemplateLine line : moveTemplate.getMoveTemplateLineList()){
+			LOG.debug("Adding percent: {}",line.getPercentage());
+			if(line.getDebitCreditSelect().equals("0"))
+				debitPercent = debitPercent.add(line.getPercentage());
+			else	
+				creditPercent = creditPercent.add(line.getPercentage());
+		}
+		
+		LOG.debug("Credit percent: {}, Debit percent: {}",new Object[]{creditPercent,debitPercent});
+		if(creditPercent.compareTo(BigDecimal.ZERO) != 0 && debitPercent.compareTo(BigDecimal.ZERO) != 0 && creditPercent.compareTo(debitPercent) == 0){
+			mts.validateMoveTemplateLine(moveTemplate);
+			response.setReload(true);
+		}
+		else
+			response.setFlash("Template move is not balanced");
+			
+	}
+	
+	public void generateMove(ActionRequest request, ActionResponse response){
+		HashMap<String,Object> moveTemplateMap = (HashMap<String, Object>) request.getContext().get("moveTemplate");
+		MoveTemplate moveTemplate = MoveTemplate.find(Long.parseLong(moveTemplateMap.get("id").toString()));
+		List<HashMap<String,Object>> dataList = (List<HashMap<String, Object>>) request.getContext().get("dataInputList");
+		LOG.debug("MoveTemplate : {}",moveTemplate);
+		LOG.debug("Data inputlist : {}",dataList);
+		List<Long> moveList = mts.generateMove(moveTemplate, dataList);
+		if(moveList == null)
+			response.setFlash("Error in move generation");
+		else
+			response.setView(ActionView
+		            .define("Generated moves")
+		            .model(Move.class.getName())
+		            .add("grid", "move-grid")
+		            .add("form", "move-form")
+		            .domain("self.id in ("+Joiner.on(",").join(moveList)+")")
+		            .map());
+		
+	}
+}
