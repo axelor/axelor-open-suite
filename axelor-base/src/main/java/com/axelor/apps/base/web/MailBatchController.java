e2f7940462e6946f05eb4bdd810d2d3cd965ef35
Jonathan Michelon <j.michelon@axelor.com>
2015-05-04 10:11:15 +0200

    BATCH : Mail Batch Fixed RM#3278

--- axelor-base/src/main/java/com/axelor/apps/base/web/MailBatchController.java
+++ axelor-base/src/main/java/com/axelor/apps/base/web/MailBatchController.java
@@ -2,7 +2,9 @@ package com.axelor.apps.base.web;
 
 import com.axelor.apps.base.db.Batch;
 import com.axelor.apps.base.db.MailBatch;
+import com.axelor.apps.base.db.repo.MailBatchRepository;
 import com.axelor.apps.base.service.batch.MailBatchService;
+import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.google.inject.Inject;
@@ -19,6 +21,19 @@ public class MailBatchController{
 		Batch batch = null;
 		
 		batch = mailBatchService.remindMail(mailBatchService.find(mailBatch.getId()));
+		
+		if(batch != null)
+			response.setFlash(batch.getComment());
+		response.setReload(true);
+	}
+	
+	public void remindTimesheetGeneral(ActionRequest request, ActionResponse response){
+		
+		MailBatch mailBatch = Beans.get(MailBatchRepository.class).findByCode(MailBatchRepository.CODE_BATCH_EMAIL_TIME_SHEET);
+		
+		Batch batch = null;
+		
+		batch = mailBatchService.remindMail(mailBatch);
 		
 		if(batch != null)
 			response.setFlash(batch.getComment());
