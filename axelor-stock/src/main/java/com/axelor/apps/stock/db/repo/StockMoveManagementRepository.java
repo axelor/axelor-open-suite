f1f3c8d8aa69c7862defcbd91ccfb881b8eda694
sra-axelor <s.rabere@axelor.com>
2015-06-06 14:33:54 +0200

    hotfix on stockmove copy The copy method was overrided but the modifications was made on the original stockmove instead of the new copy stockmove

--- axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/StockMoveManagementRepository.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/StockMoveManagementRepository.java
@@ -3,12 +3,16 @@ package com.axelor.apps.stock.db.repo;
 import com.axelor.apps.stock.db.StockMove;
 
 public class StockMoveManagementRepository extends StockMoveRepository {
- @Override
-public StockMove copy(StockMove entity, boolean deep) {
-	entity.setStatusSelect(1);
-	entity.setStockMoveSeq(null);
-	entity.setName(null);
-	entity.setRealDate(null);
-	return super.copy(entity, deep);
-}
+	@Override
+	public StockMove copy(StockMove entity, boolean deep) {
+
+		StockMove copy = super.copy(entity, deep);
+
+		copy.setStatusSelect(1);
+		copy.setStockMoveSeq(null);
+		copy.setName(null);
+		copy.setRealDate(null);
+
+		return copy;
+	}
 }
