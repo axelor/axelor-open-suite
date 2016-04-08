dc184cb657fdf7a29ab2517d75de73a584465691
Antoine Mahieu <a.mahieu@axelor.com>
2016-04-08 18:27:59 +0200

    #5195 : improve ProjectTask views and add save override

---
+++ axelor-project/src/main/java/com/axelor/apps/project/db/repo/ProjectTaskManagementRepository.java
@@ -0,0 +1,25 @@
+package com.axelor.apps.project.db.repo;
+
+import com.axelor.apps.project.db.ProjectTask;
+import com.google.common.base.Strings;
+
+public class ProjectTaskManagementRepository extends ProjectTaskRepository {
+	
+	
+	@Override
+	public ProjectTask save(ProjectTask projectTask){
+		
+		projectTask.setFullName(projectTask.getCode() + " " + projectTask.getName());
+		
+		if (projectTask.getChildProjectTaskList() != null && !projectTask.getChildProjectTaskList().isEmpty()){
+			for (ProjectTask child : projectTask.getChildProjectTaskList()) {
+				String code = ( Strings.isNullOrEmpty(child.getCode()) ) ? "" : child.getCode() + " ";
+				String name = ( Strings.isNullOrEmpty(child.getName()) ) ? "" : child.getName();
+				child.setFullName(code + name);
+			}
+		}
+		
+		return super.save(projectTask);
+	}
+
+}
