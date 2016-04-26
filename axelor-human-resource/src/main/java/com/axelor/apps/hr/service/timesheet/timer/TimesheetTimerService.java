ec5ab4717eccba097d51a0a43b8552fb6122a121
Franklin Zirah <f.zirah@axelor.com>
2016-04-26 19:27:12 +0200

    add some files

---
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/timesheet/timer/TimesheetTimerService.java
@@ -0,0 +1,12 @@
+package com.axelor.apps.hr.service.timesheet.timer;
+
+import com.axelor.apps.hr.db.TSTimer;
+
+public interface TimesheetTimerService {
+
+	public void pause(TSTimer timer);
+	public void stop(TSTimer timer);
+	public void calculateDuration(TSTimer timer);
+	public void generateTimesheetLine(TSTimer timer);
+	
+}
