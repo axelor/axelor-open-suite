beed825d5006e4ce84f5c31c35577e48957e98b5
soukaina <s.sbitti@axelor.com>
2016-05-10 15:40:08 +0200

    #5288 & #5351: update

--- axelor-business-production/src/main/java/com/axelor/apps/businessproduction/service/InvoicingProjectServiceBusinessProdImpl.java
+++ axelor-business-production/src/main/java/com/axelor/apps/businessproduction/service/InvoicingProjectServiceBusinessProdImpl.java
@@ -6,15 +6,10 @@ import org.joda.time.LocalDateTime;
 
 import com.axelor.apps.businessproject.service.InvoicingProjectService;
 import com.axelor.apps.businessproject.db.InvoicingProject;
-import com.axelor.apps.businessproject.db.repo.ElementsToInvoiceRepository;
-import com.axelor.apps.hr.db.repo.ExpenseLineRepository;
-import com.axelor.apps.hr.db.repo.TimesheetLineRepository;
 import com.axelor.apps.production.db.repo.ManufOrderRepository;
 import com.axelor.apps.project.db.ProjectTask;
 import com.axelor.apps.project.db.repo.ProjectTaskRepository;
 import com.axelor.apps.project.service.ProjectTaskService;
-import com.axelor.apps.purchase.db.repo.PurchaseOrderLineRepository;
-import com.axelor.apps.sale.db.repo.SaleOrderLineRepository;
 import com.axelor.inject.Beans;
 
 public class InvoicingProjectServiceBusinessProdImpl extends InvoicingProjectService{
@@ -32,37 +27,20 @@ public class InvoicingProjectServiceBusinessProdImpl extends InvoicingProjectSer
 		for (ProjectTask projectTaskChild : projectTaskChildrenList) {
 			this.setLines(invoicingProject, projectTaskChild, counter);
 		}
-
 		return;
 	}
 	
 	@Override
 	public void fillLines(InvoicingProject invoicingProject,ProjectTask projectTask){
-		if(projectTask.getProjTaskInvTypeSelect() == ProjectTaskRepository.INVOICING_TYPE_FLAT_RATE || projectTask.getProjTaskInvTypeSelect() == ProjectTaskRepository.INVOICING_TYPE_TIME_BASED)  {
-			
-			invoicingProject.getSaleOrderLineSet().addAll(Beans.get(SaleOrderLineRepository.class)
-					.all().filter("self.saleOrder.project = ?1 AND self.toInvoice = true AND self.invoiced = false AND (self.saleOrder.creationDate < ?2 or ?3 is null)", projectTask, invoicingProject.getDeadlineDate(),invoicingProject.getDeadlineDate()).fetch());
-			
-			invoicingProject.getPurchaseOrderLineSet().addAll(Beans.get(PurchaseOrderLineRepository.class)
-					.all().filter("self.projectTask = ?1 AND self.toInvoice = true AND self.invoiced = false AND (self.purchaseOrder.orderDate < ?2 or ?3 is null)", projectTask, invoicingProject.getDeadlineDate(),invoicingProject.getDeadlineDate()).fetch());
-			
-			invoicingProject.getLogTimesSet().addAll(Beans.get(TimesheetLineRepository.class)
-					.all().filter("self.affectedToTimeSheet.statusSelect = 3 AND self.projectTask = ?1 AND self.toInvoice = true AND self.invoiced = false AND (self.date < ?2 or ?3 is null)", projectTask, invoicingProject.getDeadlineDate(),invoicingProject.getDeadlineDate()).fetch());
-			
-			invoicingProject.getExpenseLineSet().addAll(Beans.get(ExpenseLineRepository.class)
-					.all().filter("self.projectTask = ?1 AND self.toInvoice = true AND self.invoiced = false AND (self.expenseDate < ?2 or ?3 is null)", projectTask, invoicingProject.getDeadlineDate(),invoicingProject.getDeadlineDate()).fetch());
-			
-			invoicingProject.getElementsToInvoiceSet().addAll(Beans.get(ElementsToInvoiceRepository.class)
-					.all().filter("self.project = ?1 AND self.toInvoice = true AND self.invoiced = false AND (self.date < ?2 or ?3 is null)", projectTask, invoicingProject.getDeadlineDate(),invoicingProject.getDeadlineDate()).fetch());
-			
+		super.fillLines(invoicingProject, projectTask);
+		if(projectTask.getProjTaskInvTypeSelect() == ProjectTaskRepository.INVOICING_TYPE_FLAT_RATE || projectTask.getProjTaskInvTypeSelect() == ProjectTaskRepository.INVOICING_TYPE_TIME_BASED)  { 
 			LocalDateTime deadlineDateToDateTime = null;
+			
 			if (invoicingProject.getDeadlineDate() != null){
 				deadlineDateToDateTime = LocalDateTime.fromDateFields(invoicingProject.getDeadlineDate().toDate());
 			}
 			invoicingProject.getManufOrderSet().addAll(Beans.get(ManufOrderRepository.class)
 					.all().filter("self.productionOrder.projectTask = ?1 AND (self.realStartDateT < ?2 or ?3 is null)", projectTask, deadlineDateToDateTime, invoicingProject.getDeadlineDate()).fetch());
-			if(projectTask.getProjTaskInvTypeSelect() == ProjectTaskRepository.INVOICING_TYPE_FLAT_RATE && !projectTask.getInvoiced())
-				invoicingProject.addProjectTaskSetItem(projectTask);
 		}
 	}
 	
