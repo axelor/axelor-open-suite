3102c2a17a9d092f2b7fc9d79e626bdfeb8fa15d
soukaina <s.sbitti@axelor.com>
2016-09-29 21:11:57 +0200

    Updated BankOrders and payment mode

--- axelor-account/src/main/java/com/axelor/apps/account/web/BankOrderController.java
+++ axelor-account/src/main/java/com/axelor/apps/account/web/BankOrderController.java
@@ -11,6 +11,7 @@ import com.axelor.apps.account.exception.IExceptionMessage;
 import com.axelor.apps.account.service.bankOrder.BankOrderService;
 import com.axelor.db.JPA;
 import com.axelor.exception.AxelorException;
+import com.axelor.exception.service.TraceBackService;
 import com.axelor.i18n.I18n;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
@@ -20,19 +21,21 @@ public class BankOrderController {
 	
 	private final Logger log = LoggerFactory.getLogger( getClass() );
 	
-	
 	@Inject
 	protected BankOrderService bankOrderService;
+	
 	
 	
 	public void validate(ActionRequest request, ActionResponse response ) throws AxelorException{
 		
 		BankOrder bankOrder = request.getContext().asType(BankOrder.class);
 		
-		log.debug("BANK ORDER LINES {}", bankOrder.getBankOrderLineList().size());
-		log.debug("BANK ORDER TYPE {}", bankOrder.getOrderType());
+		try {
+			bankOrderService.validate(bankOrder);
+		} catch (Exception e) {
+			TraceBackService.trace(response, e);
+		}
 		
-		bankOrderService.validate(bankOrder);
 		//TODO create sequence for bank order and bank order line
 		//TODO validate fields
 	}
@@ -41,15 +44,17 @@ public class BankOrderController {
 	public void sign(ActionRequest request, ActionResponse response ) throws AxelorException{
 		
 		BankOrder bankOrder  = JPA.em().find(BankOrder.class, new Long((Integer)((Map)request.getContext().get("_contextBankOrder")).get("id")));
-		log.debug("BANK ORDER {}", bankOrder);
 		EbicsUser ebicsUser = new EbicsUser();
 		String password = null;
 		if (request.getContext().get("ebicsUser") != null){
 			ebicsUser = JPA.em().find(EbicsUser.class, new Long((Integer)((Map)request.getContext().get("ebicsUser")).get("id")));
 		}
+		else response.setError(I18n.get(IExceptionMessage.EBICS_MISSING_NAME));
 		if (request.getContext().get("password") != null){
 			password = (String)request.getContext().get("password");
 		}
+		else response.setError(I18n.get(IExceptionMessage.EBICS_MISSING_PASSWORD));
+		
 		if(!ebicsUser.getPassword().equals(password)){
 			response.setValue("password", "");
 			response.setError(I18n.get(IExceptionMessage.EBICS_WRONG_PASSWORD));
