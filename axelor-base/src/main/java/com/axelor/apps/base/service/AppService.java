32d326931b738ee74b196f0b04dab76043983339
sos-axelor <sos@axelor.com>
2017-01-02 19:40:39 +0530

    Demo data modules removed

---
+++ axelor-base/src/main/java/com/axelor/apps/base/service/AppService.java
@@ -0,0 +1,10 @@
+package com.axelor.apps.base.service;
+
+import com.axelor.apps.base.db.App;
+
+public interface AppService {
+	
+	public String importDataDemo(App app);
+	
+	public void importDataInit(App app);
+}
