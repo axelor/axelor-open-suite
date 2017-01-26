7ea190e83f5707db7a226a34661ad508e1682688
Geoffrey DUBAUX - Axelor <g.dubaux@axelor.com>
2017-01-26 11:58:57 +0100

    RM6810 - The payment move date should be get from payment date

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveLineService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveLineService.java
@@ -176,9 +176,9 @@ public class MoveLineService {
 	 *
 	 * @return
 	 */
-	public MoveLine createMoveLine(Move move, Partner partner, Account account, BigDecimal amount, boolean isDebit, LocalDate dueDate, int ref, String descriptionOption){
+	public MoveLine createMoveLine(Move move, Partner partner, Account account, BigDecimal amount, boolean isDebit, LocalDate date, int ref, String descriptionOption){
 
-		return this.createMoveLine(move, partner, account, amount, isDebit, today, dueDate, ref, descriptionOption);
+		return this.createMoveLine(move, partner, account, amount, isDebit, date, date, ref, descriptionOption);
 	}
 
 
