a392d8c3ff49d0fbdca67b5338f22d0fb2715f15
Geoffrey DUBAUX - Axelor <g.dubaux@axelor.com>
2017-03-10 14:09:25 +0100

    EBICS - Added support for some bank statement file format

--- axelor-bank-payment/src/main/java/com/axelor/apps/bank/payment/ebics/web/EbicsPartnerController.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bank/payment/ebics/web/EbicsPartnerController.java
@@ -1,21 +1,27 @@
 package com.axelor.apps.bank.payment.ebics.web;
 
 import com.axelor.apps.bank.payment.db.EbicsPartner;
+import com.axelor.apps.bank.payment.db.repo.EbicsPartnerRepository;
 import com.axelor.apps.bank.payment.ebics.service.EbicsPartnerService;
 import com.axelor.exception.service.TraceBackService;
-import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import com.google.inject.Inject;
 
 public class EbicsPartnerController {
 	
+	@Inject
+	EbicsPartnerService ebicsPartnerService;
+	
+	@Inject
+	EbicsPartnerRepository ebicsPartnerRepository;
 	
 	public void getBankStatement(ActionRequest request, ActionResponse response )  {
 		
 		try {
 			EbicsPartner ebicsPartner = request.getContext().asType(EbicsPartner.class);
 		
-			Beans.get(EbicsPartnerService.class).getBankStatements(ebicsPartner);
+			ebicsPartnerService.getBankStatements(ebicsPartnerRepository.find(ebicsPartner.getId()));
 		}
 		catch(Exception e)  {
 			TraceBackService.trace(response, e);
