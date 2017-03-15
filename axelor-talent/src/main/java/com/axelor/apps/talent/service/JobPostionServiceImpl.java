270ab3c27304e5c42f2bb70992e9f5c654473dee
sos-axelor <sos@axelor.com>
2017-03-15 19:23:14 +0530

    Menu structure updated

---
+++ axelor-talent/src/main/java/com/axelor/apps/talent/service/JobPostionServiceImpl.java
@@ -0,0 +1,14 @@
+package com.axelor.apps.talent.service;
+
+import com.axelor.apps.talent.db.JobPosition;
+
+public class JobPostionServiceImpl implements JobPostionService {
+
+	@Override
+	public String computeReference(JobPosition jobPosition) {
+		
+		
+		return null;
+	}
+
+}
