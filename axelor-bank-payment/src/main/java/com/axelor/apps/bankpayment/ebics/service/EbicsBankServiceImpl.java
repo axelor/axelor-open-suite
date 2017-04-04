d7ec8a29f69fc3012346fc53b89090d24655f11c
Diogo De Araujo <d.dearaujo@axelor.com>
2017-04-04 10:53:08 +0200

    #7323 saving EbicsBank doesn't replace hostId anymore

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/ebics/service/EbicsBankServiceImpl.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/ebics/service/EbicsBankServiceImpl.java
@@ -8,7 +8,6 @@ public class EbicsBankServiceImpl implements EbicsBankService {
 	public void computeFullName(EbicsBank ebicsBank) {
 		ebicsBank.setFullName(ebicsBank.getBank().getFullName());
 		ebicsBank.setName(ebicsBank.getBank().getBankName());
-		ebicsBank.setHostId(ebicsBank.getBank().getCode());
 	}
 
 	
