57bb45e0c766041f73444ed8ecb165408a7c8a5f
Phi-Long Do <p.do@axelor.com>
2017-05-31 10:33:27 +0200

    Human resources instead of cash management.

--- axelor-account/src/main/java/com/axelor/apps/account/service/batch/BatchCreditTransferExpensePayment.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/batch/BatchCreditTransferExpensePayment.java
@@ -6,7 +6,7 @@ public class BatchCreditTransferExpensePayment extends BatchStrategy {
 
 	@Override
 	protected void process() {
-		throw new UnsupportedOperationException(I18n.get("This batch requires the cash management module."));
+		throw new UnsupportedOperationException(I18n.get("This batch requires the human resources module."));
 	}
 
 }
