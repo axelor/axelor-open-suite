e31b017cd3bf0cff5667f377b5270b1f24276da1
gdu-axelor <g.dubaux@axelor.com>
2017-09-17 01:50:42 +0200

    Revert "Payment Schedule : added back payment schedules with generation of CFONB files"

--- axelor-account/src/main/java/com/axelor/apps/account/service/PaymentScheduleExportService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/PaymentScheduleExportService.java
@@ -26,8 +26,6 @@ import java.util.List;
 import javax.persistence.Query;
 
 import java.time.LocalDate;
-
-import com.axelor.inject.Beans;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -360,7 +358,7 @@ public class PaymentScheduleExportService{
 
 	public boolean isDebitBlocking(PaymentScheduleLine paymentScheduleLine)  {
 
-		PaymentSchedule paymentSchedule = Beans.get(PaymentScheduleRepository.class).find(paymentScheduleLine.getPaymentSchedule().getId());
+		PaymentSchedule paymentSchedule = paymentScheduleLine.getPaymentSchedule();
 
 		return blockingService.isDebitBlockingBlocking(paymentSchedule.getPartner(), paymentSchedule.getCompany());
 
