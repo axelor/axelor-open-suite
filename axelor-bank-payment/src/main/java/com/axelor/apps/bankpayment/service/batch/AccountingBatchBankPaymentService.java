8ee469f0fc64a9010b90159a94be9dd1f8fa07cd
Phi-Long Do <p.do@axelor.com>
2017-10-12 14:55:02 +0200

    RM8474: add bank statement batch.

---
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/batch/AccountingBatchBankPaymentService.java
@@ -0,0 +1,57 @@
+package com.axelor.apps.bankpayment.service.batch;
+
+import com.axelor.apps.account.db.AccountingBatch;
+import com.axelor.apps.account.db.repo.AccountingBatchRepository;
+import com.axelor.apps.account.service.batch.AccountingBatchService;
+import com.axelor.apps.base.db.Batch;
+import com.axelor.db.Model;
+import com.axelor.exception.AxelorException;
+import com.axelor.inject.Beans;
+
+public class AccountingBatchBankPaymentService extends AccountingBatchService {
+
+    @Override
+    public Batch run(Model batchModel) throws AxelorException {
+        Batch batch;
+        AccountingBatch accountingBatch = (AccountingBatch) batchModel;
+
+        switch (accountingBatch.getActionSelect()) {
+        case AccountingBatchRepository.ACTION_DIRECT_DEBIT:
+            batch = directDebit(accountingBatch);
+            break;
+        case AccountingBatchRepository.ACTION_BANK_STATEMENT:
+            batch = bankStatement(accountingBatch);
+            break;
+        default:
+            batch = super.run(accountingBatch);
+        }
+
+        return batch;
+    }
+
+    @Override
+    public Batch directDebit(AccountingBatch accountingBatch) {
+        Class<? extends BatchDirectDebit> batchClass;
+
+        switch (accountingBatch.getDirectDebitDataTypeSelect()) {
+        case AccountingBatchRepository.DIRECT_DEBIT_DATA_CUSTOMER_INVOICE:
+            batchClass = BatchDirectDebitCustomerInvoice.class;
+            break;
+        case AccountingBatchRepository.DIRECT_DEBIT_DATA_PAYMENT_SCHEDULE:
+            batchClass = BatchDirectDebitPaymentSchedule.class;
+            break;
+        case AccountingBatchRepository.DIRECT_DEBIT_DATA_MONTHLY_PAYMENT_SCHEDULE:
+            batchClass = BatchDirectDebitMonthlyPaymentSchedule.class;
+            break;
+        default:
+            throw new IllegalArgumentException("Invalid direct debit data type");
+        }
+
+        return Beans.get(batchClass).run(accountingBatch);
+    }
+
+    public Batch bankStatement(AccountingBatch accountingBatch) {
+        return Beans.get(BatchBankStatement.class).run(accountingBatch);
+    }
+
+}
