fe4fc342d18635c968c4af5934976dc11720c390
Antoine Mahieu <a.mahieu@axelor.com>
2017-10-13 18:10:32 +0200

    fix product onChange in contractLine

--- axelor-contract/src/main/java/com/axelor/apps/contract/web/ContractLineController.java
+++ axelor-contract/src/main/java/com/axelor/apps/contract/web/ContractLineController.java
@@ -20,32 +20,27 @@ public class ContractLineController {
 	
 	public void changeProduct(ActionRequest request, ActionResponse response) {
 		ContractLine contractLine = request.getContext().asType(ContractLine.class);
-		Partner partner = null;
-		Company company = null;
-		if(request.getContext().getParentContext().getContextClass() == Contract.class){
-			Contract contract = request.getContext().getParentContext().asType(Contract.class);
-			partner = contract.getPartner();
-			company = contract.getCompany();
-		}else if (request.getContext().getParentContext().getContextClass() == ContractVersion.class){
-			ContractVersion contractVersion = request.getContext().getParentContext().asType(ContractVersion.class);
-			Contract contract = contractVersion.getContract();
-			partner = contract.getPartner();
-			company = contract.getCompany();
-		}
 		
-		
-		Product product = contractLine.getProduct();
-		
-		if(contractLine == null || product == null) {
+		if(contractLine == null || contractLine.getProduct() == null) {
 			this.resetProductInformation(response);
 			return;
 		}
-
+		
+		Contract contract = null;
+		if(request.getContext().getParentContext().getContextClass() == Contract.class){
+			contract = request.getContext().getParentContext().asType(Contract.class);
+		}else if (request.getContext().getParentContext().getContextClass() == ContractVersion.class){
+			ContractVersion contractVersion = request.getContext().getParentContext().asType(ContractVersion.class);
+			contract = contractVersion.getContractNext() == null ? contractVersion.getContract() : contractVersion.getContractNext() ;
+		}
+		Product product = contractLine.getProduct();
+		
+		
 		try  {
 			BigDecimal price = product.getSalePrice();
-			if (partner != null && company != null){
+			if (contract != null){
 				TaxLine taxLine = Beans.get(AccountManagementService.class).getTaxLine(
-						LocalDate.now(), product, company, partner.getFiscalPosition(), false);
+						LocalDate.now(), product, contract.getCompany(), contract.getPartner().getFiscalPosition(), false);
 				response.setValue("taxLine", taxLine);
 
 				
