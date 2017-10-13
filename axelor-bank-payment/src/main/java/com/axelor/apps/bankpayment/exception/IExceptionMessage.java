86cef07eed994641117de761c4f14c10fa716ffe
Flavien Schriever <f.schriever@axelor.com>
2017-10-13 12:54:23 +0200

    Fix anomaly #8351 : Check if the field "EBICS User Transport" is fill in EBICS Partner when user carry out a bank order.

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/exception/IExceptionMessage.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/exception/IExceptionMessage.java
@@ -126,7 +126,6 @@ public interface IExceptionMessage {
 	static final String EBICS_TEST_MODE_NOT_ENABLED = /*$$(*/ "Test mode is not enabled or test file is missing" /*)*/;
 	static final String EBICS_MISSING_CERTIFICATES = /*$$(*/  "Please add certificates to print" /*)*/;
 	static final String EBICS_INVALID_BANK_URL = /*$$(*/  "Invalid bank url. It must be start with http:// or https://" /*)*/;
-	
-	
+	static final String EBICS_MISSING_USER_TRANSPORT = /*$$(*/ "Please insert a EBICS user for transport in the EBICS partner" /*)*/;
 	
 }
