96e43ad12126131b078f97aca56bd208650d6379
Pierre Belloy <p.belloy@axelor.com>
2017-10-30 16:50:49 +0100

    Update license headers.

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/workflow/WorkflowValidationServiceSupplychainImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/workflow/WorkflowValidationServiceSupplychainImpl.java
@@ -1,17 +1,17 @@
-/**
+/*
  * Axelor Business Solutions
- * <p>
+ *
  * Copyright (C) 2017 Axelor (<http://axelor.com>).
- * <p>
+ *
  * This program is free software: you can redistribute it and/or  modify
  * it under the terms of the GNU Affero General Public License, version 3,
  * as published by the Free Software Foundation.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
