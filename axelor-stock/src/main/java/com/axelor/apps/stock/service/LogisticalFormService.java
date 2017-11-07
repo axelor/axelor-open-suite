861ac4799a505386171f7182e8fe43e28ab7f93c
Phi-Long Do <p.do@axelor.com>
2017-11-07 17:26:21 +0100

    RM7439: pallet/parcel line default numbering.

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/LogisticalFormService.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/LogisticalFormService.java
@@ -5,8 +5,27 @@ import com.axelor.apps.stock.db.StockMove;
 
 public interface LogisticalFormService {
 
+	/**
+	 * Add detail lines from the stock move.
+	 * 
+	 * @param logisticalForm
+	 * @param stockMove
+	 */
 	void addLines(LogisticalForm logisticalForm, StockMove stockMove);
 
+	/**
+	 * Add parcel or pallet line.
+	 * 
+	 * @param logisticalForm
+	 * @param typeSelect
+	 */
+	void addParcelPalletLine(LogisticalForm logisticalForm, int typeSelect);
+
+	/**
+	 * Compute totals.
+	 * 
+	 * @param logisticalForm
+	 */
 	void compute(LogisticalForm logisticalForm);
 
 }
