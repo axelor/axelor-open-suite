a068d8c84c43d903997187c0a91b84effb62c5e2
Tushar BODARYA <t.bodarya@axelor.com>
2017-11-17 16:29:49 +0530

    Anomaly #8986: Duplicate object finding feature

--- axelor-base/src/main/java/com/axelor/apps/base/web/AppBaseController.java
+++ axelor-base/src/main/java/com/axelor/apps/base/web/AppBaseController.java
@@ -68,9 +68,6 @@ public class AppBaseController {
 	private MapService mapService;
 	
 	@Inject
-	private MetaFieldRepository metaFieldRepo;
-	
-	@Inject
 	private CurrencyConversionService ccs;
 
 	@Inject
@@ -95,59 +92,6 @@ public class AppBaseController {
 		return fieldSet;
 	}
 
-	@SuppressWarnings("unchecked")
-	public void showDuplicate(ActionRequest request, ActionResponse response){
-		
-		String model = (String)request.getContext().get("object");
-		List<String> fields = new ArrayList<String>();
-		
-		if(model == null){
-			model = request.getModel();
-			String searchFields = (String)request.getContext().get("searchFields");
-			if(searchFields != null){
-				fields.addAll(Arrays.asList(searchFields.split(";")));
-			}
-		}else{
-			List<HashMap<String,Object>> fieldsSet = (List<HashMap<String,Object>>)request.getContext().get("fieldsSet");
-			for(HashMap<String,Object> field : fieldsSet){
-				if(field.get("selected") != null && (Boolean)field.get("selected")){
-					MetaField metaField = metaFieldRepo.find(Long.parseLong(field.get("id").toString()));
-					fields.add(metaField.getName());
-				}
-			}
-		}
-		
-		LOG.debug("Duplicate record model: {}",model);
-		
-		if(fields.size() > 0){
-			LOG.debug("Duplicate record joinList: {}", fields);
-			String selectedRecored =  String.valueOf((request.getContext().get("_ids")));
-			String filter = request.getContext().get("_domain_").toString();
-			if(selectedRecored.equals("null") || selectedRecored.isEmpty()) {
-				selectedRecored = null;
-			} else {
-				selectedRecored = selectedRecored.substring(1, selectedRecored.length()-1);
-			}
-			String ids = findDuplicateRecords(fields,model,selectedRecored,filter);
-			if(ids.isEmpty())
-				response.setFlash(I18n.get(IExceptionMessage.GENERAL_1));
-			else{
-				String domain = "self.id in ("+ids+") And " + request.getContext().get("_domain_");
-				response.setView(ActionView
-						  .define(I18n.get(IExceptionMessage.GENERAL_2))
-						  .model(model)
-						  .add("grid", "partner-grid")
-						  .add("form", "partner-form")
-						  .domain(domain)
-						  .context("_domain", domain)
-						  .map());
-				response.setCanClose(true);
-			}
-		}
-		else
-			response.setFlash(I18n.get(IExceptionMessage.GENERAL_3));
-	}
-
 	public void exportObjects(ActionRequest request, ActionResponse response){
 		MetaFile metaFile = eos.exportObject();
 		if(metaFile == null){
@@ -163,102 +107,6 @@ public class AppBaseController {
 					  .context("_showRecord", metaFile.getId().toString())
 					  .map());
 		}
-	}
-	
-	private String findDuplicateRecords(List<String> fieldList,String object,String selectedRecored,String filter){
-		
-		
-		List<List<String>> allRecords = getAllRecords(fieldList, object,selectedRecored,filter);
-		
-		Map<String, List<String>> recordMap = new HashMap<String, List<String>>();
-		
-		for(List<String> rec : allRecords){
-			
-			List<String> record = new ArrayList<String>();
-			for(String field : rec) {
-				if(field != null){
-					record.add(StringTool.deleteAccent(field.toLowerCase()));
-				}
-			}
-			
-			String recId = record.get(0);
-			record.remove(0);
-			if(!record.isEmpty()){
-				recordMap.put(recId, record);
-			}
-		}
-		
-		Iterator<String> keys = recordMap.keySet().iterator();
-		
-		List<String> ids = getDuplicateIds(keys, recordMap, new ArrayList<String>());
-
-		return Joiner.on(",").join(ids);
-	}
-	
-	@SuppressWarnings("unchecked")
-	private List<List<String>> getAllRecords(List<String> fieldList,String object,String selectedRecored,String filter){
-		
-		String query = "SELECT new List( CAST ( self.id AS string )";
-		
-		for(String field : fieldList) {
-			query += ", self." + field;
-		}
-		List<List<Object>> resultList = new ArrayList<>();
-		if(selectedRecored == null || selectedRecored.isEmpty()) {
-			resultList = JPA.em().createQuery(query + ") FROM " + object + " self WHERE " + filter).getResultList();
-		} else {
-			resultList = JPA.em().createQuery(query + ") FROM " + object + " self where self.id in("+selectedRecored+") AND " + filter).getResultList();
-		}
-		
-		List<List<String>> records = new ArrayList<List<String>>();
-		
-		for(List<Object> result : resultList){
-			
-			List<String> record = new ArrayList<String>();
-			for(Object field : result){
-				if(field == null){
-					continue;
-				}
-				if(field instanceof Model){
-					record.add(((Model)field).getId().toString());
-				}
-				else{
-					record.add(field.toString());
-				}
-			}
-			
-			records.add(record);
-		}
-		
-		return records;
-	}
-	
-	private List<String> getDuplicateIds(Iterator<String> keys, Map<String, List<String>> recordMap, List<String> ids){
-		
-		if(!keys.hasNext()){
-			return ids;
-		}
-		
-		String recId = keys.next();
-		List<String> record = recordMap.get(recId);
-		keys.remove();
-		recordMap.remove(recId);
-		
-		Iterator<String> compareKeys = recordMap.keySet().iterator();
-		
-		while(compareKeys.hasNext()){
-			String compareId = compareKeys.next();
-			List<String> value = recordMap.get(compareId);
-			if(value.containsAll(record)){
-				ids.add(recId);
-				ids.add(compareId);
-				compareKeys.remove();
-				recordMap.remove(compareId);
-				keys = recordMap.keySet().iterator();
-			}
-		}
-		
-		return getDuplicateIds(keys, recordMap, ids);
 	}
 	
 	public void checkMapApi(ActionRequest request, ActionResponse response)  {
