779bb811becb825a6d69ddac3b2d6534c385a054
Flavien Schriever <f.schriever@axelor.com>
2017-12-01 18:19:44 +0100

    RM#8561: Add if-module for catch Exception

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankorder/BankOrderServiceImpl.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankorder/BankOrderServiceImpl.java
@@ -34,6 +34,7 @@ import java.time.LocalDateTime;
 import com.axelor.apps.bankpayment.db.*;
 import com.axelor.apps.bankpayment.service.config.BankPaymentConfigService;
 import com.axelor.apps.base.db.AppBankPayment;
+import com.axelor.apps.base.service.app.AppService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -254,7 +255,7 @@ public class BankOrderServiceImpl implements BankOrderService {
 		
 		setSequenceOnBankOrderLines(bankOrder);
 
-		if (Beans.get(AppBankPayment.class).getEnableModuleEBICS()) {
+		if (Beans.get(AppService.class).isApp("bank-payment") && Beans.get(AppBankPayment.class).getEnableModuleEBICS()) {
 			generateFile(bankOrder);
 
 			setSequenceOnBankOrderLines(bankOrder);
