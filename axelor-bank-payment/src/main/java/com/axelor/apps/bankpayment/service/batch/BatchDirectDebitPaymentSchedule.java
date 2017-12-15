934666786d8b312c90e4832ad07bc09b943be0ca
Phi-Long Do <p.do@axelor.com>
2017-12-15 17:07:11 +0100

    RM8157: null payment shedule fields

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/batch/BatchDirectDebitPaymentSchedule.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/batch/BatchDirectDebitPaymentSchedule.java
@@ -18,6 +18,7 @@
 package com.axelor.apps.bankpayment.service.batch;
 
 import java.lang.invoke.MethodHandles;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -43,6 +44,7 @@ import com.axelor.apps.bankpayment.db.BankOrder;
 import com.axelor.apps.bankpayment.service.bankorder.BankOrderMergeService;
 import com.axelor.apps.base.db.BankDetails;
 import com.axelor.apps.base.db.repo.BankDetailsRepository;
+import com.axelor.apps.base.service.app.AppBaseService;
 import com.axelor.db.JPA;
 import com.axelor.db.Query;
 import com.axelor.exception.AxelorException;
@@ -57,155 +59,157 @@ import com.google.inject.persist.Transactional;
 
 public class BatchDirectDebitPaymentSchedule extends BatchDirectDebit {
 
-	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	@Override
-	protected void process() {
-		processPaymentScheduleLines(PaymentScheduleRepository.TYPE_TERMS);
-	}
+    @Override
+    protected void process() {
+        processPaymentScheduleLines(PaymentScheduleRepository.TYPE_TERMS);
+    }
 
-	protected void processPaymentScheduleLines(int paymentScheduleType) {
-		AccountingBatch accountingBatch = batch.getAccountingBatch();
-		List<String> filterList = new ArrayList<>();
-		List<Pair<String, Object>> bindingList = new ArrayList<>();
+    protected void processPaymentScheduleLines(int paymentScheduleType) {
+        AccountingBatch accountingBatch = batch.getAccountingBatch();
+        List<String> filterList = new ArrayList<>();
+        List<Pair<String, Object>> bindingList = new ArrayList<>();
 
-		filterList.add("self.paymentSchedule.statusSelect = :paymentScheduleStatusSelect");
-		bindingList.add(Pair.of("paymentScheduleStatusSelect", PaymentScheduleRepository.STATUS_CONFIRMED));
+        filterList.add("self.paymentSchedule.statusSelect = :paymentScheduleStatusSelect");
+        bindingList.add(Pair.of("paymentScheduleStatusSelect", PaymentScheduleRepository.STATUS_CONFIRMED));
 
-		filterList.add("self.paymentSchedule.typeSelect = :paymentScheduleTypeSelect");
-		bindingList.add(Pair.of("paymentScheduleTypeSelect", paymentScheduleType));
+        filterList.add("self.paymentSchedule.typeSelect = :paymentScheduleTypeSelect");
+        bindingList.add(Pair.of("paymentScheduleTypeSelect", paymentScheduleType));
 
-		filterList.add("self.statusSelect = :statusSelect");
-		bindingList.add(Pair.of("statusSelect", PaymentScheduleLineRepository.STATUS_IN_PROGRESS));
+        filterList.add("self.statusSelect = :statusSelect");
+        bindingList.add(Pair.of("statusSelect", PaymentScheduleLineRepository.STATUS_IN_PROGRESS));
 
-		if (accountingBatch.getDueDate() != null) {
-			filterList.add("self.scheduleDate <= :dueDate");
-			bindingList.add(Pair.of("dueDate", accountingBatch.getDueDate()));
-		}
+        LocalDate dueDate = accountingBatch.getDueDate() != null ? accountingBatch.getDueDate()
+                : Beans.get(AppBaseService.class).getTodayDate();
+        filterList.add("self.scheduleDate <= :dueDate");
+        bindingList.add(Pair.of("dueDate", dueDate));
 
-		if (accountingBatch.getCompany() != null) {
-			filterList.add("self.paymentSchedule.company = :company");
-			bindingList.add(Pair.of("company", accountingBatch.getCompany()));
-		}
+        if (accountingBatch.getCompany() != null) {
+            filterList.add("self.paymentSchedule.company IS NULL OR self.paymentSchedule.company = :company");
+            bindingList.add(Pair.of("company", accountingBatch.getCompany()));
+        }
 
-		if (accountingBatch.getBankDetails() != null) {
-			Set<BankDetails> bankDetailsSet = Sets.newHashSet(accountingBatch.getBankDetails());
+        if (accountingBatch.getBankDetails() != null) {
+            Set<BankDetails> bankDetailsSet = Sets.newHashSet(accountingBatch.getBankDetails());
 
-			if (accountingBatch.getIncludeOtherBankAccounts() && appBaseService.getAppBase().getManageMultiBanks()) {
-				bankDetailsSet.addAll(accountingBatch.getCompany().getBankDetailsSet());
-			}
+            if (accountingBatch.getIncludeOtherBankAccounts() && appBaseService.getAppBase().getManageMultiBanks()) {
+                bankDetailsSet.addAll(accountingBatch.getCompany().getBankDetailsSet());
+            }
 
-			filterList.add("self.paymentSchedule.companyBankDetails IN (:bankDetailsSet)");
-			bindingList.add(Pair.of("bankDetailsSet", bankDetailsSet));
-		}
+            filterList.add(
+                    "self.paymentSchedule.companyBankDetails IS NULL OR self.paymentSchedule.companyBankDetails IN (:bankDetailsSet)");
+            bindingList.add(Pair.of("bankDetailsSet", bankDetailsSet));
+        }
 
-		if (accountingBatch.getPaymentMode() != null) {
-			filterList.add("self.paymentSchedule.paymentMode = :paymentMode");
-			bindingList.add(Pair.of("paymentMode", accountingBatch.getPaymentMode()));
-		}
+        if (accountingBatch.getPaymentMode() != null) {
+            filterList
+                    .add("self.paymentSchedule.paymentMode IS NULL OR self.paymentSchedule.paymentMode = :paymentMode");
+            bindingList.add(Pair.of("paymentMode", accountingBatch.getPaymentMode()));
+        }
 
-		List<PaymentScheduleLine> paymentScheduleLineList = processQuery(filterList, bindingList);
+        List<PaymentScheduleLine> paymentScheduleLineList = processQuery(filterList, bindingList);
 
-		if (!paymentScheduleLineList.isEmpty()) {
-			try {
-				mergeBankOrders(paymentScheduleLineList);
-			} catch (AxelorException e) {
-				TraceBackService.trace(e, IException.DIRECT_DEBIT, batch.getId());
-				LOG.error(e.getMessage());
-			}
-		}
+        if (!paymentScheduleLineList.isEmpty()) {
+            try {
+                mergeBankOrders(paymentScheduleLineList);
+            } catch (AxelorException e) {
+                TraceBackService.trace(e, IException.DIRECT_DEBIT, batch.getId());
+                LOG.error(e.getMessage());
+            }
+        }
 
-	}
+    }
 
-	private List<PaymentScheduleLine> processQuery(List<String> filterList, List<Pair<String, Object>> bindingList) {
-		List<PaymentScheduleLine> doneList = new ArrayList<>();
+    private List<PaymentScheduleLine> processQuery(List<String> filterList, List<Pair<String, Object>> bindingList) {
+        List<PaymentScheduleLine> doneList = new ArrayList<>();
 
-		List<Long> anomalyList = Lists.newArrayList(0L);
-		filterList.add("self.id NOT IN (:anomalyList)");
-		bindingList.add(Pair.of("anomalyList", anomalyList));
+        List<Long> anomalyList = Lists.newArrayList(0L);
+        filterList.add("self.id NOT IN (:anomalyList)");
+        bindingList.add(Pair.of("anomalyList", anomalyList));
 
-		String filter = Joiner.on(" AND ").join(Lists.transform(filterList, new Function<String, String>() {
-			@Override
-			public String apply(String input) {
-				return String.format("(%s)", input);
-			}
-		}));
+        String filter = Joiner.on(" AND ").join(Lists.transform(filterList, new Function<String, String>() {
+            @Override
+            public String apply(String input) {
+                return String.format("(%s)", input);
+            }
+        }));
 
-		Query<PaymentScheduleLine> query = Beans.get(PaymentScheduleLineRepository.class).all().filter(filter);
+        Query<PaymentScheduleLine> query = Beans.get(PaymentScheduleLineRepository.class).all().filter(filter);
 
-		for (Pair<String, Object> binding : bindingList) {
-			query.bind(binding.getLeft(), binding.getRight());
-		}
+        for (Pair<String, Object> binding : bindingList) {
+            query.bind(binding.getLeft(), binding.getRight());
+        }
 
-		Set<Long> treatedSet = new HashSet<>();
-		List<PaymentScheduleLine> paymentScheduleLineList;
-		PaymentScheduleLineService paymentScheduleLineService = Beans.get(PaymentScheduleLineService.class);
-		BankDetailsRepository bankDetailsRepo = Beans.get(BankDetailsRepository.class);
+        Set<Long> treatedSet = new HashSet<>();
+        List<PaymentScheduleLine> paymentScheduleLineList;
+        PaymentScheduleLineService paymentScheduleLineService = Beans.get(PaymentScheduleLineService.class);
+        BankDetailsRepository bankDetailsRepo = Beans.get(BankDetailsRepository.class);
 
-		BankDetails companyBankDetails = batch.getAccountingBatch().getBankDetails();
+        BankDetails companyBankDetails = batch.getAccountingBatch().getBankDetails();
 
-		while (!(paymentScheduleLineList = query.fetch(FETCH_LIMIT)).isEmpty()) {
-			if (!JPA.em().contains(companyBankDetails)) {
-				companyBankDetails = bankDetailsRepo.find(companyBankDetails.getId());
-			}
+        while (!(paymentScheduleLineList = query.fetch(FETCH_LIMIT)).isEmpty()) {
+            if (!JPA.em().contains(companyBankDetails)) {
+                companyBankDetails = bankDetailsRepo.find(companyBankDetails.getId());
+            }
 
-			for (PaymentScheduleLine paymentScheduleLine : paymentScheduleLineList) {
-				if (treatedSet.contains(paymentScheduleLine.getId())) {
-					throw new IllegalArgumentException("Payment generation error");
-				}
+            for (PaymentScheduleLine paymentScheduleLine : paymentScheduleLineList) {
+                if (treatedSet.contains(paymentScheduleLine.getId())) {
+                    throw new IllegalArgumentException("Payment generation error");
+                }
 
-				treatedSet.add(paymentScheduleLine.getId());
+                treatedSet.add(paymentScheduleLine.getId());
 
-				try {
-					paymentScheduleLineService.createPaymentMove(paymentScheduleLine, companyBankDetails);
-					doneList.add(paymentScheduleLine);
-					incrementDone();
-				} catch (Exception e) {
-					incrementAnomaly();
-					anomalyList.add(paymentScheduleLine.getId());
-					query.bind("anomalyList", anomalyList);
-					TraceBackService.trace(e, IException.DIRECT_DEBIT, batch.getId());
-					LOG.error(e.getMessage());
-				}
-			}
+                try {
+                    paymentScheduleLineService.createPaymentMove(paymentScheduleLine, companyBankDetails);
+                    doneList.add(paymentScheduleLine);
+                    incrementDone();
+                } catch (Exception e) {
+                    incrementAnomaly();
+                    anomalyList.add(paymentScheduleLine.getId());
+                    query.bind("anomalyList", anomalyList);
+                    TraceBackService.trace(e, IException.DIRECT_DEBIT, batch.getId());
+                    LOG.error(e.getMessage());
+                }
+            }
 
-			JPA.clear();
-		}
+            JPA.clear();
+        }
 
-		return doneList;
-	}
+        return doneList;
+    }
 
-	/**
-	 * Merge bank orders from a list of payment schedule lines.
-	 * 
-	 * @param paymentScheduleLineList
-	 * @return
-	 * @throws AxelorException
-	 */
-	@Transactional(rollbackOn = { AxelorException.class, Exception.class })
-	protected BankOrder mergeBankOrders(List<PaymentScheduleLine> paymentScheduleLineList) throws AxelorException {
-		BankOrderMergeService bankOrderMergeService = Beans.get(BankOrderMergeService.class);
-		ReconcileRepository reconcileRepo = Beans.get(ReconcileRepository.class);
-		InvoicePaymentRepository invoicePaymentRepo = Beans.get(InvoicePaymentRepository.class);
-		List<InvoicePayment> invoicePaymentList = new ArrayList<>();
+    /**
+     * Merge bank orders from a list of payment schedule lines.
+     * 
+     * @param paymentScheduleLineList
+     * @return
+     * @throws AxelorException
+     */
+    @Transactional(rollbackOn = { AxelorException.class, Exception.class })
+    protected BankOrder mergeBankOrders(List<PaymentScheduleLine> paymentScheduleLineList) throws AxelorException {
+        BankOrderMergeService bankOrderMergeService = Beans.get(BankOrderMergeService.class);
+        ReconcileRepository reconcileRepo = Beans.get(ReconcileRepository.class);
+        InvoicePaymentRepository invoicePaymentRepo = Beans.get(InvoicePaymentRepository.class);
+        List<InvoicePayment> invoicePaymentList = new ArrayList<>();
 
-		for (PaymentScheduleLine paymentScheduleLine : paymentScheduleLineList) {
-			PaymentSchedule paymentSchedule = paymentScheduleLine.getPaymentSchedule();
-			MoveLine creditMoveLine = paymentScheduleLine.getAdvanceMoveLine();
+        for (PaymentScheduleLine paymentScheduleLine : paymentScheduleLineList) {
+            PaymentSchedule paymentSchedule = paymentScheduleLine.getPaymentSchedule();
+            MoveLine creditMoveLine = paymentScheduleLine.getAdvanceMoveLine();
 
-			for (Invoice invoice : paymentSchedule.getInvoiceSet()) {
-				MoveLine debitMoveLine = moveService.getMoveLineService().getDebitCustomerMoveLine(invoice);
-				Reconcile reconcile = reconcileRepo.findByMoveLines(debitMoveLine, creditMoveLine);
+            for (Invoice invoice : paymentSchedule.getInvoiceSet()) {
+                MoveLine debitMoveLine = moveService.getMoveLineService().getDebitCustomerMoveLine(invoice);
+                Reconcile reconcile = reconcileRepo.findByMoveLines(debitMoveLine, creditMoveLine);
 
-				if (reconcile == null) {
-					continue;
-				}
+                if (reconcile == null) {
+                    continue;
+                }
 
-				invoicePaymentList.addAll(invoicePaymentRepo.findByReconcile(reconcile).fetch());
-			}
-		}
+                invoicePaymentList.addAll(invoicePaymentRepo.findByReconcile(reconcile).fetch());
+            }
+        }
 
-		return bankOrderMergeService.mergeFromInvoicePayments(invoicePaymentList);
-	}
+        return bankOrderMergeService.mergeFromInvoicePayments(invoicePaymentList);
+    }
 
 }
