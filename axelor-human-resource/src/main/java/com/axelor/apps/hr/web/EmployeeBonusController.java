03f29d8465424b0a5625327871a225b4a6b4e5c7
Arthur Lefebvre <a.lefebvre@axelor.com>
2017-12-20 18:34:59 +0100

    RM9392: closed pay period
    + warning if in a closed pay period
    + cannot select pay period

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/web/EmployeeBonusController.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/web/EmployeeBonusController.java
@@ -18,6 +18,7 @@
 package com.axelor.apps.hr.web;
 
 import com.axelor.apps.ReportFactory;
+import com.axelor.apps.base.service.PeriodService;
 import com.axelor.apps.hr.db.EmployeeBonusMgt;
 import com.axelor.apps.hr.db.repo.EmployeeBonusMgtRepository;
 import com.axelor.apps.hr.report.IReport;
@@ -26,6 +27,7 @@ import com.axelor.auth.AuthUtils;
 import com.axelor.auth.db.User;
 import com.axelor.exception.AxelorException;
 import com.axelor.i18n.I18n;
+import com.axelor.inject.Beans;
 import com.axelor.meta.schema.actions.ActionView;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
@@ -42,9 +44,16 @@ public class EmployeeBonusController {
 	
 
 	public void compute(ActionRequest request, ActionResponse response) throws AxelorException{
-		
-		employeeBonusService.compute(employeeBonusMgtRepo.find( request.getContext().asType( EmployeeBonusMgt.class ).getId() ));
+		EmployeeBonusMgt employeeBonusMgt = request.getContext().asType(EmployeeBonusMgt.class);
+		employeeBonusMgt = employeeBonusMgtRepo.find(employeeBonusMgt.getId());
+		employeeBonusService.compute(employeeBonusMgt);
 		response.setReload(true);
+		try {
+			Beans.get(PeriodService.class).checkPeriod(employeeBonusMgt.getPayPeriod());
+			Beans.get(PeriodService.class).checkPeriod(employeeBonusMgt.getLeavePeriod());
+		} catch (AxelorException e) {
+			response.setFlash(e.getMessage());
+		}
 	}
 	
 	public void print(ActionRequest request, ActionResponse response) throws AxelorException{
