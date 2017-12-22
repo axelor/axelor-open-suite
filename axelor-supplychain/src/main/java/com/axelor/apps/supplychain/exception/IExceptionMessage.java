a01e418d261d29e03f2b3d0dadcd0020aa5181c7
Phi-Long Do <p.do@axelor.com>
2017-12-22 10:47:14 +0100

    RM9038: rework change validation.

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/exception/IExceptionMessage.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/exception/IExceptionMessage.java
@@ -104,6 +104,7 @@ public interface IExceptionMessage {
 	 */
 	static final String SO_NO_DELIVERY_STOCK_MOVE_TO_GENERATE = /*$$(*/ "No delivery stock move to generate for this sale order" /*)*/;
 	static final String SO_ACTIVE_DELIVERY_STOCK_MOVE_ALREADY_EXISTS = /*$$(*/ "An active stock move (%s) already exists for the sale order %s." /*)*/;
+	String SO_CANT_REMOVED_DELIVERED_LINE = /*$$(*/ "Can't remove delivered detail line %s." /*)*/;
 
 	/**
 	 * Timetable Controller
@@ -127,7 +128,7 @@ public interface IExceptionMessage {
 	
 	static final String SUPPLY_CHAIN_CONFIG = /*$$(*/ "You must configure a Supply chain module for the company %s" /*)*/;
 
-	String SUPPLYCHAIN_MISSING_CANCEL_REASON_ON_CHANGING_SALE_ORDER = /*$$(*/ "You must configure a default cancel reason on changing order in app supplychain." /*)*/;
+	String SUPPLYCHAIN_MISSING_CANCEL_REASON_ON_CHANGING_SALE_ORDER = /*$$(*/ "You must configure a cancel reason on changing sale order in app supplychain." /*)*/;
 
 }
 
