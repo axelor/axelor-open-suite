e0a974d108ff7d98353fdd3c2e800f2e9c6d499f
Flavien Schriever <f.schriever@axelor.com>
2017-12-29 12:07:50 +0100

    RM#9470: Fix share collection reference error

--- axelor-account/src/main/java/com/axelor/apps/account/service/debtrecovery/DebtRecoveryActionService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/debtrecovery/DebtRecoveryActionService.java
@@ -268,7 +268,12 @@ public class DebtRecoveryActionService {
 	public void updateDebtRecoveryHistory(DebtRecovery debtRecovery, Set<Message> debtRecoveryMessageSet)  {
 
 		if(!debtRecovery.getDebtRecoveryHistoryList().isEmpty())  {
-			debtRecovery.getDebtRecoveryHistoryList().get(debtRecovery.getDebtRecoveryHistoryList().size()-1).setDebtRecoveryMessageSet(debtRecoveryMessageSet);
+			debtRecovery.getDebtRecoveryHistoryList().sort(Comparator.comparing(debtRecoveryHistory ->
+					debtRecovery.getDebtRecoveryMethodLine().getDebtRecoveryLevel().getName()
+			));
+			DebtRecoveryHistory debtRecoveryHistory = debtRecovery.getDebtRecoveryHistoryList().get(debtRecovery.getDebtRecoveryHistoryList().size()-1);
+			debtRecoveryHistory.clearDebtRecoveryMessageSet();
+			debtRecoveryMessageSet.forEach(debtRecoveryHistory::addDebtRecoveryMessageSetItem);
 		}
 
 	}
