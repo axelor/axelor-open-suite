550bb5fabcf8bdcab2ce5d4d7dcd16acdafa330d
Sébastien Le Ray <sebastien@orniz.org>
2018-01-26 17:42:34 +0100

    qms: DocumentVersionController can be a singleton

--- axelor-qms/src/main/java/com/axelor/apps/qms/web/QMSDocumentVersionController.java
+++ axelor-qms/src/main/java/com/axelor/apps/qms/web/QMSDocumentVersionController.java
@@ -12,7 +12,9 @@ import com.axelor.meta.db.MetaFile;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
+@Singleton
 public class QMSDocumentVersionController {
 	private QMSDocumentRepository documentRepository;
 
