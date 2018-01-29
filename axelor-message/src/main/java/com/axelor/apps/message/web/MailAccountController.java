ec63c1650fffc7879a2c9d93826381a9f60f6337
jso-axelor <j.solanki@axelor.com>
2018-01-29 16:54:01 +0530

    Feature #10098 SMTP ACCOUNT : password

--- axelor-message/src/main/java/com/axelor/apps/message/web/MailAccountController.java
+++ axelor-message/src/main/java/com/axelor/apps/message/web/MailAccountController.java
@@ -18,17 +18,24 @@
 package com.axelor.apps.message.web;
 
 import java.io.IOException;
-
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.NoSuchPaddingException;
 import javax.mail.MessagingException;
 
+import com.axelor.app.AppSettings;
 import com.axelor.apps.message.db.MailAccount;
 import com.axelor.apps.message.db.repo.MailAccountRepository;
 import com.axelor.apps.message.exception.IExceptionMessage;
 import com.axelor.apps.message.service.MailAccountService;
+import com.axelor.apps.tool.service.CipherService;
 import com.axelor.exception.service.TraceBackService;
 import com.axelor.i18n.I18n;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import com.axelor.rpc.Context;
 import com.google.inject.Inject;
 
 public class MailAccountController {
@@ -39,12 +46,20 @@ public class MailAccountController {
 	@Inject
 	private MailAccountRepository mailAccountRepo;
 	
+	@Inject
+	private CipherService cipherService;
+	
 	public void validateSmtpAccount(ActionRequest request,ActionResponse response){
 		
 		MailAccount account = request.getContext().asType(MailAccount.class);
 		
 		try {
-			
+			if(!AppSettings.get().get("application.encryptionkey").isEmpty()) {
+				cipherService.initEncryptOrDecrypt();
+				String password = cipherService.decrypt(account.getPassword());
+				account.setPassword(password);
+			}
+
 			mailAccountService.checkMailAccountConfiguration(account);
 
 			response.setValue("isValid", Boolean.TRUE );
@@ -67,14 +82,30 @@ public class MailAccountController {
 		}
 	}
 	
-	public void fetchEmails(ActionRequest request, ActionResponse response) throws MessagingException, IOException  {
+	public void fetchEmails(ActionRequest request, ActionResponse response) throws MessagingException, IOException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException  {
 		
 		MailAccount account = request.getContext().asType(MailAccount.class);
 		account = mailAccountRepo.find(account.getId());
+		
+		if(!AppSettings.get().get("application.encryptionkey").isEmpty()) {
+			cipherService.initEncryptOrDecrypt();
+			String password = cipherService.decrypt(account.getPassword());
+			account.setPassword(password);
+		}
 		
 		int totalFetched = mailAccountService.fetchEmails(account, true);
 		
 		response.setFlash(I18n.get(String.format("Total email fetched: %s", totalFetched)));
 	}
+	
+	public void validate(ActionRequest request, ActionResponse response) throws InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException {
+		
+		Context context = request.getContext();
 
+		if(!AppSettings.get().get("application.encryptionkey").isEmpty()) {
+			cipherService.initEncryptOrDecrypt();
+			String password = cipherService.encrypt((String) context.get("loginPassword"));
+	        response.setValue("password", password);	
+		}        
+	}
 }
