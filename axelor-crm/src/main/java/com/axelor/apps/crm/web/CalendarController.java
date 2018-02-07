be583357db69a210101eac4ceb39edffb7269d7b
aga-axelor <a.gandhi@axelor.com>
2018-02-07 16:13:29 +0530

    Anomaly #10092: Fix EVENT : calendar

--- axelor-crm/src/main/java/com/axelor/apps/crm/web/CalendarController.java
+++ axelor-crm/src/main/java/com/axelor/apps/crm/web/CalendarController.java
@@ -18,16 +18,12 @@
 package com.axelor.apps.crm.web;
 
 import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.axelor.apps.crm.db.Event;
-import com.axelor.apps.crm.db.repo.EventRepository;
 import com.axelor.apps.crm.service.CalendarService;
 import com.axelor.auth.AuthUtils;
 import com.axelor.auth.db.User;
@@ -35,9 +31,7 @@ import com.axelor.i18n.I18n;
 import com.axelor.meta.schema.actions.ActionView;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
-import com.axelor.team.db.Team;
 import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
 public class CalendarController {
@@ -47,20 +41,8 @@ public class CalendarController {
 	@Inject
 	private CalendarService calendarService;
 	
-	@Inject
-	private EventRepository eventRepo;
-	
 	public void showMyEvents(ActionRequest request, ActionResponse response){
 		User user = AuthUtils.getUser();
-		List<Long> eventIdlist = new ArrayList<Long>();
-		eventIdlist.add(new Long(0));
-		
-		List<Event> eventList = eventRepo.all().filter("self.user.id = ?1 or self.calendar.user.id = ?1 "
-				+ " or self.attendees.user.id = ?1 or self.organizer.user.id = ?1",
-				user.getId()).fetch();
-		eventIdlist.addAll(Lists.transform(eventList, it->it.getId()));
-		
-		log.debug("My event ids found: {}", eventIdlist);
 		
 		response.setView(ActionView
 	            .define(I18n.get("My Calendar"))
@@ -69,32 +51,13 @@ public class CalendarController {
 	            .add("grid", "event-grid")
 	            .add("form", "event-form")
 	            .context("_typeSelect", 2)
-	            .domain("self.id in (" + Joiner.on(',').join(eventIdlist) + ")")
-	            .map());
+				.domain("self.user.id = :_userId or self.calendar.user.id = :_userId or self.attendees.user.id = :_userId or self.organizer.user.id = :_userId")
+				.context("_userId", user.getId())
+				.map());
 	}
 	
 	public void showTeamEvents(ActionRequest request, ActionResponse response){
 		User user = AuthUtils.getUser();
-		Team team = user.getActiveTeam();
-		List<Long> eventIdlist = new ArrayList<Long>();
-		eventIdlist.add(new Long(0));
-		
-		Set<User> userSet = new HashSet<User>();
-		if(team == null || team.getMembers() == null || team.getMembers().isEmpty()){
-			userSet.add(user);
-		}
-		else{
-			userSet = team.getMembers();
-		}
-		
-		log.debug("Team members: {}", userSet);
-		
-		List<Event> eventList = eventRepo.all().filter("self.user in (?1) or self.calendar.user in (?1)"
-				+ " or self.attendees.user in (?1) or self.organizer.user in (?1)",
-				userSet).fetch();
-		eventIdlist.addAll(Lists.transform(eventList, it->it.getId()));
-		
-		log.debug("Team event ids found: {}", eventIdlist);
 		
 		response.setView(ActionView
 	            .define(I18n.get("Team Calendar"))
@@ -104,7 +67,8 @@ public class CalendarController {
 	            .add("form", "event-form")
 	            .context("_typeSelect", 2)
 	            .context("_internalUser", user.getId())
-	            .domain("self.id in ("+Joiner.on(",").join(eventIdlist)+")")
+	            .domain("self.team.id = :_teamId")
+	            .context("_teamId", user.getActiveTeam() != null ? user.getActiveTeam().getId() : null)
 	            .map());
 	}
 	
@@ -112,9 +76,9 @@ public class CalendarController {
 		User user = AuthUtils.getUser();
 		List<Long> eventIdlist = calendarService.showSharedEvents(user);
 		eventIdlist.add(new Long(0));
-		
+
 		log.debug("Shared event ids found: {}", eventIdlist);
-		
+
 		response.setView(ActionView
 	            .define(I18n.get("Shared Calendar"))
 	            .model(Event.class.getName())
