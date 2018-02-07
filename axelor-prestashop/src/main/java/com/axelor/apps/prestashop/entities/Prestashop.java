2a31dc9998980ac2c4c00dd6315416ca2d31ca18
Sébastien Le Ray <sebastien@orniz.org>
2018-02-07 16:17:17 +0100

    prestashop: allow extra tags on Prestashop envelop
    More than one tag can be enclosed in the prestashop's one (eg. content
    with image serialized as base64).

--- axelor-prestashop/src/main/java/com/axelor/apps/prestashop/entities/Prestashop.java
+++ axelor-prestashop/src/main/java/com/axelor/apps/prestashop/entities/Prestashop.java
@@ -1,11 +1,15 @@
 package com.axelor.apps.prestashop.entities;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlElementRef;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.w3c.dom.Element;
 
 /**
  * Common root tag for all API calls. Interesting content
@@ -17,6 +21,10 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 public class Prestashop {
 	@XmlElementRef
 	private PrestashopContainerEntity content;
+	// Some structures (eg. images) have an additional content element
+	// avoid parsing issues
+	@XmlAnyElement
+	private List<Element> additionalAttributes;
 
 	@SuppressWarnings("unchecked")
 	public <T extends PrestashopContainerEntity> T getContent() {
@@ -25,6 +33,14 @@ public class Prestashop {
 
 	public void setContent(PrestashopContainerEntity content) {
 		this.content = content;
+	}
+
+	public List<Element> getAdditionalAttributes() {
+		return additionalAttributes;
+	}
+
+	public void setAdditionalAttributes(List<Element> additionalAttributes) {
+		this.additionalAttributes = additionalAttributes;
 	}
 
 	@Override
