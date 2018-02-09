8ef86cf9b65119b73f4f9c87704d4a29e8a03193
Sébastien Le Ray <sebastien@orniz.org>
2018-02-09 11:03:05 +0100

    prestashop: full customers export rewrite

--- axelor-prestashop/src/main/java/com/axelor/apps/prestashop/exports/service/ExportCustomerServiceImpl.java
+++ axelor-prestashop/src/main/java/com/axelor/apps/prestashop/exports/service/ExportCustomerServiceImpl.java
@@ -19,35 +19,26 @@ package com.axelor.apps.prestashop.exports.service;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.StringWriter;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactoryConfigurationError;
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.axelor.apps.base.db.AppPrestashop;
 import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.base.db.repo.PartnerRepository;
-import com.axelor.apps.prestashop.db.Customers;
-import com.axelor.apps.prestashop.db.Prestashop;
+import com.axelor.apps.prestashop.entities.PrestashopCustomer;
 import com.axelor.apps.prestashop.entities.PrestashopResourceType;
-import com.axelor.apps.prestashop.exception.IExceptionMessage;
 import com.axelor.apps.prestashop.service.library.PSWebServiceClient;
-import com.axelor.apps.prestashop.service.library.PSWebServiceClient.Options;
 import com.axelor.apps.prestashop.service.library.PrestaShopWebserviceException;
-import com.axelor.exception.AxelorException;
-import com.axelor.exception.db.IException;
 import com.axelor.i18n.I18n;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -55,6 +46,8 @@ import com.google.inject.persist.Transactional;
 
 @Singleton
 public class ExportCustomerServiceImpl implements ExportCustomerService {
+	private Logger log = LoggerFactory.getLogger(getClass());
+
 	private PartnerRepository partnerRepo;
 
 	@Inject
@@ -64,20 +57,12 @@ public class ExportCustomerServiceImpl implements ExportCustomerService {
 
 	@Override
 	@Transactional
-	public void exportCustomer(AppPrestashop appConfig, ZonedDateTime endDate, BufferedWriter bwExport)
-			throws IOException, TransformerConfigurationException, TransformerException, ParserConfigurationException,
-			SAXException, PrestaShopWebserviceException, JAXBException, TransformerFactoryConfigurationError {
+	public void exportCustomer(AppPrestashop appConfig, ZonedDateTime endDate, BufferedWriter logBuffer) throws IOException, PrestaShopWebserviceException {
 		int done = 0;
-		int anomaly = 0;
+		int errors = 0;
 
-		String prestaShopId = null;
-		String schema = null;
-		Document document = null;
-
-		bwExport.newLine();
-		bwExport.write("-----------------------------------------------");
-		bwExport.newLine();
-		bwExport.write("Customer");
+		logBuffer.write(String.format("%n====== CUSTOMERS ======%n"));
+		log.debug("Starting customers export to PrestaShop");
 
 		final StringBuilder filter = new StringBuilder(128);
 		final List<Object> params = new ArrayList<>(4);
@@ -96,100 +81,99 @@ public class ExportCustomerServiceImpl implements ExportCustomerService {
 			filter.append(" AND (self.prestaShopId IS NOT NULL)");
 		}
 
-		for (Partner partner : partnerRepo.all().filter(filter.toString(), params.toArray(new Object[0])).fetch()) {
+		final PSWebServiceClient ws = new PSWebServiceClient(appConfig.getPrestaShopUrl(), appConfig.getPrestaShopKey());
+
+		final List<PrestashopCustomer> remoteCustomers = ws.fetchAll(PrestashopResourceType.CUSTOMERS);
+		final Map<Integer, PrestashopCustomer> customersById = new HashMap<>();
+		final Map<String, PrestashopCustomer> customersBySiret = new HashMap<>();
+		final Map<String, PrestashopCustomer> customersByCompany = new HashMap<>();
+		for(PrestashopCustomer c : remoteCustomers) {
+			customersById.put(c.getId(), c);
+			customersBySiret.put(c.getSiret(), c);
+			customersByCompany.put(c.getCompany(), c);
+		}
+
+		final LocalDateTime now = LocalDateTime.now();
+
+		for (Partner localCustomer : partnerRepo.all().filter(filter.toString(), params.toArray(new Object[0])).fetch()) {
+			logBuffer.write(String.format("Exporting customer #%d (%s) - ", localCustomer.getId(), localCustomer.getName()));
 			try {
+				PrestashopCustomer remoteCustomer;
+				if(localCustomer.getPrestaShopId() != null) {
+					logBuffer.write("prestashop id=" + localCustomer.getPrestaShopId());
+					remoteCustomer = customersById.get(localCustomer.getPrestaShopId());
+					if(remoteCustomer == null) {
+						logBuffer.write(String.format(" [ERROR] Not found remotely%n"));
+						log.error("Unable to fetch remote customer #{} ({}), something's probably very wrong, skipping",
+								localCustomer.getPrestaShopId(), localCustomer.getName());
+						++errors;
+						continue;
+					} // Note: contrary to currencies and products, we don't check that various fields match since customer can edit them
+				} else {
+					remoteCustomer = null;
 
-				Customers customer = new Customers();
-				customer.setId(Objects.toString(partner.getPrestaShopId(), null));
-
-				if (partner.getPartnerTypeSelect() == 1) {
-					if (partner.getContactPartnerSet().size() != 0) {
-						customer.setCompany(partner.getName());
-						if(!partner.getContactPartnerSet().iterator().next().getFirstName().isEmpty() && !partner.getContactPartnerSet().iterator().next().getName().isEmpty()) {
-							customer.setFirstname(partner.getContactPartnerSet().iterator().next().getFirstName());
-							customer.setLastname(partner.getContactPartnerSet().iterator().next().getName());
-						} else {
-							throw new AxelorException(IException.NO_VALUE, I18n.get(IExceptionMessage.INVALID_CONTACT));
+					if(StringUtils.isNotBlank(localCustomer.getRegistrationCode())) {
+						remoteCustomer = customersBySiret.get(localCustomer.getRegistrationCode());
+						if(remoteCustomer != null) {
+							logBuffer.write(String.format("remotely found by registration code (%s), remote id: %d", localCustomer.getRegistrationCode(), remoteCustomer.getId()));
 						}
-					} else {
-						throw new AxelorException(IException.NO_VALUE, I18n.get(IExceptionMessage.INVALID_CONTACT));
 					}
-				} else {
+					if(remoteCustomer == null && localCustomer.getPartnerTypeSelect() == PartnerRepository.PARTNER_TYPE_COMPANY) {
+						remoteCustomer = customersByCompany.get(localCustomer.getName());
+						if(remoteCustomer != null) {
+							logBuffer.write(String.format("remotely found by company name, remote id: %d", remoteCustomer.getId()));
+						}
+					}
 
-					if (!partner.getName().isEmpty() && !partner.getFirstName().isEmpty()) {
-						customer.setFirstname(partner.getFirstName());
-						customer.setLastname(partner.getName());
-					} else {
-						throw new AxelorException(IException.NO_VALUE, I18n.get(IExceptionMessage.INVALID_INDIVIDUAL));
+					if(remoteCustomer == null) {
+						logBuffer.write("failed to find by registration code or company name, creating");
+						remoteCustomer = new PrestashopCustomer();
+						remoteCustomer.setNote(I18n.get("Imported from Axelor"));
+						remoteCustomer.setPassword(RandomStringUtils.randomGraph(16));
+						if(localCustomer.getEmailAddress() != null) {
+							remoteCustomer.setEmail(localCustomer.getEmailAddress().getAddress());
+						}
+
+						if(localCustomer.getPartnerTypeSelect() == PartnerRepository.PARTNER_TYPE_INDIVIDUAL) {
+							remoteCustomer.setFirstname(localCustomer.getFirstName());
+							remoteCustomer.setLastname(localCustomer.getName());
+						} else {
+							remoteCustomer.setCompany(localCustomer.getName());
+							if(localCustomer.getContactPartnerSet().isEmpty() == false) {
+								Partner localContact = localCustomer.getContactPartnerSet().iterator().next();
+								remoteCustomer.setFirstname(localContact.getFirstName());
+								remoteCustomer.setLastname(localContact.getName());
+							} else {
+								logBuffer.write(String.format(" [WARNING] No contact filled, required for Pretashop, skipping%n"));
+								continue;
+							}
+						}
 					}
 				}
 
-				if (partner.getPaymentCondition() != null) {
-					customer.setMax_payment_days(partner.getPaymentCondition().getPaymentTime().toString());
+				// Only push elements that cannot be edited by user
+				remoteCustomer.setSiret(localCustomer.getRegistrationCode());
+				remoteCustomer.setWebsite(localCustomer.getWebSite());
+				if(localCustomer.getPaymentCondition() != null) {
+					remoteCustomer.setMaxPaymentDays(localCustomer.getPaymentCondition().getPaymentTime());
+				}
+				if(localCustomer.getAccountingSituationList().isEmpty() == false) {
+					// FIXME We should have a per company configuration…
+					remoteCustomer.setAllowedOutstandingAmount(localCustomer.getAccountingSituationList().get(0).getAcceptedCredit().setScale(appConfig.getExportPriceScale(), BigDecimal.ROUND_HALF_UP));
 				}
 
-				if (partner.getEmailAddress() != null) {
-					customer.setEmail(partner.getEmailAddress().getAddress());
-				} else {
-					throw new AxelorException(IException.NO_VALUE, I18n.get(IExceptionMessage.INVALID_EMAIL));
-				}
-
-				customer.setId_default_group("3");
-				customer.setWebsite(partner.getWebSite());
-				customer.setSecure_key(partner.getSecureKey());
-				customer.setActive("1");
-				customer.setId_shop("1");
-				customer.setId_shop_group("1");
-				customer.setPasswd("NULL"); // required
-
-				Prestashop prestaShop = new Prestashop();
-				prestaShop.setPrestashop(customer);
-
-				StringWriter sw = new StringWriter();
-				JAXBContext contextObj = JAXBContext.newInstance(Prestashop.class);
-				Marshaller marshallerObj = contextObj.createMarshaller();
-				marshallerObj.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-				marshallerObj.marshal(prestaShop, sw);
-				schema = sw.toString();
-
-				PSWebServiceClient ws;
-				Options options = new Options();
-				options.setResourceType(PrestashopResourceType.CUSTOMERS);
-				options.setXmlPayload(schema);
-
-				if (partner.getPrestaShopId() == null) {
-					ws = new PSWebServiceClient(appConfig.getPrestaShopUrl() + "/api/customers?schema=synopsis", appConfig.getPrestaShopKey());
-					document = ws.add(options);
-				} else {
-					options.setRequestedId(partner.getPrestaShopId());
-					ws = new PSWebServiceClient(appConfig.getPrestaShopUrl(), appConfig.getPrestaShopKey());
-					document = ws.edit(options);
-				}
-
-				prestaShopId = document.getElementsByTagName("id").item(0).getTextContent();
-				partner.setSecureKey(document.getElementsByTagName("secure_key").item(0).getTextContent());
-				partner.setPrestaShopId(Integer.valueOf(prestaShopId));
-				partnerRepo.save(partner);
-				done++;
-
-			} catch (AxelorException e) {
-				bwExport.newLine();
-				bwExport.newLine();
-				bwExport.write("Id - " + partner.getId().toString() + " " + e.getMessage());
-				anomaly++;
-				continue;
-
-			} catch (Exception e) {
-				bwExport.newLine();
-				bwExport.newLine();
-				bwExport.write("Id - " + partner.getId().toString() + " " + e.getMessage());
-				anomaly++;
-				continue;
+				remoteCustomer.setUpdateDate(now);
+				remoteCustomer = ws.save(PrestashopResourceType.CUSTOMERS, remoteCustomer);
+				localCustomer.setPrestaShopId(remoteCustomer.getId());
+				logBuffer.write(String.format(" [SUCCESS]%n"));
+				++done;
+			} catch(PrestaShopWebserviceException | IOException e) {
+				logBuffer.write(String.format(" [ERROR] %s (full trace is in application logs)%n", e.getLocalizedMessage()));
+				log.error(String.format("Exception while synchronizing customer #%d (%s)", localCustomer.getId(), localCustomer.getName()), e);
+				++errors;
 			}
 		}
 
-		bwExport.newLine();
-		bwExport.newLine();
-		bwExport.write("Succeed : " + done + " " + "Anomaly : " + anomaly);
+		logBuffer.write(String.format("%n=== END OF CUSTOMERS IMPORT, done: %d, errors: %d ===%n", done, errors));
 	}
 }
