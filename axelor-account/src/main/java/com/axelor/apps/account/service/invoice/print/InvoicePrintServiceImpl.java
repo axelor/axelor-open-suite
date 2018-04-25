0472cd7abd5af3c7352b7b4b67329d345e9425e8
Arthur Lefebvre <a.lefebvre@axelor.com>
2018-04-25 18:02:15 +0200

    Clean Invoice report to work with one invoice

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoice/print/InvoicePrintServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoice/print/InvoicePrintServiceImpl.java
@@ -129,7 +129,7 @@ public class InvoicePrintServiceImpl implements InvoicePrintService {
 
         ReportSettings reportSetting = ReportFactory.createReport(IReport.INVOICE, title + " - ${date}");
 
-        return reportSetting.addParam("InvoiceId", invoice.getId().toString())
+        return reportSetting.addParam("InvoiceId", invoice.getId())
                 .addParam("Locale", locale)
                 .addParam("InvoicesCopy", invoice.getInvoicesCopySelect());
     }
