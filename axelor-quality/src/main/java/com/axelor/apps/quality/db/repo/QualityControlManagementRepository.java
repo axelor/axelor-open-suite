b1b950c0005c79a87566630f20b5ba182ad01371
Sébastien Le Ray <sebastien@orniz.org>
2018-04-30 20:15:41 +0200

    quality: allow to fix list of control points
    Add an option in quality module to remove the ability to add custom
    control points on a quality control. Only points defined in the quality
    process will be added.

--- axelor-quality/src/main/java/com/axelor/apps/quality/db/repo/QualityControlManagementRepository.java
+++ axelor-quality/src/main/java/com/axelor/apps/quality/db/repo/QualityControlManagementRepository.java
@@ -18,27 +18,45 @@
 package com.axelor.apps.quality.db.repo;
 
 import com.axelor.apps.base.db.IAdministration;
+import com.axelor.apps.base.db.repo.AppQualityRepository;
 import com.axelor.apps.base.service.administration.SequenceService;
 import com.axelor.apps.quality.db.QualityControl;
+import com.axelor.apps.quality.service.QualityControlService;
 import com.google.common.base.Strings;
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
+@Singleton
 public class QualityControlManagementRepository extends QualityControlRepository {
-	
-	@Inject
 	private SequenceService sequenceService;
+	private AppQualityRepository appRepository;
+	private QualityControlService qualityControlService;
+
+	@Inject
+	public QualityControlManagementRepository(SequenceService sequenceService, AppQualityRepository appRepository,
+			QualityControlService qualityControlService) {
+		this.sequenceService = sequenceService;
+		this.appRepository = appRepository;
+		this.qualityControlService = qualityControlService;
+	}
 
 	/**
 	 * Generate and set sequence in reference with predefined prefix.
 	 * @param qualityControl  Overridden quality control object to set reference on onSave event.
-	 * 
+	 *
 	 */
 	@Override
 	public QualityControl save(QualityControl qualityControl) {
-		
-		if (Strings.isNullOrEmpty(qualityControl.getReference()))
+
+		if (Strings.isNullOrEmpty(qualityControl.getReference())) {
 			qualityControl.setReference(sequenceService.getSequenceNumber(IAdministration.QUALITY_CONTROL, null));
+			if(appRepository.all().fetchOne().getAllowPerControlControlPoints() == Boolean.FALSE) {
+				qualityControl = super.save(qualityControl);
+				qualityControlService.preFillOperations(qualityControl);
+				return qualityControl;
+			}
+		}
 		return super.save(qualityControl);
 	}
-	
+
 }
