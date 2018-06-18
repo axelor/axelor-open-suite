2a2c275f472ef55aa53924128791a841fea4e51d
fsc-axelor <f.schriever@axelor.com>
2018-06-18 11:59:18 +0200

    RM12488: Move all past generate function to State

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/projectgenerator/state/ProjectGeneratorStateAlone.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/projectgenerator/state/ProjectGeneratorStateAlone.java
@@ -1,18 +1,28 @@
 package com.axelor.apps.businessproject.service.projectgenerator.state;
 
+import com.axelor.apps.businessproject.service.ProjectBusinessService;
 import com.axelor.apps.project.db.Project;
+import com.axelor.apps.project.db.repo.ProjectRepository;
 import com.axelor.apps.sale.db.SaleOrder;
-import com.axelor.meta.schema.actions.ActionView.ActionViewBuilder;
+import com.google.inject.Inject;
 
 public class ProjectGeneratorStateAlone implements ProjectGeneratorState {
 
-  @Override
-  public Project generate(SaleOrder saleOrder) {
-    return null;
+  protected ProjectBusinessService projectBusinessService;
+  protected ProjectRepository projectRepository;
+
+  @Inject
+  public ProjectGeneratorStateAlone(
+      ProjectBusinessService projectBusinessService, ProjectRepository projectRepository) {
+    this.projectBusinessService = projectBusinessService;
+    this.projectRepository = projectRepository;
   }
 
   @Override
-  public ActionViewBuilder fill(Project project, SaleOrder saleOrder) {
-    return null;
+  public Project generate(SaleOrder saleOrder) {
+    Project project = projectBusinessService.generateProject(saleOrder);
+    project.setIsProject(false);
+    project.setIsBusinessProject(true);
+    return projectRepository.save(project);
   }
 }
--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/projectgenerator/state/ProjectGeneratorStatePhase.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/projectgenerator/state/ProjectGeneratorStatePhase.java
@@ -1,18 +1,53 @@
 package com.axelor.apps.businessproject.service.projectgenerator.state;
 
+import com.axelor.apps.base.db.Product;
+import com.axelor.apps.base.db.repo.ProductRepository;
+import com.axelor.apps.businessproject.service.ProjectBusinessService;
 import com.axelor.apps.project.db.Project;
+import com.axelor.apps.project.db.repo.ProjectRepository;
 import com.axelor.apps.sale.db.SaleOrder;
+import com.axelor.apps.sale.db.SaleOrderLine;
+import com.axelor.apps.sale.db.repo.SaleOrderLineRepository;
+import com.axelor.apps.tool.StringTool;
+import com.axelor.exception.AxelorException;
+import com.axelor.meta.schema.actions.ActionView;
 import com.axelor.meta.schema.actions.ActionView.ActionViewBuilder;
+import com.google.inject.Inject;
+import com.google.inject.persist.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ProjectGeneratorStatePhase implements ProjectGeneratorState {
+public class ProjectGeneratorStatePhase extends ProjectGeneratorStateAlone
+    implements ProjectGeneratorState {
 
-  @Override
-  public Project generate(SaleOrder saleOrder) {
-    return null;
+  protected SaleOrderLineRepository saleOrderLineRepository;
+
+  @Inject
+  public ProjectGeneratorStatePhase(
+      ProjectBusinessService projectBusinessService,
+      ProjectRepository projectRepository,
+      SaleOrderLineRepository saleOrderLineRepository) {
+    super(projectBusinessService, projectRepository);
+    this.saleOrderLineRepository = saleOrderLineRepository;
   }
 
   @Override
+  @Transactional(rollbackOn = {AxelorException.class, Exception.class})
   public ActionViewBuilder fill(Project project, SaleOrder saleOrder) {
-    return null;
+    List<Project> projects = new ArrayList<>();
+    for (SaleOrderLine saleOrderLine : saleOrder.getSaleOrderLineList()) {
+      Product product = saleOrderLine.getProduct();
+      if (ProductRepository.PRODUCT_TYPE_SERVICE.equals(product.getProductTypeSelect())
+          && saleOrderLine.getSaleSupplySelect() == SaleOrderLineRepository.SALE_SUPPLY_PRODUCE) {
+        Project phase = projectBusinessService.generatePhaseProject(saleOrderLine, project);
+        saleOrderLineRepository.save(saleOrderLine);
+        projects.add(phase);
+      }
+    }
+    return ActionView.define(String.format("Project%s generated", (projects.size() > 1 ? "s" : "")))
+        .model(Project.class.getName())
+        .add("grid", "project-grid")
+        .add("form", "project-form")
+        .domain(String.format("self.id in (%s)", StringTool.getIdListString(projects)));
   }
 }
