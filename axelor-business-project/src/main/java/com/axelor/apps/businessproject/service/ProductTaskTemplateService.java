4069bec58fc8b4b2b0d45077300f19b7aae3f283
fsc-axelor <f.schriever@axelor.com>
2018-06-19 18:19:59 +0200

    RM12488: Implement ProjectGeneratorFactoryTaskTemplate

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/ProductTaskTemplateService.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/ProductTaskTemplateService.java
@@ -1,7 +1,19 @@
 package com.axelor.apps.businessproject.service;
 
 import com.axelor.apps.businessproject.db.ProductTaskTemplate;
+import com.axelor.apps.project.db.Project;
+import com.axelor.apps.project.db.TaskTemplate;
+import com.axelor.team.db.TeamTask;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ProductTaskTemplateService {
+  List<TeamTask> convert(
+      List<? extends TaskTemplate> templates,
+      Project project,
+      TeamTask parent,
+      LocalDateTime startDate,
+      int qty);
+
   void remove(ProductTaskTemplate productTaskTemplate);
 }
