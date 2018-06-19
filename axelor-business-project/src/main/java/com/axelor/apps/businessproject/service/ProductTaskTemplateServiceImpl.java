9318358c409c76167b6a1beff2de16001dbdca30
fsc-axelor <f.schriever@axelor.com>
2018-06-19 18:19:59 +0200

    RM12488: Implement ProjectGeneratorFactoryTaskTemplate

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/ProductTaskTemplateServiceImpl.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/ProductTaskTemplateServiceImpl.java
@@ -2,17 +2,65 @@ package com.axelor.apps.businessproject.service;
 
 import com.axelor.apps.businessproject.db.ProductTaskTemplate;
 import com.axelor.apps.businessproject.db.repo.ProductTaskTemplateRepository;
+import com.axelor.apps.project.db.Project;
+import com.axelor.apps.project.db.TaskTemplate;
+import com.axelor.apps.project.service.TeamTaskService;
 import com.axelor.exception.AxelorException;
+import com.axelor.team.db.TeamTask;
+import com.axelor.team.db.repo.TeamTaskRepository;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductTaskTemplateServiceImpl implements ProductTaskTemplateService {
 
   protected ProductTaskTemplateRepository repository;
+  protected TeamTaskService teamTaskService;
+  protected TeamTaskRepository teamTaskRepository;
 
   @Inject
-  public ProductTaskTemplateServiceImpl(ProductTaskTemplateRepository repository) {
+  public ProductTaskTemplateServiceImpl(
+      ProductTaskTemplateRepository repository,
+      TeamTaskService teamTaskService,
+      TeamTaskRepository teamTaskRepository) {
     this.repository = repository;
+    this.teamTaskService = teamTaskService;
+    this.teamTaskRepository = teamTaskRepository;
+  }
+
+  @Override
+  @Transactional(rollbackOn = {AxelorException.class, Exception.class})
+  public List<TeamTask> convert(
+      List<? extends TaskTemplate> templates,
+      Project project,
+      TeamTask parent,
+      LocalDateTime startDate,
+      int qty) {
+    List<TeamTask> tasks = new ArrayList<>();
+
+    for (TaskTemplate template : templates) {
+      int qtyTmp = (template.getIsUniqueTaskForMultipleQuantity() ? 1 : qty);
+      while (qtyTmp-- > 0) {
+        LocalDateTime dateWithDelay = startDate.plusHours(template.getDelayToStart().longValue());
+
+        TeamTask task =
+            teamTaskService.create(template.getName(), project, template.getAssignedTo());
+        task.setParentTask(parent);
+        task.setTaskDate(dateWithDelay.toLocalDate());
+        task.setTaskEndDate(
+            dateWithDelay.plusHours(template.getDuration().longValue()).toLocalDate());
+        task.setTotalPlannedHrs(template.getTotalPlannedHrs());
+        tasks.add(teamTaskRepository.save(task));
+
+        List<TeamTask> children =
+            convert(template.getTaskTemplateList(), project, task, dateWithDelay, 1);
+        tasks.addAll(children);
+      }
+    }
+
+    return tasks;
   }
 
   @Override
--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/projectgenerator/factory/ProjectGeneratorFactoryTaskTemplate.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/projectgenerator/factory/ProjectGeneratorFactoryTaskTemplate.java
@@ -1,26 +1,48 @@
 package com.axelor.apps.businessproject.service.projectgenerator.factory;
 
+import com.axelor.apps.businessproject.service.ProductTaskTemplateService;
 import com.axelor.apps.businessproject.service.ProjectBusinessService;
 import com.axelor.apps.businessproject.service.projectgenerator.ProjectGeneratorFactory;
 import com.axelor.apps.project.db.Project;
 import com.axelor.apps.project.db.repo.ProjectRepository;
+import com.axelor.apps.project.service.TeamTaskService;
 import com.axelor.apps.sale.db.SaleOrder;
+import com.axelor.apps.sale.db.SaleOrderLine;
+import com.axelor.apps.tool.StringTool;
 import com.axelor.exception.AxelorException;
+import com.axelor.meta.schema.actions.ActionView;
 import com.axelor.meta.schema.actions.ActionView.ActionViewBuilder;
+import com.axelor.team.db.TeamTask;
+import com.axelor.team.db.repo.TeamTaskRepository;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.apache.commons.collections.CollectionUtils;
 
 public class ProjectGeneratorFactoryTaskTemplate implements ProjectGeneratorFactory {
 
   private ProjectBusinessService projectBusinessService;
   private ProjectRepository projectRepository;
+  private TeamTaskService teamTaskService;
+  private TeamTaskRepository teamTaskRepository;
+  private ProductTaskTemplateService productTaskTemplateService;
 
   @Inject
   public ProjectGeneratorFactoryTaskTemplate(
-      ProjectBusinessService projectBusinessService, ProjectRepository projectRepository) {
+      ProjectBusinessService projectBusinessService,
+      ProjectRepository projectRepository,
+      TeamTaskService teamTaskService,
+      TeamTaskRepository teamTaskRepository,
+      ProductTaskTemplateService productTaskTemplateService) {
     this.projectBusinessService = projectBusinessService;
     this.projectRepository = projectRepository;
+    this.teamTaskService = teamTaskService;
+    this.teamTaskRepository = teamTaskRepository;
+    this.productTaskTemplateService = productTaskTemplateService;
   }
 
   @Override
@@ -35,6 +57,34 @@ public class ProjectGeneratorFactoryTaskTemplate implements ProjectGeneratorFact
   @Override
   @Transactional(rollbackOn = {AxelorException.class, Exception.class})
   public ActionViewBuilder fill(Project project, SaleOrder saleOrder, LocalDateTime startDate) {
-    return null;
+    List<TeamTask> tasks = new ArrayList<>();
+
+    TeamTask root =
+        teamTaskService.create(saleOrder.getFullName(), project, project.getAssignedTo());
+    root.setTaskDate(startDate.toLocalDate());
+    tasks.add(teamTaskRepository.save(root));
+
+    for (SaleOrderLine orderLine : saleOrder.getSaleOrderLineList()) {
+      if (!CollectionUtils.isEmpty(orderLine.getProduct().getTaskTemplateList())) {
+        List<TeamTask> convertedTasks =
+            productTaskTemplateService.convert(
+                orderLine
+                    .getProduct()
+                    .getTaskTemplateList()
+                    .stream()
+                    .filter(template -> Objects.isNull(template.getParentTaskTemplate()))
+                    .collect(Collectors.toList()),
+                project,
+                root,
+                startDate,
+                orderLine.getQty().intValue());
+        tasks.addAll(convertedTasks);
+      }
+    }
+    return ActionView.define(String.format("Task%s generated", (tasks.size() > 1 ? "s" : "")))
+        .model(TeamTask.class.getName())
+        .add("grid", "team-task-grid")
+        .add("form", "team-task-form")
+        .domain(String.format("self.id in (%s)", StringTool.getIdListString(tasks)));
   }
 }
