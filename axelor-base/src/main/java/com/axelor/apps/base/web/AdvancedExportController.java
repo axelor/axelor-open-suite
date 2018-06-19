dd931258d2351f83c9f2089232e7315dee21629b
man-axelor <m.antoun@axelor.com>
2018-06-19 10:59:49 +0200

    Redmine 12562: Fix merge problem by using previous version of file and doing the same things in Birt to fix the problem

--- axelor-base/src/main/java/com/axelor/apps/base/web/AdvancedExportController.java
+++ axelor-base/src/main/java/com/axelor/apps/base/web/AdvancedExportController.java
@@ -51,8 +51,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
