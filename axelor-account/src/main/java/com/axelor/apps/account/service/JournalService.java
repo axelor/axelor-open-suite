12a599dd338c13e5ebe766c643c31bcdac8666f2
Geoffrey DUBAUX - Axelor <g.dubaux@axelor.com>
2018-10-15 00:56:48 +0200

    RM14224 - added a method to compute a journal balance

--- axelor-account/src/main/java/com/axelor/apps/account/service/JournalService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/JournalService.java
@@ -23,12 +23,25 @@ import com.axelor.apps.account.db.Journal;
 import com.axelor.apps.account.exception.IExceptionMessage;
 import com.axelor.apps.account.service.config.AccountConfigService;
 import com.axelor.apps.base.db.Company;
+import com.axelor.db.JPA;
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.db.repo.TraceBackRepository;
 import com.axelor.i18n.I18n;
+import java.lang.invoke.MethodHandles;
 import java.math.BigDecimal;
+import javax.persistence.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JournalService {
+
+  private final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  /** Debit balance = debit - credit */
+  public static final Integer BALANCE_TYPE_DEBIT_BALANCE = 1;
+
+  /** Credit balance = credit - debit */
+  public static final Integer BALANCE_TYPE_CREDIT_BALANCE = 1;
 
   private AccountConfigService accountConfigService;
 
@@ -93,5 +106,40 @@ public class JournalService {
     }
 
     return null;
+  }
+
+  /**
+   * Compute the balance of the journal, depending of the account type and balance type
+   *
+   * @param journal Journal
+   * @param accountType Technical type select of AccountType
+   * @param balanceType
+   *     <p>1 : debit balance = debit - credit
+   *     <p>2 : credit balance = credit - debit
+   * @return The balance (debit balance or credit balance)
+   */
+  public BigDecimal computeBalance(Journal journal, int accountType, int balanceType) {
+
+    Query balanceQuery =
+        JPA.em()
+            .createQuery(
+                "select sum(self.debit - self.credit) from MoveLine self where self.move.journal = :journal "
+                    + "and self.move.ignoreInAccountingOk IN ('false', null) and self.move.statusSelect IN (2, 3) and self.account.accountType.technicalTypeSelect = :accountType");
+
+    balanceQuery.setParameter("journal", journal);
+    balanceQuery.setParameter("accountType", accountType);
+
+    BigDecimal balance = (BigDecimal) balanceQuery.getSingleResult();
+
+    if (balance != null) {
+      if (balanceType == BALANCE_TYPE_CREDIT_BALANCE) {
+        balance = balance.negate();
+      }
+      log.debug("Account balance : {}", balance);
+
+      return balance;
+    } else {
+      return BigDecimal.ZERO;
+    }
   }
 }
