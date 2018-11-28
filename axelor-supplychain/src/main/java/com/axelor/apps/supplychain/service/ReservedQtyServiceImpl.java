499a3fa87163003bf4cd5ce6a0e1d8c64bbb994e
Arthur Lefebvre <a.lefebvre@axelor.com>
2018-11-28 17:19:23 +0100

    Reserved qty: move config to allocate on receipt.
    Move the field `autoAllocateOnReceipt` from app supplychain to
    supplychain config.

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/ReservedQtyServiceImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/ReservedQtyServiceImpl.java
@@ -18,6 +18,7 @@
 package com.axelor.apps.supplychain.service;
 
 import com.axelor.apps.base.db.CancelReason;
+import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.Product;
 import com.axelor.apps.base.db.Unit;
 import com.axelor.apps.base.service.UnitConversionService;
@@ -30,8 +31,9 @@ import com.axelor.apps.stock.db.repo.StockLocationRepository;
 import com.axelor.apps.stock.db.repo.StockMoveLineRepository;
 import com.axelor.apps.stock.db.repo.StockMoveRepository;
 import com.axelor.apps.stock.service.StockLocationLineService;
+import com.axelor.apps.supplychain.db.SupplyChainConfig;
 import com.axelor.apps.supplychain.exception.IExceptionMessage;
-import com.axelor.apps.supplychain.service.app.AppSupplychainService;
+import com.axelor.apps.supplychain.service.config.SupplyChainConfigService;
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.db.repo.TraceBackRepository;
 import com.axelor.i18n.I18n;
@@ -47,18 +49,18 @@ public class ReservedQtyServiceImpl implements ReservedQtyService {
   protected StockLocationLineService stockLocationLineService;
   protected StockMoveLineRepository stockMoveLineRepository;
   protected UnitConversionService unitConversionService;
-  protected AppSupplychainService appSupplychainService;
+  protected SupplyChainConfigService supplychainConfigService;
 
   @Inject
   public ReservedQtyServiceImpl(
       StockLocationLineService stockLocationLineService,
       StockMoveLineRepository stockMoveLineRepository,
       UnitConversionService unitConversionService,
-      AppSupplychainService appSupplychainService) {
+      SupplyChainConfigService supplyChainConfigService) {
     this.stockLocationLineService = stockLocationLineService;
     this.stockMoveLineRepository = stockMoveLineRepository;
     this.unitConversionService = unitConversionService;
-    this.appSupplychainService = appSupplychainService;
+    this.supplychainConfigService = supplyChainConfigService;
   }
 
   @Override
@@ -198,8 +200,10 @@ public class ReservedQtyServiceImpl implements ReservedQtyService {
     if (stockLocationLine == null) {
       return;
     }
+    Company company = stockLocationLine.getStockLocation().getCompany();
+    SupplyChainConfig supplyChainConfig = supplychainConfigService.getSupplyChainConfig(company);
     if (toStatus == StockMoveRepository.STATUS_REALIZED
-        && appSupplychainService.getAppSupplychain().getAutoAllocateOnReceipt()) {
+        && supplyChainConfig.getAutoAllocateOnReceipt()) {
       reallocateQty(stockMoveLine, stockLocation, stockLocationLine, product, qty);
     }
     checkReservedQtyStocks(stockLocationLine, toStatus);
