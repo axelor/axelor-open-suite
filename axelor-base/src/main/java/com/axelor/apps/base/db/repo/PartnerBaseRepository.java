0d108a5c4dc863372b9b29f0239eb51270ad81bb
Geoffrey DUBAUX - Axelor <g.dubaux@axelor.com>
2019-01-03 12:04:01 +0100

    Update license year

--- axelor-base/src/main/java/com/axelor/apps/base/db/repo/PartnerBaseRepository.java
+++ axelor-base/src/main/java/com/axelor/apps/base/db/repo/PartnerBaseRepository.java
@@ -1,7 +1,7 @@
 /*
  * Axelor Business Solutions
  *
- * Copyright (C) 2018 Axelor (<http://axelor.com>).
+ * Copyright (C) 2019 Axelor (<http://axelor.com>).
  *
  * This program is free software: you can redistribute it and/or  modify
  * it under the terms of the GNU Affero General Public License, version 3,
@@ -89,8 +89,8 @@ public class PartnerBaseRepository extends PartnerRepository {
 
       if (!partner.getIsContact() && partner.getContactPartnerSet() != null) {
         for (Partner contact : partner.getContactPartnerSet()) {
-          if(contact.getMainPartner() == null) {
-              contact.setMainPartner(partner);
+          if (contact.getMainPartner() == null) {
+            contact.setMainPartner(partner);
           }
         }
       }
