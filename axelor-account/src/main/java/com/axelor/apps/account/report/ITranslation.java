61e3d94d8f82f4559a74bb7d6b7ce9cbb1c018f6
sve-axelor <s.vekaria@axelor.com>
2019-04-16 13:43:56 +0530

    Feature #17508 INVOICE : new process to print (change in printing for operationSubTypeSelect = Advance payment invoice)

--- axelor-account/src/main/java/com/axelor/apps/account/report/ITranslation.java
+++ axelor-account/src/main/java/com/axelor/apps/account/report/ITranslation.java
@@ -89,6 +89,7 @@ public interface ITranslation {
       "Invoice.supplierPartnerSeq"; /*)*/
   public static final String INVOICE_CUSTOMER_TAX_NUMBER = /*$$(*/
       "Invoice.customerTaxNumber"; /*)*/
+  public static final String INVOICE_AMOUNT_REMAINING = /*$$(*/ "Invoice.amountRemaining"; /*)*/
 
   public static final String ACCOUNTING_REPORT_EDITION_DATE = /*$$(*/
       "AccountingReport.editionDate"; /*)*/
