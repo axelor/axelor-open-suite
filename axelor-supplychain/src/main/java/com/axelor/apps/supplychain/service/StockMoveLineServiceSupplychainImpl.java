c65c607f96f361912f3cb4a276a111fb416b7071
jpo-axelor <j.pokharna@gmail.com>
2019-06-13 15:28:02 +0530

    RM#17952 AVAILABITY REQUEST : error message if product stock < realQty

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockMoveLineServiceSupplychainImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockMoveLineServiceSupplychainImpl.java
@@ -419,12 +419,13 @@ public class StockMoveLineServiceSupplychainImpl extends StockMoveLineServiceImp
   @Override
   public boolean isAvailableProduct(StockMove stockMove, StockMoveLine stockMoveLine) {
     updateAvailableQty(stockMoveLine, stockMove.getFromStockLocation());
-    BigDecimal availableQty =
-        stockMoveLine.getTrackingNumber() != null
-            ? stockMoveLine.getAvailableQty()
-            : stockMoveLine.getAvailableQtyForProduct();
+    BigDecimal availableQty = stockMoveLine.getAvailableQty();
+    if (stockMoveLine.getProduct().getTrackingNumberConfiguration() != null
+        && stockMoveLine.getTrackingNumber() == null) {
+      availableQty = stockMoveLine.getAvailableQtyForProduct();
+    }
     BigDecimal realQty = stockMoveLine.getRealQty();
-    if (availableQty.compareTo(realQty.subtract(stockMoveLine.getReservedQty())) < 0) {
+    if (availableQty.compareTo(realQty) < 0) {
       return false;
     }
     return true;
