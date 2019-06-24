24eed5c0b703d1c6b502ed7eec615dcb0b321338
Julien Domptail <j.domptail@axelor.com>
2019-06-24 18:46:32 +0200

    RM#18739: udpate after feedback

--- axelor-account/src/main/java/com/axelor/apps/account/service/PeriodServiceAccount.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/PeriodServiceAccount.java
@@ -1,9 +1,10 @@
 package com.axelor.apps.account.service;
 
+import com.axelor.apps.account.db.Move;
 import com.axelor.apps.base.db.Period;
 import com.axelor.db.Query;
 
 public interface PeriodServiceAccount {
 
-  public Query getMoveListToValidateQuery(Period period);
+  public Query<Move> getMoveListToValidateQuery(Period period);
 }
