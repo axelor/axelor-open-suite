6e00dc56040688cf238b564618558bafcf8c12dc
David Trochel <d.trochel@axelor.com>
2019-08-29 18:49:44 +0200

    RM20342: Refactor code of batch for timesheet validation reminder.

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/batch/MailBatchServiceHR.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/batch/MailBatchServiceHR.java
@@ -20,38 +20,27 @@ package com.axelor.apps.hr.service.batch;
 import com.axelor.apps.base.db.Batch;
 import com.axelor.apps.base.db.MailBatch;
 import com.axelor.apps.base.db.repo.MailBatchRepository;
-import com.axelor.apps.base.exceptions.IExceptionMessage;
 import com.axelor.apps.base.service.batch.MailBatchService;
 import com.axelor.db.Model;
 import com.axelor.exception.AxelorException;
-import com.axelor.exception.db.repo.TraceBackRepository;
-import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
 
 public class MailBatchServiceHR extends MailBatchService {
+
   @Override
   public Batch run(Model batchModel) throws AxelorException {
-    Batch batch = super.run(batchModel);
     MailBatch mailBatch = (MailBatch) batchModel;
 
     switch (mailBatch.getActionSelect()) {
-      case MailBatchRepository.ACTION_REMIN_TIMESHEET:
-        batch = reminderTimesheet(mailBatch);
-        break;
+      case MailBatchRepository.ACTION_TIMESHEET_VALIDATION_REMINDER:
+        return runTimesheetValidationReminderBatch(mailBatch);
 
       default:
-        throw new AxelorException(
-            TraceBackRepository.CATEGORY_INCONSISTENCY,
-            I18n.get(IExceptionMessage.BASE_BATCH_1),
-            mailBatch.getActionSelect(),
-            mailBatch.getCode());
+        return super.run(batchModel);
     }
-
-    return batch;
   }
 
-  public Batch reminderTimesheet(MailBatch mailBatch) {
-
-    return Beans.get(BatchReminderTimesheet.class).run(mailBatch);
+  public Batch runTimesheetValidationReminderBatch(MailBatch mailBatch) {
+    return Beans.get(BatchTimesheetValidationReminder.class).run(mailBatch);
   }
 }
