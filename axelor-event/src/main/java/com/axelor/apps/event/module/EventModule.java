b8ddb685f081636407a4e25755c6ce2cbb4646c7
bga-axelor <51814011+bga-axelor@users.noreply.github.com>
2019-09-16 09:33:01 +0530

    Update EventModule.java

--- axelor-event/src/main/java/com/axelor/apps/event/module/EventModule.java
+++ axelor-event/src/main/java/com/axelor/apps/event/module/EventModule.java
@@ -7,12 +7,8 @@ public class EventModule extends AxelorModule {
   @Override
   protected void configure() {
 
-    //    bind(InvoiceLineServiceGST.class).to(InvoiceLineServiceGSTImpl.class);
-    //    bind(AddressService.class).to(AddressServiceImpl.class);
-    //    bind(InvoiceLineSupplychainService.class).to(InvoiceLineServiceGSTImpl.class);
-    //    bind(InvoiceServiceProjectImpl.class).to(InvoiceServiceGSTImpl.class);
-    //    bind(SaleOrderLineServiceSupplyChainImpl.class).to(SaleOrderLineServiceGSTImpl.class);
-    //    bind(InvoicePrintServiceImpl.class).to(InvoicePrintGSTServiceImpl.class);
-    //    bind(InvoiceServiceGST.class).to(InvoiceServiceGSTImpl.class);
+    bind(ConvertCSVFileService.class).to(ConvertCSVFileServiceImpl.class);
+    //bind(ImportDemoDataService.class).to(ImportDemoDataServiceImpl.class);
+   
   }
 }
