5911058ed7b2dffeee623df99eb4a9abf4de675f
Arthur Lefebvre <a.lefebvre@axelor.com>
2019-10-24 17:47:46 +0200

    Apply spotless java google code format

--- axelor-base/src/main/java/com/axelor/apps/base/web/YearController.java
+++ axelor-base/src/main/java/com/axelor/apps/base/web/YearController.java
@@ -7,15 +7,14 @@ import com.axelor.exception.service.TraceBackService;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
-import com.google.inject.Inject;
 
-public class YearController {  
+public class YearController {
 
   public void generatePeriods(ActionRequest request, ActionResponse response)
       throws AxelorException {
 
     try {
-      Year year = request.getContext().asType(Year.class);      
+      Year year = request.getContext().asType(Year.class);
       response.setValue("periodList", Beans.get(YearService.class).generatePeriods(year));
     } catch (Exception e) {
       TraceBackService.trace(response, e);
