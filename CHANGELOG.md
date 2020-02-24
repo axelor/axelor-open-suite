97a4292f95484eb7933c1eea6728c5730af223fa
shve-axelor <shve.axelor@gmail.com>
2020-02-24 17:33:39 +0530

    Anomaly #25851 EMAIL : rollback and context

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -34,6 +34,7 @@
 - PRODUCT : display button 'Update stock location' only for storable and stock managed products.
 - ADDRESS : addressL4 is emptied when zip is filled
 - INVOICE Report : Fixed issue in displaying proforma invoice comment from grid button
+- DEBT RECOVERY : rollback debt recovery process if to recipients is empty or not in generated message.  
 
 ## [5.2.4] - 2020-02-05
 ## Improvements
--- axelor-account/src/main/java/com/axelor/apps/account/service/debtrecovery/DebtRecoveryActionService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/debtrecovery/DebtRecoveryActionService.java
@@ -116,12 +116,6 @@ public class DebtRecoveryActionService {
           debtRecoveryMethodLine.getDebtRecoveryMethod().getName(),
           debtRecoveryMethodLine.getDebtRecoveryLevel().getName());
 
-    } else if (partner.getEmailAddress() == null
-        || partner.getEmailAddress().getAddress() == null) {
-      throw new AxelorException(
-          TraceBackRepository.CATEGORY_MISSING_FIELD,
-          I18n.get(IExceptionMessage.DEBT_RECOVERY_ACTION_5),
-          debtRecoveryMethodLine.getDebtRecoveryLevelLabel());
     } else {
 
       // On enregistre la date de la relance
@@ -216,12 +210,6 @@ public class DebtRecoveryActionService {
           debtRecoveryMethodLine.getDebtRecoveryMethod().getName(),
           debtRecoveryMethodLine.getDebtRecoveryLevel().getName());
 
-    } else if (partner.getEmailAddress() == null
-        || partner.getEmailAddress().getAddress() == null) {
-      throw new AxelorException(
-          TraceBackRepository.CATEGORY_MISSING_FIELD,
-          I18n.get(IExceptionMessage.DEBT_RECOVERY_ACTION_5),
-          debtRecoveryMethodLine.getDebtRecoveryLevelLabel());
     } else {
 
       // On enregistre la date de la relance
@@ -257,6 +245,13 @@ public class DebtRecoveryActionService {
     for (Message message : messageSet) {
       message = Beans.get(MessageRepository.class).save(message);
       message = Beans.get(MessageService.class).sendMessage(message);
+
+      if (CollectionUtils.isEmpty(message.getToEmailAddressSet())) {
+        throw new AxelorException(
+            TraceBackRepository.CATEGORY_MISSING_FIELD,
+            I18n.get(IExceptionMessage.DEBT_RECOVERY_ACTION_5),
+            debtRecovery.getDebtRecoveryMethodLine().getDebtRecoveryLevelLabel());
+      }
 
       if (!debtRecovery.getDebtRecoveryMethodLine().getManualValidationOk()
           && message.getMailAccount() == null) {
