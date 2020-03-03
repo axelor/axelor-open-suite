7b4440dd0b2a3c6974bab0c68112b86548373f9a
Edwin Roger-Gorlin <e.roger-gorlin@axelor.com>
2020-03-03 11:11:31 +0100

    RM26211  Fixed the issue we had when we try to update customer's accepted credit

---
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/web/SupplychainSaleConfigController.java
@@ -0,0 +1,20 @@
+package com.axelor.apps.supplychain.web;
+
+import com.axelor.apps.sale.db.SaleConfig;
+import com.axelor.apps.supplychain.service.SupplychainSaleConfigServiceImpl;
+import com.axelor.exception.service.TraceBackService;
+import com.axelor.inject.Beans;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+
+public class SupplychainSaleConfigController {
+  public void updateCustomerCredit(ActionRequest request, ActionResponse response) {
+    try {
+      SaleConfig saleConfig = request.getContext().asType(SaleConfig.class);
+      Beans.get(SupplychainSaleConfigServiceImpl.class).updateCustomerCredit(saleConfig);
+
+    } catch (Exception e) {
+      TraceBackService.trace(response, e);
+    }
+  }
+}
