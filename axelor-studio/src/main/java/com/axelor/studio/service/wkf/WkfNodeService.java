3a446e8c4c1862d93ed67ff2597a734b6f81c957
Arthur Lefebvre <a.lefebvre@axelor.com>
2020-03-16 11:01:21 +0100

    Release 5.2.6

--- axelor-studio/src/main/java/com/axelor/studio/service/wkf/WkfNodeService.java
+++ axelor-studio/src/main/java/com/axelor/studio/service/wkf/WkfNodeService.java
@@ -44,7 +44,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
-
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
