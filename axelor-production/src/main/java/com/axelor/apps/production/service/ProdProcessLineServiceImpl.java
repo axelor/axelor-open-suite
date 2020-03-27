9b3d75361ebfc43b1d778addc3aa4558fea8d8ae
Florent Pruneta <f.pruneta@axelor.com>
2020-03-27 10:26:12 +0100

    Add WorkCenterGroup

--- axelor-production/src/main/java/com/axelor/apps/production/service/ProdProcessLineServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/ProdProcessLineServiceImpl.java
@@ -18,10 +18,16 @@
 package com.axelor.apps.production.service;
 
 import com.axelor.apps.production.db.ProdHumanResource;
+import com.axelor.apps.production.db.ProdProcessLine;
 import com.axelor.apps.production.db.WorkCenter;
+import com.axelor.apps.production.db.WorkCenterGroup;
 import com.axelor.apps.production.db.repo.ProdProcessLineRepository;
+import com.axelor.apps.production.db.repo.WorkCenterGroupRepository;
 import com.axelor.apps.production.db.repo.WorkCenterRepository;
+import com.axelor.db.JPA;
+import com.axelor.inject.Beans;
 import com.google.inject.Inject;
+import com.google.inject.persist.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -73,5 +79,23 @@ public class ProdProcessLineServiceImpl implements ProdProcessLineService {
     } else {
       return new BigDecimal(1);
     }
+  }
+
+  @Transactional
+  public void copyWorkCenterGroup(
+      ProdProcessLine prodProcessLine, WorkCenterGroup workCenterGroup) {
+    WorkCenterGroup workCenterGroupCopy = JPA.copy(workCenterGroup, false);
+    workCenterGroupCopy.setTemplate(false);
+    Beans.get(WorkCenterGroupRepository.class).save(workCenterGroupCopy);
+    workCenterGroup
+        .getWorkCenterSet()
+        .forEach(
+            (k -> {
+              workCenterGroupCopy.addWorkCenterSetItem(k);
+            }));
+
+    Beans.get(WorkCenterGroupRepository.class).save(workCenterGroupCopy);
+    prodProcessLine.setWorkCenterGroup(workCenterGroupCopy);
+    Beans.get(ProdProcessLineRepository.class).save(prodProcessLine);
   }
 }
