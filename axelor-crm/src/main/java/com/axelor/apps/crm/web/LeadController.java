273d1ca3cf6e34b08dcf5a51e422167d285054df
Edwin Roger-Gorlin <e.roger-gorlin@axelor.com>
2020-04-01 10:07:07 +0200

    RM26950 revert previous changes

--- axelor-crm/src/main/java/com/axelor/apps/crm/web/LeadController.java
+++ axelor-crm/src/main/java/com/axelor/apps/crm/web/LeadController.java
@@ -84,15 +84,7 @@ public class LeadController {
     if (!leadIds.equals("")) {
       String title = " ";
       if (lead.getFirstName() != null) {
-        title +=
-            lstSelectedleads == null
-                ? "Lead "
-                    + lead.getName()
-                    + " "
-                    + lead.getFirstName()
-                    + " - "
-                    + lead.getEnterpriseName()
-                : "Leads";
+        title += lstSelectedleads == null ? "Lead " + lead.getFirstName() : "Leads";
       }
 
       String fileLink =
