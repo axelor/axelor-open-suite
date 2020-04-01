10d92a4a2a1844141b192459655bc3778a4177fe
Maxence GALLANCHER <m.gallancher@axelor.com>
2020-04-01 12:18:19 +0200

    LeaveReason: Reaname 'leaveReason' fieldinto 'name'
    And consequent changes in java & xml
    
    RM #26192

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/web/leave/LeaveController.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/web/leave/LeaveController.java
@@ -270,7 +270,7 @@ public class LeaveController {
           response.setAlert(
               String.format(
                       I18n.get(IExceptionMessage.LEAVE_ALLOW_NEGATIVE_VALUE_REASON),
-                      leaveRequest.getLeaveLine().getLeaveReason().getLeaveReason())
+                      leaveRequest.getLeaveLine().getLeaveReason().getName())
                   + " "
                   + instruction);
           return;
@@ -278,7 +278,7 @@ public class LeaveController {
           response.setNotify(
               String.format(
                   I18n.get(IExceptionMessage.LEAVE_ALLOW_NEGATIVE_ALERT),
-                  leaveRequest.getLeaveLine().getLeaveReason().getLeaveReason()));
+                  leaveRequest.getLeaveLine().getLeaveReason().getName()));
         }
       }
 
