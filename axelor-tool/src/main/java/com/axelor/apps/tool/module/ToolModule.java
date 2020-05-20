15ce4a7dc60e322679455daa17944a120dabd365
Phi-Long Do <p.do@axelor.com>
2020-05-20 13:55:50 +0200

    Migrate axelor-tool to CDI

--- axelor-tool/src/main/java/com/axelor/apps/tool/module/ToolModule.java
+++ axelor-tool/src/main/java/com/axelor/apps/tool/module/ToolModule.java
@@ -17,20 +17,10 @@
  */
 package com.axelor.apps.tool.module;
 
-import com.axelor.app.AxelorModule;
-import com.axelor.apps.tool.service.ArchivingToolService;
-import com.axelor.apps.tool.service.ArchivingToolServiceImpl;
-import com.axelor.apps.tool.service.CipherService;
-import com.axelor.apps.tool.service.CipherServiceImpl;
-import com.axelor.apps.tool.service.TranslationService;
-import com.axelor.apps.tool.service.TranslationServiceImpl;
+import javax.interceptor.Interceptor;
 
-public class ToolModule extends AxelorModule {
+public class ToolModule {
+  private ToolModule() {}
 
-  @Override
-  protected void configure() {
-    bind(CipherService.class).to(CipherServiceImpl.class);
-    bind(TranslationService.class).to(TranslationServiceImpl.class);
-    bind(ArchivingToolService.class).to(ArchivingToolServiceImpl.class);
-  }
+  public static final int PRIORITY = Interceptor.Priority.APPLICATION + 200;
 }
