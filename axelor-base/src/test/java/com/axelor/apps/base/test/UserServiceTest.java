8cd22383c46bb7ddea23ef96ee063f084268576a
Phi-Long Do <p.do@axelor.com>
2020-05-27 14:13:59 +0200

    Remove test modules

--- axelor-base/src/test/java/com/axelor/apps/base/test/UserServiceTest.java
+++ axelor-base/src/test/java/com/axelor/apps/base/test/UserServiceTest.java
@@ -17,40 +17,15 @@
  */
 package com.axelor.apps.base.test;
 
-import com.axelor.app.AppModule;
-import com.axelor.app.AxelorModule;
-import com.axelor.apps.base.module.AdminModule;
-import com.axelor.apps.base.module.BaseModule;
 import com.axelor.apps.base.service.user.UserService;
-import com.axelor.apps.base.test.UserServiceTest.MyModule;
-import com.axelor.apps.message.module.MessageModule;
-import com.axelor.apps.tool.module.ToolModule;
 import com.axelor.inject.Beans;
-import com.axelor.test.GuiceModules;
-import com.axelor.test.GuiceRunner;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(GuiceRunner.class)
-@GuiceModules({MyModule.class})
 public class UserServiceTest {
 
   static UserService userService;
-
-  public static class MyModule extends AxelorModule {
-
-    @Override
-    protected void configure() {
-      bind(Beans.class).asEagerSingleton();
-      install(new ToolModule());
-      install(new MessageModule());
-      install(new AdminModule());
-      install(new BaseModule());
-      install(new AppModule());
-    }
-  }
 
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
