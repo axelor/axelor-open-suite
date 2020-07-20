3191d1065c8d661dae68dbadaefe9b670043f8f1
Edwin Roger-Gorlin <e.roger-gorlin@axelor.com>
2020-07-20 15:53:46 +0200

    Product : On copy it won't duplicate ProductCompany anymore

--- axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/ProductStockRepository.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/ProductStockRepository.java
@@ -159,6 +159,7 @@ public class ProductStockRepository extends ProductBaseRepository {
   @Override
   public Product copy(Product product, boolean deep) {
     Product copy = super.copy(product, deep);
+    copy.clearProductCompanyList();
     copy.setAvgPrice(BigDecimal.ZERO);
     return copy;
   }
