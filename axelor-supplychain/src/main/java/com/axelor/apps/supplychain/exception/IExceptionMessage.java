429bca3667e345696a61fe3785d2e42d7e5551f3
Arthur Lefebvre <a.lefebvre@axelor.com>
2020-07-24 13:38:23 +0200

    Sale Order: use delivery date for reservation
    Add a new config to use delivery date to decide reservation priority.

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/exception/IExceptionMessage.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/exception/IExceptionMessage.java
@@ -136,9 +136,11 @@ public interface IExceptionMessage {
   static final String SO_MISSING_STOCK_LOCATION = /*$$(*/
       "Stock location is missing for the sale order %s." /*)*/;
 
-  /** Sale order Stock Service Implement */
   static final String PO_NO_DELIVERY_STOCK_MOVE_TO_GENERATE = /*$$(*/
       "No delivery stock move to generate for this purchase order" /*)*/;
+
+  static final String RESERVATION_SALE_ORDER_DATE_CONFIG_INCORRECT_VALUE = /*$$(*/
+      "Please configure a correct value for the sale order date used for reservation." /*)*/;
 
   /** Purchase Order Stock Service Implement */
   static final String PO_MISSING_STOCK_LOCATION = /*$$(*/
