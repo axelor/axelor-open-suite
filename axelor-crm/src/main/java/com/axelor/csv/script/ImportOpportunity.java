109fd62ddbc2173e90cda10f871be64f01bf4a9f
Antoine Olivier <a.olivier@axelor.com>
2020-08-25 13:34:39 +0200

    RM30690 : Add sequence to opportunity demo data

---
+++ axelor-crm/src/main/java/com/axelor/csv/script/ImportOpportunity.java
@@ -0,0 +1,24 @@
+package com.axelor.csv.script;
+
+import com.axelor.apps.crm.db.Opportunity;
+import com.axelor.apps.crm.service.OpportunityService;
+import com.axelor.exception.AxelorException;
+import com.axelor.exception.service.TraceBackService;
+import com.axelor.inject.Beans;
+import java.util.Map;
+
+public class ImportOpportunity {
+  public Object importOpportunity(Object bean, Map<String, Object> values) {
+    assert bean instanceof Opportunity;
+
+    Opportunity opportunity = (Opportunity) bean;
+
+    try {
+      Beans.get(OpportunityService.class).setSequence(opportunity);
+    } catch (Exception e) {
+      TraceBackService.trace(e);
+    }
+
+    return opportunity;
+  }
+}
