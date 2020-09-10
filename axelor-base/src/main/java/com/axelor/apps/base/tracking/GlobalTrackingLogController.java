385a7b5cb1728df23e2656e5457c4e832e4cd631
Arthur Lefebvre <a.lefebvre@axelor.com>
2020-09-10 15:34:25 +0200

    Global tracking log: refactor to match conventions
    RM23186

--- axelor-base/src/main/java/com/axelor/apps/base/tracking/GlobalTrackingLogController.java
+++ axelor-base/src/main/java/com/axelor/apps/base/tracking/GlobalTrackingLogController.java
@@ -1,5 +1,6 @@
 package com.axelor.apps.base.tracking;
 
+import com.axelor.exception.service.TraceBackService;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.axelor.rpc.Context;
@@ -7,14 +8,18 @@ import com.axelor.rpc.Context;
 public class GlobalTrackingLogController {
 
   public void showGlobalTrackingLogsInWizard(ActionRequest request, ActionResponse response) {
+    try {
 
-    Context context = request.getContext();
-    boolean showLines = context.get("metaModel") != null && context.get("metaField") != null;
+      Context context = request.getContext();
+      boolean showLines = context.get("metaModel") != null && context.get("metaField") != null;
 
-    response.setAttr("globalTrackingLogDashlet", "hidden", showLines);
-    response.setAttr("globalTrackingLogLineDashlet", "hidden", !showLines);
+      response.setAttr("globalTrackingLogDashlet", "hidden", showLines);
+      response.setAttr("globalTrackingLogLineDashlet", "hidden", !showLines);
 
-    response.setAttr(
-        showLines ? "globalTrackingLogLineDashlet" : "globalTrackingLogDashlet", "refresh", true);
+      response.setAttr(
+          showLines ? "globalTrackingLogLineDashlet" : "globalTrackingLogDashlet", "refresh", true);
+    } catch (Exception e) {
+      TraceBackService.trace(response, e);
+    }
   }
 }
