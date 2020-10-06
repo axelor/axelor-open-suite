ed0b0510ffcea0d6a1adb4fc381825e1d6bdb626
Arthur Lefebvre <a.lefebvre@axelor.com>
2020-10-06 11:17:06 +0200

    Revert "Fixed build error"
    This reverts commit 3abe2a50d52da7a2a8cd920dee68aa6607798ccd.

--- axelor-tool/src/main/java/com/axelor/tool/template/TemplateMaker.java
+++ axelor-tool/src/main/java/com/axelor/tool/template/TemplateMaker.java
@@ -52,7 +52,7 @@ public class TemplateMaker {
       String timeZone, Locale locale, char delimiterStartChar, char delimiterStopChar) {
     this.locale = locale;
     this.timeZone = timeZone;
-    this.st = new StringTemplates(delimiterStartChar, delimiterStopChar);
+    this.st = new StringTemplates(delimiterStartChar, delimiterStopChar).withLocale(locale);
   }
 
   public void setContext(Model model) {
