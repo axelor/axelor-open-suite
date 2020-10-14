3f8eb392b8e644d0684a5dbef40faddcfe949a0f
Arthur Lefebvre <a.lefebvre@axelor.com>
2020-10-14 18:51:47 +0200

    MRP: Stop if a loop in BOM components is detected
    Fixes RM32043.

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/MrpServiceImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/MrpServiceImpl.java
@@ -979,7 +979,7 @@ public class MrpServiceImpl implements MrpService {
     return false;
   }
 
-  protected void assignProductAndLevel(Set<Product> productList) {
+  protected void assignProductAndLevel(Set<Product> productList) throws AxelorException {
 
     productMap = Maps.newHashMap();
 
@@ -989,7 +989,7 @@ public class MrpServiceImpl implements MrpService {
     }
   }
 
-  protected void assignProductAndLevel(Product product) {
+  protected void assignProductAndLevel(Product product) throws AxelorException {
 
     log.debug("Add of the product : {}", product.getFullName());
     this.productMap.put(product.getId(), 0);
