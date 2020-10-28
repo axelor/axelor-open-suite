fb8475efcde60f6f752290dce2519ecdd88c2d60
Lawrence DENEIPLE <l.deneiple@axelor.com>
2020-10-28 17:33:44 +0100

    Use Optional to manage AuthUtils.getUser() returning null value

--- axelor-base/src/main/java/com/axelor/apps/base/service/DateService.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/DateService.java
@@ -2,13 +2,16 @@ package com.axelor.apps.base.service;
 
 import com.axelor.apps.base.service.app.AppBaseService;
 import com.axelor.auth.AuthUtils;
+import com.axelor.auth.db.User;
 import com.axelor.inject.Beans;
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class DateService {
 
   public LocalDate date() {
     return Beans.get(AppBaseService.class)
-        .getTodayDate(AuthUtils.getUser() != null ? AuthUtils.getUser().getActiveCompany() : null);
+        .getTodayDate(
+            Optional.ofNullable(AuthUtils.getUser()).map(User::getActiveCompany).orElse(null));
   }
 }
