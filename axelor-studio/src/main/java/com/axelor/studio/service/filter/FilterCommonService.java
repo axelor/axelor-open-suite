axelor/axelor-open-suite

commit 8f6a6fcc629bc2ed3efe35691f570e9f0c5e4fd2

Replace __date__ by a global configuration (#6030)
Replace all __date__ occurences by __config__.date to fix timezone related issues and add __config__.date methode
RM32195

axelor-account/src/main/resources/views/InvoiceLine.xml
@@ -71,7 +71,7 @@
 					<field name="discountAmount" colSpan="3" onChange="action-invoice-line-method-compute,action-invoice-line-method-compute-analytic-distribution,action-budget-validate-exceed-line-amount" hideIf="discountTypeSelect == 0"/>
 					<field name="priceDiscounted" colSpan="3" hidden="true" readonly="true"/>
 				</panel>
-				<field name="taxLine" canEdit="false" onChange="action-invoice-line-method-convert-tax,action-invoice-line-method-compute,action-invoice-line-method-compute-analytic-distribution,action-budget-validate-exceed-line-amount" grid-view="tax-line-grid" form-view="tax-line-form" domain="self.endDate = null or self.endDate &gt; :__date__"/>
+				<field name="taxLine" canEdit="false" onChange="action-invoice-line-method-convert-tax,action-invoice-line-method-compute,action-invoice-line-method-compute-analytic-distribution,action-budget-validate-exceed-line-amount" grid-view="tax-line-grid" form-view="tax-line-form" domain="self.endDate = null or self.endDate &gt; :__config__.date"/>
 				<field name="exTaxTotal" colSpan="3" readonly="true"/>
 				<field name="inTaxTotal" colSpan="3" readonly="true"/>
 				<field name="$isReadOnly" hidden="true"/>

axelor-human-resource/src/main/resources/views/SaleOrderLine.xml
@@ -4,7 +4,7 @@
 	xsi:schemaLocation="http://axelor.com/xml/ns/object-views http://axelor.com/xml/ns/object-views/object-views_5.3.xsd">
 
 	<action-attrs name="action-sale-order-line-attrs-domain-product" id="hr-action-sale-order-line-attrs-domain-product">
-		<attribute name="domain" for="product" expr="self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__date__) and self.sellable = true and dtype = 'Product'"/>
+		<attribute name="domain" for="product" expr="self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.sellable = true and dtype = 'Product'"/>
 	</action-attrs>
 	
 </object-views>

axelor-purchase/src/main/resources/views/PurchaseOrderLine.xml
@@ -123,7 +123,7 @@
 					<field name="discountAmount" colSpan="3" onChange="action-purchase-order-line-method-compute,action-purchase-order-line-method-compute-analytic-distribution,action-budget-validate-exceed-line-amount" hideIf="discountTypeSelect == 3"/>
 					<field name="priceDiscounted" colSpan="3" readonly="true" hidden="true"/>
 				</panel>
-				<field name="taxLine" canEdit="false" onChange="action-group-purchase-purchase-orderline-taxline-onchange" domain="self.endDate = null or self.endDate &gt; :__date__" grid-view="tax-line-grid" form-view="tax-line-form"/>
+				<field name="taxLine" canEdit="false" onChange="action-group-purchase-purchase-orderline-taxline-onchange" domain="self.endDate = null or self.endDate &gt; :__config__.date" grid-view="tax-line-grid" form-view="tax-line-form"/>
 				<field name="exTaxTotal" colSpan="3" readonly="true"/>
 				<field name="inTaxTotal" colSpan="3" readonly="true"/>
 				<field name="fixedAssets" widget="boolean-switch"/>
@@ -391,8 +391,8 @@
 	<action-attrs name="action-purchase-order-line-attrs-product-domain">
 		<attribute name="domain" for="product"
 		expr="eval: isFilterOnSupplier &amp;&amp; __config__.app.getApp('purchase').getManageSupplierCatalog()
-		? &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__date__) and self.purchasable = true and self.id IN (SELECT product.id FROM SupplierCatalog WHERE supplierPartner.id = ${__parent__?.supplierPartner?.id}) and dtype = 'Product'&quot; 
-		: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__date__) and self.purchasable = true and dtype = 'Product'&quot;" />
+		? &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.purchasable = true and self.id IN (SELECT product.id FROM SupplierCatalog WHERE supplierPartner.id = ${__parent__?.supplierPartner?.id}) and dtype = 'Product'&quot;
+		: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.purchasable = true and dtype = 'Product'&quot;" />
 	</action-attrs>
 	
     <action-attrs name="action-purchase-order-line-attrs-discountamount-title">

axelor-purchase/src/main/resources/views/PurchaseRequestLine.xml
@@ -31,8 +31,8 @@
 	</action-record>
 	
 	<action-attrs name="action-purchase-request-line-attrs-product-domain">
-		<attribute name="domain" for="product" if="__parent__?.supplierUser?.id" expr="eval: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__date__) and self.purchasable = true and self.id IN (SELECT product.id FROM SupplierCatalog WHERE supplierPartner.id = ${__parent__?.supplierUser?.id}) AND dtype = 'Product'&quot;"/>
-		<attribute name="domain" for="product" if="!__parent__?.supplierUser?.id" expr="eval: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__date__) and self.purchasable = true &quot;"/>
+		<attribute name="domain" for="product" if="__parent__?.supplierUser?.id" expr="eval: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.purchasable = true and self.id IN (SELECT product.id FROM SupplierCatalog WHERE supplierPartner.id = ${__parent__?.supplierUser?.id}) AND dtype = 'Product'&quot;"/>
+		<attribute name="domain" for="product" if="!__parent__?.supplierUser?.id" expr="eval: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.purchasable = true &quot;"/>
 	</action-attrs>
 
     <action-attrs name="action-purchase-request-line-attrs-set-scale">

axelor-sale/src/main/resources/views/PackLine.xml
@@ -59,7 +59,7 @@
     </action-group>
 
     <action-attrs name="action-pack-line-attrs-domain-product">
-    	<attribute name="domain" for="product" expr="eval: &quot; self.isModel = false and (self.endDate = null or self.endDate &gt; :__date__) and self.sellable = true and dtype = 'Product'&quot;"/>
+    	<attribute name="domain" for="product" expr="eval: &quot; self.isModel = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.sellable = true and dtype = 'Product'&quot;"/>
     </action-attrs>
 
 </object-views>

axelor-sale/src/main/resources/views/SaleOrderLine.xml
@@ -159,7 +159,7 @@
 				</panel>
 
 				<panel name="taxPanel" colSpan="12">
-					<field name="taxLine" canEdit="false" colSpan="6" readonlyIf="$isReadOnly !== false || deliveryState == 2 || deliveryState == 3 || amountInvoiced &gt; 0" onChange="action-group-sale-saleorderline-tax-line-onchange" domain="self.endDate = null or self.endDate &gt; :__date__" grid-view="tax-line-grid" form-view="tax-line-form"/>
+					<field name="taxLine" canEdit="false" colSpan="6" readonlyIf="$isReadOnly !== false || deliveryState == 2 || deliveryState == 3 || amountInvoiced &gt; 0" onChange="action-group-sale-saleorderline-tax-line-onchange" domain="self.endDate = null or self.endDate &gt; :__config__.date" grid-view="tax-line-grid" form-view="tax-line-form"/>
 					<field name="exTaxTotal" readonly="true" colSpan="3"/>
 					<field name="inTaxTotal" readonly="true" colSpan="3"/>
 				</panel>
@@ -268,7 +268,7 @@
 					<field name="priceDiscounted" colSpan="4" hidden="true"/>
 				</panel>
 			</panel>
-			<field name="taxLine" canEdit="false"  colSpan="4" onChange="action-group-sale-saleorderline-tax-line-onchange" domain="self.endDate = null or self.endDate &gt; :__date__" grid-view="tax-line-grid" form-view="tax-line-form"/>
+			<field name="taxLine" canEdit="false"  colSpan="4" onChange="action-group-sale-saleorderline-tax-line-onchange" domain="self.endDate = null or self.endDate &gt; :__config__.date" grid-view="tax-line-grid" form-view="tax-line-form"/>
 			<field name="exTaxTotal" readonly="true"/>
 			<field name="inTaxTotal" readonly="true"/>
 			<field name="taxEquiv" hidden="true" grid-view="tax-equiv-grid" form-view="tax-equiv-form"/>
@@ -587,7 +587,7 @@
 	</action-attrs>
 	
 	<action-attrs name="action-sale-order-line-attrs-domain-product">
-		<attribute name="domain" for="product" expr="self.isModel = false and (self.endDate = null or self.endDate &gt; :__date__) and self.sellable = true and dtype = 'Product'"/>
+		<attribute name="domain" for="product" expr="self.isModel = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.sellable = true and dtype = 'Product'"/>
 	</action-attrs>
 	
   	<action-attrs name="action-sale-order-line-attrs-scale-and-precision">

axelor-studio/src/main/java/com/axelor/studio/service/filter/FilterCommonService.java
@@ -33,11 +33,11 @@ public class FilterCommonService {
     if (value != null) {
       if (addColon) {
         value = value.replace("$user", ":__user__");
-        value = value.replace("$date", ":__date__");
+        value = value.replace("$date", ":__config__.date");
         value = value.replace("$time", ":__datetime__");
       } else {
         value = value.replace("$user", "__user__");
-        value = value.replace("$date", "__date__");
+        value = value.replace("$date", "__config__.date");
         value = value.replace("$time", "__datetime__");
       }
     }

axelor-supplychain/src/main/resources/views/Mrp.xml
@@ -57,7 +57,7 @@
 	        <panel-related name="productCategorySetPanel" field="productCategorySet" canEdit="false" form-view="product-category-form" grid-view="product-category-grid" canNew="false" colSpan="12"/>
 	        <panel-related name="productFamilySetPanel" field="productFamilySet" canEdit="false" form-view="product-family-form" grid-view="product-family-grid" canNew="false" colSpan="12"/>
 	        <panel-related name="saleOrderLineSetPanel" field="saleOrderLineSet" canEdit="false" form-view="sale-order-line-menu-form" grid-view="sale-order-line-menu-grid" canNew="false" colSpan="12" onSelect="action-mrp-attrs-sale-order-domain"/>
-	        <panel-related name="mrpForecastSetPanel" field="mrpForecastSet" form-view="mrp-forecast-form" grid-view="mrp-forecast-grid" colSpan="12" domain="self.statusSelect = 2 AND self.product.productTypeSelect = 'storable' AND self.product.excludeFromMrp = false AND self.forecastDate &gt;= :__date__  AND self.product.stockManaged = true AND (:mrpTypeSelect = 1 OR self.product.productSubTypeSelect = 1)"/>
+	        <panel-related name="mrpForecastSetPanel" field="mrpForecastSet" form-view="mrp-forecast-form" grid-view="mrp-forecast-grid" colSpan="12" domain="self.statusSelect = 2 AND self.product.productTypeSelect = 'storable' AND self.product.excludeFromMrp = false AND self.forecastDate &gt;= :__config__.date  AND self.product.stockManaged = true AND (:mrpTypeSelect = 1 OR self.product.productSubTypeSelect = 1)"/>
 	    </panel>
 	    <panel name="statusPanel" showIf="statusSelect == 1" colSpan="12">
 	        <field name="statusSelect" hidden="true"/>

commit 853fa13fcd8b045c9b5f4e386d2fe3c714c3c7ce

Fix error on selecting tax or product in so line
Fix regression caused by 8f6a6fcc62 using `__config__.date` in domain
RM32195

axelor-account/src/main/resources/views/InvoiceLine.xml
@@ -71,7 +71,7 @@
 					<field name="discountAmount" colSpan="3" onChange="action-invoice-line-method-compute,action-invoice-line-method-compute-analytic-distribution,action-budget-validate-exceed-line-amount" hideIf="discountTypeSelect == 0"/>
 					<field name="priceDiscounted" colSpan="3" hidden="true" readonly="true"/>
 				</panel>
-				<field name="taxLine" canEdit="false" onChange="action-invoice-line-method-convert-tax,action-invoice-line-method-compute,action-invoice-line-method-compute-analytic-distribution,action-budget-validate-exceed-line-amount" grid-view="tax-line-grid" form-view="tax-line-form" domain="self.endDate = null or self.endDate &gt; :__config__.date"/>
+				<field name="taxLine" canEdit="false" onChange="action-invoice-line-method-convert-tax,action-invoice-line-method-compute,action-invoice-line-method-compute-analytic-distribution,action-budget-validate-exceed-line-amount" grid-view="tax-line-grid" form-view="tax-line-form" domain="self.endDate = null or self.endDate &gt; :__date__"/>
 				<field name="exTaxTotal" colSpan="3" readonly="true"/>
 				<field name="inTaxTotal" colSpan="3" readonly="true"/>
 				<field name="$isReadOnly" hidden="true"/>

axelor-human-resource/src/main/resources/views/SaleOrderLine.xml
@@ -4,7 +4,7 @@
 	xsi:schemaLocation="http://axelor.com/xml/ns/object-views http://axelor.com/xml/ns/object-views/object-views_5.3.xsd">
 
 	<action-attrs name="action-sale-order-line-attrs-domain-product" id="hr-action-sale-order-line-attrs-domain-product">
-		<attribute name="domain" for="product" expr="self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.sellable = true and dtype = 'Product'"/>
+		<attribute name="domain" for="product" expr="self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__date__) and self.sellable = true and dtype = 'Product'"/>
 	</action-attrs>
 	
 </object-views>

axelor-purchase/src/main/resources/views/PurchaseOrderLine.xml
@@ -123,7 +123,7 @@
 					<field name="discountAmount" colSpan="3" onChange="action-purchase-order-line-method-compute,action-purchase-order-line-method-compute-analytic-distribution,action-budget-validate-exceed-line-amount" hideIf="discountTypeSelect == 3"/>
 					<field name="priceDiscounted" colSpan="3" readonly="true" hidden="true"/>
 				</panel>
-				<field name="taxLine" canEdit="false" onChange="action-group-purchase-purchase-orderline-taxline-onchange" domain="self.endDate = null or self.endDate &gt; :__config__.date" grid-view="tax-line-grid" form-view="tax-line-form"/>
+				<field name="taxLine" canEdit="false" onChange="action-group-purchase-purchase-orderline-taxline-onchange" domain="self.endDate = null or self.endDate &gt; :__date__" grid-view="tax-line-grid" form-view="tax-line-form"/>
 				<field name="exTaxTotal" colSpan="3" readonly="true"/>
 				<field name="inTaxTotal" colSpan="3" readonly="true"/>
 				<field name="fixedAssets" widget="boolean-switch"/>
@@ -391,8 +391,8 @@
 	<action-attrs name="action-purchase-order-line-attrs-product-domain">
 		<attribute name="domain" for="product"
 		expr="eval: isFilterOnSupplier &amp;&amp; __config__.app.getApp('purchase').getManageSupplierCatalog()
-		? &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.purchasable = true and self.id IN (SELECT product.id FROM SupplierCatalog WHERE supplierPartner.id = ${__parent__?.supplierPartner?.id}) and dtype = 'Product'&quot;
-		: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.purchasable = true and dtype = 'Product'&quot;" />
+		? &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__date__) and self.purchasable = true and self.id IN (SELECT product.id FROM SupplierCatalog WHERE supplierPartner.id = ${__parent__?.supplierPartner?.id}) and dtype = 'Product'&quot;
+		: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__date__) and self.purchasable = true and dtype = 'Product'&quot;" />
 	</action-attrs>
 	
     <action-attrs name="action-purchase-order-line-attrs-discountamount-title">

axelor-purchase/src/main/resources/views/PurchaseRequestLine.xml
@@ -31,8 +31,8 @@
 	</action-record>
 	
 	<action-attrs name="action-purchase-request-line-attrs-product-domain">
-		<attribute name="domain" for="product" if="__parent__?.supplierUser?.id" expr="eval: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.purchasable = true and self.id IN (SELECT product.id FROM SupplierCatalog WHERE supplierPartner.id = ${__parent__?.supplierUser?.id}) AND dtype = 'Product'&quot;"/>
-		<attribute name="domain" for="product" if="!__parent__?.supplierUser?.id" expr="eval: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.purchasable = true &quot;"/>
+		<attribute name="domain" for="product" if="__parent__?.supplierUser?.id" expr="eval: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__date__) and self.purchasable = true and self.id IN (SELECT product.id FROM SupplierCatalog WHERE supplierPartner.id = ${__parent__?.supplierUser?.id}) AND dtype = 'Product'&quot;"/>
+		<attribute name="domain" for="product" if="!__parent__?.supplierUser?.id" expr="eval: &quot; self.isModel = false and self.expense = false and (self.endDate = null or self.endDate &gt; :__date__) and self.purchasable = true &quot;"/>
 	</action-attrs>
 
     <action-attrs name="action-purchase-request-line-attrs-set-scale">

axelor-sale/src/main/resources/views/PackLine.xml
@@ -59,7 +59,7 @@
     </action-group>
 
     <action-attrs name="action-pack-line-attrs-domain-product">
-    	<attribute name="domain" for="product" expr="eval: &quot; self.isModel = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.sellable = true and dtype = 'Product'&quot;"/>
+    	<attribute name="domain" for="product" expr="eval: &quot; self.isModel = false and (self.endDate = null or self.endDate &gt; :__date__) and self.sellable = true and dtype = 'Product'&quot;"/>
     </action-attrs>
 
 </object-views>

axelor-sale/src/main/resources/views/SaleOrderLine.xml
@@ -159,7 +159,7 @@
 				</panel>
 
 				<panel name="taxPanel" colSpan="12">
-					<field name="taxLine" canEdit="false" colSpan="6" readonlyIf="$isReadOnly !== false || deliveryState == 2 || deliveryState == 3 || amountInvoiced &gt; 0" onChange="action-group-sale-saleorderline-tax-line-onchange" domain="self.endDate = null or self.endDate &gt; :__config__.date" grid-view="tax-line-grid" form-view="tax-line-form"/>
+					<field name="taxLine" canEdit="false" colSpan="6" readonlyIf="$isReadOnly !== false || deliveryState == 2 || deliveryState == 3 || amountInvoiced &gt; 0" onChange="action-group-sale-saleorderline-tax-line-onchange" domain="self.endDate = null or self.endDate &gt; :__date__" grid-view="tax-line-grid" form-view="tax-line-form"/>
 					<field name="exTaxTotal" readonly="true" colSpan="3"/>
 					<field name="inTaxTotal" readonly="true" colSpan="3"/>
 				</panel>
@@ -268,7 +268,7 @@
 					<field name="priceDiscounted" colSpan="4" hidden="true"/>
 				</panel>
 			</panel>
-			<field name="taxLine" canEdit="false"  colSpan="4" onChange="action-group-sale-saleorderline-tax-line-onchange" domain="self.endDate = null or self.endDate &gt; :__config__.date" grid-view="tax-line-grid" form-view="tax-line-form"/>
+			<field name="taxLine" canEdit="false"  colSpan="4" onChange="action-group-sale-saleorderline-tax-line-onchange" domain="self.endDate = null or self.endDate &gt; :__date__" grid-view="tax-line-grid" form-view="tax-line-form"/>
 			<field name="exTaxTotal" readonly="true"/>
 			<field name="inTaxTotal" readonly="true"/>
 			<field name="taxEquiv" hidden="true" grid-view="tax-equiv-grid" form-view="tax-equiv-form"/>
@@ -587,7 +587,7 @@
 	</action-attrs>
 	
 	<action-attrs name="action-sale-order-line-attrs-domain-product">
-		<attribute name="domain" for="product" expr="self.isModel = false and (self.endDate = null or self.endDate &gt; :__config__.date) and self.sellable = true and dtype = 'Product'"/>
+		<attribute name="domain" for="product" expr="self.isModel = false and (self.endDate = null or self.endDate &gt; :__date__) and self.sellable = true and dtype = 'Product'"/>
 	</action-attrs>
 	
   	<action-attrs name="action-sale-order-line-attrs-scale-and-precision">

axelor-studio/src/main/java/com/axelor/studio/service/filter/FilterCommonService.java
@@ -33,11 +33,11 @@ public class FilterCommonService {
     if (value != null) {
       if (addColon) {
         value = value.replace("$user", ":__user__");
-        value = value.replace("$date", ":__config__.date");
+        value = value.replace("$date", ":__date__");
         value = value.replace("$time", ":__datetime__");
       } else {
         value = value.replace("$user", "__user__");
-        value = value.replace("$date", "__config__.date");
+        value = value.replace("$date", "__date__");
         value = value.replace("$time", "__datetime__");
       }
     }

axelor-supplychain/src/main/resources/views/Mrp.xml
@@ -59,7 +59,7 @@
 	        <panel-related name="productCategorySetPanel" field="productCategorySet" canEdit="false" form-view="product-category-form" grid-view="product-category-grid" canNew="false" colSpan="12"/>
 	        <panel-related name="productFamilySetPanel" field="productFamilySet" canEdit="false" form-view="product-family-form" grid-view="product-family-grid" canNew="false" colSpan="12"/>
 	        <panel-related name="saleOrderLineSetPanel" field="saleOrderLineSet" canEdit="false" form-view="sale-order-line-menu-form" grid-view="sale-order-line-menu-grid" canNew="false" colSpan="12" onSelect="action-mrp-attrs-sale-order-domain"/>
-	        <panel-related name="mrpForecastSetPanel" field="mrpForecastSet" form-view="mrp-forecast-form" grid-view="mrp-forecast-grid" colSpan="12" domain="self.statusSelect = 2 AND self.product.productTypeSelect = 'storable' AND self.product.excludeFromMrp = false AND self.forecastDate &gt;= :__config__.date  AND self.product.stockManaged = true AND (:mrpTypeSelect = 1 OR self.product.productSubTypeSelect = 1)"/>
+	        <panel-related name="mrpForecastSetPanel" field="mrpForecastSet" form-view="mrp-forecast-form" grid-view="mrp-forecast-grid" colSpan="12" domain="self.statusSelect = 2 AND self.product.productTypeSelect = 'storable' AND self.product.excludeFromMrp = false AND self.forecastDate &gt;= :__date__  AND self.product.stockManaged = true AND (:mrpTypeSelect = 1 OR self.product.productSubTypeSelect = 1)"/>
 	    </panel>
 	    <panel name="statusPanel" showIf="statusSelect == 1" colSpan="12">
 	        <field name="statusSelect" hidden="true"/>