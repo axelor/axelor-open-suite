e831862810fee0ac4f39b8f9b592ec014301bcab
Edwin Roger-Gorlin <e.roger-gorlin@axelor.com>
2020-11-26 17:10:46 +0100

    RM32935 Correct behavior for getYears

--- axelor-base/src/main/java/com/axelor/apps/base/service/FrequencyServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/FrequencyServiceImpl.java
@@ -406,9 +406,8 @@ public class FrequencyServiceImpl implements FrequencyService {
   public List<Integer> getYears(LocalDate startDate, LocalDate endDate) {
     ArrayList<Integer> years = new ArrayList<>();
 
-    int startYear = startDate.getYear();
-    for (long i = 0; i <= ChronoUnit.YEARS.between(startDate, endDate); i++) {
-      years.add(startYear++);
+    for (int i = startDate.getYear(); i <= endDate.getYear(); i++) {
+        years.add(i);
     }
 
     return years;
