54c360bfd19ace41da5506396946c30c11f2168b
msh-axelor <msh.axelor@gmail.com>
2020-12-01 12:07:28 +0530

    Anomaly #33200: BatchMoveLineExport - BLOCKER : line 43

--- axelor-account/src/main/java/com/axelor/apps/account/service/batch/BatchMoveLineExport.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/batch/BatchMoveLineExport.java
@@ -40,7 +40,7 @@ public class BatchMoveLineExport extends BatchStrategy {
 
   private final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-  protected boolean stop = false;
+  protected boolean end = false;
 
   protected long moveLineDone = 0;
   protected long moveDone = 0;
@@ -76,7 +76,7 @@ public class BatchMoveLineExport extends BatchStrategy {
           ExceptionOriginRepository.MOVE_LINE_EXPORT_ORIGIN,
           batch.getId());
       incrementAnomaly();
-      stop = true;
+      end = true;
     }
 
     checkPoint();
@@ -85,7 +85,7 @@ public class BatchMoveLineExport extends BatchStrategy {
   @Override
   protected void process() {
 
-    if (!stop) {
+    if (!end) {
       try {
         Company company = batch.getAccountingBatch().getCompany();
         LocalDate startDate = batch.getAccountingBatch().getStartDate();
