0146328bb8051ff5ee5b1ff0929de990f893d081
Antoine Olivier <a.olivier@axelor.com>
2020-12-09 14:25:59 +0100

    RM#23258 : Fix tax number computing

--- axelor-base/src/main/java/com/axelor/apps/base/service/PartnerServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/PartnerServiceImpl.java
@@ -679,21 +679,33 @@ public class PartnerServiceImpl implements PartnerService {
 
   @Override
   public String getTaxNbrFromRegistrationCode(Partner partner) {
-    String regCode = partner.getRegistrationCode();
     String taxNbr = "";
 
-    if (regCode != null) {
-      regCode = regCode.replaceAll(" ", "");
+    if (partner.getMainAddress() != null
+        && partner.getMainAddress().getAddressL7Country() != null) {
+      String countryCode = partner.getMainAddress().getAddressL7Country().getAlpha2Code();
+      String regCode = partner.getRegistrationCode();
 
-      if (regCode.length() == 14) {
-        int siren = Integer.parseInt(regCode.substring(0, 9));
-        siren = Math.floorMod(siren, 97);
-        siren = Math.floorMod(12 + 3 * siren, 97);
-        taxNbr = Integer.toString(siren);
+      if (regCode != null) {
+        regCode = regCode.replaceAll(" ", "");
+
+        if (regCode.length() == 14) {
+          String siren = regCode.substring(0, 9);
+          String taxKey = getTaxKeyFromSIREN(siren);
+
+          taxNbr = String.format("%s%s%s", countryCode, taxKey, siren);
+        }
       }
     }
 
     return taxNbr;
+  }
+
+  protected String getTaxKeyFromSIREN(String sirenStr) {
+    int siren = Integer.parseInt(sirenStr);
+    int taxKey = Math.floorMod(siren, 97);
+    taxKey = Math.floorMod(12 + 3 * taxKey, 97);
+    return Integer.toString(taxKey);
   }
 
   public Partner isThereDuplicatePartnerInArchive(Partner partner) {
