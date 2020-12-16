9b6872408f75736d8db5fcaa3a227c92c51dc2f5
Arthur Lefebvre <a.lefebvre@axelor.com>
2020-12-16 18:16:44 +0100

    Refund: fix file name in auto generated printings
    RM21944

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoice/print/InvoicePrintServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoice/print/InvoicePrintServiceImpl.java
@@ -211,7 +211,7 @@ public class InvoicePrintServiceImpl implements InvoicePrintService {
           invoice);
     }
 
-    String title = I18n.get("Invoice");
+    String title = I18n.get(InvoiceToolService.isRefund(invoice) ? "Refund" : "Invoice");
     if (invoice.getInvoiceId() != null) {
       title += " " + invoice.getInvoiceId();
     }
