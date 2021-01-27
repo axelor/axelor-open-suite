2e960ab5b0c182bb058e29cb8a480be0d3ebf3dc
nsw-axelor <n.swami@axelor.com>
2021-01-27 17:20:47 +0530

    Feature #33611: TeamTask : Improve creation from ProjectTemplate

---
+++ axelor-project/src/main/java/com/axelor/apps/project/db/repo/TaskTemplateManagementRepository.java
@@ -0,0 +1,41 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2021 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.axelor.apps.project.db.repo;
+
+import com.axelor.apps.project.db.TaskTemplate;
+import com.axelor.apps.project.exception.IExceptionMessage;
+import com.axelor.apps.project.service.TaskTemplateService;
+import com.axelor.i18n.I18n;
+import com.axelor.inject.Beans;
+import javax.persistence.PersistenceException;
+
+public class TaskTemplateManagementRepository extends TaskTemplateRepository {
+
+  @Override
+  public TaskTemplate save(TaskTemplate taskTemplate) {
+
+    if (taskTemplate.getVersion() != 0
+        && Beans.get(TaskTemplateService.class)
+            .isParentTaskTemplateCreatedLoop(taskTemplate, taskTemplate.getParentTaskTemplate())) {
+      throw new PersistenceException(
+          I18n.get(IExceptionMessage.TASK_TEMPLATE_PARENT_TASK_CREATED_LOOP));
+    }
+
+    return super.save(taskTemplate);
+  }
+}
