2103849d0a46b77152a1f6fea5be52bccf084657
sgu-axelor <s.gupta@axelor.com>
2021-02-01 11:12:12 +0530

    Anomaly RM#34659 - Cash Management : Feature and corrections

--- axelor-cash-management/src/main/java/com/axelor/apps/cash/management/module/CashManagementModule.java
+++ axelor-cash-management/src/main/java/com/axelor/apps/cash/management/module/CashManagementModule.java
@@ -21,8 +21,10 @@ import com.axelor.app.AxelorModule;
 import com.axelor.apps.businessproject.service.InvoiceServiceProjectImpl;
 import com.axelor.apps.cash.management.db.repo.CashManagementForecastGeneratorRepository;
 import com.axelor.apps.cash.management.db.repo.CashManagementForecastRecapRepository;
+import com.axelor.apps.cash.management.db.repo.CashManagementForecastRepository;
 import com.axelor.apps.cash.management.db.repo.ForecastGeneratorRepository;
 import com.axelor.apps.cash.management.db.repo.ForecastRecapRepository;
+import com.axelor.apps.cash.management.db.repo.ForecastRepository;
 import com.axelor.apps.cash.management.service.InvoiceServiceManagementImpl;
 
 public class CashManagementModule extends AxelorModule {
@@ -32,5 +34,6 @@ public class CashManagementModule extends AxelorModule {
     bind(ForecastRecapRepository.class).to(CashManagementForecastRecapRepository.class);
     bind(InvoiceServiceProjectImpl.class).to(InvoiceServiceManagementImpl.class);
     bind(ForecastGeneratorRepository.class).to(CashManagementForecastGeneratorRepository.class);
+    bind(ForecastRepository.class).to(CashManagementForecastRepository.class);
   }
 }
