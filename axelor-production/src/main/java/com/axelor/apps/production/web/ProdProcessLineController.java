5cddc8b7ace8529704846fc50be923c790e16209
Arthur Lefebvre <a.lefebvre@axelor.com>
2021-02-09 15:45:39 +0100

    Add missing try catch in controller

--- axelor-production/src/main/java/com/axelor/apps/production/web/ProdProcessLineController.java
+++ axelor-production/src/main/java/com/axelor/apps/production/web/ProdProcessLineController.java
@@ -21,29 +21,26 @@ import com.axelor.apps.production.db.ProdProcessLine;
 import com.axelor.apps.production.db.WorkCenterGroup;
 import com.axelor.apps.production.db.repo.ProdProcessLineRepository;
 import com.axelor.apps.production.service.ProdProcessLineService;
+import com.axelor.exception.service.TraceBackService;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
-import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 @Singleton
 public class ProdProcessLineController {
-  protected ProdProcessLineService prodProcessLineService;
-
-  @Inject
-  public ProdProcessLineController(ProdProcessLineService prodProcessLineService) {
-    this.prodProcessLineService = prodProcessLineService;
-  }
 
   public void copyWorkCenterGroup(ActionRequest request, ActionResponse response) {
-
-    Long prodProcessId = request.getContext().asType(ProdProcessLine.class).getId();
-    ProdProcessLine prodProcess = Beans.get(ProdProcessLineRepository.class).find(prodProcessId);
-    WorkCenterGroup workCenterGroup = prodProcess.getWorkCenterGroup();
-    if (workCenterGroup != null) {
-      prodProcessLineService.copyWorkCenterGroup(prodProcess, workCenterGroup);
+    try {
+      Long prodProcessId = request.getContext().asType(ProdProcessLine.class).getId();
+      ProdProcessLine prodProcess = Beans.get(ProdProcessLineRepository.class).find(prodProcessId);
+      WorkCenterGroup workCenterGroup = prodProcess.getWorkCenterGroup();
+      if (workCenterGroup != null) {
+        Beans.get(ProdProcessLineService.class).copyWorkCenterGroup(prodProcess, workCenterGroup);
+      }
+      response.setCanClose(true);
+    } catch (Exception e) {
+      TraceBackService.trace(response, e);
     }
-    response.setCanClose(true);
   }
 }
