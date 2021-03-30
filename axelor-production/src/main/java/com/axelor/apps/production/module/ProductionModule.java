4ab5bd00c10543e6e6381d3fe78c10fca269f400
Arthur Lefebvre <a.lefebvre@axelor.com>
2021-03-30 11:13:42 +0200

    Purchase order: fix printing from email
    Remove query sent from java to the report, instead use postgresql
    function inside rptdesign to manage the case of missing module.
    RM36683

--- axelor-production/src/main/java/com/axelor/apps/production/module/ProductionModule.java
+++ axelor-production/src/main/java/com/axelor/apps/production/module/ProductionModule.java
@@ -83,14 +83,12 @@ import com.axelor.apps.production.service.manuforder.ManufOrderService;
 import com.axelor.apps.production.service.manuforder.ManufOrderServiceImpl;
 import com.axelor.apps.production.service.operationorder.OperationOrderService;
 import com.axelor.apps.production.service.operationorder.OperationOrderServiceImpl;
-import com.axelor.apps.production.service.print.PurchaseOrderPrintServiceProductionImpl;
 import com.axelor.apps.production.service.productionorder.ProductionOrderSaleOrderService;
 import com.axelor.apps.production.service.productionorder.ProductionOrderSaleOrderServiceImpl;
 import com.axelor.apps.production.service.productionorder.ProductionOrderService;
 import com.axelor.apps.production.service.productionorder.ProductionOrderServiceImpl;
 import com.axelor.apps.production.service.productionorder.ProductionOrderWizardService;
 import com.axelor.apps.production.service.productionorder.ProductionOrderWizardServiceImpl;
-import com.axelor.apps.purchase.service.print.PurchaseOrderPrintServiceImpl;
 import com.axelor.apps.sale.service.configurator.ConfiguratorCreatorImportServiceImpl;
 import com.axelor.apps.sale.service.configurator.ConfiguratorServiceImpl;
 import com.axelor.apps.stock.db.repo.ProductStockRepository;
@@ -155,6 +153,5 @@ public class ProductionModule extends AxelorModule {
     bind(MpsChargeService.class).to(MpsChargeServiceImpl.class);
     bind(MachineRepository.class).to(MachineToolManagementRepository.class);
     bind(PurchaseOrderServiceSupplychainImpl.class).to(PurchaseOrderServiceProductionImpl.class);
-    bind(PurchaseOrderPrintServiceImpl.class).to(PurchaseOrderPrintServiceProductionImpl.class);
   }
 }
