6a0da06538e05b59984d5a92132dcdffe8e6cb1a
pda-axelor <p.dave@axelor.com>
2021-04-02 12:55:58 +0530

    Anomaly RM#36752 INVOICE : delete invoice generated from business invoicing

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/db/repo/InvoiceProjectRepository.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/db/repo/InvoiceProjectRepository.java
@@ -18,9 +18,14 @@
 package com.axelor.apps.businessproject.db.repo;
 
 import com.axelor.apps.account.db.Invoice;
+import com.axelor.apps.businessproject.db.InvoicingProject;
 import com.axelor.apps.businessproject.service.app.AppBusinessProjectService;
 import com.axelor.apps.supplychain.db.repo.InvoiceSupplychainRepository;
+import com.axelor.common.ObjectUtils;
 import com.axelor.inject.Beans;
+import com.axelor.team.db.TeamTask;
+import com.axelor.team.db.repo.TeamTaskRepository;
+import java.util.List;
 
 public class InvoiceProjectRepository extends InvoiceSupplychainRepository {
 
@@ -28,10 +33,25 @@ public class InvoiceProjectRepository extends InvoiceSupplychainRepository {
   public void remove(Invoice entity) {
 
     if (Beans.get(AppBusinessProjectService.class).isApp("business-project")) {
-      Beans.get(InvoicingProjectRepository.class)
-          .all()
-          .filter("self.invoice.id = ?", entity.getId())
-          .remove();
+      List<InvoicingProject> invoiceProjectList =
+          Beans.get(InvoicingProjectRepository.class)
+              .all()
+              .filter("self.invoice.id = ?", entity.getId())
+              .fetch();
+      List<TeamTask> teamTaskList =
+          Beans.get(TeamTaskRepository.class)
+              .all()
+              .filter("self.invoiceLine.invoice = ?1", entity)
+              .fetch();
+      if (ObjectUtils.notEmpty(teamTaskList)) {
+        for (TeamTask teamTask : teamTaskList) {
+          teamTask.setInvoiceLine(null);
+        }
+      }
+      for (InvoicingProject invoiceProject : invoiceProjectList) {
+        invoiceProject.setInvoice(null);
+        invoiceProject.setStatusSelect(InvoicingProjectRepository.STATUS_DRAFT);
+      }
     }
 
     super.remove(entity);
