49db1ea5f5ce2e2fbedc58730ac0b113b6af393a
ssb-axelor <s.sbitti@axelor.com>
2021-04-26 18:57:24 +0200

    Feature RM37356 DAS2

--- axelor-account/src/main/java/com/axelor/apps/account/service/AccountingReportServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/AccountingReportServiceImpl.java
@@ -210,6 +210,14 @@ public class AccountingReportServiceImpl implements AccountingReportService {
         this.addParams("self.credit > 0");
         this.addParams("(self.account is null OR self.account.reconcileOk = 'true')");
       }
+      if (accountingReport.getReportType().getTypeSelect()
+    		  == AccountingReportRepository.REPORT_FEES_DECLARATION_SUPPORT ) {
+    	  this.addParams("self.serviceTypeCode is not null AND self.das2ActivityName is not null");
+      }
+      if (accountingReport.getReportType().getTypeSelect()
+              == AccountingReportRepository.REPORT_FEES_DECLARATION_PREPERATORY_PROCESS) {
+        this.addParams("self.serviceTypeCode is not null AND self.serviceType.amountToReport is not true AND self.das2ActivityName is not null");
+      }
     }
 
     if (this.compareReportType(
