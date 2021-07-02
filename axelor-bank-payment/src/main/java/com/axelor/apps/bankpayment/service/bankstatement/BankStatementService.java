d4a6138cf7990da90331c9ba0454d7f3ec92fa9d
Simon Hamilton <s.hamilton@axelor.com>
2021-07-02 17:31:50 +0200

    DRAFT:39297
    Added move generation on import of bank statement

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankstatement/BankStatementService.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankstatement/BankStatementService.java
@@ -18,13 +18,21 @@
 package com.axelor.apps.bankpayment.service.bankstatement;
 
 import com.axelor.apps.ReportFactory;
+import com.axelor.apps.account.db.BankStatementRule;
+import com.axelor.apps.account.db.Move;
+import com.axelor.apps.account.db.MoveLine;
+import com.axelor.apps.account.db.repo.BankStatementRuleRepository;
+import com.axelor.apps.account.db.repo.MoveRepository;
 import com.axelor.apps.bankpayment.db.BankStatement;
 import com.axelor.apps.bankpayment.db.BankStatementFileFormat;
+import com.axelor.apps.bankpayment.db.BankStatementLine;
 import com.axelor.apps.bankpayment.db.repo.BankStatementFileFormatRepository;
 import com.axelor.apps.bankpayment.db.repo.BankStatementRepository;
 import com.axelor.apps.bankpayment.exception.IExceptionMessage;
 import com.axelor.apps.bankpayment.report.IReport;
 import com.axelor.apps.bankpayment.service.bankstatement.file.afb120.BankStatementFileAFB120Service;
+import com.axelor.apps.base.db.repo.YearBaseRepository;
+import com.axelor.apps.base.service.PeriodService;
 import com.axelor.apps.report.engine.ReportSettings;
 import com.axelor.db.JPA;
 import com.axelor.exception.AxelorException;
@@ -38,10 +46,17 @@ import java.io.IOException;
 public class BankStatementService {
 
   protected BankStatementRepository bankStatementRepository;
+  protected PeriodService periodService;
+  protected MoveRepository moveRepository;
 
   @Inject
-  public BankStatementService(BankStatementRepository bankStatementRepository) {
+  public BankStatementService(
+      BankStatementRepository bankStatementRepository,
+      PeriodService periodService,
+      MoveRepository moveRepository) {
     this.bankStatementRepository = bankStatementRepository;
+    this.periodService = periodService;
+    this.moveRepository = moveRepository;
   }
 
   public void runImport(BankStatement bankStatement, boolean alertIfFormatNotSupported)
@@ -148,5 +163,67 @@ public class BankStatementService {
     return JPA.em().contains(bankStatement)
         ? bankStatement
         : bankStatementRepository.find(bankStatement.getId());
+  }
+
+  @Transactional
+  public Move generateMove(
+      BankStatementLine bankStatementLine, BankStatementRule bankStatementRule) {
+    if (bankStatementRule == null) {
+      bankStatementRule = Beans.get(BankStatementRuleRepository.class).all().fetchOne();
+    }
+    Move move = new Move();
+    move.setCompany(bankStatementRule.getAccountManagement().getCompany());
+    move.setJournal(bankStatementRule.getAccountManagement().getJournal());
+    move.setPeriod(
+        periodService.getPeriod(
+            bankStatementLine.getValueDate(), move.getCompany(), YearBaseRepository.TYPE_FISCAL));
+    move.setDate(bankStatementLine.getValueDate());
+    if (bankStatementLine.getOperationDate() != null) {}
+
+    move.setPartner(bankStatementRule.getPartner());
+    move.setCurrency(bankStatementLine.getCurrency());
+    move.setPaymentMode(bankStatementRule.getAccountManagement().getPaymentMode());
+    move.setTechnicalOriginSelect(MoveRepository.TECHNICAL_ORIGIN_AUTOMATIC);
+    move.setFunctionalOriginSelect(MoveRepository.FUNCTIONAL_ORIGIN_PAYMENT);
+    move.clearMoveLineList();
+    move.addMoveLineListItem(generateMoveLine(bankStatementLine, bankStatementRule, move, true));
+    move.addMoveLineListItem(generateMoveLine(bankStatementLine, bankStatementRule, move, false));
+    return moveRepository.save(move);
+  }
+
+  public MoveLine generateMoveLine(
+      BankStatementLine bankStatementLine,
+      BankStatementRule bankStatementRule,
+      Move move,
+      boolean isFirstLine) {
+    MoveLine moveLine = new MoveLine();
+    moveLine.setMove(move);
+    moveLine.setDate(bankStatementLine.getValueDate());
+    if (isFirstLine) {
+      if (move.getCurrency().equals(move.getCompany().getCurrency())) {
+        moveLine.setDebit(bankStatementLine.getCredit());
+        moveLine.setCredit(bankStatementLine.getDebit());
+      } else {
+        // TODO MODIFY to fit spec
+        moveLine.setDebit(bankStatementLine.getCredit());
+        moveLine.setCredit(bankStatementLine.getDebit());
+      }
+      moveLine.setAccount(bankStatementRule.getCounterpartAccount());
+    } else {
+      if (move.getCurrency().equals(move.getCompany().getCurrency())) {
+        moveLine.setDebit(bankStatementLine.getDebit());
+        moveLine.setCredit(bankStatementLine.getCredit());
+      } else {
+        // TODO Modify to fit spec
+        moveLine.setDebit(bankStatementLine.getDebit());
+        moveLine.setCredit(bankStatementLine.getCredit());
+      }
+      moveLine.setAccount(bankStatementRule.getAccountManagement().getCashAccount());
+    }
+
+    moveLine.setOrigin(bankStatementLine.getOrigin());
+    moveLine.setDescription(bankStatementLine.getDescription());
+
+    return moveLine;
   }
 }
--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/web/BankStatementController.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/web/BankStatementController.java
@@ -19,6 +19,9 @@ package com.axelor.apps.bankpayment.web;
 
 import com.axelor.apps.bankpayment.db.BankReconciliation;
 import com.axelor.apps.bankpayment.db.BankStatement;
+import com.axelor.apps.bankpayment.db.BankStatementLineAFB120;
+import com.axelor.apps.bankpayment.db.repo.BankStatementLineAFB120Repository;
+import com.axelor.apps.bankpayment.db.repo.BankStatementLineRepository;
 import com.axelor.apps.bankpayment.db.repo.BankStatementRepository;
 import com.axelor.apps.bankpayment.service.bankreconciliation.BankReconciliationCreateService;
 import com.axelor.apps.bankpayment.service.bankstatement.BankStatementService;
@@ -29,69 +32,80 @@ import com.axelor.meta.schema.actions.ActionView;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.google.common.base.Joiner;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.util.List;
 
 @Singleton
 public class BankStatementController {
 
-  public void runImport(ActionRequest request, ActionResponse response) {
+	protected BankStatementService bankStatementService;
+	protected BankStatementLineRepository bankStatementLineRepository;
+	protected BankStatementLineAFB120Repository bankStatementLineAFB120Repository;
 
-    try {
-      BankStatement bankStatement = request.getContext().asType(BankStatement.class);
-      bankStatement = Beans.get(BankStatementRepository.class).find(bankStatement.getId());
-      Beans.get(BankStatementService.class).runImport(bankStatement, true);
+	@Inject
+	public BankStatementController(BankStatementService bankStatementService,
+			BankStatementLineRepository bankStatementLineRepository,
+			BankStatementLineAFB120Repository bankStatementLineAFB120Repository) {
 
-    } catch (Exception e) {
-      TraceBackService.trace(response, e);
-    }
-    response.setReload(true);
-  }
+		this.bankStatementService = bankStatementService;
+		this.bankStatementLineRepository = bankStatementLineRepository;
+		this.bankStatementLineAFB120Repository = bankStatementLineAFB120Repository;
+	}
 
-  public void print(ActionRequest request, ActionResponse response) {
-    try {
-      BankStatement bankStatement = request.getContext().asType(BankStatement.class);
-      bankStatement = Beans.get(BankStatementRepository.class).find(bankStatement.getId());
-      String name = bankStatement.getName();
-      String fileLink = Beans.get(BankStatementService.class).print(bankStatement);
-      response.setView(ActionView.define(name).add("html", fileLink).map());
+	public void runImport(ActionRequest request, ActionResponse response) {
+		try {
+			BankStatement bankStatement = request.getContext().asType(BankStatement.class);
+			bankStatement = Beans.get(BankStatementRepository.class).find(bankStatement.getId());
+			bankStatementService.runImport(bankStatement, true);
+			List<BankStatementLineAFB120> bankStatementLines = bankStatementLineAFB120Repository.all()
+					.filter("self.lineTypeSelect = :lineTypeSelect AND self.bankStatement = :bankStatement")
+					.bind("lineTypeSelect", BankStatementLineAFB120Repository.LINE_TYPE_MOVEMENT)
+					.bind("bankStatement", bankStatement).fetch();
+			for (BankStatementLineAFB120 bankStatementLineAFB120 : bankStatementLines) {
+				bankStatementService.generateMove(bankStatementLineAFB120, null);
+				// TODO link moveline 2 to bankStatementLine
+			}
+		} catch (Exception e) {
+			TraceBackService.trace(response, e);
+		}
+		response.setReload(true);
+	}
 
-    } catch (Exception e) {
-      TraceBackService.trace(response, e);
-    }
+	public void print(ActionRequest request, ActionResponse response) {
+		try {
+			BankStatement bankStatement = request.getContext().asType(BankStatement.class);
+			bankStatement = Beans.get(BankStatementRepository.class).find(bankStatement.getId());
+			String name = bankStatement.getName();
+			String fileLink = Beans.get(BankStatementService.class).print(bankStatement);
+			response.setView(ActionView.define(name).add("html", fileLink).map());
 
-    response.setReload(true);
-  }
+		} catch (Exception e) {
+			TraceBackService.trace(response, e);
+		}
 
-  public void runBankReconciliation(ActionRequest request, ActionResponse response) {
+		response.setReload(true);
+	}
 
-    try {
-      BankStatement bankStatement = request.getContext().asType(BankStatement.class);
-      bankStatement = Beans.get(BankStatementRepository.class).find(bankStatement.getId());
-      List<BankReconciliation> bankReconciliationList =
-          Beans.get(BankReconciliationCreateService.class)
-              .createAllFromBankStatement(bankStatement);
+	public void runBankReconciliation(ActionRequest request, ActionResponse response) {
 
-      if (bankReconciliationList != null) {
-        response.setView(
-            ActionView.define(I18n.get("Bank reconciliations"))
-                .model(BankReconciliation.class.getName())
-                .add("grid", "bank-reconciliation-grid")
-                .add("form", "bank-reconciliation-form")
-                .param("search-filters", "bank-reconciliation-filters")
-                .domain(
-                    "self.id in ("
-                        + Joiner.on(",")
-                            .join(
-                                bankReconciliationList.stream()
-                                    .map(BankReconciliation::getId)
-                                    .toArray())
-                        + ")")
-                .map());
-      }
-    } catch (Exception e) {
-      TraceBackService.trace(response, e);
-    }
-    response.setReload(true);
-  }
+		try {
+			BankStatement bankStatement = request.getContext().asType(BankStatement.class);
+			bankStatement = Beans.get(BankStatementRepository.class).find(bankStatement.getId());
+			List<BankReconciliation> bankReconciliationList = Beans.get(BankReconciliationCreateService.class)
+					.createAllFromBankStatement(bankStatement);
+
+			if (bankReconciliationList != null) {
+				response.setView(ActionView.define(I18n.get("Bank reconciliations"))
+						.model(BankReconciliation.class.getName()).add("grid", "bank-reconciliation-grid")
+						.add("form", "bank-reconciliation-form").param("search-filters", "bank-reconciliation-filters")
+						.domain("self.id in (" + Joiner.on(",")
+								.join(bankReconciliationList.stream().map(BankReconciliation::getId).toArray()) + ")")
+						.map());
+			}
+		} catch (Exception e) {
+			TraceBackService.trace(response, e);
+		}
+		response.setReload(true);
+	}
 }
