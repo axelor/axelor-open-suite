5ff67aea2caa53311802286572c411d4dd1feb74
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2021-09-08 15:36:53 +0200

    Revert "Merge branch 'wip-6.2-38970' into wip-6.2-accounting"
    This reverts commit 554556a88b7952ab5ec5e45cebabbd8c10388bc8, reversing
    changes made to cabb806f12c5ece641d092f16f963ad70b608128.

--- axelor-account/src/main/java/com/axelor/apps/account/report/ITranslation.java
+++ axelor-account/src/main/java/com/axelor/apps/account/report/ITranslation.java
@@ -660,11 +660,6 @@ public interface ITranslation {
   public static final String ACCOUNTING_REPORT_17_TOTAL_BY_ANALYTIC_TEMPLATE =
       /*$$(*/ "AccountingReportType17.total_by_analytic_template"; /*)*/
 
-  public static final String ACCOUNTING_REPORT_LABEL_PROVISIONAL =
-      /*$$(*/ "AccountingReportType.provisional"; /*)*/
-  public static final String ACCOUNTING_REPORT_LABEL_STATUS_SELECT_TITLE =
-      /*$$(*/ "AccountingReportType.status_select_title"; /*)*/
-
   public static final String ACCOUNTING_REPORT_19_DAS2_ACTIVITY =
       /*$$(*/ "AccountingReportType19.das2ActivityName"; /*)*/
   public static final String ACCOUNTING_REPORT_19_COMPANY =
