ac539ccdf9decac92e911e53a450b563d53d294a
pda-axelor <p.dave@axelor.com>
2021-09-24 12:56:10 +0530

    Anomaly RM#41552 PURCHASEORDER : erroneous receiptState

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/PurchaseOrderStockServiceImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/PurchaseOrderStockServiceImpl.java
@@ -406,6 +406,9 @@ public class PurchaseOrderStockServiceImpl implements PurchaseOrderStockService 
     if (taxLine != null) {
       taxRate = taxLine.getValue();
     }
+    if (purchaseOrderLine.getReceiptState() == 0) {
+      purchaseOrderLine.setReceiptState(PurchaseOrderLineRepository.RECEIPT_STATE_NOT_RECEIVED);
+    }
 
     return stockMoveLineServiceSupplychain.createStockMoveLine(
         product,
