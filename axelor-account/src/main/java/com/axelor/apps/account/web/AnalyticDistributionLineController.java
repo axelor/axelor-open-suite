ad804d3e608039d606173c523aff6c7ac32ebb17
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2021-10-07 17:30:36 +0200

    RM42205 Changes on analyticMoveLine and translations

--- axelor-account/src/main/java/com/axelor/apps/account/web/AnalyticDistributionLineController.java
+++ axelor-account/src/main/java/com/axelor/apps/account/web/AnalyticDistributionLineController.java
@@ -19,13 +19,17 @@ package com.axelor.apps.account.web;
 
 import com.axelor.apps.account.db.AnalyticDistributionTemplate;
 import com.axelor.apps.account.db.AnalyticMoveLine;
+import com.axelor.apps.account.db.InvoiceLine;
+import com.axelor.apps.account.db.MoveLine;
 import com.axelor.apps.account.service.AnalyticMoveLineService;
+import com.axelor.apps.account.service.config.AccountConfigService;
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.ResponseMessageType;
 import com.axelor.exception.service.TraceBackService;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import com.axelor.rpc.Context;
 import com.google.inject.Singleton;
 import java.time.LocalDate;
 
@@ -55,6 +59,26 @@ public class AnalyticDistributionLineController {
       AnalyticMoveLine analyticMoveLine = request.getContext().asType(AnalyticMoveLine.class);
       if (analyticMoveLine != null) {
         response.setValue("date", LocalDate.now());
+        Context parentContext = request.getContext().getParent();
+        if (MoveLine.class.equals(parentContext.getContextClass())) {
+          MoveLine moveLine = parentContext.asType(MoveLine.class);
+          if (moveLine.getMove() != null && moveLine.getMove().getCompany() != null) {
+            response.setValue(
+                "analyticJournal",
+                Beans.get(AccountConfigService.class)
+                    .getAccountConfig(moveLine.getMove().getCompany())
+                    .getAnalyticJournal());
+          }
+        } else if (InvoiceLine.class.equals(parentContext.getContextClass())) {
+          InvoiceLine invoiceLine = parentContext.asType(InvoiceLine.class);
+          if (invoiceLine.getInvoice() != null && invoiceLine.getInvoice().getCompany() != null) {
+            response.setValue(
+                "analyticJournal",
+                Beans.get(AccountConfigService.class)
+                    .getAccountConfig(invoiceLine.getInvoice().getCompany())
+                    .getAnalyticJournal());
+          }
+        }
       }
     } catch (Exception e) {
       TraceBackService.trace(response, e, ResponseMessageType.ERROR);
