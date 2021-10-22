0d9fa66880888324ade248eedf14b1c21f248c69
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2021-10-22 14:28:56 +0200

    Anomaly #42346: Set name on BatchLeaveManagement for leaveLine (#7031)
    (cherry picked from commit e5b640826163d700d690a0bdef820a985c8b3a2f)

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/leave/LeaveServiceImpl.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/leave/LeaveServiceImpl.java
@@ -899,6 +899,7 @@ public class LeaveServiceImpl implements LeaveService {
     if ((leaveLine == null) || (leaveLine.getLeaveReason() != leaveReason)) {
       leaveLine = createLeaveReasonToJustify(employee, leaveReason);
     }
+    leaveLine.setName(leaveReason.getLeaveReason());
     return leaveLine;
   }
 
