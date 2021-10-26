8592646784b1af650b49e82ff523b051cfec2a43
ple-axelor <p.leelgar@axelor.com>
2021-10-26 13:19:10 +0530

    Anomaly RM#42213 : Advanced export: archived management
    (cherry picked from commit 0f34afa2bf77ad179ce34b3a7d6ee19ac1d15f14)

--- axelor-base/src/main/java/com/axelor/apps/base/service/advancedExport/AdvancedExportServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/advancedExport/AdvancedExportServiceImpl.java
@@ -131,8 +131,7 @@ public class AdvancedExportServiceImpl implements AdvancedExportService {
       throw new AxelorException(e, TraceBackRepository.CATEGORY_CONFIGURATION_ERROR);
     }
     return createQuery(
-        createQueryBuilder(
-            advancedExport.getMetaModel(), selectFieldBuilder, recordIds, orderByFieldBuilder));
+        createQueryBuilder(advancedExport, selectFieldBuilder, recordIds, orderByFieldBuilder));
   }
 
   /**
@@ -295,7 +294,7 @@ public class AdvancedExportServiceImpl implements AdvancedExportService {
    * @return
    */
   private StringBuilder createQueryBuilder(
-      MetaModel metaModel,
+      AdvancedExport advancedExport,
       StringBuilder selectFieldBuilder,
       List<Long> recordIds,
       StringBuilder orderByFieldBuilder) {
@@ -306,6 +305,7 @@ public class AdvancedExportServiceImpl implements AdvancedExportService {
     selectionJoinField = String.join(" ", selectionJoinFieldSet);
 
     params = null;
+    MetaModel metaModel = advancedExport.getMetaModel();
     String criteria = getCriteria(metaModel, recordIds);
 
     if (!orderByFieldBuilder.toString().equals(""))
@@ -320,6 +320,9 @@ public class AdvancedExportServiceImpl implements AdvancedExportService {
     queryBuilder.append(
         (!Strings.isNullOrEmpty(selectionJoinField)) ? selectionJoinField + " " : "");
     queryBuilder.append((!Strings.isNullOrEmpty(criteria)) ? criteria : "");
+    if (!advancedExport.getIncludeArchivedRecords()) {
+      queryBuilder.append("WHERE self.archived = 'f' OR self.archived IS NULL");
+    }
     queryBuilder.append((!Strings.isNullOrEmpty(orderByCol)) ? orderByCol : "");
 
     return queryBuilder;
