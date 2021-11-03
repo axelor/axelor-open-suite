f3cef27d98a34f2c8e76c32cbfc8bdcef186d978
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2021-11-03 16:33:45 +0100

    42312 changes in translation

--- axelor-account/src/main/java/com/axelor/apps/account/web/AnalyticDistributionTemplateController.java
+++ axelor-account/src/main/java/com/axelor/apps/account/web/AnalyticDistributionTemplateController.java
@@ -4,6 +4,7 @@ import com.axelor.apps.account.db.AnalyticDistributionTemplate;
 import com.axelor.apps.account.service.AnalyticDistributionTemplateService;
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.service.TraceBackService;
+import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
@@ -18,7 +19,7 @@ public class AnalyticDistributionTemplateController {
       if (!Beans.get(AnalyticDistributionTemplateService.class)
           .validateTemplatePercentages(analyticDistributionTemplate)) {
         response.setError(
-            "The distribution is wrong, some axes percentage values are not equal to 100%");
+            I18n.get("The configured distribution is incorrect, the sum of percentages for each axis must be equal to 100%"));
       }
     } catch (Exception e) {
       TraceBackService.trace(response, e);
