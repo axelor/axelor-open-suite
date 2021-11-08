7f5ec94c1be887bec7a3b691c1684060381b39b5
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2021-11-08 18:09:52 +0100

    Reverted Feature #42427

--- axelor-account/src/main/java/com/axelor/apps/account/module/AccountModule.java
+++ axelor-account/src/main/java/com/axelor/apps/account/module/AccountModule.java
@@ -92,8 +92,6 @@ import com.axelor.apps.account.service.AnalyticDistributionTemplateService;
 import com.axelor.apps.account.service.AnalyticDistributionTemplateServiceImpl;
 import com.axelor.apps.account.service.AnalyticGroupingService;
 import com.axelor.apps.account.service.AnalyticGroupingServiceImpl;
-import com.axelor.apps.account.service.AnalyticJournalControlService;
-import com.axelor.apps.account.service.AnalyticJournalControlServiceImpl;
 import com.axelor.apps.account.service.AnalyticMoveLineService;
 import com.axelor.apps.account.service.AnalyticMoveLineServiceImpl;
 import com.axelor.apps.account.service.BankDetailsServiceAccountImpl;
@@ -401,7 +399,5 @@ public class AccountModule extends AxelorModule {
     bind(YearControlService.class).to(YearControlServiceImpl.class);
 
     bind(AnalyticAxisControlService.class).to(AnalyticAxisControlServiceImpl.class);
-
-    bind(AnalyticJournalControlService.class).to(AnalyticJournalControlServiceImpl.class);
   }
 }
