47c7b0551ca8148ff5a51a147becc4ffefc625d5
Simon Hamilton <s.hamilton@axelor.com>
2021-11-08 16:18:49 +0100

    42887

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveValidateServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveValidateServiceImpl.java
@@ -319,16 +319,18 @@ public class MoveValidateServiceImpl implements MoveValidateService {
 
   @Override
   public void updateValidateStatus(Move move, boolean daybook) throws AxelorException {
-    if (daybook
-        && (move.getStatusSelect() == MoveRepository.STATUS_NEW
-            || move.getStatusSelect() == MoveRepository.STATUS_SIMULATED)
-        && !(move.getTechnicalOriginSelect() == MoveRepository.TECHNICAL_ORIGIN_AUTOMATIC)
-        && (move.getFunctionalOriginSelect() == MoveRepository.FUNCTIONAL_ORIGIN_OPENING
-            || move.getFunctionalOriginSelect() == MoveRepository.FUNCTIONAL_ORIGIN_CLOSURE)) {
-      move.setStatusSelect(MoveRepository.STATUS_ACCOUNTED);
-    } else {
+    if (!daybook
+        || (daybook
+            && (move.getStatusSelect() == MoveRepository.STATUS_NEW
+                || move.getStatusSelect() == MoveRepository.STATUS_SIMULATED)
+            && (move.getTechnicalOriginSelect() == MoveRepository.TECHNICAL_ORIGIN_AUTOMATIC)
+            && (move.getFunctionalOriginSelect() == MoveRepository.FUNCTIONAL_ORIGIN_OPENING
+                || move.getFunctionalOriginSelect() == MoveRepository.FUNCTIONAL_ORIGIN_CLOSURE))) {
       move.setStatusSelect(MoveRepository.STATUS_VALIDATED);
       move.setValidationDate(appBaseService.getTodayDate(move.getCompany()));
+      this.generateFixedAssetMoveLine(move);
+    } else {
+      move.setStatusSelect(MoveRepository.STATUS_ACCOUNTED);
     }
   }
 
