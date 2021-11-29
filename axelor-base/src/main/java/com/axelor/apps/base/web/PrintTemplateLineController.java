b36c25996d3038b71865609b2b79bf56de000c05
Hinal Mistry <h.mistry@axelor.com>
2021-11-29 17:17:45 +0530

    RM#36825 TEMPLATE : test

--- axelor-base/src/main/java/com/axelor/apps/base/web/PrintTemplateLineController.java
+++ axelor-base/src/main/java/com/axelor/apps/base/web/PrintTemplateLineController.java
@@ -17,13 +17,11 @@
  */
 package com.axelor.apps.base.web;
 
-import com.axelor.apps.base.db.PrintTemplateLineTest;
-import com.axelor.apps.base.db.repo.PrintTemplateLineTestRepository;
-import com.axelor.apps.base.exceptions.IExceptionMessage;
+import com.axelor.apps.base.db.PrintTemplateLine;
+import com.axelor.apps.base.db.repo.PrintTemplateLineRepository;
 import com.axelor.apps.base.service.PrintTemplateLineService;
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.service.TraceBackService;
-import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
 import com.axelor.meta.db.MetaModel;
 import com.axelor.meta.db.repo.MetaModelRepository;
@@ -31,44 +29,30 @@ import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.axelor.rpc.Context;
 import java.io.IOException;
-import java.util.LinkedHashMap;
 
 public class PrintTemplateLineController {
 
   public void checkTemplateLineExpression(ActionRequest request, ActionResponse response) {
 
     Context context = request.getContext();
-    PrintTemplateLineTest printTemplateLineTest = context.asType(PrintTemplateLineTest.class);
-    printTemplateLineTest =
-        Beans.get(PrintTemplateLineTestRepository.class).find(printTemplateLineTest.getId());
+    PrintTemplateLine printTemplateLine =
+        Beans.get(PrintTemplateLineRepository.class)
+            .find(Long.valueOf(context.get("_printTemplateLine").toString()));
     MetaModel metaModel =
         Beans.get(MetaModelRepository.class)
             .all()
-            .filter("self.fullName = ?", printTemplateLineTest.getReference())
+            .filter("self.fullName = ?", context.get("reference"))
             .fetchOne();
     try {
-      Beans.get(PrintTemplateLineService.class)
-          .checkExpression(
-              Long.valueOf(printTemplateLineTest.getReferenceId().toString()),
-              metaModel,
-              printTemplateLineTest.getPrintTemplateLine());
+      String result =
+          Beans.get(PrintTemplateLineService.class)
+              .checkExpression(
+                  Long.valueOf(context.get("referenceId").toString()),
+                  metaModel,
+                  printTemplateLine);
+      response.setValue("$contentResult", result);
     } catch (NumberFormatException | ClassNotFoundException | AxelorException | IOException e) {
       TraceBackService.trace(response, e);
     }
-
-    response.setReload(true);
-  }
-
-  @SuppressWarnings("unchecked")
-  public void addItemToReferenceSelection(ActionRequest request, ActionResponse response) {
-    LinkedHashMap<String, Object> metaModelMap =
-        (LinkedHashMap<String, Object>) request.getContext().get("metaModel");
-    if (metaModelMap == null) {
-      return;
-    }
-    Long metaModelId = Long.parseLong(metaModelMap.get("id").toString());
-    MetaModel metaModel = Beans.get(MetaModelRepository.class).find(metaModelId);
-    Beans.get(PrintTemplateLineService.class).addItemToReferenceSelection(metaModel);
-    response.setNotify(I18n.get(IExceptionMessage.PRINT_TEMPLATE_LINE_TEST_REFRESH));
   }
 }
--- axelor-message/src/main/java/com/axelor/apps/message/service/TemplateService.java
+++ axelor-message/src/main/java/com/axelor/apps/message/service/TemplateService.java
@@ -18,24 +18,41 @@
 package com.axelor.apps.message.service;
 
 import com.axelor.app.internal.AppFilter;
+import com.axelor.apps.message.db.Message;
 import com.axelor.apps.message.db.Template;
 import com.axelor.apps.message.exception.IExceptionMessage;
+import com.axelor.common.StringUtils;
+import com.axelor.db.JPA;
 import com.axelor.db.Model;
 import com.axelor.db.mapper.Mapper;
 import com.axelor.db.mapper.Property;
+import com.axelor.event.Observes;
+import com.axelor.events.StartupEvent;
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.db.repo.TraceBackRepository;
+import com.axelor.exception.service.TraceBackService;
 import com.axelor.i18n.I18n;
+import com.axelor.inject.Beans;
 import com.axelor.meta.db.MetaModel;
+import com.axelor.meta.db.MetaSelect;
+import com.axelor.meta.db.MetaSelectItem;
+import com.axelor.meta.db.repo.MetaModelRepository;
+import com.axelor.meta.db.repo.MetaSelectItemRepository;
+import com.axelor.meta.db.repo.MetaSelectRepository;
 import com.axelor.tool.template.TemplateMaker;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 import com.google.inject.Singleton;
+import com.google.inject.persist.Transactional;
+import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 @Singleton
 public class TemplateService {
+
+  public static final String ALL_MODEL_SELECT = "all.model.reference.select";
 
   public void checkTargetReceptor(Template template) throws AxelorException {
     String target = template.getTarget();
@@ -96,5 +113,77 @@ public class TemplateService {
     maker.setTemplate(template.getContent());
     maker.setContext(bean, context, beanName);
     return maker.make();
+  }
+
+  @Transactional
+  public void createSelectionOfAllModels(@Observes StartupEvent event) {
+
+    MetaSelectRepository metaSelectRepo = Beans.get(MetaSelectRepository.class);
+    MetaModelRepository metaModelRepo = Beans.get(MetaModelRepository.class);
+    MetaSelectItemRepository metaSelectItemRepo = Beans.get(MetaSelectItemRepository.class);
+
+    MetaSelect allModelSelect = metaSelectRepo.findByName(ALL_MODEL_SELECT);
+
+    if (allModelSelect != null) {
+      List<MetaModel> addDifferences =
+          metaModelRepo
+              .all()
+              .filter(
+                  "self.fullName NOT IN (SELECT item.value FROM MetaSelectItem item WHERE item.select = :selectId)")
+              .bind("selectId", allModelSelect.getId())
+              .fetch();
+      if (!addDifferences.isEmpty()) {
+        addMetaSelectItems(addDifferences, allModelSelect);
+      }
+      List<MetaSelectItem> removeDifferences =
+          metaSelectItemRepo
+              .all()
+              .filter(
+                  "self.select = :selectId AND self.value NOT IN (SELECT model.fullName FROM MetaModel model)")
+              .bind("selectId", allModelSelect.getId())
+              .fetch();
+      if (!removeDifferences.isEmpty()) {
+        allModelSelect.getItems().removeAll(removeDifferences);
+      }
+    } else {
+      allModelSelect = new MetaSelect();
+      allModelSelect.setName(ALL_MODEL_SELECT);
+      List<MetaModel> metaModelList = metaModelRepo.all().order("name").fetch();
+      addMetaSelectItems(metaModelList, allModelSelect);
+    }
+
+    metaSelectRepo.save(allModelSelect);
+  }
+
+  public void addMetaSelectItems(List<MetaModel> metaModelList, MetaSelect metaSelect) {
+    for (MetaModel model : metaModelList) {
+      MetaSelectItem item = new MetaSelectItem();
+      item.setTitle(model.getName());
+      item.setValue(model.getFullName());
+      item.setSelect(metaSelect);
+      metaSelect.addItem(item);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public Message generateDraftMessage(Template template, MetaModel metaModel, String referenceId)
+      throws ClassNotFoundException, AxelorException {
+
+    if (metaModel == null) {
+      return null;
+    }
+    String model = metaModel.getFullName();
+    Model modelObject = null;
+
+    if (StringUtils.notEmpty(model)) {
+      Class<? extends Model> modelClass = (Class<? extends Model>) Class.forName(model);
+      modelObject = JPA.find(modelClass, Long.valueOf(referenceId));
+    }
+    try {
+      return Beans.get(TemplateMessageService.class).generateMessage(modelObject, template, true);
+    } catch (InstantiationException | IllegalAccessException | IOException e) {
+      TraceBackService.trace(e);
+    }
+    return null;
   }
 }
