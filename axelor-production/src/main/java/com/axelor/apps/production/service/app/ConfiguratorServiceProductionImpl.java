d561e25522e310d6ad61b222811671e306755c8a
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2021-12-08 16:38:16 +0100

    Configurator formula: add parentSaleOrderLineId
    This new variable will allow to get the id of the parent sale order when
    the configurator is used to add a sale order line.
    RM44189

--- axelor-production/src/main/java/com/axelor/apps/production/service/app/ConfiguratorServiceProductionImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/app/ConfiguratorServiceProductionImpl.java
@@ -67,14 +67,18 @@ public class ConfiguratorServiceProductionImpl extends ConfiguratorServiceImpl {
    * @param configurator
    * @param jsonAttributes
    * @param jsonIndicators
+   * @param saleOrderId
    * @throws AxelorException
    */
   @Override
   @Transactional(rollbackOn = {Exception.class})
-  public void generate(
-      Configurator configurator, JsonContext jsonAttributes, JsonContext jsonIndicators)
+  public void generateProduct(
+      Configurator configurator,
+      JsonContext jsonAttributes,
+      JsonContext jsonIndicators,
+      Long saleOrderId)
       throws AxelorException {
-    super.generate(configurator, jsonAttributes, jsonIndicators);
+    super.generateProduct(configurator, jsonAttributes, jsonIndicators, saleOrderId);
     ConfiguratorBOM configuratorBOM = configurator.getConfiguratorCreator().getConfiguratorBom();
     if (configuratorBOM != null) {
       Product generatedProduct = configurator.getProduct();
