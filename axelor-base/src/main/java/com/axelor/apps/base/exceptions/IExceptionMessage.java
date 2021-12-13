d1476f3eb7f44358a2b55cff50303abe8e61868c
Hassan KAPADIA - Axelor <h.kapadia@axelor.com>
2021-12-13 12:45:24 +0530

    Anomaly RM#42494: GEONAMES: Addition of errorLog

--- axelor-base/src/main/java/com/axelor/apps/base/exceptions/IExceptionMessage.java
+++ axelor-base/src/main/java/com/axelor/apps/base/exceptions/IExceptionMessage.java
@@ -438,4 +438,6 @@ public interface IExceptionMessage {
   public static final String INVALID_DATA_FILE_EXTENSION = /*$$(*/
       "Please upload csv or txt or zip files only." /*)*/;
   public static final String NO_TEXT_FILE_FOUND = /*$$(*/ "%s file not found in %s." /*)*/;
+  public static final String CITIES_IMPORT_FAILED = /*$$(*/
+      "Error: Cities cannot be imported. Please see the attached error file for more details" /*)*/;
 }
