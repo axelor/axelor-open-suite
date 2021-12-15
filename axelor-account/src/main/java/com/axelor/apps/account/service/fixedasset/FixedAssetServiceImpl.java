5368ff5f663e44090d36f77e9209f9e94ba3468f
Antoine Olivier <a.olivier@axelor.com>
2021-12-15 14:53:29 +0100

    spotlessApply

--- axelor-account/src/main/java/com/axelor/apps/account/service/fixedasset/FixedAssetServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/fixedasset/FixedAssetServiceImpl.java
@@ -50,7 +50,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
