55c5e2b1bfe5ad4400158da8db26130beb6f5f8c
pda-axelor <p.dave@axelor.com>
2022-01-06 11:19:12 +0530

    Feature RM#44371 PAYMENTSESSION : Create new object and views

---
+++ axelor-account/src/main/java/com/axelor/apps/account/service/PaymentSessionServiceImpl.java
@@ -0,0 +1,50 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2021 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.axelor.apps.account.service;
+
+import com.axelor.apps.account.db.PaymentSession;
+import com.axelor.auth.db.User;
+import com.axelor.common.ObjectUtils;
+import java.util.Locale;
+
+public class PaymentSessionServiceImpl implements PaymentSessionService {
+
+  @Override
+  public String computeName(PaymentSession paymentSession) {
+    StringBuilder name = new StringBuilder("Session");
+    User createdBy = paymentSession.getCreatedBy();
+    Boolean isFr =
+        ObjectUtils.notEmpty(createdBy)
+            && ObjectUtils.notEmpty(createdBy.getLanguage())
+            && createdBy.getLanguage().equals(Locale.FRENCH.getLanguage());
+    if (ObjectUtils.notEmpty(paymentSession.getPaymentMode())) {
+      name.append(" " + paymentSession.getPaymentMode().getName());
+    }
+    if (ObjectUtils.notEmpty(paymentSession.getCreatedOn())) {
+      name.append(
+          (isFr ? " du " : " on the ")
+              + paymentSession
+                  .getCreatedOn()
+                  .format(java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
+    }
+    if (ObjectUtils.notEmpty(createdBy)) {
+      name.append((isFr ? " par " : " by ") + createdBy.getName());
+    }
+    return name.toString();
+  }
+}
