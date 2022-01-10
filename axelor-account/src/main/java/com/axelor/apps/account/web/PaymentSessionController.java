da6c9f8f401d695a1d187647e26a5103b4935f30
alg-axelor <91078097+alg-axelor@users.noreply.github.com>
2022-01-10 15:18:00 +0100

    44597 : Payment session form : compute field values

--- axelor-account/src/main/java/com/axelor/apps/account/web/PaymentSessionController.java
+++ axelor-account/src/main/java/com/axelor/apps/account/web/PaymentSessionController.java
@@ -18,6 +18,7 @@
 package com.axelor.apps.account.web;
 
 import com.axelor.apps.account.db.AccountManagement;
+import com.axelor.apps.account.db.Journal;
 import com.axelor.apps.account.db.PaymentSession;
 import com.axelor.apps.base.db.BankDetails;
 import com.axelor.exception.service.TraceBackService;
@@ -30,26 +31,48 @@ import org.apache.commons.collections.CollectionUtils;
 @Singleton
 public class PaymentSessionController {
 
-  public void onChangeCompany(ActionRequest request, ActionResponse response) {
+  public void setBankDetails(ActionRequest request, ActionResponse response) {
     try {
       PaymentSession paymentSession = request.getContext().asType(PaymentSession.class);
-      if (paymentSession.getCompany() != null) {
-        if (paymentSession.getPaymentMode() != null
-            && CollectionUtils.isNotEmpty(
-                paymentSession.getPaymentMode().getAccountManagementList())) {
-          Optional<BankDetails> bankDetails =
-              paymentSession.getPaymentMode().getAccountManagementList().stream()
-                  .filter(
-                      accountManagement ->
-                          paymentSession.getCompany().equals(accountManagement.getCompany())
-                              && accountManagement.getBankDetails() != null)
-                  .map(AccountManagement::getBankDetails)
-                  .findFirst();
-          if (bankDetails.isPresent()) {
-            response.setValue("bankDetails", bankDetails.get());
-          }
+      if (paymentSession.getCompany() != null
+          && paymentSession.getPaymentMode() != null
+          && CollectionUtils.isNotEmpty(
+              paymentSession.getPaymentMode().getAccountManagementList())) {
+        Optional<BankDetails> bankDetails =
+            paymentSession.getPaymentMode().getAccountManagementList().stream()
+                .filter(
+                    accountManagement ->
+                        paymentSession.getCompany().equals(accountManagement.getCompany())
+                            && accountManagement.getBankDetails() != null)
+                .map(AccountManagement::getBankDetails)
+                .findFirst();
+        if (bankDetails.isPresent()) {
+          response.setValue("bankDetails", bankDetails.get());
         }
-        response.setValue("currency", paymentSession.getCompany().getCurrency());
+      }
+    } catch (Exception e) {
+      TraceBackService.trace(response, e);
+    }
+  }
+
+  public void setJournal(ActionRequest request, ActionResponse response) {
+    try {
+      PaymentSession paymentSession = request.getContext().asType(PaymentSession.class);
+      if (paymentSession.getCompany() != null
+          && paymentSession.getPaymentMode() != null
+          && CollectionUtils.isNotEmpty(
+              paymentSession.getPaymentMode().getAccountManagementList())) {
+        Optional<Journal> journal =
+            paymentSession.getPaymentMode().getAccountManagementList().stream()
+                .filter(
+                    accountManagement ->
+                        paymentSession.getCompany().equals(accountManagement.getCompany())
+                            && accountManagement.getJournal() != null)
+                .map(AccountManagement::getJournal)
+                .findFirst();
+        if (journal.isPresent()) {
+          response.setValue("journal", journal.get());
+        }
       }
     } catch (Exception e) {
       TraceBackService.trace(response, e);
