9e8c83e97aac0fe6353fab0fa9bdb4113974ba9f
ssb-axelor <s.sbitti@axelor.com>
2022-01-14 10:48:34 +0100

    RM45202 fix - add injection

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveSimulateServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveSimulateServiceImpl.java
@@ -3,17 +3,18 @@ package com.axelor.apps.account.service.move;
 import com.axelor.apps.account.db.Move;
 import com.axelor.apps.account.db.repo.MoveRepository;
 import com.axelor.exception.AxelorException;
-import com.axelor.inject.Beans;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 
 public class MoveSimulateServiceImpl implements MoveSimulateService {
 
   protected MoveValidateService moveValidateService;
+  protected MoveRepository moveRepository;
 
   @Inject
-  public MoveSimulateServiceImpl(MoveValidateService moveValidateService) {
+  public MoveSimulateServiceImpl(MoveValidateService moveValidateService, MoveRepository moveRepository) {
     this.moveValidateService = moveValidateService;
+    this.moveRepository = moveRepository;
   }
 
   @Override
@@ -21,6 +22,6 @@ public class MoveSimulateServiceImpl implements MoveSimulateService {
   public void simulate(Move move) throws AxelorException {
     moveValidateService.checkPreconditions(move);
     move.setStatusSelect(MoveRepository.STATUS_SIMULATED);
-    Beans.get(MoveRepository.class).save(move);
+    moveRepository.save(move);
   }
 }
