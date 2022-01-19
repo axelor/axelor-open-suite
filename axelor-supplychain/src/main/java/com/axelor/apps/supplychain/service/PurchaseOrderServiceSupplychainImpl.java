4b1467b317b59de2af1c1fe191a418b2b03a8796
hde-axelor <79825954+hde-axelor@users.noreply.github.com>
2022-01-19 15:33:59 +0530

    Anomaly RM#45423 : Budget : reset budget lines on PO cancel (#7334)
    (cherry picked from commit 008b0b3d70bd9afaa7aaf0e4ac1541aad417cd3a)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/PurchaseOrderServiceSupplychainImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/PurchaseOrderServiceSupplychainImpl.java
@@ -381,6 +381,11 @@ public class PurchaseOrderServiceSupplychainImpl extends PurchaseOrderServiceImp
 
     if (Beans.get(AppSupplychainService.class).isApp("supplychain")) {
       budgetSupplychainService.updateBudgetLinesFromPurchaseOrder(purchaseOrder);
+
+      if (purchaseOrder.getPurchaseOrderLineList() != null) {
+        purchaseOrder.getPurchaseOrderLineList().stream()
+            .forEach(poLine -> poLine.clearBudgetDistributionList());
+      }
     }
   }
 
