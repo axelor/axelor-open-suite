6f1d3b034c2014fc1dce0e8af71da6f41f74e739
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2022-01-24 18:06:25 +0100

    45333 management of dashboard and menu entry

--- axelor-cash-management/src/main/java/com/axelor/apps/cash/management/service/ChartServiceImpl.java
+++ axelor-cash-management/src/main/java/com/axelor/apps/cash/management/service/ChartServiceImpl.java
@@ -1,14 +1,5 @@
 package com.axelor.apps.cash.management.service;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.inject.Inject;
-
 import com.axelor.apps.base.db.BankDetails;
 import com.axelor.apps.cash.management.db.ForecastRecap;
 import com.axelor.apps.cash.management.db.ForecastRecapLine;
@@ -16,72 +7,80 @@ import com.axelor.apps.cash.management.db.repo.ForecastRecapLineRepository;
 import com.axelor.apps.cash.management.db.repo.ForecastRecapRepository;
 import com.axelor.apps.tool.date.DateTool;
 import com.axelor.auth.db.User;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Inject;
 
-public class ChartServiceImpl implements ChartService{
+public class ChartServiceImpl implements ChartService {
 
-	protected ForecastRecapRepository forecastRepo;
-	protected ForecastRecapLineRepository forecastLineRepo;
-	  
-	  @Inject 
-	  public ChartServiceImpl(ForecastRecapRepository forecastRepo, ForecastRecapLineRepository forecastLineRepo) {
-		this.forecastRepo = forecastRepo;
-		this.forecastLineRepo = forecastLineRepo;
-	}
+  protected ForecastRecapRepository forecastRepo;
+  protected ForecastRecapLineRepository forecastLineRepo;
 
-	  @Override
-	  public List<Map<String, Object>> getCashBalanceData(User user, BankDetails bankDetails) {
-	    List<Map<String, Object>> dataList = new ArrayList<>();
-	    ForecastRecap recap =
-	        forecastRepo
-	            .all()
-	            .filter(
-	                "self.isUseForReport = true AND (self.user = :user OR :user is null) AND (:bankDetails is null OR :bankDetails MEMBER OF self.bankDetailsSet)")
-	            .bind("user", user)
-	            .bind("bankDetails", bankDetails)
-	            .fetchOne();
+  @Inject
+  public ChartServiceImpl(
+      ForecastRecapRepository forecastRepo, ForecastRecapLineRepository forecastLineRepo) {
+    this.forecastRepo = forecastRepo;
+    this.forecastLineRepo = forecastLineRepo;
+  }
 
-	    if (recap == null) {
-	      return dataList;
-	    }
+  @Override
+  public List<Map<String, Object>> getCashBalanceData(User user, BankDetails bankDetails) {
+    List<Map<String, Object>> dataList = new ArrayList<>();
+    ForecastRecap recap =
+        forecastRepo
+            .all()
+            .filter(
+                "self.isReport = true AND (self.userRecap = :user OR :user is null) AND (:bankDetails is null OR :bankDetails = self.bankDetails)")
+            .bind("user", user)
+            .bind("bankDetails", bankDetails)
+            .fetchOne();
 
-	    List<ForecastRecapLine> recapLineList =
-	        forecastLineRepo
-	            .all()
-	            .filter("self.estimatedDate is not null AND self.forecastRecap = ?1", recap)
-	            .fetch();
+    if (recap == null) {
+      return dataList;
+    }
 
-	    LocalDate startDate = recap.getFromDate();
-	    LocalDate toDate = recap.getToDate();
-	    final String keyWeek = "week";
-	    final String keyBalance = "balance";
+    List<ForecastRecapLine> recapLineList =
+        forecastLineRepo
+            .all()
+            .filter("self.estimatedDate is not null AND self.forecastRecap = ?1", recap)
+            .fetch();
 
-	    int i = 1;
+    LocalDate startDate = recap.getFromDate();
+    LocalDate toDate = recap.getToDate();
+    final String keyWeek = "week";
+    final String keyBalance = "balance";
 
-	    Map<String, Object> firstEntry = new HashMap<>();
-	    firstEntry.put(keyWeek, i++);
-	    firstEntry.put(keyBalance, recap.getStartingBalance());
-	    dataList.add(firstEntry);
+    int i = 1;
 
-	    LocalDate date = startDate;
-	    while (date.isBefore(toDate)) {
-	      Map<String, Object> entry = new HashMap<>();
-	      entry.put(keyWeek, i++);
-	      entry.put(keyBalance, getRecapLinesTotal(recap, recapLineList, startDate, date.plusWeeks(1)));
-	      dataList.add(entry);
-	      date = date.plusWeeks(1);
-	    }
+    Map<String, Object> firstEntry = new HashMap<>();
+    firstEntry.put(keyWeek, i++);
+    firstEntry.put(keyBalance, recap.getStartingBalance());
+    dataList.add(firstEntry);
 
-	    return dataList;
-	  }
-	
-	  protected BigDecimal getRecapLinesTotal(
-		      ForecastRecap recap,
-		      List<ForecastRecapLine> recapLineList,
-		      LocalDate fromDate,
-		      LocalDate toDate) {
-		    return recapLineList.stream()
-		        .filter(line -> DateTool.isBetween(fromDate, toDate, line.getEstimatedDate()))
-		        .map(ForecastRecapLine::getAmount)
-		        .reduce(recap.getStartingBalance(), BigDecimal::add);
-		  }
+    LocalDate date = startDate;
+    while (date.isBefore(toDate)) {
+      Map<String, Object> entry = new HashMap<>();
+      entry.put(keyWeek, i++);
+      entry.put(keyBalance, getRecapLinesTotal(recap, recapLineList, startDate, date.plusWeeks(1)));
+      dataList.add(entry);
+      date = date.plusWeeks(1);
+    }
+
+    return dataList;
+  }
+
+  protected BigDecimal getRecapLinesTotal(
+      ForecastRecap recap,
+      List<ForecastRecapLine> recapLineList,
+      LocalDate fromDate,
+      LocalDate toDate) {
+    return recapLineList.stream()
+        .filter(line -> DateTool.isBetween(fromDate, toDate, line.getEstimatedDate()))
+        .map(ForecastRecapLine::getAmount)
+        .reduce(recap.getStartingBalance(), BigDecimal::add);
+  }
 }
