32d2db0996fc22b2db270f8b6e5517cbd946366c
David Trochel <d.trochel@axelor.com>
2022-02-10 11:14:17 +0100

    Anomaly #46312 Lambdas should be replaced with method references
    Signed-off-by: David Trochel <d.trochel@axelor.com>

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/SaleOrderStockServiceImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/SaleOrderStockServiceImpl.java
@@ -51,6 +51,7 @@ import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
@@ -59,6 +60,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -132,7 +134,7 @@ public class SaleOrderStockServiceImpl implements SaleOrderStockService {
 
     for (LocalDate estimatedDeliveryDate :
         saleOrderLinePerDateMap.keySet().stream()
-            .filter(x -> x != null)
+            .filter(Objects::nonNull)
             .sorted((x, y) -> x.compareTo(y))
             .collect(Collectors.toList())) {
 
