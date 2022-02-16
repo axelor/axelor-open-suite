4e15eb425256f97db07b77faa917b5af9ee5006c
bsu-axelor <b.suvagiya@axelor.com>
2022-02-16 12:19:11 +0530

    Anomaly #46214 : [PROD] Issue on consumed products from production order

--- axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderServiceImpl.java
@@ -301,12 +301,6 @@ public class ManufOrderServiceImpl implements ManufOrderService {
       }
     }
 
-    if (!manufOrder.getIsConsProOnOperation()) {
-      this.createToConsumeProdProductList(manufOrder);
-    }
-
-    this.createToProduceProdProductList(manufOrder);
-
     return manufOrder;
   }
 
