be2a48d792728215c901b8ac277617c66c908c49
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2022-03-28 16:54:59 +0200

    remove supplierInvoiceNb and originDate from the copy when duplicating an invoice

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceToolService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceToolService.java
@@ -243,5 +243,7 @@ public class InvoiceToolService {
     copy.setDecisionPfpTakenDate(null);
     copy.setInternalReference(null);
     copy.setExternalReference(null);
+    copy.setOriginDate(null);
+    copy.setSupplierInvoiceNb(null);
   }
 }
