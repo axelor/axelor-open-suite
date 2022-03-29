d21f7945f7ae6da14dab75a927d9545b4462dacc
Edwin Roger-Gorlin <e.roger-gorlin@axelor.com>
2022-03-29 13:50:30 +0200

    Added status filter

--- axelor-production/src/main/java/com/axelor/apps/production/service/BillOfMaterialServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/BillOfMaterialServiceImpl.java
@@ -443,10 +443,11 @@ public class BillOfMaterialServiceImpl implements BillOfMaterialService {
     return billOfMaterialRepo
         .all()
         .filter(
-            "self.product = ?1 AND self.company = ?2 AND self.id != ?3",
+            "self.product = ?1 AND self.company = ?2 AND self.id != ?3 AND self.statusSelect = ?4",
             originalProduct,
             company,
-            defaultBOM != null ? defaultBOM.getId() : 0)
+            defaultBOM != null ? defaultBOM.getId() : 0,
+            BillOfMaterialRepository.STATUS_APPLICABLE)
         .fetch();
   }
 }
