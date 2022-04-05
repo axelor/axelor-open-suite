ebfe64dee22fd869ed1ed353b3433afec775ab25
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2022-04-05 10:24:05 +0200

    add javadoc

--- axelor-quality/src/main/java/com/axelor/apps/quality/service/ControlPointWorkflowService.java
+++ axelor-quality/src/main/java/com/axelor/apps/quality/service/ControlPointWorkflowService.java
@@ -5,5 +5,11 @@ import com.axelor.exception.AxelorException;
 
 public interface ControlPointWorkflowService {
 
+  /**
+   * Set the control point status to closed.
+   *
+   * @param controlPoint
+   * @throws AxelorException if the control point wasn't on hold.
+   */
   void closeControlPoint(ControlPoint controlPoint) throws AxelorException;
 }
