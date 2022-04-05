4119e4ae7fe6d92664c4b2b0cc7d21b6a919afde
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2022-04-05 10:14:33 +0200

    add javadoc

--- axelor-production/src/main/java/com/axelor/apps/production/service/MrpForecastService.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/MrpForecastService.java
@@ -5,7 +5,19 @@ import com.axelor.exception.AxelorException;
 
 public interface MrpForecastService {
 
+  /**
+   * Set the forecast status to confirmed.
+   *
+   * @param mrpForecast
+   * @throws AxelorException if the forecast wasn't drafted.
+   */
   void confirm(MrpForecast mrpForecast) throws AxelorException;
 
+  /**
+   * Set the forecast status to canceled.
+   *
+   * @param mrpForecast
+   * @throws AxelorException if the forecast wasn't drafted nor canceled.
+   */
   void cancel(MrpForecast mrpForecast) throws AxelorException;
 }
