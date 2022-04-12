7e5fb10846501dfa95190be101f364cc0941031f
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2022-04-12 14:19:51 +0200

    Anomaly #47389 : Checking before changing status : Stock Management (#7723)

---
+++ axelor-production/src/main/java/com/axelor/apps/production/service/MrpForecastService.java
@@ -0,0 +1,23 @@
+package com.axelor.apps.production.service;
+
+import com.axelor.apps.supplychain.db.MrpForecast;
+import com.axelor.exception.AxelorException;
+
+public interface MrpForecastService {
+
+  /**
+   * Set the forecast status to confirmed.
+   *
+   * @param mrpForecast
+   * @throws AxelorException if the forecast wasn't drafted.
+   */
+  void confirm(MrpForecast mrpForecast) throws AxelorException;
+
+  /**
+   * Set the forecast status to canceled.
+   *
+   * @param mrpForecast
+   * @throws AxelorException if the forecast wasn't drafted nor canceled.
+   */
+  void cancel(MrpForecast mrpForecast) throws AxelorException;
+}
---
+++ axelor-production/src/main/java/com/axelor/apps/production/service/MrpForecastServiceImpl.java
@@ -0,0 +1,41 @@
+package com.axelor.apps.production.service;
+
+import com.axelor.apps.production.exceptions.IExceptionMessage;
+import com.axelor.apps.supplychain.db.MrpForecast;
+import com.axelor.apps.supplychain.db.repo.MrpForecastRepository;
+import com.axelor.exception.AxelorException;
+import com.axelor.exception.db.repo.TraceBackRepository;
+import com.axelor.i18n.I18n;
+import com.google.inject.persist.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MrpForecastServiceImpl implements MrpForecastService {
+
+  @Transactional(rollbackOn = {Exception.class})
+  @Override
+  public void confirm(MrpForecast mrpForecast) throws AxelorException {
+    if (mrpForecast.getStatusSelect() == null
+        || mrpForecast.getStatusSelect() != MrpForecastRepository.STATUS_DRAFT) {
+      throw new AxelorException(
+          TraceBackRepository.CATEGORY_INCONSISTENCY,
+          I18n.get(IExceptionMessage.MRP_FORECAST_CONFIRM_WRONG_STATUS));
+    }
+    mrpForecast.setStatusSelect(MrpForecastRepository.STATUS_CONFIRMED);
+  }
+
+  @Transactional(rollbackOn = {Exception.class})
+  @Override
+  public void cancel(MrpForecast mrpForecast) throws AxelorException {
+    List<Integer> authorizedStatus = new ArrayList<>();
+    authorizedStatus.add(MrpForecastRepository.STATUS_DRAFT);
+    authorizedStatus.add(MrpForecastRepository.STATUS_CONFIRMED);
+    if (mrpForecast.getStatusSelect() == null
+        || !authorizedStatus.contains(mrpForecast.getStatusSelect())) {
+      throw new AxelorException(
+          TraceBackRepository.CATEGORY_INCONSISTENCY,
+          I18n.get(IExceptionMessage.MRP_FORECAST_CANCEL_WRONG_STATUS));
+    }
+    mrpForecast.setStatusSelect(MrpForecastRepository.STATUS_CANCELLED);
+  }
+}
