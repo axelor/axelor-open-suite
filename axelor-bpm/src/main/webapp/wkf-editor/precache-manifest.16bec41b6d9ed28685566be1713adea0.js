9aae65b33ee49ff7850689efbea2f8baaefed04e
dde-axelor <d.deka@axelor.com>
2022-04-14 11:04:39 +0530

    47187:BPM label translation

--- axelor-bpm/src/main/webapp/wkf-editor/precache-manifest.16bec41b6d9ed28685566be1713adea0.js
+++ axelor-bpm/src/main/webapp/wkf-editor/precache-manifest.16bec41b6d9ed28685566be1713adea0.js
@@ -1,23 +1,6 @@
-/*
- * Axelor Business Solutions
- *
- * Copyright (C) 2022 Axelor (<http://axelor.com>).
- *
- * This program is free software: you can redistribute it and/or  modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "09547a83b5234e952105d06aa502ed95",
+    "revision": "da37d7d0eea832277d0d472fd185b26e",
     "url": "./index.html"
   },
   {
@@ -29,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/3.24a5c644.chunk.css"
   },
   {
-    "revision": "2dc1323d103786528489",
+    "revision": "6f923bcc65002d79add8",
     "url": "./static/css/main.4e427609.chunk.css"
   },
   {
@@ -45,8 +28,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/3.ef50a943.chunk.js"
   },
   {
-    "revision": "2dc1323d103786528489",
-    "url": "./static/js/main.8ef6e8e0.chunk.js"
+    "revision": "6f923bcc65002d79add8",
+    "url": "./static/js/main.976b951a.chunk.js"
   },
   {
     "revision": "f9b12699d006eb3e0095",
