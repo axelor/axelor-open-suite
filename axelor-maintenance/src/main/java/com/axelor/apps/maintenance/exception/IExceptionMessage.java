ce4b035e723714d542752b2773f9b8ddfc23ebf6
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2022-04-29 10:56:21 +0200

    Maintenance: Add clear error message for missing sequence in equipement maintenance (#7967)
    RM48695

---
+++ axelor-maintenance/src/main/java/com/axelor/apps/maintenance/exception/IExceptionMessage.java
@@ -0,0 +1,7 @@
+package com.axelor.apps.maintenance.exception;
+
+/** Interface of Exceptions. Enum all exception of axelor-maintenance. */
+public interface IExceptionMessage {
+  static final String EQUIPEMENT_MAINTENANCE_MISSING_SEQUENCE = /*$$(*/
+      "Missing equipement maintenance sequence." /*)*/;
+}
