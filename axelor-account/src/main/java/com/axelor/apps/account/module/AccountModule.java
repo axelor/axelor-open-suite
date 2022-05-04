d7f9f449da4236126128ce180f9456831531a522
fpr-axelor <f.pruneta@axelor.com>
2022-05-04 14:26:14 +0200

    Revert "47925 Sequence - need only one sequence for fixed asset by company (#7831)" (#8025)
    This reverts commit e827642bff4b00f2b07f24d2b455fa28e00e10b7.

--- axelor-account/src/main/java/com/axelor/apps/account/module/AccountModule.java
+++ axelor-account/src/main/java/com/axelor/apps/account/module/AccountModule.java
@@ -57,7 +57,6 @@ import com.axelor.apps.account.db.repo.ReconcileGroupAccountRepository;
 import com.axelor.apps.account.db.repo.ReconcileGroupRepository;
 import com.axelor.apps.account.db.repo.ReconcileManagementRepository;
 import com.axelor.apps.account.db.repo.ReconcileRepository;
-import com.axelor.apps.account.db.repo.SequenceAccountManagementRepository;
 import com.axelor.apps.account.db.repo.SubrogationReleaseManagementRepository;
 import com.axelor.apps.account.db.repo.SubrogationReleaseRepository;
 import com.axelor.apps.account.service.AccountManagementAccountService;
@@ -237,7 +236,6 @@ import com.axelor.apps.account.service.umr.UmrNumberServiceImpl;
 import com.axelor.apps.base.db.repo.PartnerAddressRepository;
 import com.axelor.apps.base.db.repo.PartnerBaseRepository;
 import com.axelor.apps.base.db.repo.PeriodRepository;
-import com.axelor.apps.base.db.repo.SequenceBaseRepository;
 import com.axelor.apps.base.service.BankDetailsServiceImpl;
 import com.axelor.apps.base.service.PeriodServiceImpl;
 import com.axelor.apps.base.service.tax.AccountManagementServiceImpl;
@@ -479,7 +477,5 @@ public class AccountModule extends AxelorModule {
     bind(AnalyticAccountService.class).to(AnalyticAccountServiceImpl.class);
 
     bind(NoteBillsCreateService.class).to(NoteBillsCreateServiceImpl.class);
-
-    bind(SequenceBaseRepository.class).to(SequenceAccountManagementRepository.class);
   }
 }
