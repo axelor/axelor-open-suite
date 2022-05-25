a0f306a0d69c937f21c10c8dccc74b6cc36b910a
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2022-05-25 11:55:02 +0200

    Fix circular depency injection in MO
    Happened when planning a manuf order, caused by a4b1ce9fc
    RM50021
    
    (cherry picked from commit be3d3640faf9bc451c1f07d727d00a85b83bc142)
    (cherry picked from commit 044f0f2b11fdc049f507fb5df2bf4679ff453e64)

--- axelor-production/src/main/java/com/axelor/apps/production/service/StockMoveProductionServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/StockMoveProductionServiceImpl.java
@@ -22,7 +22,6 @@ import com.axelor.apps.base.service.UnitConversionService;
 import com.axelor.apps.base.service.app.AppBaseService;
 import com.axelor.apps.purchase.db.repo.PurchaseOrderRepository;
 import com.axelor.apps.sale.db.repo.SaleOrderRepository;
-import com.axelor.apps.sale.service.saleorder.SaleOrderWorkflowService;
 import com.axelor.apps.stock.db.StockMove;
 import com.axelor.apps.stock.db.repo.StockMoveLineRepository;
 import com.axelor.apps.stock.db.repo.StockMoveRepository;
@@ -51,8 +50,7 @@ public class StockMoveProductionServiceImpl extends StockMoveServiceSupplychainI
       SaleOrderRepository saleOrderRepo,
       UnitConversionService unitConversionService,
       ReservedQtyService reservedQtyService,
-      ProductRepository productRepository,
-      SaleOrderWorkflowService saleOrderWorkflowService) {
+      ProductRepository productRepository) {
     super(
         stockMoveLineService,
         stockMoveToolService,
@@ -65,8 +63,7 @@ public class StockMoveProductionServiceImpl extends StockMoveServiceSupplychainI
         saleOrderRepo,
         unitConversionService,
         reservedQtyService,
-        productRepository,
-        saleOrderWorkflowService);
+        productRepository);
   }
 
   @Override
