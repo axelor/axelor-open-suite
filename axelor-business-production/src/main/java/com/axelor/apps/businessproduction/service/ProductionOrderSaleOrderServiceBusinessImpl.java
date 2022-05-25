axelor/axelor-open-suite

commit a4b1ce9fcdfc231f10b27b97741f601d5d4a9d56

Anomaly #49772 : Invoice: ventilation impossible if the SO is already completed (#8196)
* RM#49772 Fix invoice ventilation if SO was completed

* RM#49772 call SaleOrderWorkflowService interface instead of implementation, update terminateorConfimSaleOrderStatus method

axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockMoveServiceSupplychainImpl.java
@@ -30,6 +30,7 @@ import com.axelor.apps.sale.db.SaleOrder;
 import com.axelor.apps.sale.db.SaleOrderLine;
 import com.axelor.apps.sale.db.repo.SaleOrderLineRepository;
 import com.axelor.apps.sale.db.repo.SaleOrderRepository;
+import com.axelor.apps.sale.service.saleorder.SaleOrderWorkflowService;
 import com.axelor.apps.stock.db.StockMove;
 import com.axelor.apps.stock.db.StockMoveLine;
 import com.axelor.apps.stock.db.TrackingNumber;
@@ -75,6 +76,7 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
   protected UnitConversionService unitConversionService;
   protected ReservedQtyService reservedQtyService;
   protected PartnerSupplychainService partnerSupplychainService;
+  protected SaleOrderWorkflowService saleOrderWorkflowService;
 
   @Inject private StockMoveLineServiceSupplychain stockMoveLineServiceSupplychain;
 
@@ -92,7 +94,8 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
       UnitConversionService unitConversionService,
       ReservedQtyService reservedQtyService,
       ProductRepository productRepository,
-      PartnerSupplychainService partnerSupplychainService) {
+      PartnerSupplychainService partnerSupplychainService,
+      SaleOrderWorkflowService saleOrderWorkflowService) {
     super(
         stockMoveLineService,
         stockMoveToolService,
@@ -107,6 +110,7 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
     this.unitConversionService = unitConversionService;
     this.reservedQtyService = reservedQtyService;
     this.partnerSupplychainService = partnerSupplychainService;
+    this.saleOrderWorkflowService = saleOrderWorkflowService;
   }
 
   @Override
@@ -249,11 +253,12 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
    *
    * @param saleOrder
    */
-  protected void terminateOrConfirmSaleOrderStatus(SaleOrder saleOrder) {
-    if (saleOrder.getDeliveryState() == SaleOrderRepository.DELIVERY_STATE_DELIVERED) {
-      saleOrder.setStatusSelect(SaleOrderRepository.STATUS_ORDER_COMPLETED);
-    } else {
-      saleOrder.setStatusSelect(SaleOrderRepository.STATUS_ORDER_CONFIRMED);
+  protected void terminateOrConfirmSaleOrderStatus(SaleOrder saleOrder) throws AxelorException {
+    if (saleOrder.getDeliveryState() == SaleOrderRepository.DELIVERY_STATE_DELIVERED
+        && saleOrder.getStatusSelect() == SaleOrderRepository.STATUS_ORDER_CONFIRMED) {
+      saleOrderWorkflowService.completeSaleOrder(saleOrder);
+    } else if (saleOrder.getStatusSelect() == SaleOrderRepository.STATUS_FINALIZED_QUOTATION) {
+      saleOrderWorkflowService.confirmSaleOrder(saleOrder);
     }
   }
 

axelor-sale/src/main/java/com/axelor/apps/sale/service/saleorder/SaleOrderWorkflowService.java
@@ -27,19 +27,20 @@ import com.google.inject.persist.Transactional;
 public interface SaleOrderWorkflowService {
 
   @Transactional
-  public Partner validateCustomer(SaleOrder saleOrder);
+  Partner validateCustomer(SaleOrder saleOrder);
 
-  public String getSequence(Company company) throws AxelorException;
+  String getSequence(Company company) throws AxelorException;
 
-  public void cancelSaleOrder(
-      SaleOrder saleOrder, CancelReason cancelReason, String cancelReasonStr)
+  void cancelSaleOrder(SaleOrder saleOrder, CancelReason cancelReason, String cancelReasonStr)
       throws AxelorException;
 
-  public void finalizeQuotation(SaleOrder saleOrder) throws AxelorException;
+  void finalizeQuotation(SaleOrder saleOrder) throws AxelorException;
 
-  public void confirmSaleOrder(SaleOrder saleOrder) throws AxelorException;
+  void confirmSaleOrder(SaleOrder saleOrder) throws AxelorException;
 
-  public void saveSaleOrderPDFAsAttachment(SaleOrder saleOrder) throws AxelorException;
+  void saveSaleOrderPDFAsAttachment(SaleOrder saleOrder) throws AxelorException;
 
-  public String getFileName(SaleOrder saleOrder);
+  String getFileName(SaleOrder saleOrder);
+
+  void completeSaleOrder(SaleOrder saleOrder) throws AxelorException;
 }

axelor-sale/src/main/java/com/axelor/apps/sale/web/SaleOrderController.java
@@ -48,7 +48,6 @@ import com.axelor.apps.sale.service.saleorder.SaleOrderLineService;
 import com.axelor.apps.sale.service.saleorder.SaleOrderMarginService;
 import com.axelor.apps.sale.service.saleorder.SaleOrderService;
 import com.axelor.apps.sale.service.saleorder.SaleOrderWorkflowService;
-import com.axelor.apps.sale.service.saleorder.SaleOrderWorkflowServiceImpl;
 import com.axelor.apps.sale.service.saleorder.print.SaleOrderPrintService;
 import com.axelor.apps.tool.StringTool;
 import com.axelor.common.ObjectUtils;
@@ -232,7 +231,7 @@ public class SaleOrderController {
     saleOrder = Beans.get(SaleOrderRepository.class).find(saleOrder.getId());
 
     try {
-      Beans.get(SaleOrderWorkflowServiceImpl.class).completeSaleOrder(saleOrder);
+      Beans.get(SaleOrderWorkflowService.class).completeSaleOrder(saleOrder);
     } catch (Exception e) {
       TraceBackService.trace(response, e);
     }

axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/SaleOrderInvoiceServiceImpl.java
@@ -38,7 +38,7 @@ import com.axelor.apps.sale.db.SaleOrderLineTax;
 import com.axelor.apps.sale.db.repo.SaleOrderRepository;
 import com.axelor.apps.sale.service.saleorder.SaleOrderComputeService;
 import com.axelor.apps.sale.service.saleorder.SaleOrderLineService;
-import com.axelor.apps.sale.service.saleorder.SaleOrderWorkflowServiceImpl;
+import com.axelor.apps.sale.service.saleorder.SaleOrderWorkflowService;
 import com.axelor.apps.stock.db.repo.StockMoveRepository;
 import com.axelor.apps.supplychain.db.Timetable;
 import com.axelor.apps.supplychain.db.repo.TimetableRepository;
@@ -86,7 +86,7 @@ public class SaleOrderInvoiceServiceImpl implements SaleOrderInvoiceService {
 
   protected StockMoveRepository stockMoveRepository;
 
-  protected SaleOrderWorkflowServiceImpl saleOrderWorkflowServiceImpl;
+  protected SaleOrderWorkflowService saleOrderWorkflowService;
 
   protected CommonInvoiceService commonInvoiceService;
 
@@ -99,7 +99,7 @@ public class SaleOrderInvoiceServiceImpl implements SaleOrderInvoiceService {
       InvoiceServiceSupplychainImpl invoiceService,
       SaleOrderLineService saleOrderLineService,
       StockMoveRepository stockMoveRepository,
-      SaleOrderWorkflowServiceImpl saleOrderWorkflowServiceImpl,
+      SaleOrderWorkflowService saleOrderWorkflowService,
       CommonInvoiceService commonInvoiceService) {
 
     this.appBaseService = appBaseService;
@@ -109,7 +109,7 @@ public class SaleOrderInvoiceServiceImpl implements SaleOrderInvoiceService {
     this.invoiceService = invoiceService;
     this.stockMoveRepository = stockMoveRepository;
     this.saleOrderLineService = saleOrderLineService;
-    this.saleOrderWorkflowServiceImpl = saleOrderWorkflowServiceImpl;
+    this.saleOrderWorkflowService = saleOrderWorkflowService;
     this.commonInvoiceService = commonInvoiceService;
   }
 
@@ -670,8 +670,9 @@ public class SaleOrderInvoiceServiceImpl implements SaleOrderInvoiceService {
     saleOrder.setAmountInvoiced(amountInvoiced);
 
     if (appSupplychainService.getAppSupplychain().getCompleteSaleOrderOnInvoicing()
-        && amountInvoiced.compareTo(saleOrder.getExTaxTotal()) == 0) {
-      saleOrderWorkflowServiceImpl.completeSaleOrder(saleOrder);
+        && amountInvoiced.compareTo(saleOrder.getExTaxTotal()) == 0
+        && saleOrder.getStatusSelect() == SaleOrderRepository.STATUS_ORDER_CONFIRMED) {
+      saleOrderWorkflowService.completeSaleOrder(saleOrder);
     }
   }
 

commit df9808ff1b64af27fa88e01b81f2187dd09f7083

Fix circular depency injection in MO
Happened when planning a manuf order, caused by a4b1ce9fc
RM50021

axelor-business-production/src/main/java/com/axelor/apps/businessproduction/service/ProductionOrderSaleOrderServiceBusinessImpl.java
@@ -23,32 +23,20 @@ import com.axelor.apps.production.db.repo.ProductionOrderRepository;
 import com.axelor.apps.production.service.app.AppProductionService;
 import com.axelor.apps.production.service.productionorder.ProductionOrderSaleOrderServiceImpl;
 import com.axelor.apps.production.service.productionorder.ProductionOrderService;
-import com.axelor.apps.project.db.Project;
 import com.axelor.apps.sale.db.SaleOrder;
 import com.axelor.exception.AxelorException;
 import com.google.inject.Inject;
-import com.google.inject.persist.Transactional;
-import java.lang.invoke.MethodHandles;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ProductionOrderSaleOrderServiceBusinessImpl
     extends ProductionOrderSaleOrderServiceImpl {
-
-  private final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-  protected ProductionOrderServiceBusinessImpl productionOrderServiceBusinessImpl;
 
   @Inject
   public ProductionOrderSaleOrderServiceBusinessImpl(
       UnitConversionService unitConversionService,
       ProductionOrderService productionOrderService,
       ProductionOrderRepository productionOrderRepo,
-      ProductionOrderServiceBusinessImpl productionOrderServiceBusinessImpl,
       AppProductionService appProductionService) {
     super(unitConversionService, productionOrderService, productionOrderRepo, appProductionService);
-
-    this.productionOrderServiceBusinessImpl = productionOrderServiceBusinessImpl;
   }
 
   @Override
@@ -61,51 +49,5 @@ public class ProductionOrderSaleOrderServiceBusinessImpl
       productionOrder.setProject(saleOrder.getProject());
     }
     return productionOrder;
-  }
-
-  @Transactional(rollbackOn = {Exception.class})
-  public void createSaleOrder(ProductionOrder productionOrder) throws AxelorException {
-
-    logger.debug(
-        "Création d'un devis client pour l'ordre de production : {}",
-        new Object[] {productionOrder.getProductionOrderSeq()});
-
-    Project project = productionOrder.getProject();
-
-    project.getClientPartner();
-
-    //		if(businessFolder.getCompany() != null)  {
-    //
-    //			SaleOrder saleOrder = saleOrderServiceStockImpl.createSaleOrder(
-    //					businessFolder,
-    //					user,
-    //					businessFolder.getCompany(),
-    //					null,
-    //					partner.getCurrency(),
-    //					null,
-    //					null,
-    //					null,
-    //					saleOrderServiceStockImpl.getLocation(businessProject.getCompany()),
-    //					today,
-    //					Beans.get(PriceListRepository.class).all().filter("self.partner = ?1 AND self.typeSelect
-    // = 1", partner).fetchOne(),
-    //					partner);
-    //
-    //			Beans.get(SaleOrderRepository.class).save(saleOrder);
-    //
-    //		}
-    //
-    //		//TODO
-    //
-    //		for(SaleOrderLine saleOrderLine : saleOrderLineList)  {
-    //
-    //
-    //	purchaseOrder.addPurchaseOrderLineListItem(purchaseOrderLineService.createPurchaseOrderLine(purchaseOrder, saleOrderLine));
-    //
-    //		}
-    //
-    //		purchaseOrderService.computePurchaseOrder(purchaseOrder);
-    //
-    //		purchaseOrder.save();
   }
 }

axelor-business-production/src/main/java/com/axelor/apps/businessproduction/web/ProductionOrderBusinessController.java
@@ -1,42 +1,0 @@
-/*
- * Axelor Business Solutions
- *
- * Copyright (C) 2022 Axelor (<http://axelor.com>).
- *
- * This program is free software: you can redistribute it and/or  modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-package com.axelor.apps.businessproduction.web;
-
-import com.axelor.apps.businessproduction.service.ProductionOrderSaleOrderServiceBusinessImpl;
-import com.axelor.apps.production.db.ProductionOrder;
-import com.axelor.apps.production.db.repo.ProductionOrderRepository;
-import com.axelor.exception.AxelorException;
-import com.axelor.inject.Beans;
-import com.axelor.rpc.ActionRequest;
-import com.axelor.rpc.ActionResponse;
-import com.google.inject.Singleton;
-
-@Singleton
-public class ProductionOrderBusinessController {
-
-  public void generateSaleOrder(ActionRequest request, ActionResponse response)
-      throws AxelorException {
-
-    ProductionOrder productionOrder = request.getContext().asType(ProductionOrder.class);
-
-    Beans.get(ProductionOrderSaleOrderServiceBusinessImpl.class)
-        .createSaleOrder(Beans.get(ProductionOrderRepository.class).find(productionOrder.getId()));
-
-    response.setReload(true);
-  }
-}

axelor-business-production/src/main/resources/views/ProductionOrder.xml
@@ -1,11 +1,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<object-views xmlns="http://axelor.com/xml/ns/object-views"
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:schemaLocation="http://axelor.com/xml/ns/object-views http://axelor.com/xml/ns/object-views/object-views_5.4.xsd">
-
-  <action-method name="action-production-order-method-generate-sale-order">
-    <call class="com.axelor.apps.businessproduction.web.ProductionOrderBusinessController"
-      method="generateSaleOrder"/>
-  </action-method>
-
-</object-views>

axelor-production/src/main/resources/views/ProductionOrder.xml
@@ -54,8 +54,6 @@
         grid-view="project-grid"
         if=" __config__.app.getApp('production').getManageBusinessProduction()"/>
     </panel>
-    <!-- <button name="generateSaleOrder" title="Generate sale order" colSpan="2" onClick="action-production-order-method-generate-sale-order"
-      showIf="businessProject"/> -->
     <panel-tabs name="mainPanelTab">
       <panel-related name="manufOrderListPanel" field="manufOrderSet" colSpan="12"
         edit-window="blank" canNew="false" form-view="manuf-order-form">

axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockMoveServiceSupplychainImpl.java
@@ -94,8 +94,7 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
       UnitConversionService unitConversionService,
       ReservedQtyService reservedQtyService,
       ProductRepository productRepository,
-      PartnerSupplychainService partnerSupplychainService,
-      SaleOrderWorkflowService saleOrderWorkflowService) {
+      PartnerSupplychainService partnerSupplychainService) {
     super(
         stockMoveLineService,
         stockMoveToolService,
@@ -110,7 +109,6 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
     this.unitConversionService = unitConversionService;
     this.reservedQtyService = reservedQtyService;
     this.partnerSupplychainService = partnerSupplychainService;
-    this.saleOrderWorkflowService = saleOrderWorkflowService;
   }
 
   @Override
@@ -254,6 +252,8 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
    * @param saleOrder
    */
   protected void terminateOrConfirmSaleOrderStatus(SaleOrder saleOrder) throws AxelorException {
+    // have to use Beans.get because of circular dependency
+    SaleOrderWorkflowService saleOrderWorkflowService = Beans.get(SaleOrderWorkflowService.class);
     if (saleOrder.getDeliveryState() == SaleOrderRepository.DELIVERY_STATE_DELIVERED
         && saleOrder.getStatusSelect() == SaleOrderRepository.STATUS_ORDER_CONFIRMED) {
       saleOrderWorkflowService.completeSaleOrder(saleOrder);