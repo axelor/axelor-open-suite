044f0f2b11fdc049f507fb5df2bf4679ff453e64
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2022-05-25 11:55:02 +0200

    Fix circular depency injection in MO
    Happened when planning a manuf order, caused by a4b1ce9fc
    RM50021
    
    (cherry picked from commit be3d3640faf9bc451c1f07d727d00a85b83bc142)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockMoveServiceSupplychainImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockMoveServiceSupplychainImpl.java
@@ -92,8 +92,7 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
       SaleOrderRepository saleOrderRepo,
       UnitConversionService unitConversionService,
       ReservedQtyService reservedQtyService,
-      ProductRepository productRepository,
-      SaleOrderWorkflowService saleOrderWorkflowService) {
+      ProductRepository productRepository) {
     super(
         stockMoveLineService,
         stockMoveToolService,
@@ -107,7 +106,6 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
     this.saleOrderRepo = saleOrderRepo;
     this.unitConversionService = unitConversionService;
     this.reservedQtyService = reservedQtyService;
-    this.saleOrderWorkflowService = saleOrderWorkflowService;
   }
 
   @Override
@@ -243,6 +241,8 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
    * @param saleOrder
    */
   protected void terminateOrConfirmSaleOrderStatus(SaleOrder saleOrder) throws AxelorException {
+    // have to use Beans.get because of circular dependency
+    SaleOrderWorkflowService saleOrderWorkflowService = Beans.get(SaleOrderWorkflowService.class);
     if (saleOrder.getDeliveryState() == SaleOrderRepository.DELIVERY_STATE_DELIVERED
         && saleOrder.getStatusSelect() == SaleOrderRepository.STATUS_ORDER_CONFIRMED) {
       saleOrderWorkflowService.completeSaleOrder(saleOrder);
