e39610228fc4dc311c3828b7ec463fc9e415ee75
ilyas jaai <100699184+ija-axelor@users.noreply.github.com>
2022-05-27 11:59:40 +0100

    commit after spotlessApply

--- axelor-account/src/main/java/com/axelor/apps/account/web/MoveController.java
+++ axelor-account/src/main/java/com/axelor/apps/account/web/MoveController.java
@@ -32,7 +32,6 @@ import com.axelor.apps.account.service.config.AccountConfigService;
 import com.axelor.apps.account.service.extract.ExtractContextMoveService;
 import com.axelor.apps.account.service.move.MoveComputeService;
 import com.axelor.apps.account.service.move.MoveCounterPartService;
-import com.axelor.apps.account.service.move.MoveCreateFromInvoiceServiceImpl;
 import com.axelor.apps.account.service.move.MoveLineControlService;
 import com.axelor.apps.account.service.move.MoveRemoveService;
 import com.axelor.apps.account.service.move.MoveReverseService;
