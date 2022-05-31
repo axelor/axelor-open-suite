9877661b60dbdfc4f33837ecece71dbe45b862b7
Akshay Gandhi <a.gandhi@axelor.com>
2022-05-31 14:06:34 +0530

    RM#24290 Studio Editor: Fix UI and other changes

---
+++ axelor-studio/src/main/webapp/studio/custom-model/precache-manifest.4a6e72431357ee7c39b73ba5d36e7567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9e424add2b81777034e29f249818fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aaa87a7d79a7b4454ab8",
+    "url": "./static/css/main.a75fbd36.chunk.css"
+  },
+  {
+    "revision": "7c6598fdf25784779bea",
+    "url": "./static/js/2.c25b3fb7.chunk.js"
+  },
+  {
+    "revision": "ec406ee4360237332a305ebb6cbba0fe",
+    "url": "./static/js/2.c25b3fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa87a7d79a7b4454ab8",
+    "url": "./static/js/main.b685689a.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
