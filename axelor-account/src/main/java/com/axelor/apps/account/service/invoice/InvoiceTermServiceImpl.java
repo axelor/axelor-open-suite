5b6e8dbbe09a4c050b93364d14a94dfc8ca551d5
Antoine Olivier <a.olivier@axelor.com>
2022-06-07 14:46:01 +0200

    RM#50339 : Fix invoice term amount when created from move line

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceTermServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceTermServiceImpl.java
@@ -141,11 +141,12 @@ public class InvoiceTermServiceImpl implements InvoiceTermService {
   }
 
   protected BigDecimal computePercentageSum(MoveLine moveLine) {
-
     BigDecimal sum = BigDecimal.ZERO;
+    BigDecimal total = moveLine.getCredit().max(moveLine.getDebit());
+
     if (CollectionUtils.isNotEmpty(moveLine.getInvoiceTermList())) {
       for (InvoiceTerm invoiceTerm : moveLine.getInvoiceTermList()) {
-        sum = sum.add(invoiceTerm.getPercentage());
+        sum = sum.add(this.computeCustomizedPercentageUnscaled(invoiceTerm.getAmount(), total));
       }
     }
     return sum;
@@ -328,7 +329,9 @@ public class InvoiceTermServiceImpl implements InvoiceTermService {
     if (percentageSum.compareTo(BigDecimal.ZERO) > 0) {
       invoiceTermPercentage = new BigDecimal(100).subtract(percentageSum);
     }
-    invoiceTerm.setPercentage(invoiceTermPercentage);
+    invoiceTerm.setPercentage(
+        invoiceTermPercentage.setScale(
+            AppBaseService.DEFAULT_NB_DECIMAL_DIGITS, RoundingMode.HALF_UP));
     BigDecimal amount;
     if (moveLine.getCredit().compareTo(moveLine.getDebit()) <= 0) {
       amount = moveLine.getDebit();
@@ -908,12 +911,16 @@ public class InvoiceTermServiceImpl implements InvoiceTermService {
 
   @Override
   public BigDecimal computeCustomizedPercentage(BigDecimal amount, BigDecimal inTaxTotal) {
+    return this.computeCustomizedPercentageUnscaled(amount, inTaxTotal)
+        .setScale(AppBaseService.DEFAULT_NB_DECIMAL_DIGITS, RoundingMode.HALF_UP);
+  }
+
+  protected BigDecimal computeCustomizedPercentageUnscaled(
+      BigDecimal amount, BigDecimal inTaxTotal) {
     BigDecimal percentage = BigDecimal.ZERO;
     if (inTaxTotal.compareTo(BigDecimal.ZERO) != 0) {
       percentage =
-          amount
-              .multiply(new BigDecimal(100))
-              .divide(inTaxTotal, AppBaseService.DEFAULT_NB_DECIMAL_DIGITS, RoundingMode.HALF_UP);
+          amount.multiply(new BigDecimal(100)).divide(inTaxTotal, 10, RoundingMode.HALF_UP);
     }
     return percentage;
   }
