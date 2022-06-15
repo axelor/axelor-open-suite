a48b6d6c013096b0913cc7f3793f313b9de82b0e
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2022-06-15 14:43:32 +0200

    RM #50161 : transform version to int in conflict management

--- axelor-tool/src/main/java/com/axelor/apps/tool/api/ObjectFinder.java
+++ axelor-tool/src/main/java/com/axelor/apps/tool/api/ObjectFinder.java
@@ -3,21 +3,19 @@ package com.axelor.apps.tool.api;
 import com.axelor.auth.db.AuditableModel;
 import com.axelor.db.JPA;
 import com.axelor.db.Model;
-import java.util.Objects;
 import javax.ws.rs.NotFoundException;
 
 public class ObjectFinder {
-  public static Integer NO_VERSION = -1;
+  public static int NO_VERSION = -1;
 
-  public static <T extends Model> T find(
-      Class<T> objectClass, Long objectId, Integer versionProvided) {
+  public static <T extends Model> T find(Class<T> objectClass, Long objectId, int versionProvided) {
     T object = JPA.find(objectClass, objectId);
     if (object == null) {
       throw new NotFoundException(
           "Object from " + objectClass + " with id " + objectId + " was not found.");
     } else {
       T objectFound = JPA.find(objectClass, objectId);
-      if (!Objects.equals(versionProvided, NO_VERSION)) {
+      if (versionProvided != NO_VERSION) {
         ConflictChecker.checkVersion((AuditableModel) objectFound, versionProvided);
       }
       return objectFound;
