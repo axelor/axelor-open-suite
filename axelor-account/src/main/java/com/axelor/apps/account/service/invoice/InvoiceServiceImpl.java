942baf62b7e437ea4974b6d51915038493dc05b6
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2022-06-20 15:55:03 +0200

    50280 refacto of code for analytic

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceServiceImpl.java
@@ -1112,21 +1112,6 @@ public class InvoiceServiceImpl extends InvoiceRepository implements InvoiceServ
         .size();
   }
 
-  @Override
-  public List<Long> getInvoiceLineIds(Invoice invoice) {
-    List<Long> invoiceLineListIds = new ArrayList<Long>();
-    if (invoice != null && invoice.getId() != null) {
-      invoice = invoiceRepo.find(invoice.getId());
-      if (invoice != null) {
-        for (InvoiceLine invoiceLine : invoice.getInvoiceLineList()) {
-          invoiceLineListIds.add(invoiceLine.getId());
-        }
-      }
-    }
-    invoiceLineListIds.add((long) 0);
-    return invoiceLineListIds;
-  }
-
   public BigDecimal calculateFinancialDiscountAmount(Invoice invoice, BigDecimal amount)
       throws AxelorException {
     return calculateFinancialDiscountAmountUnscaled(invoice, amount)
