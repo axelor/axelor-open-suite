2c9b4f0bdaf81ab01a2d1e52043f335703b684b0
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2022-06-23 10:16:37 +0200

    Anomaly #49932: Set draft status on copy

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/db/repo/MrpManagementRepository.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/db/repo/MrpManagementRepository.java
@@ -74,6 +74,7 @@ public class MrpManagementRepository extends MrpRepository {
 
     Mrp copy = super.copy(entity, deep);
     copy.setMrpSeq(null);
+    copy.setStatusSelect(MrpRepository.STATUS_DRAFT);
     return copy;
   }
 }
