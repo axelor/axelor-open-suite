12a2832ffab5cde1d643f0ace0deb593f01c91b0
Dipankar Deka <d.deka@axelor.com>
2022-07-07 15:54:36 +0530

    Anomaly #51693 : Studio: No error display on delete of a custom model

---
+++ axelor-studio/src/main/webapp/studio/custom-model/precache-manifest.fa5ee7b84242a7e182e07da16e37a7c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d635ab1d27d2b50259eb57add1ef2d58",
+    "url": "./index.html"
+  },
+  {
+    "revision": "10fb1584448a4c9bb62a",
+    "url": "./static/css/main.a75fbd36.chunk.css"
+  },
+  {
+    "revision": "7c6598fdf25784779bea",
+    "url": "./static/js/2.c25b3fb7.chunk.js"
+  },
+  {
+    "revision": "ec406ee4360237332a305ebb6cbba0fe",
+    "url": "./static/js/2.c25b3fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fb1584448a4c9bb62a",
+    "url": "./static/js/main.b03f99ed.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
