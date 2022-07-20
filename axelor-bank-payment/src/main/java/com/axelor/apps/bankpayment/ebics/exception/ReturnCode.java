ed359a44013edcd59358bcbeacdd7a85afa314d6
Akshay Gandhi <a.gandhi@axelor.com>
2022-07-20 16:27:17 +0530

    RM#52402 Ebics : Add Kopyleft Licence on header

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/ebics/exception/ReturnCode.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/ebics/exception/ReturnCode.java
@@ -17,6 +17,25 @@
  */
 package com.axelor.apps.bankpayment.ebics.exception;
 
+/*
+ * Copyright (c) 1990-2012 kopiLeft Development SARL, Bizerte, Tunisia
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * $Id$
+ */
+
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.db.IException;
 import com.axelor.i18n.I18n;
