2b131d0b6be8623c871241d31d78b312911e0da0
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2022-08-10 15:32:17 +0200

    Anomaly #52088: Filter move by currency and not company currency

--- axelor-account/src/main/java/com/axelor/apps/account/service/AccountingReportServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/AccountingReportServiceImpl.java
@@ -189,7 +189,7 @@ public class AccountingReportServiceImpl implements AccountingReportService {
     }
 
     if (accountingReport.getCurrency() != null) {
-      this.addParams("self.move.companyCurrency = ?%d", accountingReport.getCurrency());
+      this.addParams("self.move.currency = ?%d", accountingReport.getCurrency());
     }
 
     if (accountingReport.getDateFrom() != null) {
