0dd8880b44d610f92ca742ba3d3ee528e3453352
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2022-08-23 16:34:46 +0200

    RM#51532 Refactor

--- axelor-bank-payment/src/main/java/com/axelor/csv/script/ImportBankStatement.java
+++ axelor-bank-payment/src/main/java/com/axelor/csv/script/ImportBankStatement.java
@@ -1,26 +1,26 @@
 package com.axelor.csv.script;
 
 import com.axelor.apps.bankpayment.db.BankStatement;
+import com.axelor.apps.bankpayment.service.bankstatement.BankStatementCreateService;
 import java.util.Map;
+import javax.inject.Inject;
 
 public class ImportBankStatement {
+
+  protected BankStatementCreateService bankStatementCreateService;
+
+  @Inject
+  public ImportBankStatement(BankStatementCreateService bankStatementCreateService) {
+    this.bankStatementCreateService = bankStatementCreateService;
+  }
 
   public Object importBankStatement(Object bean, Map<String, Object> values) {
     assert bean instanceof BankStatement;
     BankStatement bankStatement = (BankStatement) bean;
 
     if (bankStatement.getName() == null) {
-      bankStatement.setName(computeName(bankStatement));
+      bankStatement.setName(bankStatementCreateService.computeName(bankStatement));
     }
     return bankStatement;
-  }
-
-  private String computeName(BankStatement bankStatement) {
-    StringBuilder builder = new StringBuilder(bankStatement.getBankStatementFileFormat().getName());
-    builder.append(" - ");
-    builder.append(bankStatement.getFromDate().toString());
-    builder.append(" - ");
-    builder.append(bankStatement.getToDate().toString());
-    return builder.toString();
   }
 }
