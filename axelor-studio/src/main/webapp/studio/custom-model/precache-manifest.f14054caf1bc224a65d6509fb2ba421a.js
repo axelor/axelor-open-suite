8ee852b602e0026454ea602d5060802200254365
Akshay Gandhi <a.gandhi@axelor.com>
2022-08-29 18:02:27 +0530

    RM#53316 Studio: Add default panel

---
+++ axelor-studio/src/main/webapp/studio/custom-model/precache-manifest.f14054caf1bc224a65d6509fb2ba421a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce81273037a0602aa9c8bd73248679be",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7f6ba38059ed165b8d1",
+    "url": "./static/css/main.a75fbd36.chunk.css"
+  },
+  {
+    "revision": "d39aa1b9dcedb9394ad2",
+    "url": "./static/js/2.5831d88a.chunk.js"
+  },
+  {
+    "revision": "ec406ee4360237332a305ebb6cbba0fe",
+    "url": "./static/js/2.5831d88a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f6ba38059ed165b8d1",
+    "url": "./static/js/main.1ad08f16.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
