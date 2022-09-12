a741ba86ae921cc3463c66233cd9284f91868fb4
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2022-09-12 11:50:19 +0200

    RM#53979: Production API : update ManufOrder status

--- axelor-production/src/main/java/com/axelor/apps/production/module/ProductionModule.java
+++ axelor-production/src/main/java/com/axelor/apps/production/module/ProductionModule.java
@@ -40,6 +40,8 @@ import com.axelor.apps.production.db.repo.UnitCostCalculationManagementRepositor
 import com.axelor.apps.production.db.repo.UnitCostCalculationRepository;
 import com.axelor.apps.production.rest.ManufOrderProductRestService;
 import com.axelor.apps.production.rest.ManufOrderProductRestServiceImpl;
+import com.axelor.apps.production.rest.ManufOrderRestService;
+import com.axelor.apps.production.rest.ManufOrderRestServiceImpl;
 import com.axelor.apps.production.service.BillOfMaterialService;
 import com.axelor.apps.production.service.BillOfMaterialServiceImpl;
 import com.axelor.apps.production.service.MpsChargeService;
@@ -168,5 +170,6 @@ public class ProductionModule extends AxelorModule {
     bind(ConfiguratorProdProductService.class).to(ConfiguratorProdProductServiceImpl.class);
     bind(ProductVariantServiceSupplyChainImpl.class).to(ProductVariantServiceProductionImpl.class);
     bind(ManufOrderProductRestService.class).to(ManufOrderProductRestServiceImpl.class);
+    bind(ManufOrderRestService.class).to(ManufOrderRestServiceImpl.class);
   }
 }
