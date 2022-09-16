86ce7a28ba5fe58c277fb591fe402119b63212d1
Akshay Gandhi <a.gandhi@axelor.com>
2022-09-16 17:15:34 +0530

    Studio : Changes in studio editor

---
+++ axelor-studio/src/main/webapp/studio/custom-model/precache-manifest.672ce24d3ab3be324105c17e33b17238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24063239e16d311a792b4f6b341fab6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "534e38462246c6d67192",
+    "url": "./static/css/main.a75fbd36.chunk.css"
+  },
+  {
+    "revision": "258789d65be2447efc5c",
+    "url": "./static/js/2.a54da463.chunk.js"
+  },
+  {
+    "revision": "ec406ee4360237332a305ebb6cbba0fe",
+    "url": "./static/js/2.a54da463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534e38462246c6d67192",
+    "url": "./static/js/main.9d2b4052.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
