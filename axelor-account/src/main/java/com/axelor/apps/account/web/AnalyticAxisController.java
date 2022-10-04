a651aea835ff4110c5e9ece58b4632c4ac412275
alg-axelor <91078097+alg-axelor@users.noreply.github.com>
2022-10-04 14:00:54 +0200

    55101 - ANALYTICAXIS: change groupings management
    (cherry picked from commit ed55453d02109ab94aa76d92a289d94ecf597b54)

--- axelor-account/src/main/java/com/axelor/apps/account/web/AnalyticAxisController.java
+++ axelor-account/src/main/java/com/axelor/apps/account/web/AnalyticAxisController.java
@@ -19,16 +19,13 @@ package com.axelor.apps.account.web;
 
 import com.axelor.apps.account.db.AnalyticAxis;
 import com.axelor.apps.account.service.analytic.AnalyticAxisService;
-import com.axelor.common.ObjectUtils;
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.service.TraceBackService;
 import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class AnalyticAxisController {
 
@@ -51,28 +48,15 @@ public class AnalyticAxisController {
     }
   }
 
-  public void setGroupingDomain(ActionRequest request, ActionResponse response)
-      throws AxelorException {
+  public void removeSameAnalyticGrouping(ActionRequest request, ActionResponse response) {
     try {
       AnalyticAxis analyticAxis = request.getContext().asType(AnalyticAxis.class);
-
-      List<Long> idList = new ArrayList<Long>();
-      for (int i = 1; i <= 10; i++) {
-        idList.add(Beans.get(AnalyticAxisService.class).getAnalyticGroupingId(analyticAxis, i));
-      }
-
-      if (!ObjectUtils.isEmpty(idList)) {
-        String idListStr =
-            idList.stream().map(id -> id.toString()).collect(Collectors.joining(","));
-        for (int i = 1; i <= 10; i++) {
-          response.setAttr(
-              "analyticGrouping" + i,
-              "domain",
-              "self.id NOT IN ("
-                  + idListStr
-                  + ") AND self.analyticAxis.id = "
-                  + analyticAxis.getId());
-        }
+      String analyticGroupingChanged = request.getContext().get("_source").toString();
+      List<Integer> analyticGroupingToRemoveList =
+          Beans.get(AnalyticAxisService.class)
+              .getSameAnalyticGroupingValues(analyticAxis, analyticGroupingChanged);
+      for (Integer value : analyticGroupingToRemoveList) {
+        response.setValue("analyticGrouping" + value, null);
       }
     } catch (Exception e) {
       TraceBackService.trace(response, e);
