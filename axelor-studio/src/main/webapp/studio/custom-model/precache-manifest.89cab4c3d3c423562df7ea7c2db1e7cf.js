8dd6a4bca8924c4b6ff76b90f234880091d0b21e
Dipankar Deka <d.deka@axelor.com>
2022-10-10 10:44:44 +0530

    Anomaly #54879 : Studio UI Fix

---
+++ axelor-studio/src/main/webapp/studio/custom-model/precache-manifest.89cab4c3d3c423562df7ea7c2db1e7cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd26914ce8e48c1057515194faee70d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9a4826411902f9aff695",
+    "url": "./static/css/main.98d10164.chunk.css"
+  },
+  {
+    "revision": "b7f936685b41543e85c0",
+    "url": "./static/js/2.31d80895.chunk.js"
+  },
+  {
+    "revision": "ec406ee4360237332a305ebb6cbba0fe",
+    "url": "./static/js/2.31d80895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4826411902f9aff695",
+    "url": "./static/js/main.3d9bb179.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
