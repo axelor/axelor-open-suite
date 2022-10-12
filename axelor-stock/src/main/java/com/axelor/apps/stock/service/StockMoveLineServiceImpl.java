bf9bff8f4bbc5dd0938465fccb20710c6cdcbfae
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2022-10-12 13:57:27 +0200

    RM#54805 Fix condition for assigning tracking number when creating stock move from a sale order
    (cherry picked from commit 01afa44202fb506d56c95a573e29dcbe905a8788)

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineServiceImpl.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineServiceImpl.java
@@ -229,8 +229,9 @@ public class StockMoveLineServiceImpl implements StockMoveLineService {
                   trackingNumberConfiguration,
                   product,
                   trackingNumberConfiguration.getSaleQtyByTracking());
+            }
 
-            } else {
+            if (trackingNumberConfiguration.getHasSaleAutoSelectTrackingNbr()) {
               // Rechercher le numéro de suivi d'apèrs FIFO/LIFO
               this.assignTrackingNumber(stockMoveLine, product, stockMove.getFromStockLocation());
             }
