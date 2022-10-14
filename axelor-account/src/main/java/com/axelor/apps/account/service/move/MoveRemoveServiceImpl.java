f733144cc22f9c0f9cff312e30dc73c3e97c2d18
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2022-10-14 16:50:31 +0200

    55328 import of good lists

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveRemoveServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveRemoveServiceImpl.java
@@ -32,7 +32,7 @@ import com.axelor.exception.AxelorException;
 import com.axelor.exception.db.repo.TraceBackRepository;
 import com.axelor.exception.service.TraceBackService;
 import com.axelor.i18n.I18n;
-import com.beust.jcommander.internal.Lists;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 import java.util.List;
