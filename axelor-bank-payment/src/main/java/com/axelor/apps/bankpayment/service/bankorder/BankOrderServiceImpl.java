847fb1ec9e85bd80894b430a06e240f26a55f995
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2022-10-14 14:04:53 +0200

    RM-55013 Avoid repeating bankOrder sending or move generation from bankOrders
    (cherry picked from commit 1432f19519ec12385d190ff93644c59be7e6a3ff)
    (cherry picked from commit 52803031f084e5e37a93f55681fd5a4963620694)

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankorder/BankOrderServiceImpl.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankorder/BankOrderServiceImpl.java
@@ -349,11 +349,11 @@ public class BankOrderServiceImpl implements BankOrderService {
 
     bankOrder.setStatusSelect(BankOrderRepository.STATUS_VALIDATED);
 
-    if (bankPaymentConfigService
-        .getBankPaymentConfig(bankOrder.getSenderCompany())
-        .getGenerateMoveOnBankOrderValidation()) {
-      bankOrderMoveService.generateMoves(bankOrder);
-      validatePayment(bankOrder);
+    if (!bankOrder.getAreMovesGenerated()
+        && bankPaymentConfigService
+            .getBankPaymentConfig(bankOrder.getSenderCompany())
+            .getGenerateMoveOnBankOrderValidation()) {
+      generateMovesAndValidatePayment(bankOrder);
     }
 
     bankOrderRepo.save(bankOrder);
@@ -376,7 +376,9 @@ public class BankOrderServiceImpl implements BankOrderService {
             I18n.get(IExceptionMessage.EBICS_MISSING_USER_TRANSPORT));
       }
 
-      sendBankOrderFile(bankOrder);
+      if (!bankOrder.getHasBeenSentToBank()) {
+        sendBankOrderFile(bankOrder);
+      }
     }
     realizeBankOrder(bankOrder);
   }
@@ -399,6 +401,12 @@ public class BankOrderServiceImpl implements BankOrderService {
     dataFileToSend = MetaFiles.getPath(bankOrder.getGeneratedMetaFile()).toFile();
 
     sendFile(bankOrder, dataFileToSend, signatureFileToSend);
+    markAsSent(bankOrder);
+  }
+
+  @Transactional(rollbackOn = {Exception.class})
+  protected void markAsSent(BankOrder bankOrder) {
+    bankOrder.setHasBeenSentToBank(true);
   }
 
   @Transactional(rollbackOn = {Exception.class})
@@ -406,11 +414,11 @@ public class BankOrderServiceImpl implements BankOrderService {
 
     AppBaseService appBaseService = Beans.get(AppBaseService.class);
 
-    if (!bankPaymentConfigService
-        .getBankPaymentConfig(bankOrder.getSenderCompany())
-        .getGenerateMoveOnBankOrderValidation()) {
-      bankOrderMoveService.generateMoves(bankOrder);
-      validatePayment(bankOrder);
+    if (!bankOrder.getAreMovesGenerated()
+        && !bankPaymentConfigService
+            .getBankPaymentConfig(bankOrder.getSenderCompany())
+            .getGenerateMoveOnBankOrderValidation()) {
+      generateMovesAndValidatePayment(bankOrder);
     }
 
     bankOrder.setSendingDateTime(appBaseService.getTodayDateTime().toLocalDateTime());
@@ -421,6 +429,12 @@ public class BankOrderServiceImpl implements BankOrderService {
     }
 
     bankOrderRepo.save(bankOrder);
+  }
+
+  protected void generateMovesAndValidatePayment(BankOrder bankOrder) throws AxelorException {
+    bankOrderMoveService.generateMoves(bankOrder);
+    validatePayment(bankOrder);
+    bankOrder.setAreMovesGenerated(true);
   }
 
   protected void sendFile(BankOrder bankOrder, File dataFileToSend, File signatureFileToSend)
