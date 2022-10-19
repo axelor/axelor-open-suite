9a2bf2de46343cf403ae5702bc771b53b08b95a6
Dipankar Deka <d.deka@axelor.com>
2022-10-19 16:15:47 +0530

    Feature #55315 : Merge Studio and BPM module

---
+++ axelor-studio/src/main/webapp/wkf-editor/precache-manifest.6e3c2154e4afa67b83858a1f754baa65.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88da1e32c403bc27c877cf377bdc7735",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1a946df8f99db2366d43",
+    "url": "./static/css/2.c4daa5fe.chunk.css"
+  },
+  {
+    "revision": "a0b71f1e57824af73840",
+    "url": "./static/css/3.24a5c644.chunk.css"
+  },
+  {
+    "revision": "5af4d44f9653d74d6806",
+    "url": "./static/css/main.4e427609.chunk.css"
+  },
+  {
+    "revision": "1a946df8f99db2366d43",
+    "url": "./static/js/2.d8d78ed6.chunk.js"
+  },
+  {
+    "revision": "308b40895c213754d4f625cb5d8b532e",
+    "url": "./static/js/2.d8d78ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b71f1e57824af73840",
+    "url": "./static/js/3.ef50a943.chunk.js"
+  },
+  {
+    "revision": "5af4d44f9653d74d6806",
+    "url": "./static/js/main.0d47bd5f.chunk.js"
+  },
+  {
+    "revision": "f9b12699d006eb3e0095",
+    "url": "./static/js/runtime-main.75c3601e.js"
+  },
+  {
+    "revision": "08b5f2819c43514afa3a1985d5aad804",
+    "url": "./static/media/bpmn.08b5f281.eot"
+  },
+  {
+    "revision": "1381b44efba43dfddc655c7cbca5b272",
+    "url": "./static/media/bpmn.1381b44e.svg"
+  }
+]);
