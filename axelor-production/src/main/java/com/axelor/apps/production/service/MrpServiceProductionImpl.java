d35ba8bb7e97f751f83f2cb416ed86020093c2b8
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2022-10-28 16:20:07 +0200

    Anomaly #55600: removed todo and rename changelog

--- axelor-production/src/main/java/com/axelor/apps/production/service/MrpServiceProductionImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/MrpServiceProductionImpl.java
@@ -472,8 +472,6 @@ public class MrpServiceProductionImpl extends MrpServiceImpl {
       if (manufProposalNeedMrpLineType == null) {
         return;
       }
-      
-      Long duration = 0l; //TODO
 
       for (BillOfMaterial billOfMaterial : defaultBillOfMaterial.getBillOfMaterialSet()) {
 
