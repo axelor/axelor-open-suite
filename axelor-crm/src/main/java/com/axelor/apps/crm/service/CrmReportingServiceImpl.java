3c8b6deb31327d02227fc8cbed0269f027b5894e
h.mistry <h.mistry@axelor.com>
2022-11-17 18:54:21 +0530

    RM#52832 CRM Major changes : sheet 'CRM Reporting'

--- axelor-crm/src/main/java/com/axelor/apps/crm/service/CrmReportingServiceImpl.java
+++ axelor-crm/src/main/java/com/axelor/apps/crm/service/CrmReportingServiceImpl.java
@@ -17,9 +17,11 @@
  */
 package com.axelor.apps.crm.service;
 
+import com.axelor.apps.base.db.AppCrm;
 import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.base.service.app.AppBaseService;
+import com.axelor.apps.base.service.app.AppService;
 import com.axelor.apps.crm.db.CrmReporting;
 import com.axelor.apps.crm.exception.IExceptionMessage;
 import com.axelor.apps.tool.StringTool;
@@ -28,6 +30,7 @@ import com.axelor.db.Query;
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.db.repo.TraceBackRepository;
 import com.axelor.i18n.I18n;
+import com.axelor.inject.Beans;
 import com.axelor.meta.schema.actions.ActionView;
 import com.axelor.meta.schema.actions.ActionView.ActionViewBuilder;
 import com.google.common.base.Strings;
@@ -108,9 +111,9 @@ public class CrmReportingServiceImpl implements CrmReportingService {
     } else {
       leadQuery(crmReporting, model);
     }
+    AppCrm appCrm = (AppCrm) Beans.get(AppService.class).getApp("crm");
 
-    if (!crmReporting.getAgencySet().isEmpty()
-        && appBaseService.getAppBase().getAgenciesManagement())
+    if (!crmReporting.getAgencySet().isEmpty() && appCrm.getAgenciesManagement())
       this.addParams(
           "self."
               + model
