c5df9cdaa860f66757e8cbb802d1bd9254c2446e
h.mistry <h.mistry@axelor.com>
2022-12-02 13:30:11 +0530

    RM#45396 [TASK EDITOR] : Error on moving task from one section to another
    (cherry picked from commit 5cf7dbf4d9929b91aa6ebaed300f4ee8d59cc236)

--- axelor-project/src/main/webapp/project/task-editor/service-worker.js
+++ axelor-project/src/main/webapp/project/task-editor/service-worker.js
@@ -1,20 +1,3 @@
-/*
- * Axelor Business Solutions
- *
- * Copyright (C) 2022 Axelor (<http://axelor.com>).
- *
- * This program is free software: you can redistribute it and/or  modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 /**
  * Welcome to your Workbox-powered service worker!
  *
@@ -31,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4074a9ec385bc5eb4ba7a0b1af8cc601.js"
+  "./precache-manifest.3b5a915cce538b4375ae97a33d7d4f1c.js"
 );
 
 self.addEventListener('message', (event) => {
