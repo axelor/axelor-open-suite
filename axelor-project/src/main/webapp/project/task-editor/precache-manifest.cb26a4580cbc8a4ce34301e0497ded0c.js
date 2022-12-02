85fcd8642b6e2fc0e4205ef7c891ed279c089420
h.mistry <h.mistry@axelor.com>
2022-12-02 15:22:34 +0530

    RM#45396 [TASK EDITOR] : Error on moving task from one section to another

---
+++ axelor-project/src/main/webapp/project/task-editor/precache-manifest.cb26a4580cbc8a4ce34301e0497ded0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6269a2671c771fb888e02d793e0cc126",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5ef265eef10987e9da8",
+    "url": "./static/css/2.0b510fe8.chunk.css"
+  },
+  {
+    "revision": "4a28e50e3f9f9e595357",
+    "url": "./static/css/main.613d79ca.chunk.css"
+  },
+  {
+    "revision": "f5ef265eef10987e9da8",
+    "url": "./static/js/2.1399509d.chunk.js"
+  },
+  {
+    "revision": "8d22859a420ef73c278c92f2a492b1ef",
+    "url": "./static/js/2.1399509d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a28e50e3f9f9e595357",
+    "url": "./static/js/main.ed48ca6d.chunk.js"
+  },
+  {
+    "revision": "083e6c14fe78135d6547",
+    "url": "./static/js/runtime-main.298acfe2.js"
+  },
+  {
+    "revision": "5d75554dbc1274af3ce8f98a3ca41a50",
+    "url": "./static/media/user.5d75554d.jpg"
+  }
+]);
