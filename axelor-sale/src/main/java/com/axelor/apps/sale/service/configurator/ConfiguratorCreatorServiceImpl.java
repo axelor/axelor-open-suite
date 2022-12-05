52ffadacb3e29167200071ae1d28fbf23d224b5f
David Trochel <d.trochel@axelor.com>
2022-12-05 18:03:41 +0100

    RM#57393 Introduce constant for AXELORTMP
    Signed-off-by: David Trochel <d.trochel@axelor.com>

--- axelor-sale/src/main/java/com/axelor/apps/sale/service/configurator/ConfiguratorCreatorServiceImpl.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/service/configurator/ConfiguratorCreatorServiceImpl.java
@@ -82,6 +82,7 @@ import javax.validation.constraints.NotNull;
 
 public class ConfiguratorCreatorServiceImpl implements ConfiguratorCreatorService {
 
+  protected static final String AXELORTMP = "$AXELORTMP";
   protected static final String ACTION_CONFIGURATOR_UPDATE_INDICATORS =
       "action-configurator-update-indicators";
 
@@ -154,7 +155,7 @@ public class ConfiguratorCreatorServiceImpl implements ConfiguratorCreatorServic
     for (MetaJsonField indicatorToRemove : fieldsToRemove) {
       if (indicatorToRemove.getName() != null) {
         // This is needed as there is a constraint issue
-        indicatorToRemove.setName(indicatorToRemove.getName() + "$AXELORTMP" + creator.getId());
+        indicatorToRemove.setName(indicatorToRemove.getName() + AXELORTMP + creator.getId());
       }
       indicatorToRemove.setHidden(
           true); // Adding this line to fix field still showing even when removed indictor
@@ -641,7 +642,7 @@ public class ConfiguratorCreatorServiceImpl implements ConfiguratorCreatorServic
       String name = metaJsonField.getName();
       if (name != null) {
         // FIX FOR CONSTRAINT ISSUE
-        metaJsonField.setName(name.replace("$AXELORTMP", ""));
+        metaJsonField.setName(name.replace(AXELORTMP, ""));
       }
     }
   }
