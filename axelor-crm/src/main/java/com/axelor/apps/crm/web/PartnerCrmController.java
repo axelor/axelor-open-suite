e982d21f5fa6aaadc832b38efed10772709f388a
h.mistry <h.mistry@axelor.com>
2022-12-09 11:11:02 +0530

    RM#58145 [Code review] PartnerCrmController changes

--- axelor-crm/src/main/java/com/axelor/apps/crm/web/PartnerCrmController.java
+++ axelor-crm/src/main/java/com/axelor/apps/crm/web/PartnerCrmController.java
@@ -19,6 +19,8 @@ package com.axelor.apps.crm.web;
 
 import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.base.db.repo.PartnerRepository;
+import com.axelor.exception.ResponseMessageType;
+import com.axelor.exception.service.TraceBackService;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
@@ -27,14 +29,18 @@ public class PartnerCrmController {
 
   public void getSubsidiaryPartnersCount(ActionRequest request, ActionResponse response) {
 
-    Partner partner = request.getContext().asType(Partner.class);
-    partner = Beans.get(PartnerRepository.class).find(partner.getId());
-    long count =
-        Beans.get(PartnerRepository.class)
-            .all()
-            .filter("self.parentPartner = :id")
-            .bind("id", partner.getId())
-            .count();
-    response.setValue("$subsidiaryCount", count);
+    try {
+      Partner partner = request.getContext().asType(Partner.class);
+      partner = Beans.get(PartnerRepository.class).find(partner.getId());
+      long count =
+          Beans.get(PartnerRepository.class)
+              .all()
+              .filter("self.parentPartner = :id")
+              .bind("id", partner.getId())
+              .count();
+      response.setValue("$subsidiaryCount", count);
+    } catch (Exception e) {
+      TraceBackService.trace(response, e, ResponseMessageType.ERROR);
+    }
   }
 }
