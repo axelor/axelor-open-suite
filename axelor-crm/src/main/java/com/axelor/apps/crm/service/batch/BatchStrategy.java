aafbb4a6e3ed85ce432e5a7b697836d953a8b29b
Akshay Gandhi <a.gandhi@axelor.com>
2022-12-16 11:03:31 +0530

    RM#55314 Changes of package name for crm module

--- axelor-crm/src/main/java/com/axelor/apps/crm/service/batch/BatchStrategy.java
+++ axelor-crm/src/main/java/com/axelor/apps/crm/service/batch/BatchStrategy.java
@@ -24,7 +24,7 @@ import com.axelor.apps.crm.db.TargetConfiguration;
 import com.axelor.apps.crm.db.repo.EventReminderRepository;
 import com.axelor.apps.crm.message.MessageServiceCrmImpl;
 import com.axelor.apps.crm.service.TargetService;
-import com.axelor.apps.message.service.MailAccountService;
+import com.axelor.message.service.MailAccountService;
 import com.google.inject.Inject;
 
 public abstract class BatchStrategy extends AbstractBatch {
