fbac32fac17a51d263322caa95d989ede22eebaf
Akshay Gandhi <a.gandhi@axelor.com>
2022-12-16 11:03:19 +0530

    RM#55314 Changes of package name for contract module

--- axelor-contract/src/main/java/com/axelor/apps/contract/service/ContractVersionService.java
+++ axelor-contract/src/main/java/com/axelor/apps/contract/service/ContractVersionService.java
@@ -19,8 +19,8 @@ package com.axelor.apps.contract.service;
 
 import com.axelor.apps.contract.db.Contract;
 import com.axelor.apps.contract.db.ContractVersion;
-import com.axelor.apps.tool.date.DateTool;
 import com.axelor.exception.AxelorException;
+import com.axelor.utils.date.DateTool;
 import com.google.inject.persist.Transactional;
 import java.time.LocalDate;
 
