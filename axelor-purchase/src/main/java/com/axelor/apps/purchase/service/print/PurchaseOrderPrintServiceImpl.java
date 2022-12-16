bd1a3a3b556949d90a75d54b6a9766df438e3f66
Akshay Gandhi <a.gandhi@axelor.com>
2022-12-16 11:07:29 +0530

    RM#55314 Changes of package name for purchase module

--- axelor-purchase/src/main/java/com/axelor/apps/purchase/service/print/PurchaseOrderPrintServiceImpl.java
+++ axelor-purchase/src/main/java/com/axelor/apps/purchase/service/print/PurchaseOrderPrintServiceImpl.java
@@ -18,7 +18,6 @@
 package com.axelor.apps.purchase.service.print;
 
 import com.axelor.apps.ReportFactory;
-import com.axelor.apps.base.db.AppBase;
 import com.axelor.apps.base.service.app.AppBaseService;
 import com.axelor.apps.purchase.db.PurchaseOrder;
 import com.axelor.apps.purchase.db.repo.PurchaseOrderRepository;
@@ -26,15 +25,16 @@ import com.axelor.apps.purchase.exception.PurchaseExceptionMessage;
 import com.axelor.apps.purchase.report.IReport;
 import com.axelor.apps.purchase.service.app.AppPurchaseService;
 import com.axelor.apps.report.engine.ReportSettings;
-import com.axelor.apps.tool.ModelTool;
-import com.axelor.apps.tool.ThrowConsumer;
-import com.axelor.apps.tool.file.PdfTool;
 import com.axelor.auth.AuthUtils;
 import com.axelor.auth.db.User;
 import com.axelor.exception.AxelorException;
 import com.axelor.exception.db.repo.TraceBackRepository;
 import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
+import com.axelor.studio.db.AppBase;
+import com.axelor.utils.ModelTool;
+import com.axelor.utils.ThrowConsumer;
+import com.axelor.utils.file.PdfTool;
 import com.google.inject.Inject;
 import java.io.File;
 import java.io.IOException;
@@ -68,7 +68,7 @@ public class PurchaseOrderPrintServiceImpl implements PurchaseOrderPrintService 
     ModelTool.apply(
         PurchaseOrder.class,
         ids,
-        new ThrowConsumer<PurchaseOrder>() {
+        new ThrowConsumer<PurchaseOrder, Exception>() {
 
           @Override
           public void accept(PurchaseOrder purchaseOrder) throws Exception {
