c8d84cc30033ddef9a93b911ae6669da66869a4f
bsu-axelor <b.suvagiya@axelor.com>
2022-12-16 17:06:56 +0530

    Anomaly RM#58544: Studio: No icon display on toolbar

---
+++ axelor-studio/src/main/webapp/studio/custom-model/precache-manifest.81749af1b5d45ca8d9e53668bfa47b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23854988bb89616afa41aa423d2271a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "366a917a85f87a299b46",
+    "url": "./static/css/main.36d1753c.chunk.css"
+  },
+  {
+    "revision": "3e71615aa78fe3c9d262",
+    "url": "./static/js/2.c5d7d72d.chunk.js"
+  },
+  {
+    "revision": "ec406ee4360237332a305ebb6cbba0fe",
+    "url": "./static/js/2.c5d7d72d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "366a917a85f87a299b46",
+    "url": "./static/js/main.142c310f.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
--- axelor-studio/src/main/webapp/studio/custom-model/service-worker.js
+++ axelor-studio/src/main/webapp/studio/custom-model/service-worker.js
@@ -1,20 +1,3 @@
-/*
- * Axelor Business Solutions
- *
- * Copyright (C) 2022 Axelor (<http://axelor.com>).
- *
- * This program is free software: you can redistribute it and/or  modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 /**
  * Welcome to your Workbox-powered service worker!
  *
@@ -31,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d58d3ae7cb2e55c3dff4f2107ad228ef.js"
+  "./precache-manifest.81749af1b5d45ca8d9e53668bfa47b2b.js"
 );
 
 self.addEventListener('message', (event) => {
