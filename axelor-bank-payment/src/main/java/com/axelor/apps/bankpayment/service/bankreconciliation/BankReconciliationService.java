f515e160bb2e1d65cdbbb124a490e2b5009228a3
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2022-12-20 09:02:20 +0100

    54925 add of a default account type (#9069)

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankreconciliation/BankReconciliationService.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankreconciliation/BankReconciliationService.java
@@ -670,7 +670,10 @@ public class BankReconciliationService {
     }
 
     for (AccountManagement accountManagement : accountManagementList) {
-      if (accountManagement.getCashAccount() != null) {
+      if (accountManagement.getCashAccount() != null
+          && accountManagement.getCashAccount().getAccountType() != null
+          && AccountTypeRepository.TYPE_CASH.equals(
+              accountManagement.getCashAccount().getAccountType().getTechnicalTypeSelect())) {
         cashAccountIdSet.add(accountManagement.getCashAccount().getId().toString());
       }
     }
@@ -750,6 +753,8 @@ public class BankReconciliationService {
     }
     if (bankReconciliation.getCashAccount() != null) {
       query = query + " AND self.account = :cashAccount";
+    } else {
+      query = query + " AND self.account.accountType.technicalTypeSelect = :accountType";
     }
 
     return query;
@@ -774,6 +779,8 @@ public class BankReconciliationService {
     }
     if (bankReconciliation.getCashAccount() != null) {
       params.put("cashAccount", bankReconciliation.getCashAccount());
+    } else {
+      params.put("accountType", AccountTypeRepository.TYPE_CASH);
     }
     return params;
   }
