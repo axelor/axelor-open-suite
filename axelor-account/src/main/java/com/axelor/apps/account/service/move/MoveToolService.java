0cf629e6be2f37d4a3d0e73d1cef6260e6d58a35
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2022-12-20 08:55:02 +0100

    55292 remove of moveline description set when change of move origin (#9240)
    * 55292 remove of moveline description set when change of move origin
    
    * 55292 add of description on change function in move like for origin
    
    * 55292 add of description change action in move simplified

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveToolService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveToolService.java
@@ -180,7 +180,7 @@ public interface MoveToolService {
 
   MoveLine findMoveLineByAccount(Move move, Account account) throws AxelorException;
 
-  void setOriginAndDescriptionOnMoveLineList(Move move);
+  void setOriginOnMoveLineList(Move move);
 
   @CallMethod
   boolean isTemporarilyClosurePeriodManage(Period period, Journal journal, User user)
@@ -198,4 +198,6 @@ public interface MoveToolService {
   boolean isSimulatedMovePeriodClosed(Move move);
 
   void exceptionOnGenerateCounterpart(Move move) throws AxelorException;
+
+  void setDescriptionOnMoveLineList(Move move);
 }
