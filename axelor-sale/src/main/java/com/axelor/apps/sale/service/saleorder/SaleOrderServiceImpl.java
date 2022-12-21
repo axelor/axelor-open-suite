c592ed2d4ade83ff09b381fc61a20ff9e7e4e22c
bsu-axelor <b.suvagiya@axelor.com>
2022-12-21 15:52:01 +0530

    Anomaly RM#57918: Complementary products : problem with quantities on sale order

--- axelor-sale/src/main/java/com/axelor/apps/sale/service/saleorder/SaleOrderServiceImpl.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/service/saleorder/SaleOrderServiceImpl.java
@@ -47,7 +47,6 @@ import com.axelor.inject.Beans;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -275,11 +274,7 @@ public class SaleOrderServiceImpl implements SaleOrderService {
             newSoLine = new SaleOrderLine();
             newSoLine.setProduct(compProductSelected.getProduct());
             newSoLine.setSaleOrder(saleOrder);
-            newSoLine.setQty(
-                originSoLine
-                    .getQty()
-                    .multiply(compProductSelected.getQty())
-                    .setScale(appBaseService.getNbDecimalDigitForQty(), RoundingMode.HALF_EVEN));
+            newSoLine.setQty(compProductSelected.getQty());
 
             saleOrderLineService.computeProductInformation(newSoLine, newSoLine.getSaleOrder());
             saleOrderLineService.computeValues(newSoLine.getSaleOrder(), newSoLine);
@@ -290,11 +285,7 @@ public class SaleOrderServiceImpl implements SaleOrderService {
             saleOrderLineList.add(targetIndex, newSoLine);
           }
         } else {
-          newSoLine.setQty(
-              originSoLine
-                  .getQty()
-                  .multiply(compProductSelected.getQty())
-                  .setScale(appBaseService.getNbDecimalDigitForQty(), RoundingMode.HALF_EVEN));
+          newSoLine.setQty(compProductSelected.getQty());
 
           saleOrderLineService.computeProductInformation(newSoLine, newSoLine.getSaleOrder());
           saleOrderLineService.computeValues(newSoLine.getSaleOrder(), newSoLine);
