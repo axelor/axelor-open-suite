5eca63fa57c333f5a9eb03632c44eb4717ffe150
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2022-12-23 13:06:53 +0100

    RM#58403 Fix infinite loop in MRP
    (cherry picked from commit a28556ceb484601a5e082473204f0d22e1fc82ec)
    (cherry picked from commit 5f26e485310c0546428b6cde5262bd472c9a0a9e)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/MrpServiceImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/MrpServiceImpl.java
@@ -340,6 +340,12 @@ public class MrpServiceImpl implements MrpService {
 
     MrpLineType mrpLineType = mrpLine.getMrpLineType();
 
+    if ((mrpLineType.getElementSelect() == MrpLineTypeRepository.ELEMENT_PURCHASE_ORDER
+            || mrpLineType.getElementSelect() == MrpLineTypeRepository.ELEMENT_MANUFACTURING_ORDER)
+        && !firstPass) {
+      return false;
+    }
+
     boolean isProposalElement = this.isProposalElement(mrpLineType);
 
     BigDecimal minQty = mrpLine.getMinQty();
