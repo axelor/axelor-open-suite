fb28aa2f66e7dd68860b3207a58dc096271fa811
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2022-12-27 14:54:01 +0100

    RM#58862 Reset budget on purchase order duplication
    (cherry picked from commit 8eadbd6967d67b42fa37c1f4988befdce3eb1210)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/db/repo/PurchaseOrderSupplychainRepository.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/db/repo/PurchaseOrderSupplychainRepository.java
@@ -41,6 +41,7 @@ public class PurchaseOrderSupplychainRepository extends PurchaseOrderManagementR
 
     copy.setReceiptState(PurchaseOrderRepository.STATE_NOT_RECEIVED);
     copy.setAmountInvoiced(null);
+    copy.setBudget(null);
 
     if (copy.getPurchaseOrderLineList() != null) {
       for (PurchaseOrderLine purchaseOrderLine : copy.getPurchaseOrderLineList()) {
@@ -48,6 +49,8 @@ public class PurchaseOrderSupplychainRepository extends PurchaseOrderManagementR
         purchaseOrderLine.setReceivedQty(null);
         purchaseOrderLine.setAmountInvoiced(null);
         purchaseOrderLine.setInvoiced(null);
+        purchaseOrderLine.setBudgetDistributionList(null);
+        purchaseOrderLine.setBudgetDistributionSumAmount(null);
       }
     }
 
