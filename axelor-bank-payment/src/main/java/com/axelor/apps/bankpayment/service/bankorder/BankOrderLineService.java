23bbd0353326d0a3289331327c37664e378a1a02
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-01-03 17:11:59 +0100

    RM#58243 Improve message when bank details is inactive (#9516)
    (cherry picked from commit 2739f5cebf39aa2bcd0a649177f274f95d263bbe)

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankorder/BankOrderLineService.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankorder/BankOrderLineService.java
@@ -359,7 +359,7 @@ public class BankOrderLineService {
     }
 
     try {
-      checkBankDetails(candidateBankDetails, bankOrder);
+      checkBankDetails(candidateBankDetails, bankOrder, bankOrderLine);
     } catch (AxelorException e) {
       candidateBankDetails = null;
     }
@@ -367,7 +367,8 @@ public class BankOrderLineService {
     return candidateBankDetails;
   }
 
-  public void checkBankDetails(BankDetails bankDetails, BankOrder bankOrder)
+  public void checkBankDetails(
+      BankDetails bankDetails, BankOrder bankOrder, BankOrderLine bankOrderLine)
       throws AxelorException {
     if (bankDetails == null) {
       throw new AxelorException(
@@ -379,7 +380,8 @@ public class BankOrderLineService {
     if (!bankDetails.getActive()) {
       throw new AxelorException(
           TraceBackRepository.CATEGORY_INCONSISTENCY,
-          I18n.get(BankPaymentExceptionMessage.BANK_ORDER_LINE_BANK_DETAILS_NOT_ACTIVE));
+          I18n.get(BankPaymentExceptionMessage.BANK_ORDER_LINE_BANK_DETAILS_NOT_ACTIVE),
+          bankOrderLine.getSequence());
     }
 
     // filter on the result from bankPartner if the option is active.
