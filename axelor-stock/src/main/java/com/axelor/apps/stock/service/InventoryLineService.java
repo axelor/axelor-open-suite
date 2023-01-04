9bce9b1bd1825e8edce7b3f448a6712317c33e18
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-01-04 15:12:38 +0100

    RM#58154 Fix NPE

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/InventoryLineService.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/InventoryLineService.java
@@ -150,9 +150,11 @@ public class InventoryLineService {
               .getStockConfig(stockLocation.getCompany())
               .getInventoryValuationTypeSelect();
 
+      BigDecimal productAvgPrice = product.getAvgPrice();
+
       switch (inventoryValuationTypeSelect) {
         case StockConfigRepository.VALUATION_TYPE_WAP_VALUE:
-          price = product.getAvgPrice();
+          price = productAvgPrice;
           break;
         case StockConfigRepository.VALUATION_TYPE_ACCOUNTING_VALUE:
           price = product.getCostPrice();
@@ -164,7 +166,11 @@ public class InventoryLineService {
           price = product.getPurchasePrice();
           break;
         case StockConfigRepository.VALUATION_TYPE_WAP_STOCK_LOCATION_VALUE:
-          price = stockLocationLine.getAvgPrice();
+          if (stockLocationLine != null) {
+            price = stockLocationLine.getAvgPrice();
+          } else {
+            price = productAvgPrice;
+          }
           break;
         default:
           price = product.getAvgPrice();
