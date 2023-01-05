79217443fc36e0d79aef2f140d26095c3dad8476
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-01-05 13:56:57 +0100

    RM#58828 Set the correct company bank details to generated interco invoice

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/IntercoServiceImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/IntercoServiceImpl.java
@@ -41,6 +41,7 @@ import com.axelor.apps.base.db.PriceList;
 import com.axelor.apps.base.db.repo.CompanyRepository;
 import com.axelor.apps.base.db.repo.PriceListRepository;
 import com.axelor.apps.base.service.AddressService;
+import com.axelor.apps.base.service.BankDetailsService;
 import com.axelor.apps.base.service.PartnerPriceListService;
 import com.axelor.apps.base.service.PartnerService;
 import com.axelor.apps.base.service.TradingNameService;
@@ -79,12 +80,15 @@ import java.util.Set;
 public class IntercoServiceImpl implements IntercoService {
 
   protected PurchaseConfigService purchaseConfigService;
+  protected BankDetailsService bankDetailsService;
 
   protected static int DEFAULT_INVOICE_COPY = 1;
 
   @Inject
-  public IntercoServiceImpl(PurchaseConfigService purchaseConfigService) {
+  public IntercoServiceImpl(
+      PurchaseConfigService purchaseConfigService, BankDetailsService bankDetailsService) {
     this.purchaseConfigService = purchaseConfigService;
+    this.bankDetailsService = bankDetailsService;
   }
 
   @Override
@@ -383,7 +387,6 @@ public class IntercoServiceImpl implements IntercoService {
             .getDefaultPriceList(intercoPartner, priceListRepositoryType);
 
     Invoice intercoInvoice = invoiceRepository.copy(invoice, true);
-
     intercoInvoice.setOperationTypeSelect(generatedOperationTypeSelect);
     intercoInvoice.setCompany(intercoCompany);
     intercoInvoice.setPartner(intercoPartner);
@@ -422,6 +425,9 @@ public class IntercoServiceImpl implements IntercoService {
 
     invoiceService.compute(intercoInvoice);
     intercoInvoice.setExternalReference(invoice.getInvoiceId());
+    intercoInvoice.setCompanyBankDetails(
+        bankDetailsService.getDefaultCompanyBankDetails(
+            intercoCompany, intercoPaymentMode, intercoPartner, generatedOperationTypeSelect));
     intercoInvoice = invoiceRepository.save(intercoInvoice);
 
     // the interco invoice needs to be saved before we can attach files to it
