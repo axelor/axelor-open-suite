32818d8c4e1fcc30eb05e07279000738c0f1db43
Edwin <e.roger-gorlin@axelor.com>
2023-01-18 08:59:31 +0100

    RM59233 INVOICES : Remove blocking rule in Advance payment invoices

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoice/workflow/ventilate/VentilateState.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoice/workflow/ventilate/VentilateState.java
@@ -136,7 +136,8 @@ public class VentilateState extends WorkflowInvoice {
     if (invoice.getPartnerAccount() == null) {
       Account account = Beans.get(InvoiceService.class).getPartnerAccount(invoice, false);
 
-      if (account == null) {
+      if (account == null
+          && invoice.getOperationSubTypeSelect() != InvoiceRepository.OPERATION_SUB_TYPE_ADVANCE) {
         throw new AxelorException(
             TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
             I18n.get(AccountExceptionMessage.VENTILATE_STATE_5));
