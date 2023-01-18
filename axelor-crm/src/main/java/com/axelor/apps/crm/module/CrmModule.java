46728a755626288a81e52459259a58f77605456a
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2023-01-18 17:37:54 +0100

    Revert changes made to message controller
    see RM58141

--- axelor-crm/src/main/java/com/axelor/apps/crm/module/CrmModule.java
+++ axelor-crm/src/main/java/com/axelor/apps/crm/module/CrmModule.java
@@ -43,8 +43,6 @@ import com.axelor.apps.crm.service.OpportunityService;
 import com.axelor.apps.crm.service.OpportunityServiceImpl;
 import com.axelor.apps.crm.service.app.AppCrmService;
 import com.axelor.apps.crm.service.app.AppCrmServiceImpl;
-import com.axelor.apps.crm.web.CrmGenerateMessageController;
-import com.axelor.apps.message.web.GenerateMessageController;
 
 public class CrmModule extends AxelorModule {
 
@@ -61,7 +59,6 @@ public class CrmModule extends AxelorModule {
     bind(LeadService.class).to(LeadServiceImpl.class);
     ICalendarEventFactory.register(ICalendarRepository.CRM_SYNCHRO, Event::new);
     bind(MessageServiceBaseImpl.class).to(MessageServiceCrmImpl.class);
-    bind(GenerateMessageController.class).to(CrmGenerateMessageController.class);
     bind(CrmReportingService.class).to(CrmReportingServiceImpl.class);
   }
 }
