7bf9a8fa4cfcd25e9805b6301912f225b832507f
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2023-01-26 18:16:50 +0100

    Anomaly #59453: Sale order partial invoicing
    (cherry picked from commit 41abf601059a87e53ff6cffecd9480b5712d0401)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/SaleOrderInvoiceService.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/SaleOrderInvoiceService.java
@@ -230,7 +230,8 @@ public interface SaleOrderInvoiceService {
       int operationSelect,
       SaleOrder saleOrder,
       Map<Long, BigDecimal> qtyToInvoiceMap,
-      Map<Long, BigDecimal> priceMap)
+      Map<Long, BigDecimal> priceMap,
+      boolean isPercent)
       throws AxelorException;
 
   /**
