b9bf35c2eb34600cecfc6ffaab0c6c2ccee8483e
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2023-01-27 11:26:17 +0100

    55740 link into invoice lines and order/contract lines for analytic (#9256)

---
+++ axelor-contract/src/main/java/com/axelor/apps/contract/web/AnalyticDistributionLineController.java
@@ -0,0 +1,22 @@
+package com.axelor.apps.contract.web;
+
+import com.axelor.apps.account.db.InvoiceLine;
+import com.axelor.exception.service.TraceBackService;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+
+public class AnalyticDistributionLineController {
+
+  public void linkWithContract(ActionRequest request, ActionResponse response) {
+    try {
+      Class<?> parentClass = request.getContext().getParent().getContextClass();
+      if ((InvoiceLine.class).equals(parentClass)) {
+        InvoiceLine invoiceLine = request.getContext().getParent().asType(InvoiceLine.class);
+        response.setValue("contractLine", invoiceLine.getContractLine());
+      }
+
+    } catch (Exception e) {
+      TraceBackService.trace(response, e);
+    }
+  }
+}
