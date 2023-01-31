18085a6090557ac764177b096ac0554efb4b8707
Dipankar Deka <d.deka@axelor.com>
2023-01-31 13:05:49 +0530

    Anomaly #58965 : Studio : Difficulty to insert fields

---
+++ axelor-studio/src/main/webapp/studio/custom-model/precache-manifest.d0ab9426221e0a8a198cd942994142f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3d701eb8a195775921c628d3b9b264",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c1add9d7d4f9cb1af9d",
+    "url": "./static/css/main.98d10164.chunk.css"
+  },
+  {
+    "revision": "7b55dc0cb636c35c08c4",
+    "url": "./static/js/2.e2ddc0be.chunk.js"
+  },
+  {
+    "revision": "ec406ee4360237332a305ebb6cbba0fe",
+    "url": "./static/js/2.e2ddc0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1add9d7d4f9cb1af9d",
+    "url": "./static/js/main.7bc1269e.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
