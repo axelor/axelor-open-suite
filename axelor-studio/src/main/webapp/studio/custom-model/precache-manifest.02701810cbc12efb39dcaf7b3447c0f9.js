1a42ef247f4f2c843ef76f3d7a2ceddc6885f539
Dipankar Deka <d.deka@axelor.com>
2023-02-07 17:48:18 +0530

    Studio : Fix editor issues

---
+++ axelor-studio/src/main/webapp/studio/custom-model/precache-manifest.02701810cbc12efb39dcaf7b3447c0f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc68aad73b55a20c412bf23ae1084d8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "551aa74183966873e497",
+    "url": "./static/css/main.36d1753c.chunk.css"
+  },
+  {
+    "revision": "9001a3405f84940e5809",
+    "url": "./static/js/2.1e8249ae.chunk.js"
+  },
+  {
+    "revision": "ec406ee4360237332a305ebb6cbba0fe",
+    "url": "./static/js/2.1e8249ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551aa74183966873e497",
+    "url": "./static/js/main.d6dc2f8c.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
