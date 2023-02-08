001f2790bcbb8e0160f7989d5f23d7946b0ea37e
Dipankar Deka <d.deka@axelor.com>
2023-02-08 18:50:24 +0530

    Anomaly #60151 : [DMN] Model change issue

---
+++ axelor-bpm/src/main/webapp/wkf-editor/precache-manifest.e0fae5759b18e4460661da59f28ad1ef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff95eeaa58f22f8acede1639e9ab07f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29ae7933f164b2b42c84",
+    "url": "./static/css/2.c4daa5fe.chunk.css"
+  },
+  {
+    "revision": "a0b71f1e57824af73840",
+    "url": "./static/css/3.24a5c644.chunk.css"
+  },
+  {
+    "revision": "67342277fea6400197b3",
+    "url": "./static/css/main.4e427609.chunk.css"
+  },
+  {
+    "revision": "29ae7933f164b2b42c84",
+    "url": "./static/js/2.b69842a6.chunk.js"
+  },
+  {
+    "revision": "308b40895c213754d4f625cb5d8b532e",
+    "url": "./static/js/2.b69842a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b71f1e57824af73840",
+    "url": "./static/js/3.ef50a943.chunk.js"
+  },
+  {
+    "revision": "67342277fea6400197b3",
+    "url": "./static/js/main.095764bc.chunk.js"
+  },
+  {
+    "revision": "f9b12699d006eb3e0095",
+    "url": "./static/js/runtime-main.75c3601e.js"
+  },
+  {
+    "revision": "08b5f2819c43514afa3a1985d5aad804",
+    "url": "./static/media/bpmn.08b5f281.eot"
+  },
+  {
+    "revision": "1381b44efba43dfddc655c7cbca5b272",
+    "url": "./static/media/bpmn.1381b44e.svg"
+  }
+]);
