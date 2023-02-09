3af58c7d4c4e5bc436c483cbed96ca8b54369d01
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-02-09 15:36:32 +0100

    RM#60444 Refactor controllers to remove constructor injection

--- axelor-gdpr/src/main/java/com/axelor/apps/gdpr/web/GDPRRequestController.java
+++ axelor-gdpr/src/main/java/com/axelor/apps/gdpr/web/GDPRRequestController.java
@@ -7,96 +7,72 @@ import com.axelor.apps.gdpr.db.repo.GDPRRequestRepository;
 import com.axelor.apps.gdpr.service.GDPRAccessResponseService;
 import com.axelor.apps.gdpr.service.GDPRErasureResponseService;
 import com.axelor.exception.AxelorException;
-import com.axelor.exception.db.repo.TraceBackRepository;
 import com.axelor.exception.service.TraceBackService;
 import com.axelor.i18n.I18n;
+import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
-import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
-import java.io.IOException;
-import javax.mail.MessagingException;
-import wslite.json.JSONException;
 
 public class GDPRRequestController {
 
-  protected GDPRRequestRepository gdprRequestRepository;
-  protected GDPRAccessResponseService gdprAccessResponseService;
-  protected GDPRErasureResponseService gdprErasureResponseService;
-
-  @Inject
-  public GDPRRequestController(
-      GDPRRequestRepository gdprRequestRepository,
-      GDPRAccessResponseService gdprAccessResponseService,
-      GDPRErasureResponseService gdprErasureResponseService) {
-    this.gdprRequestRepository = gdprRequestRepository;
-    this.gdprAccessResponseService = gdprAccessResponseService;
-    this.gdprErasureResponseService = gdprErasureResponseService;
-  }
-
   public void generateResponse(ActionRequest request, ActionResponse response) {
-    GDPRRequest gdprRequest = request.getContext().asType(GDPRRequest.class);
-    gdprRequest = gdprRequestRepository.find(gdprRequest.getId());
+    try {
+      GDPRRequest gdprRequest = request.getContext().asType(GDPRRequest.class);
+      gdprRequest = Beans.get(GDPRRequestRepository.class).find(gdprRequest.getId());
+      GDPRErasureResponseService gdprErasureResponseService =
+          Beans.get(GDPRErasureResponseService.class);
 
-    if (gdprRequest.getTypeSelect() == GDPRRequestRepository.REQUEST_TYPE_ACCESS) {
-      try {
+      if (gdprRequest.getTypeSelect() == GDPRRequestRepository.REQUEST_TYPE_ACCESS) {
         GDPRAccessResponse gdprAccessResponse =
-            gdprAccessResponseService.generateAccessResponseDataFile(gdprRequest);
+            Beans.get(GDPRAccessResponseService.class).generateAccessResponseDataFile(gdprRequest);
         response.setValue("AccessResponse", gdprAccessResponse);
-      } catch (ClassNotFoundException | AxelorException | IOException e) {
-        TraceBackService.trace(e);
-      }
-    } else {
-      try {
+      } else {
         GDPRErasureResponse gdprErasureResponse =
             gdprErasureResponseService.createErasureResponse(gdprRequest);
         gdprErasureResponseService.anonymizeTrackingDatas(gdprRequest);
         response.setValue("ErasureResponse", gdprErasureResponse);
-      } catch (AxelorException | ClassNotFoundException | JSONException | IOException e) {
-        TraceBackService.trace(e);
       }
-    }
 
-    gdprRequest.setStatusSelect(GDPRRequestRepository.REQUEST_STATUS_CONFIRMED);
-    saveGdprRequest(gdprRequest);
-    response.setReload(true);
+      gdprRequest.setStatusSelect(GDPRRequestRepository.REQUEST_STATUS_CONFIRMED);
+      saveGdprRequest(gdprRequest);
+      response.setReload(true);
+    } catch (Exception e) {
+      TraceBackService.trace(response, e);
+    }
   }
 
   @Transactional(rollbackOn = {AxelorException.class, RuntimeException.class})
   public void saveGdprRequest(GDPRRequest gdprRequest) {
-    gdprRequestRepository.save(gdprRequest);
+    Beans.get(GDPRRequestRepository.class).save(gdprRequest);
   }
 
   @Transactional(rollbackOn = {AxelorException.class, RuntimeException.class})
-  public void sendResponse(ActionRequest request, ActionResponse response) throws AxelorException {
-    GDPRRequest gdprRequest = request.getContext().asType(GDPRRequest.class);
-    gdprRequest = gdprRequestRepository.find(gdprRequest.getId());
-
-    boolean success;
-
+  public void sendResponse(ActionRequest request, ActionResponse response) {
     try {
-      if (gdprRequest.getTypeSelect() == GDPRRequestRepository.REQUEST_TYPE_ACCESS) {
-        success = gdprAccessResponseService.sendEmailResponse(gdprRequest.getAccessResponse());
-      } else {
-        success = gdprErasureResponseService.sendEmailResponse(gdprRequest.getErasureResponse());
-      }
-    } catch (ClassNotFoundException
-        | InstantiationException
-        | IllegalAccessException
-        | AxelorException
-        | MessagingException
-        | IOException
-        | JSONException e) {
-      TraceBackService.trace(e);
-      throw new AxelorException(TraceBackRepository.CATEGORY_CONFIGURATION_ERROR, e.getMessage());
-    }
+      GDPRRequest gdprRequest = request.getContext().asType(GDPRRequest.class);
+      GDPRRequestRepository gdprRequestRepository = Beans.get(GDPRRequestRepository.class);
+      gdprRequest = gdprRequestRepository.find(gdprRequest.getId());
+      boolean success;
 
-    if (success) {
-      gdprRequest.setStatusSelect(GDPRRequestRepository.REQUEST_STATUS_SENT);
-      gdprRequestRepository.save(gdprRequest);
-      response.setReload(true);
-    } else {
-      response.setError(I18n.get("Missing mail template"));
+      if (gdprRequest.getTypeSelect() == GDPRRequestRepository.REQUEST_TYPE_ACCESS) {
+        success =
+            Beans.get(GDPRAccessResponseService.class)
+                .sendEmailResponse(gdprRequest.getAccessResponse());
+      } else {
+        success =
+            Beans.get(GDPRErasureResponseService.class)
+                .sendEmailResponse(gdprRequest.getErasureResponse());
+      }
+      if (success) {
+        gdprRequest.setStatusSelect(GDPRRequestRepository.REQUEST_STATUS_SENT);
+        gdprRequestRepository.save(gdprRequest);
+        response.setReload(true);
+      } else {
+        response.setError(I18n.get("Missing mail template"));
+      }
+    } catch (Exception e) {
+      TraceBackService.trace(response, e);
     }
   }
 }
