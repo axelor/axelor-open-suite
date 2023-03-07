91f486e867979774d2daeabb526558a57ebcba37
Jonathan Michelon <j.michelon@gmail.com>
2023-03-07 15:45:45 +0100

    Apply spotless

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/PurchaseOrderWorkflowServiceProjectImpl.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/PurchaseOrderWorkflowServiceProjectImpl.java
@@ -61,7 +61,7 @@ public class PurchaseOrderWorkflowServiceProjectImpl
         appAccountService,
         budgetSupplychainService,
         purchaseOrderSupplychainService,
-            purchaseOrderBudgetService);
+        purchaseOrderBudgetService);
     this.analyticMoveLineRepository = analyticMoveLineRepository;
   }
 
