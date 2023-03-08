947546892f008e086376906ceabb1b14cc7808c8
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-03-08 15:26:05 +0100

    RM#52568 Set the rollbackOn on transactional methods in account module

--- axelor-account/src/main/java/com/axelor/apps/account/service/fixedasset/FixedAssetLineMoveServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/fixedasset/FixedAssetLineMoveServiceImpl.java
@@ -271,7 +271,7 @@ public class FixedAssetLineMoveServiceImpl implements FixedAssetLineMoveService 
     }
   }
 
-  @Transactional
+  @Transactional(rollbackOn = {Exception.class})
   protected Move generateImpairementAccountMove(FixedAssetLine fixedAssetLine, boolean isSimulated)
       throws AxelorException {
     FixedAsset fixedAsset = fixedAssetLineService.getFixedAsset(fixedAssetLine);
@@ -873,7 +873,7 @@ public class FixedAssetLineMoveServiceImpl implements FixedAssetLineMoveService 
     fixedAssetRepo.save(fixedAsset);
   }
 
-  @Transactional
+  @Transactional(rollbackOn = {Exception.class})
   @Override
   public void simulate(FixedAssetLine fixedAssetLine) throws AxelorException {
     Objects.requireNonNull(fixedAssetLine);
