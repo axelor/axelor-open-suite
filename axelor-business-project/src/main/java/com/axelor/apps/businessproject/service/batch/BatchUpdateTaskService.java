58e3a8d10b3f93e383de52d78922b987760ac8f9
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-03-08 12:06:15 +0100

    RM#50956 : Project : Fix problem while changing project task (#9948)
    (cherry picked from commit ed9654f25694d389a1f763eb81b0d528fe2534a1)

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/batch/BatchUpdateTaskService.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/batch/BatchUpdateTaskService.java
@@ -95,7 +95,7 @@ public class BatchUpdateTaskService extends AbstractBatch {
       findBatch();
       for (ProjectTask projectTask : taskList) {
         try {
-          projectTask = projectTaskBusinessProjectService.updateTaskFinancialInfo(projectTask);
+          projectTask = projectTaskBusinessProjectService.setProjectTaskValues(projectTask);
         } catch (Exception e) {
           incrementAnomaly();
           TraceBackService.trace(
