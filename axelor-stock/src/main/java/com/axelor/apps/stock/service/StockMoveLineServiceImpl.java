fa1353bf98b77ecc36b97f13fb566a4623eab122
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-03-09 13:26:47 +0100

    RM#61473 Fix wap price computation for supplier arrival
    (cherry picked from commit e9f3d8b198834e71f5b8c867fdac8dcda745f613)

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineServiceImpl.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineServiceImpl.java
@@ -555,8 +555,8 @@ public class StockMoveLineServiceImpl implements StockMoveLineService {
 
       newPrice =
           unitConversionService.convert(
-              stockMoveLineUnit,
               stockLocationLineUnit,
+              stockMoveLineUnit,
               newPrice,
               newPrice.scale(),
               stockMoveLine.getProduct());
