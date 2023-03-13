1d8c0533b730d3303916c92a0ec9b5eff5c919a4
sro-axelor <s.kumarrout@axelor.com>
2023-03-13 15:31:55 +0530

    Feature RM#61648 [axelor-production: UnitCostCalculation] - Refactor date to datetime

--- axelor-production/src/main/java/com/axelor/apps/production/service/costsheet/UnitCostCalculationServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/costsheet/UnitCostCalculationServiceImpl.java
@@ -230,9 +230,11 @@ public class UnitCostCalculationServiceImpl implements UnitCostCalculationServic
   @Transactional
   protected void updateStatusToComputed(UnitCostCalculation unitCostCalculation) {
 
-    unitCostCalculation.setCalculationDate(
-        appProductionService.getTodayDate(
-            Optional.ofNullable(AuthUtils.getUser()).map(User::getActiveCompany).orElse(null)));
+    unitCostCalculation.setCalculationDateTime(
+        appProductionService
+            .getTodayDateTime(
+                Optional.ofNullable(AuthUtils.getUser()).map(User::getActiveCompany).orElse(null))
+            .toLocalDateTime());
 
     unitCostCalculation.setStatusSelect(UnitCostCalculationRepository.STATUS_COSTS_COMPUTED);
 
@@ -495,9 +497,11 @@ public class UnitCostCalculationServiceImpl implements UnitCostCalculationServic
   @Transactional
   protected void updateStatusProductCostPriceUpdated(UnitCostCalculation unitCostCalculation) {
 
-    unitCostCalculation.setUpdateCostDate(
-        appProductionService.getTodayDate(
-            Optional.ofNullable(AuthUtils.getUser()).map(User::getActiveCompany).orElse(null)));
+    unitCostCalculation.setUpdateCostDateTime(
+        appProductionService
+            .getTodayDateTime(
+                Optional.ofNullable(AuthUtils.getUser()).map(User::getActiveCompany).orElse(null))
+            .toLocalDateTime());
 
     unitCostCalculation.setStatusSelect(UnitCostCalculationRepository.STATUS_COSTS_UPDATED);
 
