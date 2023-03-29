11d68064682035c4a5b621d68bd69bb0b73d0c04
cce-axelor <c.chea@axelor.com>
2023-03-29 10:37:30 +0200

    #62135: fix wildcard import

--- axelor-base/src/main/java/com/axelor/apps/base/service/FakerServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/FakerServiceImpl.java
@@ -31,12 +31,12 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.concurrent.TimeUnit;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import net.datafaker.Faker;
 import org.apache.commons.lang3.ClassUtils;
 
