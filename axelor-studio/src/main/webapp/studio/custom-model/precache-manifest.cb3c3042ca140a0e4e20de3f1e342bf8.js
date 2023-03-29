83457ff0fdcfed30482b60d22de48a87dce65461
Akshay Gandhi <a.gandhi@axelor.com>
2023-03-29 16:56:00 +0530

    RM#62419 Studio editor : Fix theme issue

---
+++ axelor-studio/src/main/webapp/studio/custom-model/precache-manifest.cb3c3042ca140a0e4e20de3f1e342bf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a5b6622466ff8bbe4b6ff57f855364",
+    "url": "./index.html"
+  },
+  {
+    "revision": "942c51da18eb31963dce",
+    "url": "./static/css/main.3a42d71c.chunk.css"
+  },
+  {
+    "revision": "d81ab38e170312d9fae4",
+    "url": "./static/js/2.ecbc2613.chunk.js"
+  },
+  {
+    "revision": "ec406ee4360237332a305ebb6cbba0fe",
+    "url": "./static/js/2.ecbc2613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942c51da18eb31963dce",
+    "url": "./static/js/main.68a0cf09.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
