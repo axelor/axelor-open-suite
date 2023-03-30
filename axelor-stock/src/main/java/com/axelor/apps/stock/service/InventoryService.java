3338e415dc9f86769c257e2c8a55e9e2d5bd00de
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2023-03-30 15:45:04 +0200

    Anomaly #60953: Fixed update inventory line on import

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/InventoryService.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/InventoryService.java
@@ -276,7 +276,7 @@ public class InventoryService {
   }
 
   protected InventoryLine copyAndEditInventoryLine(
-      InventoryLine inventoryLine, String description, BigDecimal realQty) {
+      InventoryLine inventoryLine, String description, BigDecimal realQty) throws AxelorException {
 
     // There is not one to many for inventoryLine, so true or false is the same.
     InventoryLine inventoryLineResult = inventoryLineRepository.copy(inventoryLine, true);
@@ -285,6 +285,7 @@ public class InventoryService {
     if (inventoryLineResult.getTrackingNumber() != null) {
       inventoryLineResult.getTrackingNumber().setCounter(realQty);
     }
+    inventoryLineService.compute(inventoryLineResult, inventoryLineResult.getInventory());
     return inventoryLineResult;
   }
 
