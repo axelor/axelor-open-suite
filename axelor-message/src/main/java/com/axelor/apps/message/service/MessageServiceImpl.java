6fc89eb5c6e80beb2ae804c46fe6fe6e5f9cf136
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2023-03-30 14:35:24 +0200

    Anomaly #61221: Fixed a bug where mail could not be send without content
    (cherry picked from commit c30f2ef928ae0b6134c2f733748f022995dab4bc)

--- axelor-message/src/main/java/com/axelor/apps/message/service/MessageServiceImpl.java
+++ axelor-message/src/main/java/com/axelor/apps/message/service/MessageServiceImpl.java
@@ -426,6 +426,8 @@ public class MessageServiceImpl extends JpaSupport implements MessageService {
     }
     if (!Strings.isNullOrEmpty(message.getContent())) {
       mailBuilder.html(message.getContent());
+    } else {
+      mailBuilder.html("");
     }
 
     if (!isTemporaryEmail) {
