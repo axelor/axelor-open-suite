97cb154367ad29b3eeff15f267f16fcbfa4df4fc
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-04-03 16:53:32 +0200

    RM#60983 Error management

--- axelor-base/src/main/java/com/axelor/apps/base/module/ControllerMethodInterceptor.java
+++ axelor-base/src/main/java/com/axelor/apps/base/module/ControllerMethodInterceptor.java
@@ -1,20 +1,43 @@
 package com.axelor.apps.base.module;
 
+import com.axelor.apps.base.ResponseMessageType;
 import com.axelor.apps.base.service.exception.TraceBackService;
 import com.axelor.rpc.ActionResponse;
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
 public class ControllerMethodInterceptor implements MethodInterceptor {
+
+  public static final String EXCEPTION_ERROR_ANNOTATION_PACKAGE =
+      "com.axelor.apps.base.service.exception.ErrorException";
+
   @Override
   public Object invoke(MethodInvocation invocation) throws Throwable {
     ActionResponse response = getActionResponse(invocation);
-    try {
-      invocation.proceed();
-    } catch (Exception e) {
-      TraceBackService.trace(response, e);
+    if (manageErrorException(invocation)) {
+      try {
+        invocation.proceed();
+      } catch (Exception e) {
+        TraceBackService.trace(response, e, ResponseMessageType.ERROR);
+      }
+    } else {
+      try {
+        invocation.proceed();
+      } catch (Exception e) {
+        TraceBackService.trace(response, e);
+      }
     }
     return null;
+  }
+
+  protected boolean manageErrorException(MethodInvocation invocation) {
+    Annotation[] annotations = invocation.getMethod().getDeclaredAnnotations();
+    return Arrays.stream(annotations)
+        .anyMatch(
+            annotation ->
+                EXCEPTION_ERROR_ANNOTATION_PACKAGE.equals(annotation.annotationType().getName()));
   }
 
   protected ActionResponse getActionResponse(MethodInvocation invocation) {
