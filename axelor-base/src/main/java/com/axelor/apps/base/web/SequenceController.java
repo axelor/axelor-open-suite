413d4f25aea7a93cdf768e7ad01794836df49d48
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-04-04 15:48:26 +0200

    Anomaly #47499 : Sequence: length check issues (#7825)

--- axelor-base/src/main/java/com/axelor/apps/base/web/SequenceController.java
+++ axelor-base/src/main/java/com/axelor/apps/base/web/SequenceController.java
@@ -20,6 +20,7 @@ package com.axelor.apps.base.web;
 import com.axelor.apps.base.db.Sequence;
 import com.axelor.apps.base.service.administration.SequenceService;
 import com.axelor.apps.base.service.app.AppBaseService;
+import com.axelor.exception.ResponseMessageType;
 import com.axelor.exception.service.TraceBackService;
 import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
@@ -75,6 +76,16 @@ public class SequenceController {
       }
     } catch (Exception e) {
       TraceBackService.trace(response, e);
+    }
+  }
+
+  public void isLengthValid(ActionRequest request, ActionResponse response) {
+
+    try {
+      Sequence sequence = request.getContext().asType(Sequence.class);
+      Beans.get(SequenceService.class).checkSequenceLengthValidity(sequence);
+    } catch (Exception e) {
+      TraceBackService.trace(response, e, ResponseMessageType.ERROR);
     }
   }
 }
