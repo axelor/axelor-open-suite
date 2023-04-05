25b352b362f3d7eb145890f68d5bc9bd6c0fbd68
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-04-05 16:17:37 +0200

    RM#55132 Condition to display the entry menu

---
+++ axelor-base/src/main/java/com/axelor/apps/base/service/SwaggerService.java
@@ -0,0 +1,14 @@
+package com.axelor.apps.base.service;
+
+import com.axelor.app.AppSettings;
+
+public class SwaggerService {
+  public static boolean isSwaggerEnabled() {
+    AppSettings appSettings = AppSettings.get();
+    String resourcePackages = appSettings.get("aos.swagger.resource.packages");
+    boolean resourcesEmpty = resourcePackages == null || resourcePackages.isEmpty();
+    return Boolean.parseBoolean(appSettings.get("aos.swagger.enable"))
+        && Boolean.parseBoolean(appSettings.get("aos.api.enable"))
+        && !resourcesEmpty;
+  }
+}
