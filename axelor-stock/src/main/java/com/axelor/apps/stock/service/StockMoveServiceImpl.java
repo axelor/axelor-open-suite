5d39d7aaba196a2312d8d2a1069ec5e22bea27e8
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2023-04-20 15:12:52 +0200

    Anomaly #60928: Prevent splitting with stock move lines  (#10227)
    (cherry picked from commit e057a8e969d19efba077b2c13918f20f82245ffd)
    (cherry picked from commit b9aa119bd92151599ee6eede34d7b33156985d20)

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveServiceImpl.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveServiceImpl.java
@@ -894,7 +894,8 @@ public class StockMoveServiceImpl implements StockMoveService {
   @Override
   @Transactional
   public boolean splitStockMoveLines(
-      StockMove stockMove, List<StockMoveLine> stockMoveLines, BigDecimal splitQty) {
+      StockMove stockMove, List<StockMoveLine> stockMoveLines, BigDecimal splitQty)
+      throws AxelorException {
 
     boolean selected = false;
 
