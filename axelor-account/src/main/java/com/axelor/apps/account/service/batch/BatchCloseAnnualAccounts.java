e28e21747b42f948488cb14d9a029a6653188849
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2023-05-12 10:59:25 +0200

    64200 ACCOUNTING BATCH: fix duplicated moves in closure/opening batch

--- axelor-account/src/main/java/com/axelor/apps/account/service/batch/BatchCloseAnnualAccounts.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/batch/BatchCloseAnnualAccounts.java
@@ -193,14 +193,18 @@ public class BatchCloseAnnualAccounts extends BatchStrategy {
           accountingCloseAnnualService.assignPartner(
               openingAccountIdList, year, allocatePerPartner);
       Map<AccountByPartner, Map<Boolean, Boolean>> map = new HashMap<>();
-      openAndCloseProcess(
-          closureAccountAndPartnerPairList, accountingBatch.getCloseYear(), false, map);
-      openAndCloseProcess(
-          openingAccountAndPartnerPairList, false, accountingBatch.getOpenYear(), map);
+      map =
+          openAndCloseProcess(
+              closureAccountAndPartnerPairList, accountingBatch.getCloseYear(), false, map);
+      map =
+          openAndCloseProcess(
+              openingAccountAndPartnerPairList, false, accountingBatch.getOpenYear(), map);
+
+      generateMoves(map);
     }
   }
 
-  protected void openAndCloseProcess(
+  protected Map<AccountByPartner, Map<Boolean, Boolean>> openAndCloseProcess(
       List<Pair<Long, Long>> accountAndPartnerPairList,
       boolean close,
       boolean open,
@@ -227,8 +231,8 @@ public class BatchCloseAnnualAccounts extends BatchStrategy {
           map.put(accountByPartner, value);
         }
       }
-      generateMoves(map);
     }
+    return map;
   }
 
   protected void generateMoves(Map<AccountByPartner, Map<Boolean, Boolean>> map) {
