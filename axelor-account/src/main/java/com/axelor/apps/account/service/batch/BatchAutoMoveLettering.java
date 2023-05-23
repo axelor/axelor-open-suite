fba1f7bbc95ddd852a05d9b74386f044ceb5ffbb
alg-axelor <91078097+alg-axelor@users.noreply.github.com>
2023-05-23 10:47:40 +0200

    64538 - ACCOUNTINGBATCH : automatic reconciliations error message org.hibernate.LazyInitializationException: could not initialize proxy - no Session

--- axelor-account/src/main/java/com/axelor/apps/account/service/batch/BatchAutoMoveLettering.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/batch/BatchAutoMoveLettering.java
@@ -180,11 +180,6 @@ public class BatchAutoMoveLettering extends BatchStrategy {
   protected void reconcileWithMethod(
       List<MoveLine> debitMoveLines, List<MoveLine> creditMoveLines, int reconcileMethodSelect) {
 
-    Comparator<MoveLine> moveLineComparator = getMoveLineComparator();
-
-    creditMoveLines.sort(Comparator.nullsLast(moveLineComparator));
-    debitMoveLines.sort(Comparator.nullsLast(moveLineComparator));
-
     BigDecimal debitTotalRemaining =
         debitMoveLines.stream()
             .map(MoveLine::getAmountRemaining)
@@ -473,10 +468,23 @@ public class BatchAutoMoveLettering extends BatchStrategy {
   }
 
   protected Map<List<Object>, Pair<List<MoveLine>, List<MoveLine>>> getMoveLinesMap() {
-    return moveLineService.getPopulatedReconcilableMoveLineMap(
-        getMoveLinesQuery().fetch().stream()
-            .filter(moveLine -> moveLineControlService.canReconcile(moveLine))
-            .collect(Collectors.toList()));
+    Map<List<Object>, Pair<List<MoveLine>, List<MoveLine>>> listPairMap =
+        moveLineService.getPopulatedReconcilableMoveLineMap(
+            getMoveLinesQuery().fetch().stream()
+                .filter(moveLine -> moveLineControlService.canReconcile(moveLine))
+                .collect(Collectors.toList()));
+
+    Comparator<MoveLine> moveLineComparator = getMoveLineComparator();
+
+    listPairMap
+        .values()
+        .forEach(
+            listListPair -> {
+              listListPair.getLeft().sort(Comparator.nullsLast(moveLineComparator));
+              listListPair.getRight().sort(Comparator.nullsLast(moveLineComparator));
+            });
+
+    return listPairMap;
   }
 
   public boolean existsAlreadyRunning(AccountingBatch accountingBatch) {
