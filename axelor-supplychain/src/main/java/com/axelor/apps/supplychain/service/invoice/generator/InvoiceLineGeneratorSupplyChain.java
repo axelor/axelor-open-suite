abccb99b64e8a546aebf135f90b0be5e59a7a171
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-05-25 10:49:15 +0200

    RM#64586 Invoice generation from PurchaseOrder : take tile PO line in… (#10455)
    * RM#64586 Invoice generation from PurchaseOrder : take tile PO line into account
    
    * RM#64586 Take into account PO line without product
    
    (cherry picked from commit 5b7dd515c4998183d669247b6ad4cf6aced738d4)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/invoice/generator/InvoiceLineGeneratorSupplyChain.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/invoice/generator/InvoiceLineGeneratorSupplyChain.java
@@ -223,31 +223,33 @@ public abstract class InvoiceLineGeneratorSupplyChain extends InvoiceLineGenerat
       }
 
     } else if (purchaseOrderLine != null) {
-
-      if (purchaseOrderLine.getAnalyticDistributionTemplate() != null
-          || !ObjectUtils.isEmpty(purchaseOrderLine.getAnalyticMoveLineList())) {
-        invoiceLine.setAnalyticDistributionTemplate(
-            purchaseOrderLine.getAnalyticDistributionTemplate());
-        this.copyAnalyticMoveLines(purchaseOrderLine.getAnalyticMoveLineList(), invoiceLine);
-        analyticMoveLineList = invoiceLineService.computeAnalyticDistribution(invoiceLine);
+      if (purchaseOrderLine.getIsTitleLine()) {
+        return invoiceLine;
       } else {
-        analyticMoveLineList =
-            invoiceLineService.getAndComputeAnalyticDistribution(invoiceLine, invoice);
-        analyticMoveLineList.stream().forEach(invoiceLine::addAnalyticMoveLineListItem);
+        if (purchaseOrderLine.getAnalyticDistributionTemplate() != null
+            || !ObjectUtils.isEmpty(purchaseOrderLine.getAnalyticMoveLineList())) {
+          invoiceLine.setAnalyticDistributionTemplate(
+              purchaseOrderLine.getAnalyticDistributionTemplate());
+          this.copyAnalyticMoveLines(purchaseOrderLine.getAnalyticMoveLineList(), invoiceLine);
+          analyticMoveLineList = invoiceLineService.computeAnalyticDistribution(invoiceLine);
+        } else {
+          analyticMoveLineList =
+              invoiceLineService.getAndComputeAnalyticDistribution(invoiceLine, invoice);
+          analyticMoveLineList.stream().forEach(invoiceLine::addAnalyticMoveLineListItem);
+        }
+
+        this.copyBudgetDistributionList(purchaseOrderLine.getBudgetDistributionList(), invoiceLine);
+        invoiceLine.setBudget(purchaseOrderLine.getBudget());
+        invoiceLine.setBudgetDistributionSumAmount(
+            purchaseOrderLine.getBudgetDistributionSumAmount());
+        invoiceLine.setFixedAssets(purchaseOrderLine.getFixedAssets());
+
+        if (product != null && purchaseOrderLine.getFixedAssets()) {
+          FixedAssetCategory fixedAssetCategory =
+              accountManagementService.getProductFixedAssetCategory(product, invoice.getCompany());
+          invoiceLine.setFixedAssetCategory(fixedAssetCategory);
+        }
       }
-
-      this.copyBudgetDistributionList(purchaseOrderLine.getBudgetDistributionList(), invoiceLine);
-      invoiceLine.setBudget(purchaseOrderLine.getBudget());
-      invoiceLine.setBudgetDistributionSumAmount(
-          purchaseOrderLine.getBudgetDistributionSumAmount());
-      invoiceLine.setFixedAssets(purchaseOrderLine.getFixedAssets());
-
-      if (product != null && purchaseOrderLine.getFixedAssets()) {
-        FixedAssetCategory fixedAssetCategory =
-            accountManagementService.getProductFixedAssetCategory(product, invoice.getCompany());
-        invoiceLine.setFixedAssetCategory(fixedAssetCategory);
-      }
-
     } else if (stockMoveLine != null) {
 
       this.price = stockMoveLine.getUnitPriceUntaxed();
