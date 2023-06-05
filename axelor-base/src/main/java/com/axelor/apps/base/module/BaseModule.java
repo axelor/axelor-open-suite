c31f3d7f2f80f614d1183e5a1e842b1a5555e762
pda-axelor <51471324+pda-axelor@users.noreply.github.com>
2023-06-05 19:50:00 +0530

    Feature RM#64132 BirtTemplate: add button to reload from source (#10370)

--- axelor-base/src/main/java/com/axelor/apps/base/module/BaseModule.java
+++ axelor-base/src/main/java/com/axelor/apps/base/module/BaseModule.java
@@ -79,6 +79,8 @@ import com.axelor.apps.base.service.BankServiceImpl;
 import com.axelor.apps.base.service.BarcodeGeneratorService;
 import com.axelor.apps.base.service.BarcodeGeneratorServiceImpl;
 import com.axelor.apps.base.service.BaseReportGenerator;
+import com.axelor.apps.base.service.BirtTemplateViewService;
+import com.axelor.apps.base.service.BirtTemplateViewServiceImpl;
 import com.axelor.apps.base.service.CompanyService;
 import com.axelor.apps.base.service.CompanyServiceImpl;
 import com.axelor.apps.base.service.DMSImportWizardService;
@@ -341,5 +343,6 @@ public class BaseModule extends AxelorModule {
     bind(BirtTemplateService.class).to(BirtTemplateServiceImpl.class);
     bind(BaseServerStartListener.class);
     bind(AosSwagger.class);
+    bind(BirtTemplateViewService.class).to(BirtTemplateViewServiceImpl.class);
   }
 }
