bbab14f4b15ab83b2c71373b3dd270e58d0252cc
cce-axelor <113428800+cce-axelor@users.noreply.github.com>
2023-06-09 18:13:16 +0200

    #65313: fix use of app/project hours per day configuration, fix excep… (#10586)
    * #65313: fix use of app/project hours per day configuration, fix exception message

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/exception/BusinessProjectExceptionMessage.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/exception/BusinessProjectExceptionMessage.java
@@ -78,7 +78,7 @@ public final class BusinessProjectExceptionMessage {
       "Sold time value error for project task %s." /*)*/;
 
   public static final String PROJECT_CONFIG_DEFAULT_HOURS_PER_DAY_MISSING = /*$$(*/
-      "Please set the default number of hours per day in the configuration of app Business Project." /*)*/;
+      "Please set the default number of hours per day in the project configuration." /*)*/;
 
   public static final String PROJECT_CONFIG_DAYS_UNIT_MISSING = /*$$(*/
       "Please set days unit in the configuration of app Business Project." /*)*/;
