e38eb64a7fc19698ef34dd9f65e8bd341bbb9203
jka-axelor <j.kar@axelor.com>
2023-06-19 19:54:32 +0530

    Anomaly RM#64231 Quality control default stock location: add check when it's used
    (cherry picked from commit 82d9640dba20a85b28bcf74375a7587e37b17fdc)
    (cherry picked from commit 8ec6df97bdc0de59925476f4878ebd2132324e0e)
    (cherry picked from commit d5a13e08c0d019f5a084cdd81710ec01d6b62b7b)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/PurchaseOrderStockServiceImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/PurchaseOrderStockServiceImpl.java
@@ -212,8 +212,8 @@ public class PurchaseOrderStockServiceImpl implements PurchaseOrderStockService 
       }
 
     } else {
-      endLocation =
-          stockConfigService.getStockConfig(company).getQualityControlDefaultStockLocation();
+      StockConfig stockConfig = stockConfigService.getStockConfig(company);
+      endLocation = stockConfigService.getQualityControlDefaultStockLocation(stockConfig);
     }
 
     StockMove qualityStockMove =
