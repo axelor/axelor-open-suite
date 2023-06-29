8ef0b19c68a31c50f9691006c59fb75d57a70734
aol-axelor <a.olivier@axelor.com>
2023-06-29 14:09:16 +0200

    improve condition

--- axelor-account/src/main/java/com/axelor/apps/account/service/moveline/MoveLineCreateServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/moveline/MoveLineCreateServiceImpl.java
@@ -492,7 +492,10 @@ public class MoveLineCreateServiceImpl implements MoveLineCreateService {
 
         TaxLine taxLine = invoiceLine.getTaxLine();
 
-        if (taxLine == null && invoiceLine.getAccount() != null) {
+        if (taxLine == null
+            && invoiceLine.getAccount() != null
+            && invoiceLine.getAccount().getIsTaxRequiredOnMoveLine()
+            && invoiceLine.getAccount().getDefaultTax() != null) {
           taxLine = invoiceLine.getAccount().getDefaultTax().getActiveTaxLine();
         }
 
