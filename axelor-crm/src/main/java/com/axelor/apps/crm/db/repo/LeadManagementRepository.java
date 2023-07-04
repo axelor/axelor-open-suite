08b6841711e124e5dad889e8f2938088e4f3cfa8
nbu-axelor <132373223+nbu-axelor@users.noreply.github.com>
2023-07-04 11:22:25 +0200

    RM#66521 CRM : LEAD copy When we copy a Lead, reset the following fields : - contactDate - leadStatus

--- axelor-crm/src/main/java/com/axelor/apps/crm/db/repo/LeadManagementRepository.java
+++ axelor-crm/src/main/java/com/axelor/apps/crm/db/repo/LeadManagementRepository.java
@@ -18,13 +18,45 @@
  */
 package com.axelor.apps.crm.db.repo;
 
+import com.axelor.apps.base.AxelorException;
+import com.axelor.apps.base.db.repo.TraceBackRepository;
 import com.axelor.apps.base.service.exception.TraceBackService;
 import com.axelor.apps.crm.db.Lead;
+import com.axelor.apps.crm.exception.CrmExceptionMessage;
 import com.axelor.apps.crm.service.LeadService;
+import com.axelor.apps.crm.service.app.AppCrmService;
+import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
+import com.google.inject.Inject;
 import javax.persistence.PersistenceException;
 
 public class LeadManagementRepository extends LeadRepository {
+
+  protected AppCrmService appCrmService;
+
+  @Inject
+  public LeadManagementRepository(AppCrmService appCrmService) {
+    this.appCrmService = appCrmService;
+  }
+
+  @Override
+  public Lead copy(Lead entity, boolean deep) {
+    Lead lead = super.copy(entity, deep);
+    lead.setContactDate(null);
+    if (appCrmService.getAppCrm() != null
+        && appCrmService.getAppCrm().getLeadDefaultStatus() != null) {
+      lead.setLeadStatus(appCrmService.getAppCrm().getLeadDefaultStatus());
+    } else {
+      try {
+        throw new AxelorException(
+            TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
+            I18n.get(CrmExceptionMessage.CRM_DEFAULT_LEAD_STATUS_MISSING));
+      } catch (AxelorException e) {
+        throw new PersistenceException(e);
+      }
+    }
+    return lead;
+  }
 
   @Override
   public Lead save(Lead entity) {
