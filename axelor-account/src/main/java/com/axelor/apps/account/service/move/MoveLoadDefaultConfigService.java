21194c2f91afcc8b9a3ef4a2968cac474e9da5c9
aol-axelor <a.olivier@axelor.com>
2023-07-12 10:26:10 +0200

    fix method parameter name in service interface

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveLoadDefaultConfigService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveLoadDefaultConfigService.java
@@ -28,6 +28,5 @@ public interface MoveLoadDefaultConfigService {
 
   Account getAccountingAccountFromAccountConfig(Move move);
 
-  TaxLine getTaxLine(Move move, MoveLine moveLine, Account accountingAccount)
-      throws AxelorException;
+  TaxLine getTaxLine(Move move, MoveLine moveLine, Account account) throws AxelorException;
 }
