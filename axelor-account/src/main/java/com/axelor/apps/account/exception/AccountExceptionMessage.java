8d4c1b0c241b67a9136735ba016e96742c18f0ca
aol-axelor <61697047+aol-axelor@users.noreply.github.com>
2023-07-17 15:28:50 +0200

    RM#66264 : improve fixed asset mass validation error (#10737)
    (cherry picked from commit 54fee7b3638b6211332419fb255d214ab711379d)

--- axelor-account/src/main/java/com/axelor/apps/account/exception/AccountExceptionMessage.java
+++ axelor-account/src/main/java/com/axelor/apps/account/exception/AccountExceptionMessage.java
@@ -1590,4 +1590,7 @@ public final class AccountExceptionMessage {
 
   public static final String REVERSE_CHARGE_TAX_MISSING_ON_FISCAL_POSITION = /*$$(*/
       "A reverse charge tax is required on the fiscal position %s (From tax : %s - To tax : %s)." /*)*/;
+
+  public static final String FIXED_ASSET_MASS_VALIDATION_EXCEPTION = /*$$(*/
+      "The following error occurred during the validation of fixed asset %d : %s." /*)*/;
 }
