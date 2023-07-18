4a862e662ed21b4602b76a6d300f219e33f4c248
bsu-axelor <b.suvagiya@axelor.com>
2023-07-18 19:05:34 +0530

    Anomaly RM#66988: Stock move: NPE when emptying product on stock move line

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineServiceImpl.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineServiceImpl.java
@@ -1591,7 +1591,6 @@ public class StockMoveLineServiceImpl implements StockMoveLineService {
   public StockMoveLine resetStockMoveLine(StockMoveLine stockMoveLine) {
     if (stockMoveLine != null) {
 
-      stockMoveLine.setStockMove(null);
       stockMoveLine.setPlannedStockMove(null);
       stockMoveLine.setProduct(null);
       stockMoveLine.setFilterOnAvailableProducts(true);
