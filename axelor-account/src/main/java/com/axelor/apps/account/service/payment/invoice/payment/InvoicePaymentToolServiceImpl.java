9ff8f7e6f2863177ecc2cb7961716e003c062aa8
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2023-07-20 12:22:22 +0200

    67049 fix the check pending payment on invoice

--- axelor-account/src/main/java/com/axelor/apps/account/service/payment/invoice/payment/InvoicePaymentToolServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/payment/invoice/payment/InvoicePaymentToolServiceImpl.java
@@ -181,8 +181,11 @@ public class InvoicePaymentToolServiceImpl implements InvoicePaymentToolService 
         }
       }
     }
-
-    return invoice.getRemainingAmountAfterFinDiscount().compareTo(pendingAmount) <= 0;
+    BigDecimal remainingAmount =
+        invoice.getFinancialDiscount() != null
+            ? invoice.getRemainingAmountAfterFinDiscount()
+            : invoice.getAmountRemaining();
+    return remainingAmount.compareTo(pendingAmount) <= 0;
   }
 
   /** @inheritDoc */
