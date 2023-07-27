a5223f87d84f1016bb817a78e96e1615a6cd43cb
aba-axelor <103167557+aba-axelor@users.noreply.github.com>
2023-07-27 16:58:45 +0530

    Anomaly #66897: [MO] Plan a cancelled MO must clear the realDates on operations and order (#10854)

--- axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderWorkflowServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderWorkflowServiceImpl.java
@@ -170,6 +170,8 @@ public class ManufOrderWorkflowServiceImpl implements ManufOrderWorkflowService 
         }
         manufOrder.setPlannedStartDateT(manufOrder.getPlannedEndDateT().minusSeconds(duration));
       }
+      manufOrder.setRealStartDateT(null);
+      manufOrder.setRealEndDateT(null);
     }
 
     for (ManufOrder manufOrder : manufOrderList) {
