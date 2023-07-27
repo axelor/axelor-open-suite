dbb5cb1db91d772423c1c8c5ff6f287a7e2608fd
aba-axelor <103167557+aba-axelor@users.noreply.github.com>
2023-07-27 19:18:03 +0530

    Anomaly #66745: Add constants in unit xml domain for unitTypeSelect selection (#10892)

--- axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderWorkflowServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderWorkflowServiceImpl.java
@@ -605,7 +605,8 @@ public class ManufOrderWorkflowServiceImpl implements ManufOrderWorkflowService 
     Unit startUnit =
         Beans.get(UnitRepository.class)
             .all()
-            .filter("self.name = 'Hour' AND self.unitTypeSelect = 3")
+            .filter("self.name = 'Hour' AND self.unitTypeSelect = :unitType")
+            .bind("unitType", UnitRepository.UNIT_TYPE_TIME)
             .fetchOne();
 
     WorkCenter workCenter = operationOrder.getWorkCenter();
