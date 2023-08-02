d54638aecc9ab31546aff598444c9322ecc3e8ec
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2023-08-02 17:52:59 +0200

    67752 Budget : add new config in app budget to check if error must be throwed in invoice/moves/orders

--- axelor-budget/src/main/java/com/axelor/apps/budget/exception/BudgetExceptionMessage.java
+++ axelor-budget/src/main/java/com/axelor/apps/budget/exception/BudgetExceptionMessage.java
@@ -81,8 +81,8 @@ public final class BudgetExceptionMessage {
   public static final String ADVANCED_IMPORT_IMPORT_DATA = /*$$(*/
       "Data imported successfully" /*)*/;
 
-  public static final String NO_BUDGET_DISTRIBUTION_GENERATED = /*$$(*/
-      "The budget distribution has not been computed yet. By launching this action, you can no longer compute the budget distribution. Do you want to proceed ?" /*)*/;
+  public static final String NO_BUDGET_VALUES_FOUND_ERROR = /*$$(*/
+      "The budget distribution has not been computed yet. By launching this action, you can no longer compute the budget distribution." /*)*/;
 
   public static final String BUGDET_EXCEED_ERROR = /*$$(*/
       "Warning : There is a budget overrun for %s. The available balance is %.2f %s. If you click OK you will be in excess, do you want to continue ?"; /*)*/
