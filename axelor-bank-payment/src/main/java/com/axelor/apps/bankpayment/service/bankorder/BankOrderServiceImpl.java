49cd53961938bea110101b086a4d702851bc8b0e
sro-axelor <115447839+sro-axelor@users.noreply.github.com>
2023-08-08 15:35:42 +0530

    Anomaly RM#67726 Bank order: Bank order date is always set (#10986)
    (cherry picked from commit 110273a513d882d701c979cfa69696e70e834094)

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankorder/BankOrderServiceImpl.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankorder/BankOrderServiceImpl.java
@@ -345,6 +345,7 @@ public class BankOrderServiceImpl implements BankOrderService {
   public void confirm(BankOrder bankOrder)
       throws AxelorException, JAXBException, IOException, DatatypeConfigurationException {
     checkBankDetails(bankOrder.getSenderBankDetails(), bankOrder);
+    LocalDate todayDate = appBaseService.getTodayDate(bankOrder.getSenderCompany());
 
     if (bankOrder.getGeneratedMetaFile() == null) {
       checkLines(bankOrder);
@@ -373,7 +374,10 @@ public class BankOrderServiceImpl implements BankOrderService {
 
     if (bankOrder.getAccountingTriggerSelect()
         == PaymentModeRepository.ACCOUNTING_TRIGGER_CONFIRMATION) {
-      bankOrder.setBankOrderDate(appBaseService.getTodayDate(bankOrder.getSenderCompany()));
+      if (ObjectUtils.isEmpty(bankOrder.getBankOrderDate())
+          || bankOrder.getBankOrderDate().isBefore(todayDate)) {
+        bankOrder.setBankOrderDate(todayDate);
+      }
       this.generateMoves(bankOrder);
     }
   }
@@ -392,11 +396,15 @@ public class BankOrderServiceImpl implements BankOrderService {
     bankOrder.setValidationDateTime(LocalDateTime.now());
 
     bankOrder.setStatusSelect(BankOrderRepository.STATUS_VALIDATED);
+    LocalDate todayDate = appBaseService.getTodayDate(bankOrder.getSenderCompany());
 
     if (!bankOrder.getAreMovesGenerated()
         && bankOrder.getAccountingTriggerSelect()
             == PaymentModeRepository.ACCOUNTING_TRIGGER_VALIDATION) {
-      bankOrder.setBankOrderDate(appBaseService.getTodayDate(bankOrder.getSenderCompany()));
+      if (ObjectUtils.isEmpty(bankOrder.getBankOrderDate())
+          || bankOrder.getBankOrderDate().isBefore(todayDate)) {
+        bankOrder.setBankOrderDate(todayDate);
+      }
       bankOrder = this.generateMoves(bankOrder);
     }
 
@@ -455,11 +463,15 @@ public class BankOrderServiceImpl implements BankOrderService {
 
   @Transactional(rollbackOn = {Exception.class})
   protected void realizeBankOrder(BankOrder bankOrder) throws AxelorException {
+    LocalDate todayDate = appBaseService.getTodayDate(bankOrder.getSenderCompany());
 
     if (!bankOrder.getAreMovesGenerated()
         && bankOrder.getAccountingTriggerSelect()
             == PaymentModeRepository.ACCOUNTING_TRIGGER_REALIZATION) {
-      bankOrder.setBankOrderDate(appBaseService.getTodayDate(bankOrder.getSenderCompany()));
+      if (ObjectUtils.isEmpty(bankOrder.getBankOrderDate())
+          || bankOrder.getBankOrderDate().isBefore(todayDate)) {
+        bankOrder.setBankOrderDate(todayDate);
+      }
       bankOrder = this.generateMoves(bankOrder);
     }
 
