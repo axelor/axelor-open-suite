720d689db3398c8c2703b776ffea7f9e129e41f0
aol-axelor <61697047+aol-axelor@users.noreply.github.com>
2023-08-22 14:29:40 +0200

    RM#68057 : Fix payment invoice term (#11055)

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveCreateServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveCreateServiceImpl.java
@@ -355,6 +355,7 @@ public class MoveCreateServiceImpl implements MoveCreateService {
             description,
             companyBankDetails);
     move.setPaymentVoucher(paymentVoucher);
+    move.setPaymentCondition(null);
     return move;
   }
 
