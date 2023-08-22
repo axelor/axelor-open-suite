5f446aeb2915e90d19d3e2a7dbac843c8711c58f
aol-axelor <a.olivier@axelor.com>
2023-08-22 11:01:22 +0200

    RM#68163 : Fix payment session invoice term generation
    (cherry picked from commit 684af40b6d7badf60c3a2992aaf3c178cbec309d)

--- axelor-account/src/main/java/com/axelor/apps/account/service/PaymentSessionValidateServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/PaymentSessionValidateServiceImpl.java
@@ -35,6 +35,7 @@ import com.axelor.apps.account.exception.AccountExceptionMessage;
 import com.axelor.apps.account.service.config.AccountConfigService;
 import com.axelor.apps.account.service.invoice.InvoiceTermService;
 import com.axelor.apps.account.service.move.MoveCreateService;
+import com.axelor.apps.account.service.move.MoveLineInvoiceTermService;
 import com.axelor.apps.account.service.move.MoveValidateService;
 import com.axelor.apps.account.service.moveline.MoveLineCreateService;
 import com.axelor.apps.account.service.moveline.MoveLineTaxService;
@@ -88,6 +89,7 @@ public class PaymentSessionValidateServiceImpl implements PaymentSessionValidate
   protected AccountConfigService accountConfigService;
   protected PartnerService partnerService;
   protected PaymentModeService paymentModeService;
+  protected MoveLineInvoiceTermService moveLineInvoiceTermService;
   protected int counter = 0;
 
   @Inject
@@ -108,7 +110,8 @@ public class PaymentSessionValidateServiceImpl implements PaymentSessionValidate
       InvoicePaymentRepository invoicePaymentRepo,
       AccountConfigService accountConfigService,
       PartnerService partnerService,
-      PaymentModeService paymentModeService) {
+      PaymentModeService paymentModeService,
+      MoveLineInvoiceTermService moveLineInvoiceTermService) {
     this.appBaseService = appBaseService;
     this.moveCreateService = moveCreateService;
     this.moveValidateService = moveValidateService;
@@ -126,6 +129,7 @@ public class PaymentSessionValidateServiceImpl implements PaymentSessionValidate
     this.accountConfigService = accountConfigService;
     this.partnerService = partnerService;
     this.paymentModeService = paymentModeService;
+    this.moveLineInvoiceTermService = moveLineInvoiceTermService;
   }
 
   @Override
@@ -420,6 +424,7 @@ public class PaymentSessionValidateServiceImpl implements PaymentSessionValidate
 
     move.setPaymentSession(paymentSession);
     move.setPartnerBankDetails(partnerBankDetails);
+    move.setPaymentCondition(null);
 
     return move;
   }
@@ -476,6 +481,8 @@ public class PaymentSessionValidateServiceImpl implements PaymentSessionValidate
             description);
 
     move.addMoveLineListItem(moveLine);
+
+    moveLineInvoiceTermService.generateDefaultInvoiceTerm(move, moveLine, move.getDate(), false);
 
     return moveLine;
   }
