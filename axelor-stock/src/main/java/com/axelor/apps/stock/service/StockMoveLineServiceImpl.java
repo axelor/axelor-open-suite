b8d63edb22f0d46e6ce92738f48907c80049ff89
cma-axelor <c.malaviya@axelor.com>
2023-08-24 10:53:49 +0530

    Anomaly RM#65547 : API Stock : stock move line creation does not modify value of boolean isRealQtyModifiedByUser

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineServiceImpl.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineServiceImpl.java
@@ -1536,6 +1536,7 @@ public class StockMoveLineServiceImpl implements StockMoveLineService {
     compute(line, stockMove);
     line.setRealQty(realQty);
     line.setConformitySelect(conformitySelect);
+    line.setIsRealQtyModifiedByUser(true);
     stockMoveLineRepository.save(line);
     return line;
   }
