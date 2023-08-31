4d56e4f380b01159977b5c87c37de6827cc8638f
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-08-31 15:30:52 +0200

    RM#57379 Use nativeQuery from with JPA

--- axelor-contract/src/main/java/com/axelor/apps/contract/batch/BatchContractInvoicing.java
+++ axelor-contract/src/main/java/com/axelor/apps/contract/batch/BatchContractInvoicing.java
@@ -12,17 +12,16 @@ import com.axelor.apps.contract.db.repo.ContractRepository;
 import com.axelor.apps.contract.service.ContractService;
 import com.axelor.apps.contract.translation.ITranslation;
 import com.axelor.db.JPA;
-import com.axelor.db.internal.DBHelper;
 import com.axelor.i18n.I18n;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
-import java.sql.Array;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import javax.naming.NamingException;
+import java.util.stream.Collectors;
+import javax.persistence.Query;
+import org.apache.commons.collections.CollectionUtils;
 
 public class BatchContractInvoicing extends AbstractBatch {
 
@@ -40,54 +39,46 @@ public class BatchContractInvoicing extends AbstractBatch {
     this.contractBatchRepository = contractBatchRepository;
   }
 
-  public List<List<Contract>> getIdsGroupedBy() {
+  public List<List<Long>> getIdsGroupedBy() {
     ContractBatch contractBatch = batch.getContractBatch();
     contractBatch = contractBatchRepository.find(contractBatch.getId());
-    String query =
-        "SELECT array_agg(self.id) as list "
+    String filter =
+        "SELECT array_to_string(array_agg(self.id), ',') "
             + "FROM contract_contract as self "
             + "WHERE self.is_invoicing_management IS TRUE "
-            + "AND self.invoicing_date "
-            + "<= '"
-            + contractBatch.getInvoicingDate().toString()
-            + "'::date "
-            + "AND self.status_select != "
-            + AbstractContractRepository.CLOSED_CONTRACT
-            + " AND (SELECT automatic_invoicing FROM contract_contract_version WHERE contract_contract_version.id = self.current_contract_version) IS TRUE"
-            + " AND self.id NOT IN (SELECT DISTINCT contract_set FROM account_invoice_contract_set)"
-            + " GROUP BY self.invoiced_partner, self.invoicing_date, self.invoice_period_end_date, self.invoice_period_start_date, self.is_grouped_invoicing;";
-    Array array;
-    List<List<Contract>> contractListsList = new ArrayList<>();
-    try {
-      try (Connection connection = DBHelper.getConnection(); ) {
-        ResultSet resultSet = connection.createStatement().executeQuery(query);
-        while (resultSet.next()) {
-          array = resultSet.getArray("list");
-          contractListsList.add(findContracts(array));
-        }
-      }
-    } catch (SQLException | NamingException e) {
-      e.printStackTrace();
-    }
-    return contractListsList;
+            + "AND self.invoicing_date <= :invoicingDate "
+            + "AND self.status_select != :closedContract "
+            + "AND (SELECT automatic_invoicing FROM contract_contract_version WHERE contract_contract_version.id = self.current_contract_version) IS TRUE "
+            + "AND self.id NOT IN (SELECT DISTINCT contract_set FROM account_invoice_contract_set) "
+            + "GROUP BY self.invoiced_partner, self.invoicing_date, self.invoice_period_end_date, self.invoice_period_start_date, self.is_grouped_invoicing";
+
+    Query query =
+        JPA.em()
+            .createNativeQuery(filter)
+            .setParameter("invoicingDate", contractBatch.getInvoicingDate())
+            .setParameter("closedContract", AbstractContractRepository.CLOSED_CONTRACT);
+
+    List<String> stringList = query.getResultList();
+    return convertStringToLongList(stringList);
   }
 
-  protected List<Contract> findContracts(Array array) throws SQLException {
-    List<Contract> contractList = new ArrayList<>();
-    if (array != null) {
-      Long[] idList = (Long[]) array.getArray();
-      for (Long id : idList) {
-        Contract contract = contractRepository.find(id);
-        contractList.add(contract);
+  protected List<List<Long>> convertStringToLongList(List<String> stringList) {
+    List<List<Long>> longList = new ArrayList<>();
+    if (CollectionUtils.isNotEmpty(stringList)) {
+      for (String list : stringList) {
+        longList.add(
+            Arrays.stream(list.split(","))
+                .map(s -> Long.parseLong(s.trim()))
+                .collect(Collectors.toList()));
       }
     }
-    return contractList;
+    return longList;
   }
 
-  protected List<Contract> findContractsInList(List<Contract> contractList) {
+  protected List<Contract> findContractsInList(List<Long> contractList) {
     List<Contract> refindContractList = new ArrayList<>();
-    for (Contract contract : contractList) {
-      refindContractList.add(contractRepository.find(contract.getId()));
+    for (Long id : contractList) {
+      refindContractList.add(contractRepository.find(id));
     }
     return refindContractList;
   }
@@ -104,19 +95,15 @@ public class BatchContractInvoicing extends AbstractBatch {
   }
 
   @Override
-  protected void process() {
-    List<List<Contract>> contractListsList;
-    while (!(contractListsList = getIdsGroupedBy()).isEmpty()) {
-      for (List<Contract> contractList : contractListsList) {
-        try {
-          contractList = findContractsInList(contractList);
-          invoiceContracts(contractList);
-          incrementDone();
-          JPA.clear();
-        } catch (Exception e) {
-          incrementAnomaly();
-          TraceBackService.trace(e, "Contract invoicing batch", batch.getId());
-        }
+  protected void process() throws SQLException {
+    for (List<Long> idList : getIdsGroupedBy()) {
+      try {
+        invoiceContracts(findContractsInList(idList));
+        incrementDone();
+        JPA.clear();
+      } catch (Exception e) {
+        incrementAnomaly();
+        TraceBackService.trace(e, "Contract invoicing batch", batch.getId());
       }
     }
   }
