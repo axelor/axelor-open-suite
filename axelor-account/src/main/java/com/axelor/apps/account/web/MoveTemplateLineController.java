1319a984c99db00af2745e935091f27a46c29130
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2023-09-05 16:00:34 +0200

    RM59720 MOVETEMPLATELINE : Domain on M2O Account displays accounts without any domain
    Co-authored-by: Edwin <e.roger-gorlin@axelor.com>

---
+++ axelor-account/src/main/java/com/axelor/apps/account/web/MoveTemplateLineController.java
@@ -0,0 +1,36 @@
+package com.axelor.apps.account.web;
+
+import com.axelor.apps.account.db.MoveTemplate;
+import com.axelor.apps.account.db.MoveTemplateLine;
+import com.axelor.apps.account.service.moveline.MoveLineGroupService;
+import com.axelor.apps.base.ResponseMessageType;
+import com.axelor.apps.base.service.exception.TraceBackService;
+import com.axelor.inject.Beans;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.axelor.rpc.Context;
+
+public class MoveTemplateLineController {
+  public void accountOnSelect(ActionRequest request, ActionResponse response) {
+    try {
+      MoveTemplateLine moveTemplateLine = request.getContext().asType(MoveTemplateLine.class);
+      MoveTemplate moveTemplate = this.getMoveTemplate(request, moveTemplateLine);
+      if (moveTemplate != null) {
+        response.setAttrs(
+            Beans.get(MoveLineGroupService.class)
+                .getAccountOnSelectAttrsMap(moveTemplate.getJournal(), moveTemplate.getCompany()));
+      }
+    } catch (Exception e) {
+      TraceBackService.trace(response, e, ResponseMessageType.ERROR);
+    }
+  }
+
+  protected MoveTemplate getMoveTemplate(ActionRequest request, MoveTemplateLine moveTemplateLine) {
+    Context parentContext = request.getContext().getParent();
+    if (parentContext != null && MoveTemplate.class.equals(parentContext.getContextClass())) {
+      return request.getContext().getParent().asType(MoveTemplate.class);
+    } else {
+      return moveTemplateLine.getMoveTemplate();
+    }
+  }
+}
