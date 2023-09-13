956fca6199719ac61afd24209fe25415df9879b6
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2023-09-13 15:24:22 +0200

    66727change code in a new service

--- axelor-account/src/main/java/com/axelor/apps/account/web/TradingNameController.java
+++ axelor-account/src/main/java/com/axelor/apps/account/web/TradingNameController.java
@@ -1,38 +1,20 @@
 package com.axelor.apps.account.web;
 
-import com.axelor.apps.account.service.config.AccountConfigService;
-import com.axelor.apps.base.AxelorException;
-import com.axelor.apps.base.db.Company;
+import com.axelor.apps.account.service.analytic.TradingNameAnalyticServiceImpl;
 import com.axelor.apps.base.db.TradingName;
 import com.axelor.apps.base.service.exception.TraceBackService;
-import com.axelor.common.ObjectUtils;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
-import java.util.stream.Collectors;
 
 public class TradingNameController {
 
   public void showAnalyticPanel(ActionRequest request, ActionResponse response) {
     try {
       TradingName tradingName = request.getContext().asType(TradingName.class);
-      AccountConfigService accountConfigService = Beans.get(AccountConfigService.class);
-      boolean isHidden =
-          tradingName.getCompanySet().stream()
-                  .filter(
-                      c -> {
-                        try {
-                          return accountConfigService
-                                  .getAccountConfig(c)
-                                  .getAnalyticDistributionTypeSelect()
-                              == 4;
-                        } catch (AxelorException e) {
-                          return false;
-                        }
-                      })
-                  .count()
-              == 0;
-      response.setAttr("analyticDistributionPanel", "hidden", isHidden);
+      boolean emptyTemplate =
+          Beans.get(TradingNameAnalyticServiceImpl.class).isAnalyticTypeByTradingName(tradingName);
+      response.setAttr("analyticDistributionPanel", "hidden", emptyTemplate);
     } catch (Exception e) {
       TraceBackService.trace(response, e);
     }
@@ -41,25 +23,11 @@ public class TradingNameController {
   public void emptyAnalyticDistributionTemplate(ActionRequest request, ActionResponse response) {
     try {
       TradingName tradingName = request.getContext().asType(TradingName.class);
-      AccountConfigService accountConfigService = Beans.get(AccountConfigService.class);
       if (tradingName.getAnalyticDistributionTemplate() == null) {
         return;
       }
       boolean emptyTemplate =
-          tradingName.getCompanySet().stream()
-                  .filter(
-                      c -> {
-                        try {
-                          return accountConfigService
-                                  .getAccountConfig(c)
-                                  .getAnalyticDistributionTypeSelect()
-                              == 4;
-                        } catch (AxelorException e) {
-                          return false;
-                        }
-                      })
-                  .count()
-              == 0;
+          Beans.get(TradingNameAnalyticServiceImpl.class).isAnalyticTypeByTradingName(tradingName);
 
       if (emptyTemplate) {
         response.setValue("analyticDistributionTemplate", null);
@@ -72,18 +40,11 @@ public class TradingNameController {
   public void setAnalyticDistributionTemplate(ActionRequest request, ActionResponse response) {
     try {
       TradingName tradingName = request.getContext().asType(TradingName.class);
-      AccountConfigService accountConfigService = Beans.get(AccountConfigService.class);
-      String companyIds =
-          tradingName.getCompanySet().stream()
-              .map(Company::getId)
-              .map(Object::toString)
-              .collect(Collectors.joining(","))
-              .toString();
-      String domain =
-          !ObjectUtils.isEmpty(companyIds)
-              ? String.format("self.company.id in (%s) AND self.isSpecific = false", companyIds)
-              : "self.id = 0";
-      response.setAttr("analyticDistributionTemplate", "domain", domain);
+
+      response.setAttr(
+          "analyticDistributionTemplate",
+          "domain",
+          Beans.get(TradingNameAnalyticServiceImpl.class).getDomainOnCompany(tradingName));
     } catch (Exception e) {
       TraceBackService.trace(response, e);
     }
