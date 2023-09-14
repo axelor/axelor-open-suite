549a010277cbe87109d2579d068a0fc0e4fcd7af
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2023-09-14 17:21:48 +0200

    68987 - Fix effectiveDate when we generate payment move from voucher
    (cherry picked from commit 83155b28ff2dd2c885ed61ca4307142e6295471d)

--- axelor-account/src/main/java/com/axelor/apps/account/service/payment/paymentvoucher/PaymentVoucherConfirmService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/payment/paymentvoucher/PaymentVoucherConfirmService.java
@@ -855,7 +855,7 @@ public class PaymentVoucherConfirmService {
             currencyRate,
             !isDebitToPay,
             paymentDate,
-            moveLineToPay.getDueDate() != null ? moveLineToPay.getDueDate() : paymentDate,
+            paymentDate,
             paymentDate,
             moveLineSeq,
             invoiceName,
