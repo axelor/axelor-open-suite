708742cf3beff6ffe411275635f7687daa6b74a6
aol-axelor <a.olivier@axelor.com>
2023-09-15 09:40:15 +0200

    fix currency rate on counterpart

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveCounterPartServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveCounterPartServiceImpl.java
@@ -110,6 +110,11 @@ public class MoveCounterPartServiceImpl implements MoveCounterPartService {
     moveLine.setDueDate(move.getOriginDate());
     moveLine.setCurrencyAmount(currencyAmount);
     moveLine.setDescription(move.getDescription());
+    moveLine.setCurrencyRate(
+        move.getMoveLineList().stream()
+            .map(MoveLine::getCurrencyRate)
+            .findAny()
+            .orElse(BigDecimal.ONE));
 
     return moveLine;
   }
