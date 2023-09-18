545a6b3cabe96df0f2a2c0eac122a38a823584f1
oma-axelor <101743458+oma-axelor@users.noreply.github.com>
2023-09-18 15:52:27 +0200

    RM#65712 : fix compute new average price for stockLocationLine in the case of a unit conversion (#11077)
    (cherry picked from commit a0a4d03f666a47ced2a418f82b71a67d39ad5d28)

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineService.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineService.java
@@ -319,7 +319,7 @@ public interface StockMoveLineService {
       boolean generateOrder)
       throws AxelorException;
 
-  public BigDecimal computeNewAveragePriceLocationLine(
+  BigDecimal computeNewAveragePriceLocationLine(
       StockLocationLine stockLocationLine, StockMoveLine stockMoveLine) throws AxelorException;
 
   /**
