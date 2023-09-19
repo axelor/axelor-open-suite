1a25167da8d8b76592e049ad18f9b296a6e1a654
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-09-19 13:41:57 +0200

    RM#68787 Reword PDF Signature feature to use PDFBox instead of iText (#11155)

--- axelor-base/src/main/java/com/axelor/apps/base/module/BaseModule.java
+++ axelor-base/src/main/java/com/axelor/apps/base/module/BaseModule.java
@@ -197,6 +197,8 @@ import com.axelor.apps.base.service.pricing.PricingService;
 import com.axelor.apps.base.service.pricing.PricingServiceImpl;
 import com.axelor.apps.base.service.research.ResearchRequestService;
 import com.axelor.apps.base.service.research.ResearchRequestServiceImpl;
+import com.axelor.apps.base.service.signature.SignatureService;
+import com.axelor.apps.base.service.signature.SignatureServiceImpl;
 import com.axelor.apps.base.service.tax.AccountManagementService;
 import com.axelor.apps.base.service.tax.AccountManagementServiceImpl;
 import com.axelor.apps.base.service.tax.FiscalPositionService;
@@ -370,5 +372,6 @@ public class BaseModule extends AxelorModule {
     bind(PdfService.class).to(PdfServiceImpl.class);
     bind(PdfSignatureService.class).to(PdfSignatureServiceImpl.class);
     bind(PartnerLinkService.class).to(PartnerLinkServiceImpl.class);
+    bind(SignatureService.class).to(SignatureServiceImpl.class);
   }
 }
