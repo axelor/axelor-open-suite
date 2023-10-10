954ff15105e60cbe623b38b20b0fa1e91e807d8f
bsu-axelor <b.suvagiya@axelor.com>
2023-10-10 17:38:12 +0530

    Anomaly RM#68444: Errors when importing city with geonames
    (cherry picked from commit 66d601bac8c36bad034d4137e66d2af974c07754)

--- axelor-base/src/main/java/com/axelor/apps/base/service/imports/ImportCityServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/imports/ImportCityServiceImpl.java
@@ -411,7 +411,7 @@ public class ImportCityServiceImpl implements ImportCityService {
 
       cityList.add(
           String.format(
-              "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s%n",
+              "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s%n",
               cityLine[0], // country code
               cityLine[1], // zip code
               cityLine[2], // city name
@@ -420,7 +420,10 @@ public class ImportCityServiceImpl implements ImportCityService {
               cityLine[5], // department name
               cityLine[6], // department code
               cityLine[7], // canton name
-              cityLine[8])); // canton code
+              cityLine[8], // canton code
+              cityLine[9], // latitude
+              cityLine[10], // longitude
+              cityLine[11])); // accuracy
     }
 
     return this.createCityFileZipImport(cityList, cityTextFile);
