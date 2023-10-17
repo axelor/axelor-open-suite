1eca49fcafaa805d94d91386f2a44ee3c8dea066
oma-axelor <101743458+oma-axelor@users.noreply.github.com>
2023-10-17 10:53:32 +0200

    RM#66728 : refactor stock computeIndicators method orchestration (#11068)
    * RM#70078: fix scheduling translation issues
    ---------
    
    Co-authored-by: ale-axelor <25925065+ale-axelor@users.noreply.github.com>
    (cherry picked from commit e6a3dc2afecafa88b0c837eb90ab5d28b70dc13a)
    (cherry picked from commit 9eef89fa7a7b9d12c40b057415c412d28fb44a59)
    (cherry picked from commit 0944b6a0123e9f3e485e4bd128203a45e7574c8b)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockLocationServiceSupplychain.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockLocationServiceSupplychain.java
@@ -21,10 +21,11 @@ import com.axelor.apps.stock.service.StockLocationService;
 import com.axelor.exception.AxelorException;
 import com.axelor.meta.CallMethod;
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface StockLocationServiceSupplychain extends StockLocationService {
 
   @CallMethod
-  public BigDecimal getReservedQty(Long productId, Long locationId, Long companyId)
-      throws AxelorException;
+  BigDecimal getReservedQtyOfProductInStockLocations(
+      Long productId, List<Long> stockLocationIds, Long companyId) throws AxelorException;
 }
