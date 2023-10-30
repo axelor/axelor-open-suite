f2f33462bf1851a7d07e849317f585054d1cef3a
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2023-10-30 14:29:58 +0100

    71180 - Fix Move line counter
    (cherry picked from commit c06c615050783bff1a2f2becc52f2a4b60cb9545)

--- axelor-account/src/main/java/com/axelor/apps/account/service/moveline/MoveLineGroupServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/moveline/MoveLineGroupServiceImpl.java
@@ -93,7 +93,9 @@ public class MoveLineGroupServiceImpl implements MoveLineGroupService {
     moveLineRecordService.setCurrencyFields(moveLine, move);
     moveLineDefaultService.setFinancialDiscount(moveLine);
     moveLineService.computeFinancialDiscount(moveLine);
+    moveLineRecordService.setCounter(moveLine, move);
 
+    valuesMap.put("counter", moveLine.getCounter());
     valuesMap.put("account", moveLine.getAccount());
     valuesMap.put("partner", moveLine.getPartner());
     valuesMap.put("date", moveLine.getDate());
