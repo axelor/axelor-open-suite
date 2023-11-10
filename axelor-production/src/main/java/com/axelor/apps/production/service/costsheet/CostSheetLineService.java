3006aff972f7e48b7f7a6a0354849ee09f548d16
bsu-axelor <b.suvagiya@axelor.com>
2023-11-10 16:20:33 +0530

    Anomaly RM#71756: [COST CALCULATION] Avoid loop

--- axelor-production/src/main/java/com/axelor/apps/production/service/costsheet/CostSheetLineService.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/costsheet/CostSheetLineService.java
@@ -42,10 +42,11 @@ public interface CostSheetLineService {
       int typeSelectIcon,
       Unit unit,
       WorkCenter workCenter,
-      CostSheetLine parentCostSheetLine);
+      CostSheetLine parentCostSheetLine)
+      throws AxelorException;
 
   public CostSheetLine createProducedProductCostSheetLine(
-      Product product, Unit unit, BigDecimal consumptionQty);
+      Product product, Unit unit, BigDecimal consumptionQty) throws AxelorException;
 
   public CostSheetLine createResidualProductCostSheetLine(
       Product product, Unit unit, BigDecimal consumptionQty, Company company)
@@ -81,7 +82,8 @@ public interface CostSheetLineService {
       CostSheetLine parentCostSheetLine,
       BigDecimal consumptionQty,
       BigDecimal costPrice,
-      Unit unit);
+      Unit unit)
+      throws AxelorException;
 
   public CostSheetLine createWorkCenterMachineCostSheetLine(
       WorkCenter workCenter,
@@ -90,5 +92,6 @@ public interface CostSheetLineService {
       CostSheetLine parentCostSheetLine,
       BigDecimal consumptionQty,
       BigDecimal costPrice,
-      Unit unit);
+      Unit unit)
+      throws AxelorException;
 }
