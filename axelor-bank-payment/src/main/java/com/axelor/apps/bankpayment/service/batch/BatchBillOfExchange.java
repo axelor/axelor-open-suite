868d8bb3a02e5479c9a1bb751bb51f0c9e720597
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2023-11-15 14:59:43 +0100

    69182 fix partially paid invoices in bill of exchange process
    (cherry picked from commit 70b621ad98c72ec23417d63d5ce6ff6d06e8dc33)
    (cherry picked from commit 7960ad8e40ab0e68fd3c5bf9d4fb7b990f3c0718)

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/batch/BatchBillOfExchange.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/batch/BatchBillOfExchange.java
@@ -35,6 +35,7 @@ import com.axelor.apps.account.exception.AccountExceptionMessage;
 import com.axelor.apps.account.service.ReconcileService;
 import com.axelor.apps.account.service.app.AppAccountService;
 import com.axelor.apps.account.service.config.AccountConfigService;
+import com.axelor.apps.account.service.invoice.InvoiceTermService;
 import com.axelor.apps.account.service.move.MoveCreateService;
 import com.axelor.apps.account.service.move.MoveValidateService;
 import com.axelor.apps.account.service.moveline.MoveLineCreateService;
@@ -44,6 +45,7 @@ import com.axelor.apps.base.db.BankDetails;
 import com.axelor.apps.base.db.repo.BatchRepository;
 import com.axelor.apps.base.exceptions.BaseExceptionMessage;
 import com.axelor.apps.base.service.administration.AbstractBatch;
+import com.axelor.apps.base.service.app.AppBaseService;
 import com.axelor.common.ObjectUtils;
 import com.axelor.db.JPA;
 import com.axelor.db.Query;
@@ -57,6 +59,7 @@ import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 import java.lang.invoke.MethodHandles;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -65,6 +68,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -85,6 +89,7 @@ public class BatchBillOfExchange extends AbstractBatch {
   protected MoveValidateService moveValidateService;
   protected AccountingBatchRepository accountingBatchRepository;
   protected InvoiceTermRepository invoiceTermRepo;
+  protected InvoiceTermService invoiceTermService;
 
   @Inject
   public BatchBillOfExchange(
@@ -100,7 +105,8 @@ public class BatchBillOfExchange extends AbstractBatch {
       MoveLineService moveLineService,
       MoveValidateService moveValidateService,
       AccountingBatchRepository accountingBatchRepository,
-      InvoiceTermRepository invoiceTermRepo) {
+      InvoiceTermRepository invoiceTermRepo,
+      InvoiceTermService invoiceTermService) {
     super();
     this.invoiceRepository = invoiceRepository;
     this.appAccountService = appAccountService;
@@ -115,6 +121,7 @@ public class BatchBillOfExchange extends AbstractBatch {
     this.moveValidateService = moveValidateService;
     this.accountingBatchRepository = accountingBatchRepository;
     this.invoiceTermRepo = invoiceTermRepo;
+    this.invoiceTermService = invoiceTermService;
   }
 
   @Override
@@ -240,15 +247,13 @@ public class BatchBillOfExchange extends AbstractBatch {
    * @param move
    * @param accountConfig
    */
-  @Transactional
   protected void updateInvoice(Invoice invoice, Move move, AccountConfig accountConfig) {
 
-    Invoice invoiceToSave = invoiceRepository.find(invoice.getId());
-    invoiceToSave.setOldMove(invoice.getMove());
-    invoiceToSave.setMove(move);
-    invoiceToSave.setPartnerAccount(
+    invoice.setOldMove(invoice.getMove());
+    invoice.setMove(move);
+    invoice.setPartnerAccount(
         accountRepository.find(accountConfig.getBillOfExchReceivAccount().getId()));
-    invoiceToSave.setLcrAccounted(true);
+    invoice.setLcrAccounted(true);
     invoice.addBatchSetItem(batchRepo.find(batch.getId()));
   }
 
@@ -387,6 +392,46 @@ public class BatchBillOfExchange extends AbstractBatch {
     this.batch.setBatchTypeSelect(BatchRepository.BATCH_TYPE_BANK_PAYMENT_BATCH);
   }
 
+  protected List<InvoiceTerm> splitInvoiceTerms(List<InvoiceTerm> invoiceTermList) {
+    if (CollectionUtils.isEmpty(invoiceTermList)) {
+      return new ArrayList<>();
+    }
+
+    List<InvoiceTerm> invoiceTermToRemove = new ArrayList<>();
+
+    for (InvoiceTerm invoiceTerm : invoiceTermList) {
+      BigDecimal newAmount = invoiceTerm.getAmount().subtract(invoiceTerm.getAmountRemaining());
+      if (newAmount.signum() > 0) {
+        BigDecimal amountRemaining = invoiceTerm.getAmountRemaining();
+        BigDecimal newPercentage =
+            amountRemaining
+                .multiply(invoiceTerm.getPercentage())
+                .divide(
+                    invoiceTerm.getAmount(),
+                    AppBaseService.DEFAULT_NB_DECIMAL_DIGITS,
+                    RoundingMode.HALF_UP);
+        InvoiceTerm newInvoiceTerm = invoiceTermRepo.copy(invoiceTerm, true);
+        invoiceTerm.setAmount(newAmount);
+        invoiceTerm.setCompanyAmount(newAmount);
+        invoiceTerm.setAmountRemaining(BigDecimal.ZERO);
+        invoiceTerm.setCompanyAmountRemaining(BigDecimal.ZERO);
+        invoiceTerm.setPercentage(new BigDecimal(100).subtract(newPercentage));
+        invoiceTerm.setIsPaid(true);
+        newInvoiceTerm.setAmount(amountRemaining);
+        newInvoiceTerm.setCompanyAmount(amountRemaining);
+        newInvoiceTerm.setAmountRemaining(amountRemaining);
+        newInvoiceTerm.setCompanyAmountRemaining(amountRemaining);
+        newInvoiceTerm.setAmountPaid(BigDecimal.ZERO);
+        newInvoiceTerm.setPercentage(newPercentage);
+        MoveLine moveLine = invoiceTerm.getMoveLine();
+
+        moveLine.addInvoiceTermListItem(newInvoiceTerm);
+        invoiceTermToRemove.add(newInvoiceTerm);
+      }
+    }
+    return invoiceTermToRemove;
+  }
+
   protected void copyInvoiceTerms(
       Invoice invoice,
       Move move,
@@ -405,8 +450,11 @@ public class BatchBillOfExchange extends AbstractBatch {
       newInvoiceTermList.addAll(newDebitMoveLine.getInvoiceTermList());
     }
 
-    List<InvoiceTerm> invoiceTermList =
-        invoice.getInvoiceTermList().stream()
+    List<InvoiceTerm> invoiceTermList = invoice.getInvoiceTermList();
+    invoiceTermListToRemove.addAll(splitInvoiceTerms(invoiceTermList));
+
+    invoiceTermList =
+        invoiceTermList.stream()
             .filter(it -> !it.getIsPaid() && it.getAmountRemaining().signum() > 0)
             .collect(Collectors.toList());
     if (!ObjectUtils.isEmpty(invoiceTermList)) {
@@ -422,7 +470,7 @@ public class BatchBillOfExchange extends AbstractBatch {
     if (ObjectUtils.isEmpty(newInvoiceTermList) || ObjectUtils.isEmpty(invoiceTermListToRemove)) {
       return;
     }
-    invoice.clearInvoiceTermList();
+
     for (InvoiceTerm invoiceTerm : newInvoiceTermList) {
       invoice.addInvoiceTermListItem(invoiceTerm);
     }
@@ -431,6 +479,8 @@ public class BatchBillOfExchange extends AbstractBatch {
       invoice.removeInvoiceTermListItem(invoiceTerm);
       invoiceTerm.setInvoice(null);
     }
+
+    invoiceRepository.save(invoice);
   }
 
   protected void updateInvoiceTerms(MoveLine creditMoveLine, MoveLine debitMoveLine) {
