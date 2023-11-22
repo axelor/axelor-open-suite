f668947d4fc2380d8b53936516f29fb973614e43
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2023-11-22 18:40:43 +0100

    Config for default status

--- axelor-helpdesk/src/main/java/com/axelor/apps/helpdesk/service/TicketStatusServiceImpl.java
+++ axelor-helpdesk/src/main/java/com/axelor/apps/helpdesk/service/TicketStatusServiceImpl.java
@@ -2,10 +2,8 @@ package com.axelor.apps.helpdesk.service;
 
 import com.axelor.apps.helpdesk.db.TicketStatus;
 import com.axelor.apps.helpdesk.service.app.AppHelpdeskService;
-import com.axelor.db.Query;
 import com.axelor.studio.db.AppHelpdesk;
 import com.google.inject.Inject;
-import java.util.Optional;
 
 public class TicketStatusServiceImpl implements TicketStatusService {
 
@@ -20,25 +18,7 @@ public class TicketStatusServiceImpl implements TicketStatusService {
 
   @Override
   public TicketStatus findDefaultStatus() {
-
-    TicketStatus resolvedStatus = appHelpdesk.getResolvedTicketStatus();
-    TicketStatus closedStatus = appHelpdesk.getClosedTicketStatus();
-    TicketStatus inProgressStatus = appHelpdesk.getInProgressTicketStatus();
-
-    return Query.of(TicketStatus.class)
-        .filter(
-            "self.id != :resolvedStatusId and self.id != :closedStatusId and self.id != :inProgressStatusId")
-        .bind(
-            "resolvedStatusId",
-            Optional.ofNullable(resolvedStatus).map(TicketStatus::getId).orElse(null))
-        .bind(
-            "closedStatusId",
-            Optional.ofNullable(closedStatus).map(TicketStatus::getId).orElse(null))
-        .bind(
-            "inProgressStatusId",
-            Optional.ofNullable(inProgressStatus).map(TicketStatus::getId).orElse(null))
-        .order("priority")
-        .fetchOne();
+    return appHelpdesk.getDefaultTicketStatus();
   }
 
   @Override
