8b1099c87dccfff349f082ab76eae96d56b8ed9d
sro-axelor <s.kumarrout@axelor.com>
2023-11-24 18:03:02 +0530

    Feature RM#71243 [TOUR] New object, menu entry and views

--- axelor-crm/src/main/java/com/axelor/apps/crm/module/CrmModule.java
+++ axelor-crm/src/main/java/com/axelor/apps/crm/module/CrmModule.java
@@ -56,6 +56,10 @@ import com.axelor.apps.crm.service.PartnerCrmService;
 import com.axelor.apps.crm.service.PartnerCrmServiceImpl;
 import com.axelor.apps.crm.service.PartnerEmailDomainToolService;
 import com.axelor.apps.crm.service.PartnerEmailDomainToolServiceImpl;
+import com.axelor.apps.crm.service.TourLineService;
+import com.axelor.apps.crm.service.TourLineServiceImpl;
+import com.axelor.apps.crm.service.TourService;
+import com.axelor.apps.crm.service.TourServiceImpl;
 import com.axelor.apps.crm.service.app.AppCrmService;
 import com.axelor.apps.crm.service.app.AppCrmServiceImpl;
 
@@ -82,5 +86,7 @@ public class CrmModule extends AxelorModule {
     bind(PartnerEmailDomainToolService.class).to(PartnerEmailDomainToolServiceImpl.class);
     bind(CrmActivityService.class).to(CrmActivityServiceImpl.class);
     bind(LeadDuplicateService.class).to(LeadDuplicateServiceImpl.class);
+    bind(TourService.class).to(TourServiceImpl.class);
+    bind(TourLineService.class).to(TourLineServiceImpl.class);
   }
 }
