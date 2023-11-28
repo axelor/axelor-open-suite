c770381acadc835192e21ae9186b26d0a84b3ab9
Teng Yue <t.yue@axelor.com>
2023-11-28 12:54:26 -0500

    Updated Service methods

--- axelor-base/src/main/java/com/axelor/apps/base/service/ImportExportTranslationServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/ImportExportTranslationServiceImpl.java
@@ -14,7 +14,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.persistence.Query;
 
 public class ImportExportTranslationServiceImpl implements ImportExportTranslationService {
@@ -41,7 +43,7 @@ public class ImportExportTranslationServiceImpl implements ImportExportTranslati
     // 3. each message_key, find message_value
     // 4. add line to file
 
-    Query query =
+    Query languageQuery =
         JPA.em()
             .createNativeQuery(
                 "select base_language.code "
@@ -52,28 +54,102 @@ public class ImportExportTranslationServiceImpl implements ImportExportTranslati
                     + "on base_import_export_translation_language_set.language_set = base_language.id "
                     + "where base_import_export_translation.id = :curId ")
             .setParameter("curId", importExportTranslation.getId());
-    List resultList = query.getResultList();
+    List<String> resultList = languageQuery.getResultList();
     int languageNumber = resultList.size();
     String[] headers = new String[languageNumber + 1];
     headers[0] = "key";
     for (int i = 1; i < headers.length; i++) {
-      headers[i] = (String) resultList.get(i - 1);
+      headers[i] = resultList.get(i - 1);
     }
+
+    Query messageQuery =
+        JPA.em()
+            .createNativeQuery(
+                "select language, message_key, message_value "
+                    + "from META_TRANSLATION "
+                    + "where meta_translation.language = ANY ( "
+                    + "select base_language.code "
+                    + "from base_import_export_translation "
+                    + "left join base_import_export_translation_language_set "
+                    + "on base_import_export_translation_language_set.base_import_export_translation = base_import_export_translation.id "
+                    + "left join base_language "
+                    + "on base_import_export_translation_language_set.language_set = base_language.id "
+                    + "where base_import_export_translation.id = :curId ) "
+                    + "limit 20 "
+                    + "offset 0 ")
+            .setParameter("curId", importExportTranslation.getId());
+
+    List<Object[]> messageQueryResultList = messageQuery.getResultList();
+    Object[] messageRecord;
+    String messageKey;
+    String languageCode;
+    String messageValue;
+
+    List<String[]> translationList = new ArrayList<>();
     String[] row = new String[languageNumber + 1];
-    List<String[]> list = new ArrayList<>();
-    row[0] = "key";
-    for (int i = 1; i < row.length; i++) {
-      row[i] = "" + i;
+    String[] messageValueArray;
+    // key:messageKey, value: String arrays containing message,values
+    Map<String, String[]> map = new HashMap<>();
+    for (int i = 0; i < messageQueryResultList.size(); i++) {
+      // each record
+      messageRecord = messageQueryResultList.get(i);
+      languageCode = (String) messageRecord[0];
+      messageKey = (String) messageRecord[1];
+      messageValue = (String) messageRecord[2];
+
+      //      System.out.println(languageCode);
+      //      System.out.println(messageKey);
+      //      System.out.println(messageValue);
+
+      if (!map.containsKey(messageKey)) {
+        messageValueArray = new String[languageNumber];
+        int position = findPosition(languageCode, headers) - 1;
+        System.out.println(position);
+        messageValueArray[position] = messageValue;
+        System.out.println(messageValueArray);
+        map.put(messageKey, messageValueArray);
+
+        System.out.println("");
+      } else {
+        messageValueArray = map.get(messageKey);
+        int position = findPosition(languageCode, headers) - 1;
+        System.out.println(position);
+        messageValueArray[position] = messageValue;
+        System.out.println(messageValueArray);
+        map.put(messageKey, messageValueArray);
+        System.out.println(map);
+        System.out.println("");
+      }
     }
-    list.add(row);
+
+    for (Map.Entry entry : map.entrySet()) {
+      String key = (String) entry.getKey();
+      Object[] value = (Object[]) entry.getValue();
+      row = new String[languageNumber + 1];
+      row[0] = key;
+      for (int j = 0; j < value.length; j++) {
+        row[j + 1] = (String) value[j];
+      }
+      translationList.add(row);
+    }
+
     String fileName = "Exported Translation" + " - " + java.time.LocalDateTime.now();
     File file = MetaFiles.createTempFile(fileName, ".csv").toFile();
-    CsvHelper.csvWriter(file.getParent(), file.getName(), ';', headers, list);
+    CsvHelper.csvWriter(file.getParent(), file.getName(), ';', headers, translationList);
     try (InputStream is = new FileInputStream(file)) {
       Beans.get(MetaFiles.class).attach(is, file.getName(), importExportTranslation);
     }
     importExportTranslationRepository.save(importExportTranslation);
 
     return file.getPath();
+  }
+
+  protected int findPosition(String languageCode, String[] headers) {
+    for (int i = 1; i < headers.length; i++) {
+      if (languageCode.equals(headers[i])) {
+        return i;
+      }
+    }
+    return 0;
   }
 }
