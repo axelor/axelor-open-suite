bc91b13bdfe593ba84d721bbd847ca79757273e3
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2023-11-30 14:54:31 +0100

    71798 - Remove useless call of currencyScaleService call

--- axelor-account/src/main/java/com/axelor/apps/account/service/payment/invoice/payment/InvoiceTermPaymentServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/payment/invoice/payment/InvoiceTermPaymentServiceImpl.java
@@ -165,7 +165,6 @@ public class InvoiceTermPaymentServiceImpl implements InvoiceTermPaymentService 
         availableAmount = availableAmount.subtract(invoiceTermAmount);
       }
 
-      currencyScaleServiceAccount.getCompanyScaledValue(invoiceTermToPay, availableAmount);
       invoiceTermPaymentList.add(invoiceTermPayment);
 
       if (invoicePayment != null) {
