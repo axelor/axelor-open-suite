862ad4e924f754507a8488dfe3bde2a9f01f8c8c
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2023-11-30 18:29:36 +0100

    RM#71755 Mobile dashboard management (#11555)

---
+++ axelor-mobile-settings/src/main/java/com/axelor/apps/mobilesettings/web/MobileChartController.java
@@ -0,0 +1,17 @@
+package com.axelor.apps.mobilesettings.web;
+
+import com.axelor.apps.base.AxelorException;
+import com.axelor.apps.mobilesettings.db.MobileChart;
+import com.axelor.apps.mobilesettings.service.MobileChartService;
+import com.axelor.inject.Beans;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+
+public class MobileChartController {
+  public void getJsonResponse(ActionRequest request, ActionResponse response)
+      throws AxelorException {
+    MobileChart mobileChart = request.getContext().asType(MobileChart.class);
+    response.setValue(
+        "$response", Beans.get(MobileChartService.class).getQueryResponse(mobileChart));
+  }
+}
