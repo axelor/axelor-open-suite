134e48c0baadb6bec3682a2b7719fc0ae41be534
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2023-12-01 12:00:54 +0100

    71794 - Add CurrencyScaleServicePurchase

--- axelor-purchase/src/main/java/com/axelor/apps/purchase/module/PurchaseModule.java
+++ axelor-purchase/src/main/java/com/axelor/apps/purchase/module/PurchaseModule.java
@@ -25,6 +25,8 @@ import com.axelor.apps.purchase.db.repo.PurchaseOrderManagementRepository;
 import com.axelor.apps.purchase.db.repo.PurchaseOrderRepository;
 import com.axelor.apps.purchase.db.repo.PurchaseRequestManagementRepository;
 import com.axelor.apps.purchase.db.repo.PurchaseRequestRepository;
+import com.axelor.apps.purchase.service.CurrencyScaleServicePurchase;
+import com.axelor.apps.purchase.service.CurrencyScaleServicePurchaseImpl;
 import com.axelor.apps.purchase.service.ProductServicePurchaseImpl;
 import com.axelor.apps.purchase.service.PurchaseOrderDomainService;
 import com.axelor.apps.purchase.service.PurchaseOrderDomainServiceImpl;
@@ -75,5 +77,6 @@ public class PurchaseModule extends AxelorModule {
     bind(PurchaseOrderLineTaxService.class).to(PurchaseOrderLineTaxServiceImpl.class);
     bind(PurchaseOrderMergingService.class).to(PurchaseOrderMergingServiceImpl.class);
     bind(PurchaseOrderMergingViewService.class).to(PurchaseOrderMergingViewServiceImpl.class);
+    bind(CurrencyScaleServicePurchase.class).to(CurrencyScaleServicePurchaseImpl.class);
   }
 }
