fbfe2b846094958582498c7360df958c19ae02c3
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2023-12-05 11:48:50 +0100

    72454 fix linked pricings and logs

--- axelor-base/src/main/java/com/axelor/apps/base/service/pricing/PricingGenericService.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/pricing/PricingGenericService.java
@@ -2,8 +2,10 @@ package com.axelor.apps.base.service.pricing;
 
 import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.Company;
+import com.axelor.apps.base.db.Pricing;
 import com.axelor.db.Model;
 import com.google.inject.persist.Transactional;
+import java.util.List;
 
 public interface PricingGenericService {
   void usePricings(Company company, Model model) throws AxelorException;
@@ -11,5 +13,9 @@ public interface PricingGenericService {
   @Transactional
   void computePricingsOnModel(Company company, Model model) throws AxelorException;
 
+  List<Pricing> getPricings(Company company, Model model);
+
   void computePricingsOnChildren(Company company, Model model) throws AxelorException;
+
+  String updatePricingScaleLogs(List<StringBuilder> logsList, Model model);
 }
--- axelor-base/src/main/java/com/axelor/apps/base/service/pricing/PricingGenericServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/pricing/PricingGenericServiceImpl.java
@@ -7,8 +7,11 @@ import com.axelor.common.ObjectUtils;
 import com.axelor.db.EntityHelper;
 import com.axelor.db.JpaRepository;
 import com.axelor.db.Model;
+import com.axelor.i18n.I18n;
+import com.axelor.web.ITranslation;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,27 +36,61 @@ public class PricingGenericServiceImpl implements PricingGenericService {
   @Override
   @Transactional
   public void computePricingsOnModel(Company company, Model model) throws AxelorException {
+    List<Pricing> pricingList = getPricings(company, model);
+
+    if (ObjectUtils.isEmpty(pricingList)) {
+      return;
+    }
+    List<StringBuilder> logsList = new ArrayList<>();
+    for (Pricing pricing : pricingList) {
+      PricingComputer pricingComputer = PricingComputer.of(pricing, model);
+      pricingComputer.subscribe(pricingObserver);
+      pricingComputer.apply();
+
+      logsList.add(pricingObserver.getLogs());
+    }
+
+    updatePricingScaleLogs(logsList, model);
+
+    JpaRepository.of(EntityHelper.getEntityClass(model)).save(model);
+  }
+
+  @Override
+  public List<Pricing> getPricings(Company company, Model model) {
     List<Pricing> pricingList = pricingService.getPricings(company, model, null);
     pricingList =
         pricingList.stream()
             .filter(pricing -> pricing.getLinkedPricing() == null)
             .collect(Collectors.toList());
 
-    if (ObjectUtils.isEmpty(pricingList)) {
-      return;
-    }
-
-    for (Pricing pricing : pricingList) {
-      PricingComputer pricingComputer = PricingComputer.of(pricing, model);
-      pricingComputer.subscribe(pricingObserver);
-      pricingComputer.apply();
-    }
-
-    JpaRepository.of(EntityHelper.getEntityClass(model)).save(model);
+    return pricingList;
   }
 
   @Override
   public void computePricingsOnChildren(Company company, Model model) throws AxelorException {
     return;
+  }
+
+  @Override
+  public String updatePricingScaleLogs(List<StringBuilder> logsList, Model model) {
+    String logs = computePricingLogs(logsList);
+
+    return logs;
+  }
+
+  protected String computePricingLogs(List<StringBuilder> logsList) {
+
+    List<StringBuilder> pricingScaleLogs = new ArrayList<>();
+    for (StringBuilder logs : logsList) {
+      if (!I18n.get(ITranslation.PRICING_OBSERVER_NO_PRICING).equals(logs.toString())) {
+        pricingScaleLogs.add(logs);
+      }
+    }
+
+    if (ObjectUtils.isEmpty(pricingScaleLogs)) {
+      return I18n.get(ITranslation.PRICING_OBSERVER_NO_PRICING);
+    }
+
+    return pricingScaleLogs.stream().collect(Collectors.joining("\n"));
   }
 }
