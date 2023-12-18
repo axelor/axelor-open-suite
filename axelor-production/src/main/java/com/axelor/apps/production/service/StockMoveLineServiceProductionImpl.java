6ccde8daf582d1e624955c750c88d26effda046d
Edwin Roger-Gorlin <e.roger-gorlin@axelor.com>
2023-12-18 09:01:15 +0100

    fix

--- axelor-production/src/main/java/com/axelor/apps/production/service/StockMoveLineServiceProductionImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/StockMoveLineServiceProductionImpl.java
@@ -16,17 +16,13 @@ import com.axelor.apps.stock.service.StockMoveToolService;
 import com.axelor.apps.stock.service.TrackingNumberService;
 import com.axelor.apps.stock.service.WeightedAveragePriceService;
 import com.axelor.apps.stock.service.app.AppStockService;
-import com.axelor.apps.supplychain.db.ProductReservation;
 import com.axelor.apps.supplychain.db.repo.ProductReservationRepository;
 import com.axelor.apps.supplychain.db.repo.SupplychainBatchRepository;
 import com.axelor.apps.supplychain.service.ProductReservationService;
 import com.axelor.apps.supplychain.service.StockMoveLineServiceSupplychainImpl;
 import com.axelor.apps.supplychain.service.app.AppSupplychainService;
 import com.axelor.apps.supplychain.service.config.SupplyChainConfigService;
-import com.axelor.inject.Beans;
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class StockMoveLineServiceProductionImpl extends StockMoveLineServiceSupplychainImpl {
   public StockMoveLineServiceProductionImpl(
@@ -76,53 +72,12 @@ public class StockMoveLineServiceProductionImpl extends StockMoveLineServiceSupp
 
   @Override
   public boolean isAllocationToBeSelected(StockMoveLine stockMoveLine) {
-    if (stockMoveLine
-            .getQty()
+    return stockMoveLine
+            .getRealQty()
             .compareTo(
                 stockMoveLine.getProductReservationList().stream()
                     .map(it -> it.getQty())
                     .reduce(BigDecimal.ZERO, BigDecimal::add))
-        != 0) {
-      List<ProductReservation> productReservations =
-          Beans.get(ProductReservationRepository.class)
-              .all()
-              .filter(
-                  "self.stockLocation = :stockLocation AND self.status = :status AND self.product = :product AND self.originStockMoveLine != :stockMoveLine")
-              .bind("stockLocation", stockMoveLine.getFromStockLocation())
-              .bind("status", ProductReservationRepository.PRODUCT_RESERVATION_STATUS_IN_PROGRESS)
-              .bind("product", stockMoveLine.getProduct())
-              .bind("stockMoveLine", stockMoveLine)
-              .fetch();
-      if (productReservations.stream()
-          .anyMatch(
-              it ->
-                  (stockMoveLine.getSaleOrderLine() != null
-                          && stockMoveLine.getSaleOrderLine().equals(it.getOriginSaleOrderLine())
-                          && stockMoveLine.getQty().compareTo(it.getQty()) <= 0)
-                      || (stockMoveLine.getConsumedManufOrder() != null
-                          && stockMoveLine.getConsumedManufOrder().equals(it.getOriginManufOrder())
-                          && stockMoveLine.getQty().compareTo(it.getQty()) <= 0))) {
-        return false;
-      }
-      if (productReservations.stream()
-              .map(
-                  it ->
-                      (it.getOriginSaleOrderLine() != null
-                              ? it.getOriginSaleOrderLine().getFullName()
-                              : "")
-                          + (it.getOriginManufOrder() != null
-                              ? it.getOriginManufOrder().getManufOrderSeq()
-                              : "")
-                          + (it.getOriginStockMoveLine() != null
-                              ? it.getOriginStockMoveLine().getName()
-                              : ""))
-              .distinct()
-              .collect(Collectors.toList())
-              .size()
-          > 1) {
-        return true;
-      }
-    }
-    return false;
+        != 0;
   }
 }
