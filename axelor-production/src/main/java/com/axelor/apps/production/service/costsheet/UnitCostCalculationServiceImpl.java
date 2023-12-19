e69ff3ff0582e871b5c7bb741812ae6e905e7f10
bsu-axelor <86819435+bsu-axelor@users.noreply.github.com>
2023-12-19 19:21:59 +0530

    Anomaly RM#72266: UNITCOSTCALCULATION : irrelevant message when no product has been found (#11659)

--- axelor-production/src/main/java/com/axelor/apps/production/service/costsheet/UnitCostCalculationServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/costsheet/UnitCostCalculationServiceImpl.java
@@ -19,6 +19,8 @@ package com.axelor.apps.production.service.costsheet;
 
 import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.Product;
+import com.axelor.apps.base.db.ProductCategory;
+import com.axelor.apps.base.db.ProductFamily;
 import com.axelor.apps.base.db.repo.ProductRepository;
 import com.axelor.apps.base.service.ProductCompanyService;
 import com.axelor.apps.base.service.ProductService;
@@ -295,15 +297,26 @@ public class UnitCostCalculationServiceImpl implements UnitCostCalculationServic
 
     Set<Product> productSet = Sets.newHashSet();
 
-    if (!unitCostCalculation.getProductSet().isEmpty()) {
+    Set<Product> selectedProductSet = unitCostCalculation.getProductSet();
+    Set<ProductCategory> productCategorySet = unitCostCalculation.getProductCategorySet();
+    Set<ProductFamily> productFamilySet = unitCostCalculation.getProductFamilySet();
+    if (ObjectUtils.isEmpty(selectedProductSet)
+        && ObjectUtils.isEmpty(productCategorySet)
+        && ObjectUtils.isEmpty(productFamilySet)) {
+      throw new AxelorException(
+          TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
+          I18n.get(ProductionExceptionMessage.UNIT_COST_CALCULATION_CHOOSE_FILTERS));
+    }
 
-      productSet.addAll(unitCostCalculation.getProductSet());
+    if (!selectedProductSet.isEmpty()) {
+
+      productSet.addAll(selectedProductSet);
     }
 
     List<Integer> productSubTypeSelects =
         StringTool.getIntegerList(unitCostCalculation.getProductSubTypeSelect());
 
-    if (!unitCostCalculation.getProductCategorySet().isEmpty()) {
+    if (!productCategorySet.isEmpty()) {
 
       productSet.addAll(
           productRepository
@@ -312,7 +325,7 @@ public class UnitCostCalculationServiceImpl implements UnitCostCalculationServic
                   "self.productCategory in (?1) AND self.productTypeSelect = ?2 AND self.productSubTypeSelect in (?3)"
                       + " AND self.defaultBillOfMaterial.company in (?4) AND self.procurementMethodSelect in (?5, ?6)"
                       + " AND self.dtype = 'Product'",
-                  unitCostCalculation.getProductCategorySet(),
+                  productCategorySet,
                   ProductRepository.PRODUCT_TYPE_STORABLE,
                   productSubTypeSelects,
                   unitCostCalculation.getCompanySet(),
@@ -321,7 +334,7 @@ public class UnitCostCalculationServiceImpl implements UnitCostCalculationServic
               .fetch());
     }
 
-    if (!unitCostCalculation.getProductFamilySet().isEmpty()) {
+    if (!productFamilySet.isEmpty()) {
 
       productSet.addAll(
           productRepository
@@ -330,7 +343,7 @@ public class UnitCostCalculationServiceImpl implements UnitCostCalculationServic
                   "self.productFamily in (?1) AND self.productTypeSelect = ?2 AND self.productSubTypeSelect in (?3)"
                       + " AND self.defaultBillOfMaterial.company in (?4) AND self.procurementMethodSelect in (?5, ?6)"
                       + " AND self.dtype = 'Product'",
-                  unitCostCalculation.getProductFamilySet(),
+                  productFamilySet,
                   ProductRepository.PRODUCT_TYPE_STORABLE,
                   productSubTypeSelects,
                   unitCostCalculation.getCompanySet(),
@@ -342,7 +355,7 @@ public class UnitCostCalculationServiceImpl implements UnitCostCalculationServic
     if (productSet.isEmpty()) {
       throw new AxelorException(
           TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
-          I18n.get(ProductionExceptionMessage.UNIT_COST_CALCULATION_NO_PRODUCT));
+          I18n.get(ProductionExceptionMessage.UNIT_COST_CALCULATION_NO_PRODUCT_FOUND));
     }
 
     Company company = this.getSingleCompany(unitCostCalculation);
