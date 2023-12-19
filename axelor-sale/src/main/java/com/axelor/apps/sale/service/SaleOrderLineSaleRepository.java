d83aa3a025ff87b57db39de1e1b7eadb31b4a228
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2023-12-19 17:36:20 +0100

    71795 - Fix decimals in sale order line version grid

--- axelor-sale/src/main/java/com/axelor/apps/sale/service/SaleOrderLineSaleRepository.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/service/SaleOrderLineSaleRepository.java
@@ -18,9 +18,11 @@
  */
 package com.axelor.apps.sale.service;
 
+import com.axelor.apps.sale.db.SaleOrder;
 import com.axelor.apps.sale.db.SaleOrderLine;
 import com.axelor.apps.sale.db.repo.SaleOrderLineRepository;
 import com.axelor.apps.sale.db.repo.SaleOrderRepository;
+import com.axelor.inject.Beans;
 import java.util.Map;
 
 public class SaleOrderLineSaleRepository extends SaleOrderLineRepository {
@@ -42,6 +44,14 @@ public class SaleOrderLineSaleRepository extends SaleOrderLineRepository {
                             == SaleOrderRepository.STATUS_ORDER_CONFIRMED
                         && saleOrderLine.getSaleOrder().getOrderBeingEdited()))
                 && saleOrderLine.getDiscountsNeedReview());
+
+        SaleOrder saleOrder =
+            saleOrderLine.getSaleOrder() != null
+                ? saleOrderLine.getSaleOrder()
+                : saleOrderLine.getOldVersionSaleOrder();
+        json.put(
+            "$currencyNumberOfDecimals",
+            Beans.get(CurrencyScaleServiceSale.class).getScale(saleOrder));
       }
     }
     return super.populate(json, context);
