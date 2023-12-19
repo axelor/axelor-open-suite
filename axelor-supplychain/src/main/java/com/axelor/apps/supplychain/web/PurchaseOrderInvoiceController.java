fc3a54f270c5b598a58591c9accd0aa807c0359c
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2023-12-19 11:10:03 +0100

    71794 - Manage decimals in PurchaseOrderInvoicing

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/web/PurchaseOrderInvoiceController.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/web/PurchaseOrderInvoiceController.java
@@ -24,6 +24,7 @@ import com.axelor.apps.account.service.invoice.InvoiceViewService;
 import com.axelor.apps.base.service.exception.TraceBackService;
 import com.axelor.apps.purchase.db.PurchaseOrder;
 import com.axelor.apps.purchase.db.repo.PurchaseOrderRepository;
+import com.axelor.apps.sale.service.app.AppSaleService;
 import com.axelor.apps.supplychain.exception.SupplychainExceptionMessage;
 import com.axelor.apps.supplychain.service.PurchaseOrderInvoiceService;
 import com.axelor.apps.supplychain.service.app.AppSupplychainService;
@@ -129,6 +130,20 @@ public class PurchaseOrderInvoiceController {
       response.setAttr("$operationSelect", "value", "1");
     } catch (Exception e) {
       TraceBackService.trace(response, e);
+    }
+  }
+
+  public void setAmountToInvoiceScale(ActionRequest request, ActionResponse response) {
+    PurchaseOrder purchaseOrder = request.getContext().asType(PurchaseOrder.class);
+    boolean isPercent = (Boolean) request.getContext().getOrDefault("isPercent", false);
+
+    if (purchaseOrder != null && purchaseOrder.getCurrency() != null) {
+      response.setAttr(
+          "$amountToInvoice",
+          "scale",
+          isPercent
+              ? AppSaleService.DEFAULT_NB_DECIMAL_DIGITS
+              : purchaseOrder.getCurrency().getNumberOfDecimals());
     }
   }
 }
