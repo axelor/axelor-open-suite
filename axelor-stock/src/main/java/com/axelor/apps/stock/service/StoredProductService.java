029b7174410b331b59a612bbca81ec80b469a48d
Yahya <y.bouyboud@axelor.com>
2024-01-11 15:07:07 +0100

    RM#67790 : Add actions to store products

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StoredProductService.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StoredProductService.java
@@ -1,5 +1,6 @@
 package com.axelor.apps.stock.service;
 
+import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.Product;
 import com.axelor.apps.base.db.Unit;
 import com.axelor.apps.stock.db.MassStockMove;
@@ -11,6 +12,10 @@ import com.axelor.apps.stock.db.TrackingNumber;
 import java.math.BigDecimal;
 
 public interface StoredProductService {
+  public void createStockMoveAndStockMoveLine(StoredProduct storedProduct) throws AxelorException;
+
+  public void cancelStockMoveAndStockMoveLine(StoredProduct storedProduct) throws AxelorException;
+
   public StoredProduct createStoredProduct(
       Product product,
       TrackingNumber trackingNumber,
