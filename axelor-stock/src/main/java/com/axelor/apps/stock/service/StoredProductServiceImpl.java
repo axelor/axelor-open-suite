ed45de6a4684721b2e1aca1b997925c76a828adb
Yahya <y.bouyboud@axelor.com>
2024-01-11 15:07:07 +0100

    RM#67790 : Add actions to store products

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StoredProductServiceImpl.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StoredProductServiceImpl.java
@@ -3,6 +3,7 @@ package com.axelor.apps.stock.service;
 import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.Product;
 import com.axelor.apps.base.db.Unit;
+import com.axelor.apps.base.db.repo.TraceBackRepository;
 import com.axelor.apps.stock.db.MassStockMove;
 import com.axelor.apps.stock.db.PickedProduct;
 import com.axelor.apps.stock.db.StockLocation;
@@ -14,6 +15,8 @@ import com.axelor.apps.stock.db.repo.MassStockMoveRepository;
 import com.axelor.apps.stock.db.repo.StockMoveLineRepository;
 import com.axelor.apps.stock.db.repo.StockMoveRepository;
 import com.axelor.apps.stock.db.repo.StoredProductRepository;
+import com.axelor.apps.stock.exception.StockExceptionMessage;
+import com.axelor.i18n.I18n;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 import java.math.BigDecimal;
@@ -68,6 +71,14 @@ public class StoredProductServiceImpl implements StoredProductService {
     return storedProduct;
   }
 
+  protected StoredProduct duplicateStoredProduct(StoredProduct storedProduct) {
+    StoredProduct duplicatedStoredProduct = storedProductRepository.copy(storedProduct, false);
+    duplicatedStoredProduct.setCurrentQty(
+        storedProduct.getCurrentQty().subtract(storedProduct.getStoredQty()));
+    duplicatedStoredProduct.setStoredQty(BigDecimal.ZERO);
+    return duplicatedStoredProduct;
+  }
+
   @Transactional(rollbackOn = {Exception.class})
   protected void createStockMoveForStoredProduct(StoredProduct storedProduct)
       throws AxelorException {
@@ -102,5 +113,94 @@ public class StoredProductServiceImpl implements StoredProductService {
     stockMoveService.realize(stockMove);
     storedProduct.setStockMoveLine(stockMoveLine);
     storedProductRepository.save(storedProduct);
+  }
+
+  @Override
+  @Transactional(rollbackOn = Exception.class)
+  public void createStockMoveAndStockMoveLine(StoredProduct storedProduct) throws AxelorException {
+    BigDecimal storedQty = storedProduct.getStoredQty();
+    BigDecimal currentQty = storedProduct.getCurrentQty();
+
+    if (storedQty.compareTo(currentQty) == -1 && storedQty.compareTo(BigDecimal.ZERO) != 0) {
+      StoredProduct duplicatedStoredProduct = duplicateStoredProduct(storedProduct);
+      storedProduct.setCurrentQty(storedQty);
+      storedProductRepository.save(duplicatedStoredProduct);
+      storedProduct = storedProductRepository.save(storedProduct);
+    }
+
+    if (storedProduct.getStoredQty().compareTo(BigDecimal.ZERO) != 0
+        && storedProduct.getToStockLocation() != null
+        && storedProduct.getStockMoveLine() == null) {
+
+      if (storedProduct.getStoredQty().compareTo(storedProduct.getCurrentQty()) == 1
+          && storedProduct.getTrackingNumber() != null) {
+        throw new AxelorException(
+            TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
+            I18n.get(StockExceptionMessage.STORED_QUANTITY_GREATER_THAN_CURRENT_QTY),
+            storedProduct.getStoredProduct().getFullName(),
+            storedProduct.getTrackingNumber() != null
+                ? storedProduct.getTrackingNumber().getTrackingNumberSeq()
+                : "");
+      }
+      createStockMoveForStoredProduct(storedProduct);
+    } else if (storedProduct.getStoredQty().compareTo(BigDecimal.ZERO) == 0) {
+      throw new AxelorException(
+          TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
+          I18n.get(StockExceptionMessage.STORED_QUANTITY_IS_ZERO),
+          storedProduct.getStoredProduct().getFullName(),
+          storedProduct.getTrackingNumber() != null
+              ? storedProduct.getTrackingNumber().getTrackingNumberSeq()
+              : "");
+
+    } else if (storedProduct.getStockMoveLine() != null) {
+      throw new AxelorException(
+          TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
+          I18n.get(StockExceptionMessage.ALREADY_STORED_PRODUCT),
+          storedProduct.getStoredProduct(),
+          storedProduct.getTrackingNumber() != null
+              ? storedProduct.getTrackingNumber().getTrackingNumberSeq()
+              : "");
+    }
+    MassStockMove massStockMove = storedProduct.getMassStockMove();
+    if (checkIfAllStoredProductAreStored(massStockMove)
+        && checkIfAllPickedProductArePicked(massStockMove)) {
+      storedProduct.getMassStockMove().setStatusSelect(MassStockMoveRepository.STATUS_REALIZED);
+      massStockMoveRepository.save(storedProduct.getMassStockMove());
+    }
+  }
+
+  public boolean checkIfAllStoredProductAreStored(MassStockMove massStockMove) {
+    return !massStockMove.getStoredProductList().stream()
+        .anyMatch(
+            it ->
+                it.getStoredQty().compareTo(it.getCurrentQty()) != 0
+                    && it.getStockMoveLine() == null);
+  }
+
+  public boolean checkIfAllPickedProductArePicked(MassStockMove massStockMove) {
+    return !massStockMove.getPickedProductList().stream()
+        .anyMatch(
+            it ->
+                it.getPickedQty().compareTo(it.getCurrentQty()) != 0
+                    && it.getStockMoveLine() == null);
+  }
+
+  @Override
+  @Transactional(rollbackOn = Exception.class)
+  public void cancelStockMoveAndStockMoveLine(StoredProduct storedProduct) throws AxelorException {
+    if (storedProduct.getStockMoveLine() != null) {
+      stockMoveService.cancel(storedProduct.getStockMoveLine().getStockMove());
+      storedProduct.setStockMoveLine(null);
+      storedProduct.setStoredQty(BigDecimal.ZERO);
+      if (storedProduct
+          .getMassStockMove()
+          .getStatusSelect()
+          .equals(MassStockMoveRepository.STATUS_REALIZED)) {
+        storedProduct
+            .getMassStockMove()
+            .setStatusSelect(MassStockMoveRepository.STATUS_IN_PROGRESS);
+      }
+      storedProductRepository.save(storedProduct);
+    }
   }
 }
