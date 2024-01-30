fcb6228558195b9c4bbdca1b48ea75d29a117c20
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-01-30 18:36:09 +0100

    74106 Invoice: fix dates when ventilating with advance payment and app base today date in the future
    (cherry picked from commit 3cdf32aec765ec82a2275ee94d8907410f1626a9)

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveCreateFromInvoiceServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/MoveCreateFromInvoiceServiceImpl.java
@@ -354,7 +354,7 @@ public class MoveCreateFromInvoiceServiceImpl implements MoveCreateFromInvoiceSe
               moveToolService
                   .getTotalCurrencyAmount(creditMoveLineList)
                   .min(invoiceCustomerMoveLine.getCurrencyAmount());
-          LocalDate date = appAccountService.getTodayDate(company);
+          LocalDate date = invoice.getInvoiceDate();
 
           // credit move line creation
           MoveLine creditMoveLine =
@@ -449,7 +449,7 @@ public class MoveCreateFromInvoiceServiceImpl implements MoveCreateFromInvoiceSe
           moveToolService
               .getTotalCurrencyAmount(debitMoveLines)
               .min(invoiceCustomerMoveLine.getCurrencyAmount().abs());
-      LocalDate date = appAccountService.getTodayDate(company);
+      LocalDate date = invoice.getInvoiceDate();
 
       // debit move line creation
       MoveLine debitMoveLine =
@@ -480,7 +480,7 @@ public class MoveCreateFromInvoiceServiceImpl implements MoveCreateFromInvoiceSe
           company,
           null,
           account,
-          appAccountService.getTodayDate(company));
+          invoice.getInvoiceDate());
 
       moveValidateService.accounting(oDmove);
 
