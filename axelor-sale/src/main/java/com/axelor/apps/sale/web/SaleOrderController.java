785dba7affddccd67133d5de79c7435ed5ef770c
dar-axelor <132374601+dar-axelor@users.noreply.github.com>
2024-01-30 14:26:25 +0100

    RM#74620 Context variable "id" shouldn't be used

--- axelor-sale/src/main/java/com/axelor/apps/sale/web/SaleOrderController.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/web/SaleOrderController.java
@@ -167,10 +167,11 @@ public class SaleOrderController {
         fileLink = saleOrderPrintService.printSaleOrders(ids);
         title = I18n.get("Sale orders");
 
-      } else if (context.get("id") != null) {
+      } else if (context.get("_saleOrderId") != null) {
 
         SaleOrder saleOrder =
-            Beans.get(SaleOrderRepository.class).find(Long.parseLong(context.get("id").toString()));
+            Beans.get(SaleOrderRepository.class)
+                .find(Long.parseLong(context.get("_saleOrderId").toString()));
         title = Beans.get(SaleOrderService.class).getFileName(saleOrder);
         fileLink = saleOrderPrintService.printSaleOrder(saleOrder, proforma, format);
         response.setCanClose(true);
