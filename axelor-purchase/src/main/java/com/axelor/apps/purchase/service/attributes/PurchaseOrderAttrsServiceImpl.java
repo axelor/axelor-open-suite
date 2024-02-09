9b86e34152370e9f8592c733d23c4822213d00b2
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2024-02-09 17:50:44 +0100

    75167 - Rework currencyScaleService in purchase module

--- axelor-purchase/src/main/java/com/axelor/apps/purchase/service/attributes/PurchaseOrderAttrsServiceImpl.java
+++ axelor-purchase/src/main/java/com/axelor/apps/purchase/service/attributes/PurchaseOrderAttrsServiceImpl.java
@@ -1,7 +1,7 @@
 package com.axelor.apps.purchase.service.attributes;
 
+import com.axelor.apps.base.service.CurrencyScaleService;
 import com.axelor.apps.purchase.db.PurchaseOrder;
-import com.axelor.apps.purchase.service.CurrencyScaleServicePurchase;
 import com.axelor.common.ObjectUtils;
 import com.google.inject.Inject;
 import java.util.HashMap;
@@ -9,11 +9,11 @@ import java.util.Map;
 
 public class PurchaseOrderAttrsServiceImpl implements PurchaseOrderAttrsService {
 
-  protected CurrencyScaleServicePurchase currencyScaleServicePurchase;
+  protected CurrencyScaleService currencyScaleService;
 
   @Inject
-  public PurchaseOrderAttrsServiceImpl(CurrencyScaleServicePurchase currencyScaleServicePurchase) {
-    this.currencyScaleServicePurchase = currencyScaleServicePurchase;
+  public PurchaseOrderAttrsServiceImpl(CurrencyScaleService currencyScaleService) {
+    this.currencyScaleService = currencyScaleService;
   }
 
   protected void addAttr(
@@ -28,7 +28,7 @@ public class PurchaseOrderAttrsServiceImpl implements PurchaseOrderAttrsService 
   @Override
   public void setPurchaseOrderLineScale(
       PurchaseOrder purchaseOrder, Map<String, Map<String, Object>> attrsMap) {
-    int currencyScale = currencyScaleServicePurchase.getScale(purchaseOrder);
+    int currencyScale = currencyScaleService.getScale(purchaseOrder);
 
     this.addAttr("purchaseOrderLineList.exTaxTotal", "scale", currencyScale, attrsMap);
     this.addAttr("purchaseOrderLineList.inTaxTotal", "scale", currencyScale, attrsMap);
@@ -37,7 +37,7 @@ public class PurchaseOrderAttrsServiceImpl implements PurchaseOrderAttrsService 
   @Override
   public void setPurchaseOrderLineTaxScale(
       PurchaseOrder purchaseOrder, Map<String, Map<String, Object>> attrsMap) {
-    int currencyScale = currencyScaleServicePurchase.getScale(purchaseOrder);
+    int currencyScale = currencyScaleService.getScale(purchaseOrder);
 
     this.addAttr("purchaseOrderLineTaxList.inTaxTotal", "scale", currencyScale, attrsMap);
     this.addAttr("purchaseOrderLineTaxList.exTaxBase", "scale", currencyScale, attrsMap);
