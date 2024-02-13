fdd9d21d2b058a23be2cda0c8d1a11e2cfc289fc
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-02-13 16:03:07 +0100

    74879 Reconciliation: fix invoice term imputation when pfp not validated
    (cherry picked from commit 354487c7d85e98bd2eca604cf9be14eac5401f7c)
    (cherry picked from commit 30ffc018599b6956c6f16bdc9b987d8cc80296c8)
    (cherry picked from commit 30dc4f960ef3942659daf481bbd71f2711099137)

--- axelor-account/src/main/java/com/axelor/apps/account/service/ReconcileServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/ReconcileServiceImpl.java
@@ -39,6 +39,7 @@ import com.axelor.apps.account.db.repo.ReconcileRepository;
 import com.axelor.apps.account.db.repo.SubrogationReleaseRepository;
 import com.axelor.apps.account.exception.AccountExceptionMessage;
 import com.axelor.apps.account.service.config.AccountConfigService;
+import com.axelor.apps.account.service.invoice.InvoiceTermPfpService;
 import com.axelor.apps.account.service.invoice.InvoiceTermService;
 import com.axelor.apps.account.service.move.MoveAdjustementService;
 import com.axelor.apps.account.service.move.MoveLineControlService;
@@ -104,6 +105,7 @@ public class ReconcileServiceImpl implements ReconcileService {
   protected MoveLineControlService moveLineControlService;
   protected MoveLineRepository moveLineRepo;
   protected SubrogationReleaseWorkflowService subrogationReleaseWorkflowService;
+  protected InvoiceTermPfpService invoiceTermPfpService;
 
   @Inject
   public ReconcileServiceImpl(
@@ -125,7 +127,8 @@ public class ReconcileServiceImpl implements ReconcileService {
       InvoicePaymentToolService invoicePaymentToolService,
       MoveLineControlService moveLineControlService,
       MoveLineRepository moveLineRepo,
-      SubrogationReleaseWorkflowService subrogationReleaseWorkflowService) {
+      SubrogationReleaseWorkflowService subrogationReleaseWorkflowService,
+      InvoiceTermPfpService invoiceTermPfpService) {
 
     this.moveToolService = moveToolService;
     this.accountCustomerService = accountCustomerService;
@@ -146,6 +149,7 @@ public class ReconcileServiceImpl implements ReconcileService {
     this.moveLineControlService = moveLineControlService;
     this.moveLineRepo = moveLineRepo;
     this.subrogationReleaseWorkflowService = subrogationReleaseWorkflowService;
+    this.invoiceTermPfpService = invoiceTermPfpService;
   }
 
   /**
@@ -163,10 +167,12 @@ public class ReconcileServiceImpl implements ReconcileService {
       MoveLine debitMoveLine,
       MoveLine creditMoveLine,
       BigDecimal amount,
-      boolean canBeZeroBalanceOk) {
+      boolean canBeZeroBalanceOk)
+      throws AxelorException {
 
     if (ReconcileService.isReconcilable(debitMoveLine, creditMoveLine)
         && amount.compareTo(BigDecimal.ZERO) > 0) {
+
       log.debug(
           "Create Reconcile (Company : {}, Debit MoveLine : {}, Credit MoveLine : {}, Amount : {}, Can be zero balance ? {} )",
           debitMoveLine.getMove().getCompany(),
@@ -209,7 +215,7 @@ public class ReconcileServiceImpl implements ReconcileService {
       Reconcile reconcile, boolean updateInvoicePayments, boolean updateInvoiceTerms)
       throws AxelorException {
 
-    reconcile = initReconcileConfirmation(reconcile);
+    reconcile = initReconcileConfirmation(reconcile, updateInvoicePayments, updateInvoiceTerms);
 
     if (updateInvoicePayments) {
       this.updatePayments(reconcile, updateInvoiceTerms);
@@ -220,9 +226,11 @@ public class ReconcileServiceImpl implements ReconcileService {
   }
 
   @Transactional(rollbackOn = {Exception.class})
-  public Reconcile initReconcileConfirmation(Reconcile reconcile) throws AxelorException {
+  public Reconcile initReconcileConfirmation(
+      Reconcile reconcile, boolean updateInvoicePayments, boolean updateInvoiceTerm)
+      throws AxelorException {
 
-    this.reconcilePreconditions(reconcile);
+    this.reconcilePreconditions(reconcile, updateInvoicePayments, updateInvoiceTerm);
 
     MoveLine debitMoveLine = reconcile.getDebitMoveLine();
     MoveLine creditMoveLine = reconcile.getCreditMoveLine();
@@ -263,7 +271,9 @@ public class ReconcileServiceImpl implements ReconcileService {
   }
 
   @Override
-  public void reconcilePreconditions(Reconcile reconcile) throws AxelorException {
+  public void reconcilePreconditions(
+      Reconcile reconcile, boolean updateInvoicePayments, boolean updateInvoiceTerms)
+      throws AxelorException {
 
     MoveLine debitMoveLine = reconcile.getDebitMoveLine();
     MoveLine creditMoveLine = reconcile.getCreditMoveLine();
@@ -350,6 +360,11 @@ public class ReconcileServiceImpl implements ReconcileService {
     // Check tax lines
     this.taxLinePrecondition(creditMoveLine.getMove());
     this.taxLinePrecondition(debitMoveLine.getMove());
+
+    if (updateInvoiceTerms && updateInvoicePayments) {
+      invoiceTermPfpService.validatePfpValidatedAmount(
+          debitMoveLine, creditMoveLine, reconcile.getAmount());
+    }
   }
 
   protected void taxLinePrecondition(Move move) throws AxelorException {
@@ -672,7 +687,8 @@ public class ReconcileServiceImpl implements ReconcileService {
     } else {
       List<InvoiceTerm> invoiceTermsToPay = null;
       if (invoice != null && CollectionUtils.isNotEmpty(invoice.getInvoiceTermList())) {
-        invoiceTermsToPay = invoiceTermService.getUnpaidInvoiceTermsFiltered(invoice);
+        invoiceTermsToPay =
+            invoiceTermService.getUnpaidInvoiceTermsFilteredWithoutPfpCheck(invoice);
 
       } else if (CollectionUtils.isNotEmpty(moveLine.getInvoiceTermList())) {
         invoiceTermsToPay = this.getInvoiceTermsFromMoveLine(moveLine.getInvoiceTermList());
