185017ad2d23aa7d6013cad5d71bd7b49b4ae412
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2024-02-14 16:21:41 +0100

    Feature #75437: Added a new field price in inventory line to init price

---
+++ axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/InventoryLineManagementRepository.java
@@ -0,0 +1,29 @@
+package com.axelor.apps.stock.db.repo;
+
+import com.axelor.apps.stock.db.InventoryLine;
+import com.axelor.apps.stock.service.InventoryLineService;
+import com.axelor.inject.Beans;
+import java.util.Map;
+
+public class InventoryLineManagementRepository extends InventoryLineRepository {
+
+  @Override
+  public Map<String, Object> populate(Map<String, Object> json, Map<String, Object> context) {
+
+    if (context.get("_model") != null
+        && context.get("_model").toString().equals(InventoryLine.class.getName())) {
+
+      Long id = (Long) json.get("id");
+      if (id != null) {
+        InventoryLine inventoryLine = find(id);
+        json.put(
+            "isPresentInStockLocation",
+            inventoryLine != null
+                && Beans.get(InventoryLineService.class).isPresentInStockLocation(inventoryLine));
+      } else {
+        json.put("isPresentInStockLocation", false);
+      }
+    }
+    return super.populate(json, context);
+  }
+}
