4babcc009ff6fecb548fe5392512b31f7b4b452c
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2024-02-15 15:13:09 +0100

    Refactor reconcile group to split it into multiple services
    RM53525

---
+++ axelor-account/src/main/java/com/axelor/apps/account/service/ReconcileGroupUnletterService.java
@@ -0,0 +1,15 @@
+package com.axelor.apps.account.service;
+
+import com.axelor.apps.account.db.ReconcileGroup;
+import com.axelor.apps.base.AxelorException;
+
+public interface ReconcileGroupUnletterService {
+
+  /**
+   * Unletter every moveline and update unlettering date.
+   *
+   * @param reconcileGroup
+   * @throws AxelorException
+   */
+  void unletter(ReconcileGroup reconcileGroup) throws AxelorException;
+}
