c51a69ffedf319577f2450a0c1e69141ffb580e9
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-02-21 18:15:28 +0100

    72279 add cancellation for bank bill of exchanges (#11697)

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/module/BankPaymentModule.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/module/BankPaymentModule.java
@@ -55,6 +55,8 @@ import com.axelor.apps.bankpayment.db.repo.PaymentSessionBankPaymentRepository;
 import com.axelor.apps.bankpayment.service.AccountingReportPrintServiceBankPaymentImpl;
 import com.axelor.apps.bankpayment.service.CurrencyScaleServiceBankPayment;
 import com.axelor.apps.bankpayment.service.CurrencyScaleServiceBankPaymentImpl;
+import com.axelor.apps.bankpayment.service.InvoiceBankPaymentService;
+import com.axelor.apps.bankpayment.service.InvoiceBankPaymentServiceImpl;
 import com.axelor.apps.bankpayment.service.InvoiceTermBankPaymentService;
 import com.axelor.apps.bankpayment.service.InvoiceTermBankPaymentServiceImpl;
 import com.axelor.apps.bankpayment.service.PaymentScheduleLineBankPaymentService;
@@ -225,6 +227,7 @@ public class BankPaymentModule extends AxelorModule {
     bind(BankStatementValidateService.class).to(BankStatementValidateServiceImpl.class);
     bind(BankStatementLineDeleteService.class).to(BankStatementLineDeleteServiceImpl.class);
     bind(BankStatementLineFilterService.class).to(BankStatementLineFilterServiceImpl.class);
+    bind(InvoiceBankPaymentService.class).to(InvoiceBankPaymentServiceImpl.class);
     bind(BankStatementLineMapperAFB120Service.class)
         .to(BankStatementLineMapperAFB120ServiceImpl.class);
     bind(BankStatementLineFetchAFB120Service.class)
