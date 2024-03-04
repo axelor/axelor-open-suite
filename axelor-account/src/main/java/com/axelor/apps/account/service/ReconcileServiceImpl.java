0a6d1db4b787523c158e88b9562ece7910ea923b
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-03-04 12:04:46 +0100

    75964 Reconcile : fix passed for payment check at reconcile confirmation
    (cherry picked from commit dc8dac8237c48cadc56fb557df6aedbdf8e8946b)
    (cherry picked from commit 13e43ace8b4a3198e85ef6d37d45201fed6e3284)

--- axelor-account/src/main/java/com/axelor/apps/account/service/ReconcileServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/ReconcileServiceImpl.java
@@ -394,7 +394,7 @@ public class ReconcileServiceImpl implements ReconcileService {
 
     if (updateInvoiceTerms && updateInvoicePayments) {
       invoiceTermPfpService.validatePfpValidatedAmount(
-          debitMoveLine, creditMoveLine, reconcile.getAmount());
+          debitMoveLine, creditMoveLine, reconcile.getAmount(), reconcileCompany);
     }
   }
 
