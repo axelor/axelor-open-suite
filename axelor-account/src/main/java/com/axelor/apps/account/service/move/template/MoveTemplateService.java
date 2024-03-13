00789a031377fcafb8c94cf99c3d3033fa5bbf87
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-03-13 14:39:53 +0100

    68342 Account : Add a dashlet to see move templates related to this a… (#12224)
    * 68342 Account : Add a dashlet to see move templates related to this account

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/template/MoveTemplateService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/template/MoveTemplateService.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package com.axelor.apps.account.service.move;
+package com.axelor.apps.account.service.move.template;
 
 import com.axelor.apps.account.db.MoveTemplate;
 import com.axelor.apps.account.db.MoveTemplateType;
