a60ebb60c7ab05625d6fe4eae6a47811dc0598e9
cma-axelor <44425727+cma-axelor@users.noreply.github.com>
2024-03-14 16:32:18 +0530

    Feature RM#76238:Printing template:Add printing template (#12237)

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/InvoicingProjectService.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/InvoicingProjectService.java
@@ -533,7 +533,7 @@ public class InvoicingProjectService {
     if (invoicingProjectAnnexBirtTemplate == null) {
       throw new AxelorException(
           TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
-          I18n.get(BaseExceptionMessage.BIRT_TEMPLATE_CONFIG_NOT_FOUND));
+          I18n.get(BaseExceptionMessage.TEMPLATE_CONFIG_NOT_FOUND));
     }
 
     String title =
