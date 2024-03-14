1726764e3958adc20cbddd8c01cb9f91e0fd5ebe
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2024-03-14 15:51:45 +0100

    73523 - Fix analytic line creation in contract line

--- axelor-contract/src/main/java/com/axelor/apps/contract/module/ContractModule.java
+++ axelor-contract/src/main/java/com/axelor/apps/contract/module/ContractModule.java
@@ -46,6 +46,8 @@ import com.axelor.apps.contract.service.InvoiceLineAnalyticContractServiceImpl;
 import com.axelor.apps.contract.service.WorkflowCancelServiceContractImpl;
 import com.axelor.apps.contract.service.attributes.ContractLineAttrsService;
 import com.axelor.apps.contract.service.attributes.ContractLineAttrsServiceImpl;
+import com.axelor.apps.contract.service.record.ContractLineRecordSetService;
+import com.axelor.apps.contract.service.record.ContractLineRecordSetServiceImpl;
 import com.axelor.apps.supplychain.service.AnalyticMoveLineSupplychainServiceImpl;
 import com.axelor.apps.supplychain.service.invoice.InvoiceLineAnalyticSupplychainServiceImpl;
 import com.axelor.apps.supplychain.service.workflow.WorkflowCancelServiceSupplychainImpl;
@@ -71,5 +73,6 @@ public class ContractModule extends AxelorModule {
     bind(ContractLineViewService.class).to(ContractLineViewServiceImpl.class);
     bind(ConsumptionLineRepository.class).to(ConsumptionLineManagementRepository.class);
     bind(ContractLineAttrsService.class).to(ContractLineAttrsServiceImpl.class);
+    bind(ContractLineRecordSetService.class).to(ContractLineRecordSetServiceImpl.class);
   }
 }
