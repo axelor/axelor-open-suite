4a3d0f3462cb922da342f2c1db264f7a272ad318
bsu-axelor <86819435+bsu-axelor@users.noreply.github.com>
2024-03-20 20:45:25 +0530

    Anomaly RM#76438: STOCKMOVE: Fix filterOnAvailableProducts issue (#12250)
    (cherry picked from commit 95c36b4591c59ad587116585677f5751dcc8b0d0)

--- axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/StockMoveLineStockRepository.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/StockMoveLineStockRepository.java
@@ -58,6 +58,8 @@ public class StockMoveLineStockRepository extends StockMoveLineRepository {
               : null);
       json.put("availableStatusSelect", stockMoveLine.getAvailableStatusSelect());
     }
+    json.put("filterOnAvailableProducts", stockMove.getFilterOnAvailableProducts());
+
     return super.populate(json, context);
   }
 }
