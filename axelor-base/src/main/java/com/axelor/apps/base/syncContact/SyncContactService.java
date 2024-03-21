8c149cbe0507254af01f4196cfe2194e28c0877b
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2024-03-21 15:42:43 +0100

    Cid override => Change existing cid fields

--- axelor-base/src/main/java/com/axelor/apps/base/syncContact/SyncContactService.java
+++ axelor-base/src/main/java/com/axelor/apps/base/syncContact/SyncContactService.java
@@ -136,7 +136,7 @@ public class SyncContactService {
       return null;
     }
     SyncContactResponse response = new SyncContactResponse();
-    response.setClientid(syncContact.getCid());
+    response.setClientid(syncContact.getCidd());
     response.setKey(syncContact.getGoogleApiKey());
     response.setAuthFailed(I18n.get(SYNC_CONTACT_AUTH_FAILED));
     response.setImportSuccessful(I18n.get(SYNC_CONTACT_IMPORT_SUCCESSFUL));
