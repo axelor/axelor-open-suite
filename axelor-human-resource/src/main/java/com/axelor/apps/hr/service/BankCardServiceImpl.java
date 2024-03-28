d589a68a44a87a4dec424d3410b368a6e3eb7514
dar-axelor <132374601+dar-axelor@users.noreply.github.com>
2024-03-28 10:48:19 +0100

    RM#75949 Manage Bank Cards

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/BankCardServiceImpl.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/BankCardServiceImpl.java
@@ -2,29 +2,22 @@ package com.axelor.apps.hr.service;
 
 import com.axelor.apps.base.db.BankDetails;
 import com.axelor.apps.base.db.Company;
-import com.axelor.apps.hr.db.BankCard;
-import java.util.List;
+import com.axelor.utils.helpers.StringHelper;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
 public class BankCardServiceImpl implements BankCardService {
   @Override
   public String createDomainForBankCard(BankDetails bankDetails, Company company) {
     if (bankDetails != null) {
-      List<Long> ids =
-          bankDetails.getBankCardList().stream().map(BankCard::getId).collect(Collectors.toList());
-      return "self.id IN " + ids.toString().replace("[", "(").replace("]", ")");
+      return "self.id IN (" + StringHelper.getIdListString(bankDetails.getBankCardList()) + ")";
     }
-
-    List<BankDetails> bankDetailsList = company.getBankDetailsList();
-    if (bankDetailsList != null) {
-      List<Long> ids =
-          bankDetailsList.stream()
-              .map(BankDetails::getBankCardList)
-              .flatMapToLong(x -> x.stream().mapToLong(BankCard::getId))
-              .boxed()
-              .collect(Collectors.toList());
-      return "self.id IN " + ids.toString().replace("[", "(").replace("]", ")");
-    }
-    return "self.id IN (0)";
+    return "self.id IN ("
+        + StringHelper.getIdListString(
+            company.getBankDetailsList().stream()
+                .map(BankDetails::getBankCardList)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList()))
+        + ")";
   }
 }
