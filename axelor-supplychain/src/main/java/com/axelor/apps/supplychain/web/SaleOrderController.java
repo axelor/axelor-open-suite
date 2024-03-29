1000ad2d4a732367770a4f0e031e72a49df5f403
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2024-03-29 17:21:47 +0100

    77313 - Fix subscription invoice generation from sale order

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/web/SaleOrderController.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/web/SaleOrderController.java
@@ -49,6 +49,7 @@ import com.axelor.apps.supplychain.service.SaleOrderShipmentService;
 import com.axelor.apps.supplychain.service.SaleOrderStockService;
 import com.axelor.apps.supplychain.service.SaleOrderSupplychainService;
 import com.axelor.apps.supplychain.service.app.AppSupplychainService;
+import com.axelor.apps.supplychain.service.invoice.SubscriptionInvoiceService;
 import com.axelor.db.JPA;
 import com.axelor.db.Model;
 import com.axelor.i18n.I18n;
@@ -262,14 +263,22 @@ public class SaleOrderController {
 
       saleOrder = Beans.get(SaleOrderRepository.class).find(saleOrder.getId());
 
-      Invoice invoice =
-          saleOrderInvoiceService.generateInvoice(
-              saleOrder,
-              operationSelect,
-              amountToInvoice,
-              isPercent,
-              qtyToInvoiceMap,
-              timetableIdList);
+      Invoice invoice;
+
+      if (saleOrder.getSaleOrderTypeSelect() == SaleOrderRepository.SALE_ORDER_TYPE_SUBSCRIPTION) {
+        SubscriptionInvoiceService subscriptionInvoiceService =
+            Beans.get(SubscriptionInvoiceService.class);
+        invoice = subscriptionInvoiceService.generateSubscriptionInvoice(saleOrder);
+      } else {
+        invoice =
+            saleOrderInvoiceService.generateInvoice(
+                saleOrder,
+                operationSelect,
+                amountToInvoice,
+                isPercent,
+                qtyToInvoiceMap,
+                timetableIdList);
+      }
 
       if (invoice != null) {
         response.setCanClose(true);
