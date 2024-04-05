e7bd496a5587ffd79b8347d83935c944d2c733fc
mah-axelor <162461637+mah-axelor@users.noreply.github.com>
2024-04-05 13:23:40 +0200

    Wip 77124 Feature: Remove companyStr and replace it with companySet (#12315)

--- axelor-account/src/main/java/com/axelor/apps/account/module/AccountModule.java
+++ axelor-account/src/main/java/com/axelor/apps/account/module/AccountModule.java
@@ -28,8 +28,6 @@ import com.axelor.apps.account.db.repo.AccountingBatchAccountRepository;
 import com.axelor.apps.account.db.repo.AccountingBatchRepository;
 import com.axelor.apps.account.db.repo.AccountingReportManagementRepository;
 import com.axelor.apps.account.db.repo.AccountingReportRepository;
-import com.axelor.apps.account.db.repo.AccountingReportTypeManagementRepository;
-import com.axelor.apps.account.db.repo.AccountingReportTypeRepository;
 import com.axelor.apps.account.db.repo.AnalyticMoveLineMngtRepository;
 import com.axelor.apps.account.db.repo.AnalyticMoveLineRepository;
 import com.axelor.apps.account.db.repo.ChequeRejectionManagementRepository;
@@ -864,8 +862,6 @@ public class AccountModule extends AxelorModule {
 
     bind(FixedAssetDerogatoryLineRepository.class)
         .to(FixedAssetDerogatoryLineManagementRepository.class);
-
-    bind(AccountingReportTypeRepository.class).to(AccountingReportTypeManagementRepository.class);
 
     bind(FindFixedAssetService.class).to(FindFixedAssetServiceImpl.class);
 
