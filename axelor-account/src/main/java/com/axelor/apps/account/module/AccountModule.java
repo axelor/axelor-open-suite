f564e97381bfc3a396da28c7dd567c2e78d47f76
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-04-15 19:10:36 +0200

    78011 INVOICE : Generate VAT entry lines when recording payment on advance invoice

--- axelor-account/src/main/java/com/axelor/apps/account/module/AccountModule.java
+++ axelor-account/src/main/java/com/axelor/apps/account/module/AccountModule.java
@@ -189,6 +189,8 @@ import com.axelor.apps.account.service.invoice.InvoiceLineService;
 import com.axelor.apps.account.service.invoice.InvoiceLineServiceImpl;
 import com.axelor.apps.account.service.invoice.InvoiceLineTaxGroupService;
 import com.axelor.apps.account.service.invoice.InvoiceLineTaxGroupServiceImpl;
+import com.axelor.apps.account.service.invoice.InvoiceLineTaxToolService;
+import com.axelor.apps.account.service.invoice.InvoiceLineTaxToolServiceImpl;
 import com.axelor.apps.account.service.invoice.InvoiceMergingService;
 import com.axelor.apps.account.service.invoice.InvoiceMergingServiceImpl;
 import com.axelor.apps.account.service.invoice.InvoiceMergingViewService;
@@ -900,5 +902,7 @@ public class AccountModule extends AxelorModule {
     bind(InvoiceJournalService.class).to(InvoiceJournalServiceImpl.class);
 
     bind(FiscalPositionRepository.class).to(FiscalPositionManagementRepository.class);
+
+    bind(InvoiceLineTaxToolService.class).to(InvoiceLineTaxToolServiceImpl.class);
   }
 }
