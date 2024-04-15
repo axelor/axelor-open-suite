5b77dce54f2153a696d341f8c85f06919cf6891c
mah-axelor <162461637+mah-axelor@users.noreply.github.com>
2024-04-15 17:58:36 +0200

    move configs to HR module (#12379)
    * wip-77105: move configs from axelor-business-project module to axelor hr-module
    
    * wip-77105: move icalendarEvent from business project module to HR module
    
    * wip-77105: sync between ICalendarEvent and ProjectPlanningTime in HR module && compute event subject with groovy formula
    
    * wip-77105:add init data for event subject groovy formula in HR module
    
    * wip-77105 HR module: link to planningTime in IcalendarView
    
    * wip-77105 HR module: fix observer and prevent npe on user
    
    * wip-77105: remove ProjectPlanningTime view from Business project module
    
    * wip-77105: new config file and few corrections
    
    * RM-77105: hide event on planningTime form
    
    ---------
    
    Co-authored-by: cce-axelor <c.chea@axelor.com>

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/module/HumanResourceModule.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/module/HumanResourceModule.java
@@ -54,6 +54,7 @@ import com.axelor.apps.hr.db.repo.TimesheetLineRepository;
 import com.axelor.apps.hr.db.repo.TimesheetRepository;
 import com.axelor.apps.hr.db.repo.TimesheetTimerHRRepository;
 import com.axelor.apps.hr.db.repo.UserHRRepository;
+import com.axelor.apps.hr.event.ICalendarEventObserver;
 import com.axelor.apps.hr.service.BankCardService;
 import com.axelor.apps.hr.service.BankCardServiceImpl;
 import com.axelor.apps.hr.service.EmployeeComputeStatusService;
@@ -342,6 +343,7 @@ public class HumanResourceModule extends AxelorModule {
     bind(ExpenseLineToolService.class).to(ExpenseLineToolServiceImpl.class);
     bind(ExpenseLineUpdateService.class).to(ExpenseLineUpdateServiceImpl.class);
     bind(BankOrderValidationServiceImpl.class).to(BankOrderValidationServiceHRImpl.class);
+    bind(ICalendarEventObserver.class);
     bind(BankCardService.class).to(BankCardServiceImpl.class);
   }
 }
