6d15202dc98b7cbd2c938cb2c4e06f040e6d2a42
aol-axelor <61697047+aol-axelor@users.noreply.github.com>
2024-04-16 13:43:36 +0200

    RM#71777 : Add non deductible taxes (#12184)

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoice/generator/tax/TaxInvoiceLine.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoice/generator/tax/TaxInvoiceLine.java
@@ -172,6 +172,7 @@ public class TaxInvoiceLine extends TaxGenerator {
         taxLine.getTax(),
         invoice.getCompany(),
         invoiceJournalService.getJournal(invoice),
+        invoiceLine.getAccount(),
         vatSystem,
         invoiceLine.getFixedAssets(),
         InvoiceToolService.getFunctionalOrigin(invoice));
