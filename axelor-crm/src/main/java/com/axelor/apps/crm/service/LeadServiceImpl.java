a2895409f98da2f5563b58e91399e5f2ef89bbe1
Dwaamien <damien.farault@gmail.com>
2024-04-23 15:40:20 +0200

    correcting the isThereDuplicateLead request

--- axelor-crm/src/main/java/com/axelor/apps/crm/service/LeadServiceImpl.java
+++ axelor-crm/src/main/java/com/axelor/apps/crm/service/LeadServiceImpl.java
@@ -212,35 +212,26 @@ public class LeadServiceImpl implements LeadService {
     Long leadId = lead.getId();
     String email = lead.getEmailAddress().getAddress();
     if (leadId == null) {
-      Lead existingLeadWithName =
+      Lead existingLead =
           leadRepo
               .all()
-              .filter("lower(self.fullName) = lower(:newName) ")
+              .filter(
+                  "(lower(self.fullName) = lower(:newName) OR self.emailAddress.address = :email) ")
               .bind("newName", newName)
-              .fetchOne();
-      Lead existingLeadWithMail =
-          leadRepo
-              .all()
-              .filter("self.emailAddress.address = :email ")
               .bind("email", email)
               .fetchOne();
-      return existingLeadWithMail != null || existingLeadWithName != null;
+      return existingLead != null;
     } else {
-      Lead existingLeadWithName =
+      Lead existingLead =
           leadRepo
               .all()
-              .filter("lower(self.fullName) = lower(:newName) " + "and self.id != :leadId ")
+              .filter(
+                  "(lower(self.fullName) = lower(:newName) OR self.emailAddress.address = :email) AND self.id != :leadId ")
               .bind("newName", newName)
               .bind("leadId", leadId)
-              .fetchOne();
-      Lead existingLeadWithMail =
-          leadRepo
-              .all()
-              .filter("self.emailAddress.address = :email " + "and self.id != :leadId ")
               .bind("email", email)
-              .bind("leadId", leadId)
               .fetchOne();
-      return existingLeadWithMail != null || existingLeadWithName != null;
+      return existingLead != null;
     }
   }
 
