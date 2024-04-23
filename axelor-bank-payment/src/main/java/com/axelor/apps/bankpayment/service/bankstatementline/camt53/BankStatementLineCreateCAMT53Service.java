b3d1972f0936555daf1731b5c08fa39e521e0be6
tyu-axelor <148809551+tyu-axelor@users.noreply.github.com>
2024-04-23 15:58:29 -0400

    revise service methods according to feedbacks

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankstatementline/camt53/BankStatementLineCreateCAMT53Service.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankstatementline/camt53/BankStatementLineCreateCAMT53Service.java
@@ -22,14 +22,20 @@ import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.BankTransac
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.CashAccount20;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.CashBalance3;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.CreditDebitCode;
+import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.CreditorReferenceInformation2;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.DateAndDateTimeChoice;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.DateTimePeriodDetails;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.Document;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.EntryDetails1;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.EntryTransaction2;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.GenericAccountIdentification1;
+import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.Party6Choice;
+import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.PartyIdentification32;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.ProprietaryBankTransactionCodeStructure1;
+import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.RemittanceInformation5;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.ReportEntry2;
+import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.StructuredRemittanceInformation7;
+import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.TransactionParty2;
 import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.TransactionReferences2;
 import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.BankDetails;
@@ -153,7 +159,8 @@ public class BankStatementLineCreateCAMT53Service extends BankStatementLineCreat
       List<CashBalance3> balList = stmt.getBal();
       if (balList != null && !balList.isEmpty()) {
         for (CashBalance3 balanceEntry : balList) {
-          sequence = createBalanceLine(bankDetails, balanceEntry, sequence);
+          sequence =
+              createBalanceLine(bankDetails, balanceEntry, sequence, BALANCE_TYPE_INITIAL_BALANCE);
           if (sequence % 10 == 0) {
             JPA.clear();
             findBankStatement();
@@ -165,6 +172,17 @@ public class BankStatementLineCreateCAMT53Service extends BankStatementLineCreat
       if (ntryList != null && !ntryList.isEmpty()) {
         for (ReportEntry2 ntry : ntryList) {
           sequence = createEntryLine(bankDetails, ntry, sequence);
+          if (sequence % 10 == 0) {
+            JPA.clear();
+            findBankStatement();
+          }
+        }
+      }
+
+      if (balList != null && !balList.isEmpty()) {
+        for (CashBalance3 balanceEntry : balList) {
+          sequence =
+              createBalanceLine(bankDetails, balanceEntry, sequence, BALANCE_TYPE_FINAL_BALANCE);
           if (sequence % 10 == 0) {
             JPA.clear();
             findBankStatement();
@@ -216,6 +234,8 @@ public class BankStatementLineCreateCAMT53Service extends BankStatementLineCreat
     if (valDate != null) {
       valueDate = LocalDate.of(valDate.getYear(), valDate.getMonth(), valDate.getDay());
     }
+
+    String description = addNtryInfoIntoDescription(ntry);
 
     String currencyCode =
         Optional.of(ntry)
@@ -303,7 +323,7 @@ public class BankStatementLineCreateCAMT53Service extends BankStatementLineCreat
             debit,
             credit,
             currency,
-            null,
+            description,
             operationDate,
             valueDate,
             operationInterbankCodeLine,
@@ -316,9 +336,17 @@ public class BankStatementLineCreateCAMT53Service extends BankStatementLineCreat
     return ++sequence;
   }
 
+  /*
+  TODO: In an xml file, there may be multiple balanceLines (balList.size >2), and multiple entryLines.
+        For example, we have 2 init balance lines and 2 final balance lines.
+        Then we need to find a way to assign sequence number correctly to all balanceLines and entryLines.
+   */
   @Transactional
   protected int createBalanceLine(
-      BankDetails bankDetails, CashBalance3 balanceEntry, int sequence) {
+      BankDetails bankDetails,
+      CashBalance3 balanceEntry,
+      int sequence,
+      String balanceTypeRequired) {
     int lineTypeSelect = 0;
     String balanceType =
         Optional.of(balanceEntry)
@@ -327,6 +355,9 @@ public class BankStatementLineCreateCAMT53Service extends BankStatementLineCreat
             .map(BalanceType5Choice::getCd)
             .map(BalanceType12Code::value)
             .orElse(null);
+    if (!balanceTypeRequired.equals(balanceType)) {
+      return sequence;
+    }
     if (BALANCE_TYPE_INITIAL_BALANCE.equals(balanceType)) {
       // Initial balance
       lineTypeSelect = 1;
@@ -488,5 +519,228 @@ public class BankStatementLineCreateCAMT53Service extends BankStatementLineCreat
 
   protected BankDetails findBankDetailsByIban(String ibanOrOthers) {
     return bankDetailsRepository.all().filter("self.iban = ?1", ibanOrOthers).fetchOne();
+  }
+
+  protected String addNtryInfoIntoDescription(ReportEntry2 ntry) {
+    // <Ntry> -> <NtryDtls> -> <TxDtls>
+    EntryTransaction2 txDtl =
+        Optional.of(ntry)
+            .map(ReportEntry2::getNtryDtls)
+            .flatMap(
+                ntryDtls ->
+                    ntryDtls.stream()
+                        .findFirst()) // Convert to Stream and get first element if present
+            .map(EntryDetails1::getTxDtls)
+            .flatMap(txDtls -> txDtls.stream().findFirst())
+            .orElse(null);
+
+    StringBuilder descriptionSb = new StringBuilder();
+    // <Ntry> -> <BkTxCd> -> <Prtry> -> <Issr>
+    String issr =
+        Optional.of(ntry)
+            .map(ReportEntry2::getBkTxCd)
+            .map(BankTransactionCodeStructure4::getPrtry)
+            .map(ProprietaryBankTransactionCodeStructure1::getIssr)
+            .orElse(null);
+    if (issr != null && !issr.isEmpty()) {
+      descriptionSb.append("Ntry.BkTxCd.Prtry.Issr=");
+      descriptionSb.append(issr);
+      descriptionSb.append(";");
+    }
+    if (txDtl == null) {
+      if (descriptionSb.length() >= 1 && descriptionSb.charAt(descriptionSb.length() - 1) == ';') {
+        descriptionSb.deleteCharAt(descriptionSb.length() - 1);
+      }
+      return descriptionSb.toString();
+    } else {
+      // <TxDtls> -> <Refs> -> <ChqNb>
+      String cheqNb =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRefs)
+              .map(TransactionReferences2::getChqNb)
+              .orElse(null);
+      if (cheqNb != null && !cheqNb.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.Refs.ChqNb=");
+        descriptionSb.append(cheqNb);
+        descriptionSb.append(";");
+      }
+
+      // <TxDtls> -> <Refs> -> <InstrId>
+      String instrId =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRefs)
+              .map(TransactionReferences2::getInstrId)
+              .orElse(null);
+      if (instrId != null && !instrId.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.Refs.InstrId=");
+        descriptionSb.append(instrId);
+        descriptionSb.append(";");
+      }
+
+      // <TxDtls> -> <Refs> -> <MndtId>
+      String mndtId =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRefs)
+              .map(TransactionReferences2::getMndtId)
+              .orElse(null);
+      if (mndtId != null && !mndtId.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.Refs.MndtId=");
+        descriptionSb.append(mndtId);
+        descriptionSb.append(";");
+      }
+
+      // <TxDtls> -> <RltdPties> -> <Cdtr> -> <Nm>
+      String cdtrNm =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRltdPties)
+              .map(TransactionParty2::getCdtr)
+              .map(PartyIdentification32::getNm)
+              .orElse(null);
+      if (cdtrNm != null && !cdtrNm.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.RltdPties.Cdtr.Nm=");
+        descriptionSb.append(cdtrNm);
+        descriptionSb.append(";");
+      }
+
+      /*
+      TODO: To be discussed:
+            The "cdtrId" is still an object, but it doesn't have a toString method.
+            It has the following sub-structure:
+              <Id> ---
+                  <OrgId>
+                      ...
+                  <PrvtId>
+                      ...
+            Check Payments_Maintenance_2009.pdf page 1008.
+       */
+      // <TxDtls> -> <RltdPties> -> <Cdtr> -> <Id>
+      String cdtrIdString = null;
+      Party6Choice cdtrId =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRltdPties)
+              .map(TransactionParty2::getCdtr)
+              .map(PartyIdentification32::getId)
+              .orElse(null);
+      if (cdtrId != null) {
+        cdtrIdString = cdtrId.toString();
+      }
+      if (cdtrIdString != null && !cdtrIdString.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.RltdPties.Cdtr.Id=");
+        descriptionSb.append(cdtrIdString);
+        descriptionSb.append(";");
+      }
+
+      // <TxDtls> -> <RltdPties> -> <UltmtCdtr> -> <Nm>
+      String ultmtCdtrNm =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRltdPties)
+              .map(TransactionParty2::getUltmtCdtr)
+              .map(PartyIdentification32::getNm)
+              .orElse(null);
+      if (ultmtCdtrNm != null && !ultmtCdtrNm.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.RltdPties.UltmtCdtr.Nm=");
+        descriptionSb.append(ultmtCdtrNm);
+        descriptionSb.append(";");
+      }
+
+      /*
+      TODO: It has the same issue as the "cdtrId".
+       */
+      // <TxDtls> -> <RltdPties> -> <UltmtCdtr> -> <Id>
+      Party6Choice ultmtCdtrId =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRltdPties)
+              .map(TransactionParty2::getUltmtCdtr)
+              .map(PartyIdentification32::getId)
+              .orElse(null);
+      String ultmtCdtrIdString = null;
+      if (ultmtCdtrId != null) {
+        ultmtCdtrIdString = ultmtCdtrId.toString();
+      }
+      if (ultmtCdtrIdString != null && !ultmtCdtrIdString.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.RltdPties.UltmtCdtr.Id=");
+        descriptionSb.append(ultmtCdtrIdString);
+        descriptionSb.append(";");
+      }
+
+      // <TxDtls> -> <RltdPties> -> <Dbtr> -> <Nm>
+      String dbtrNm =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRltdPties)
+              .map(TransactionParty2::getDbtr)
+              .map(PartyIdentification32::getNm)
+              .orElse(null);
+      if (dbtrNm != null && !dbtrNm.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.RltdPties.Dbtr.Nm=");
+        descriptionSb.append(dbtrNm);
+        descriptionSb.append(";");
+      }
+
+      /*
+      TODO: It has the same issue as the "cdtrId".
+       */
+      // <TxDtls> -> <RltdPties> -> <Dbtr> -> <Id>
+      Party6Choice dbtrId =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRltdPties)
+              .map(TransactionParty2::getDbtr)
+              .map(PartyIdentification32::getId)
+              .orElse(null);
+      String dbtrIdString = null;
+      if (dbtrId != null) {
+        dbtrIdString = dbtrId.toString();
+      }
+      if (dbtrIdString != null && !dbtrIdString.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.RltdPties.Dbtr.Id=");
+        descriptionSb.append(dbtrId);
+        descriptionSb.append(";");
+      }
+
+      // <TxDtls> -> <RmtInf> -> <Ustrd>
+      List<String> unstructuredLines =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRmtInf)
+              .map(RemittanceInformation5::getUstrd)
+              .orElse(null);
+      if (unstructuredLines != null && !unstructuredLines.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.RmtInf.Ustrd=");
+        for (String unstructuredLine : unstructuredLines) {
+          descriptionSb.append(unstructuredLine);
+          descriptionSb.append(",");
+        }
+        if (descriptionSb.length() >= 1
+            && descriptionSb.charAt(descriptionSb.length() - 1) == ',') {
+          descriptionSb.deleteCharAt(descriptionSb.length() - 1);
+        }
+        descriptionSb.append(";");
+      }
+
+      // <TxDtls> -> <Rmtlnf> -> <Strd> -> <CdtrReflnf> -><Ref>
+      String strdCdtrRefInfref =
+          Optional.of(txDtl)
+              .map(EntryTransaction2::getRmtInf)
+              .map(RemittanceInformation5::getStrd)
+              .flatMap(strds -> strds.stream().findFirst())
+              .map(StructuredRemittanceInformation7::getCdtrRefInf)
+              .map(CreditorReferenceInformation2::getRef)
+              .orElse(null);
+      if (strdCdtrRefInfref != null && !strdCdtrRefInfref.isEmpty()) {
+        descriptionSb.append("Ntry.TxDtls.RmtInf.Strd.CdtrRefInf.Ref=");
+        descriptionSb.append(strdCdtrRefInfref);
+        descriptionSb.append(";");
+      }
+
+      // <TxDtls> -> <AddtlTxInf>
+      String addtlTxInf = Optional.of(txDtl).map(EntryTransaction2::getAddtlTxInf).orElse(null);
+      if (addtlTxInf != null && !addtlTxInf.isEmpty()) {
+        descriptionSb.append("RmtInf.AddtlTxInf=");
+        descriptionSb.append(addtlTxInf);
+      }
+    }
+
+    if (descriptionSb.length() >= 1 && descriptionSb.charAt(descriptionSb.length() - 1) == ';') {
+      descriptionSb.deleteCharAt(descriptionSb.length() - 1);
+    }
+    return descriptionSb.toString();
   }
 }
