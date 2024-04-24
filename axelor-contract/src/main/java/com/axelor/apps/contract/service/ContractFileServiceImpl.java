01ee89a47570eff13e3dbf518a948a0ab6ac93ad
Axelor <162461637+mah-axelor@users.noreply.github.com>
2024-04-24 11:06:26 +0200

    wip-78148 File: create FileType selection and replace EmployeeFile and ContractFile with File

--- axelor-contract/src/main/java/com/axelor/apps/contract/service/ContractFileServiceImpl.java
+++ axelor-contract/src/main/java/com/axelor/apps/contract/service/ContractFileServiceImpl.java
@@ -1,9 +1,9 @@
 package com.axelor.apps.contract.service;
 
+import com.axelor.apps.base.db.File;
+import com.axelor.apps.base.db.repo.FileRepository;
 import com.axelor.apps.base.service.DMSService;
 import com.axelor.apps.base.service.app.AppBaseService;
-import com.axelor.apps.contract.db.ContractFile;
-import com.axelor.apps.contract.db.repo.ContractFileRepository;
 import com.axelor.dms.db.DMSFile;
 import com.axelor.dms.db.repo.DMSFileRepository;
 import com.axelor.meta.MetaFiles;
@@ -13,7 +13,7 @@ import com.google.inject.persist.Transactional;
 import java.util.List;
 
 public class ContractFileServiceImpl implements ContractFileService {
-  protected ContractFileRepository contractFileRepository;
+  protected FileRepository contractFileRepository;
   protected DMSFileRepository dmsFileRepository;
   protected DMSService dmsService;
   protected MetaFiles metaFiles;
@@ -21,7 +21,7 @@ public class ContractFileServiceImpl implements ContractFileService {
 
   @Inject
   public ContractFileServiceImpl(
-      ContractFileRepository contractFileRepository,
+      FileRepository contractFileRepository,
       DMSFileRepository dmsFileRepository,
       DMSService dmsService,
       MetaFiles metaFiles,
@@ -35,14 +35,14 @@ public class ContractFileServiceImpl implements ContractFileService {
 
   @Override
   @Transactional
-  public void setDMSFile(ContractFile contractFile) {
+  public void setDMSFile(File contractFile) {
     MetaFile metaFile = contractFile.getMetaFile();
     dmsService.setDmsFile(metaFile, contractFile);
     contractFileRepository.save(contractFile);
   }
 
   @Override
-  public String getInlineUrl(ContractFile contractFile) {
+  public String getInlineUrl(File contractFile) {
     MetaFile metaFile = contractFile.getMetaFile();
     DMSFile dmsFile = contractFile.getDmsFile();
     if (metaFile == null || dmsFile == null || !"application/pdf".equals(metaFile.getFileType())) {
@@ -55,7 +55,7 @@ public class ContractFileServiceImpl implements ContractFileService {
   @Transactional(rollbackOn = {Exception.class})
   public void remove(List<Integer> contractFileIds) {
     for (Integer id : contractFileIds) {
-      ContractFile contractFile = contractFileRepository.find(Long.parseLong(String.valueOf(id)));
+      File contractFile = contractFileRepository.find(Long.parseLong(String.valueOf(id)));
       contractFileRepository.remove(contractFile);
     }
   }
--- axelor-human-resource/src/main/java/com/axelor/apps/hr/web/EmployeeFileController.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/web/EmployeeFileController.java
@@ -18,8 +18,8 @@
  */
 package com.axelor.apps.hr.web;
 
-import com.axelor.apps.hr.db.EmployeeFile;
-import com.axelor.apps.hr.db.repo.EmployeeFileRepository;
+import com.axelor.apps.base.db.File;
+import com.axelor.apps.base.db.repo.FileRepository;
 import com.axelor.apps.hr.service.EmployeeFileDMSService;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
@@ -27,14 +27,14 @@ import com.axelor.rpc.ActionResponse;
 
 public class EmployeeFileController {
   public void setDMSFile(ActionRequest request, ActionResponse response) {
-    EmployeeFile employeeFile = request.getContext().asType(EmployeeFile.class);
-    employeeFile = Beans.get(EmployeeFileRepository.class).find(employeeFile.getId());
+    File employeeFile = request.getContext().asType(File.class);
+    employeeFile = Beans.get(FileRepository.class).find(employeeFile.getId());
     Beans.get(EmployeeFileDMSService.class).setDMSFile(employeeFile);
     response.setReload(true);
   }
 
   public void setInlineUrl(ActionRequest request, ActionResponse response) {
-    EmployeeFile employeeFile = request.getContext().asType(EmployeeFile.class);
+    File employeeFile = request.getContext().asType(File.class);
     response.setValue(
         "$inlineUrl", Beans.get(EmployeeFileDMSService.class).getInlineUrl(employeeFile));
   }
