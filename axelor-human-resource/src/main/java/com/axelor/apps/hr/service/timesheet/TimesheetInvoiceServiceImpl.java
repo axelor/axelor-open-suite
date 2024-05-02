efd51e22c41ba21901c9d19a14f15d13788abe5c
dar-axelor <132374601+dar-axelor@users.noreply.github.com>
2024-05-02 17:17:48 +0200

    RM#78365 INVOICINGPROJECT Manage unit conversion for project

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/timesheet/TimesheetInvoiceServiceImpl.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/timesheet/TimesheetInvoiceServiceImpl.java
@@ -37,9 +37,10 @@ import com.axelor.apps.base.service.app.AppBaseService;
 import com.axelor.apps.hr.db.Employee;
 import com.axelor.apps.hr.db.TimesheetLine;
 import com.axelor.apps.hr.exception.HumanResourceExceptionMessage;
+import com.axelor.apps.hr.service.UnitConversionForProjectService;
 import com.axelor.apps.hr.service.app.AppHumanResourceService;
+import com.axelor.apps.project.db.Project;
 import com.axelor.i18n.I18n;
-import com.axelor.inject.Beans;
 import com.axelor.studio.db.repo.AppBaseRepository;
 import com.google.inject.Inject;
 import java.math.BigDecimal;
@@ -57,17 +58,23 @@ public class TimesheetInvoiceServiceImpl implements TimesheetInvoiceService {
   protected PartnerPriceListService partnerPriceListService;
   protected ProductCompanyService productCompanyService;
   protected PriceListService priceListService;
+  protected UnitConversionService unitConversionService;
+  protected UnitConversionForProjectService unitConversionForProjectService;
 
   @Inject
   public TimesheetInvoiceServiceImpl(
       AppHumanResourceService appHumanResourceService,
       PartnerPriceListService partnerPriceListService,
       ProductCompanyService productCompanyService,
-      PriceListService priceListService) {
+      PriceListService priceListService,
+      UnitConversionService unitConversionService,
+      UnitConversionForProjectService unitConversionForProjectService) {
     this.appHumanResourceService = appHumanResourceService;
     this.partnerPriceListService = partnerPriceListService;
     this.productCompanyService = productCompanyService;
     this.priceListService = priceListService;
+    this.unitConversionService = unitConversionService;
+    this.unitConversionForProjectService = unitConversionForProjectService;
   }
 
   @Override
@@ -139,6 +146,7 @@ public class TimesheetInvoiceServiceImpl implements TimesheetInvoiceService {
           this.createInvoiceLine(
               invoice,
               product,
+              null,
               employee,
               strDate,
               hoursDuration,
@@ -154,6 +162,7 @@ public class TimesheetInvoiceServiceImpl implements TimesheetInvoiceService {
   public List<InvoiceLine> createInvoiceLine(
       Invoice invoice,
       Product product,
+      Project project,
       Employee employee,
       String date,
       BigDecimal hoursDuration,
@@ -173,14 +182,24 @@ public class TimesheetInvoiceServiceImpl implements TimesheetInvoiceService {
     BigDecimal discountAmount = BigDecimal.ZERO;
     BigDecimal priceDiscounted = price;
 
-    BigDecimal qtyConverted =
-        Beans.get(UnitConversionService.class)
-            .convert(
-                appHumanResourceService.getAppBase().getUnitHours(),
-                (Unit) productCompanyService.get(product, "unit", invoice.getCompany()),
-                hoursDuration,
-                AppBaseService.DEFAULT_NB_DECIMAL_DIGITS,
-                product);
+    BigDecimal qtyConverted;
+    if (project != null) {
+      qtyConverted =
+          unitConversionForProjectService.convert(
+              appHumanResourceService.getAppBase().getUnitHours(),
+              (Unit) productCompanyService.get(product, "unit", invoice.getCompany()),
+              hoursDuration,
+              AppBaseService.DEFAULT_NB_DECIMAL_DIGITS,
+              project);
+    } else {
+      qtyConverted =
+          unitConversionService.convert(
+              appHumanResourceService.getAppBase().getUnitHours(),
+              (Unit) productCompanyService.get(product, "unit", invoice.getCompany()),
+              hoursDuration,
+              AppBaseService.DEFAULT_NB_DECIMAL_DIGITS,
+              product);
+    }
 
     if (priceList != null) {
       PriceListLine priceListLine =
