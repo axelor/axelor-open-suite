3be0a334e9c34e85d69191624917338fbe538e60
mah-axelor <162461637+mah-axelor@users.noreply.github.com>
2024-05-22 11:42:31 +0200

    wip-78346 Contract: copy configuration and contractLineList from cont… (#12482)
    * wip-78346 Contract: copy configuration and contractLineList from contractTemplate to generated Contract

--- axelor-contract/src/main/java/com/axelor/apps/contract/service/ContractServiceImpl.java
+++ axelor-contract/src/main/java/com/axelor/apps/contract/service/ContractServiceImpl.java
@@ -1012,7 +1012,7 @@ public class ContractServiceImpl extends ContractRepository implements ContractS
   @Override
   @Transactional(rollbackOn = {Exception.class})
   public Contract generateContractFromOpportunity(
-      Opportunity opportunity, ContractTemplate contractTemplate) {
+      Opportunity opportunity, ContractTemplate contractTemplate) throws AxelorException {
     Contract contract = new Contract();
     Currency currency = opportunity.getCurrency();
     Company company = opportunity.getCompany();
@@ -1033,9 +1033,7 @@ public class ContractServiceImpl extends ContractRepository implements ContractS
 
     ContractTemplate contractTemplate1 = JPA.copy(contractTemplate, true);
     if (contractTemplate != null) {
-      contract.setAdditionalBenefitContractLineList(
-          contractTemplate1.getAdditionalBenefitContractLineList());
-      contract.setContractTypeSelect(contractTemplate1.getContractTypeSelect());
+      copyFromTemplate(contract, contractTemplate1);
     }
     contract.setOpportunity(opportunity);
     contractRepository.save(contract);
