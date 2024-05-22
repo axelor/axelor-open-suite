00e3bfb130918b0ee1382008935fc67b53a8240c
mah-axelor <162461637+mah-axelor@users.noreply.github.com>
2024-05-22 15:19:22 +0200

    dev-79238 InvoiceLine: set unit to product saleUnit for customer invoices (#12607)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/InvoiceLineSupplychainService.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/InvoiceLineSupplychainService.java
@@ -100,7 +100,7 @@ public class InvoiceLineSupplychainService extends InvoiceLineServiceImpl {
       }
     } else {
       if (product.getSalesUnit() != null) {
-        return product.getPurchasesUnit();
+        return product.getSalesUnit();
       } else {
         return product.getUnit();
       }
