b498b657c81b6f3ef937ebaf46cc4cb31d65e573
dar-axelor <132374601+dar-axelor@users.noreply.github.com>
2024-05-28 18:17:18 +0200

    RM#79304 Value by default on project planning time (#12617)

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/project/ProjectPlanningTimeServiceImpl.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/project/ProjectPlanningTimeServiceImpl.java
@@ -35,6 +35,7 @@ import com.axelor.apps.hr.db.repo.TimesheetLineRepository;
 import com.axelor.apps.hr.db.repo.TimesheetRepository;
 import com.axelor.apps.hr.service.UnitConversionForProjectService;
 import com.axelor.apps.hr.service.publicHoliday.PublicHolidayHrService;
+import com.axelor.apps.project.db.PlannedTimeValue;
 import com.axelor.apps.project.db.Project;
 import com.axelor.apps.project.db.ProjectPlanningTime;
 import com.axelor.apps.project.db.ProjectTask;
@@ -80,6 +81,7 @@ public class ProjectPlanningTimeServiceImpl implements ProjectPlanningTimeServic
 
   protected AppProjectService appProjectService;
   protected ProjectConfigService projectConfigService;
+  protected PlannedTimeValueService plannedTimeValueService;
   protected ICalendarService iCalendarService;
   protected ICalendarEventRepository iCalendarEventRepository;
 
@@ -95,6 +97,7 @@ public class ProjectPlanningTimeServiceImpl implements ProjectPlanningTimeServic
       TimesheetLineRepository timesheetLineRepository,
       AppProjectService appProjectService,
       ProjectConfigService projectConfigService,
+      PlannedTimeValueService plannedTimeValueService,
       ICalendarService iCalendarService,
       ICalendarEventRepository iCalendarEventRepository,
       UnitConversionForProjectService unitConversionForProjectService,
@@ -110,6 +113,7 @@ public class ProjectPlanningTimeServiceImpl implements ProjectPlanningTimeServic
     this.timesheetLineRepository = timesheetLineRepository;
     this.appProjectService = appProjectService;
     this.projectConfigService = projectConfigService;
+    this.plannedTimeValueService = plannedTimeValueService;
     this.iCalendarService = iCalendarService;
     this.iCalendarEventRepository = iCalendarEventRepository;
     this.unitConversionForProjectService = unitConversionForProjectService;
@@ -501,5 +505,20 @@ public class ProjectPlanningTimeServiceImpl implements ProjectPlanningTimeServic
                 .getProjectConfig(projectPlanningTime.getProject().getCompany())
                 .getPlannedTimeValueList())
         + ")";
+  }
+
+  public BigDecimal getDefaultPlanningTime(ProjectPlanningTime projectPlanningTime)
+      throws AxelorException {
+    return projectConfigService
+        .getProjectConfig(projectPlanningTime.getProject().getCompany())
+        .getValueByDefaultOnDisplayPlannedTime();
+  }
+
+  public PlannedTimeValue getDefaultPlanningRestrictedTime(ProjectPlanningTime projectPlanningTime)
+      throws AxelorException {
+    return plannedTimeValueService.createPlannedTimeValue(
+        projectConfigService
+            .getProjectConfig(projectPlanningTime.getProject().getCompany())
+            .getValueByDefaultOnDisplayPlannedTime());
   }
 }
