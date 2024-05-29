7a001556956c07f39f653957ff1b69fc7bf7e224
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-05-29 12:14:03 +0200

    77854 MoveLine : Select default account from partner configuration only for first moveline of the move

--- axelor-account/src/main/java/com/axelor/apps/account/service/moveline/MoveLineDefaultServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/moveline/MoveLineDefaultServiceImpl.java
@@ -31,9 +31,11 @@ import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.Currency;
 import com.axelor.apps.base.db.repo.TraceBackRepository;
 import com.axelor.apps.base.service.config.CompanyConfigService;
+import com.axelor.common.ObjectUtils;
 import com.axelor.i18n.I18n;
 import com.google.inject.Inject;
 import java.math.BigDecimal;
+import java.util.Objects;
 import org.apache.commons.collections.CollectionUtils;
 
 public class MoveLineDefaultServiceImpl implements MoveLineDefaultService {
@@ -80,7 +82,10 @@ public class MoveLineDefaultServiceImpl implements MoveLineDefaultService {
     Account accountingAccount =
         moveLoadDefaultConfigService.getAccountingAccountFromAccountConfig(move);
 
-    if (accountingAccount != null) {
+    if (ObjectUtils.isEmpty(move.getMoveLineList())
+        || move.getMoveLineList().size() == 1
+            && Objects.equals(move.getMoveLineList().get(0), moveLine)
+            && accountingAccount != null) {
       moveLine.setAccount(accountingAccount);
 
       AnalyticDistributionTemplate analyticDistributionTemplate =
