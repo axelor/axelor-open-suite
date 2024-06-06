e821145be508f4ce39cbbe233a26527cabd95d3a
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-06-06 18:50:36 +0200

    80025 InvoicePayment: fix reconcile differences process for supplier … (#12746)
    * 80025 InvoicePayment: fix reconcile differences process for supplier invoices
    
    (cherry picked from commit 94c0bf2cfb890b2fa6fb63b88547600a13f380e8)

--- axelor-account/src/main/java/com/axelor/apps/account/service/ReconcileServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/ReconcileServiceImpl.java
@@ -1022,7 +1022,7 @@ public class ReconcileServiceImpl implements ReconcileService {
         && debitMoveLine.getAmountRemaining().compareTo(BigDecimal.ZERO) > 0) {
       balanceDebit(debitMoveLine);
     } else if (creditMoveLine != null
-        && creditMoveLine.getAmountRemaining().compareTo(BigDecimal.ZERO) > 0) {
+        && creditMoveLine.getAmountRemaining().compareTo(BigDecimal.ZERO) < 0) {
       balanceCredit(creditMoveLine);
     }
 
@@ -1064,7 +1064,7 @@ public class ReconcileServiceImpl implements ReconcileService {
           log.debug("Threshold respected");
 
           MoveLine debitAdjustmentMoveLine =
-              moveAdjustementService.createAdjustmentMove(creditMoveLine, creditAccount, false);
+              moveAdjustementService.createAdjustmentMove(creditMoveLine, creditAccount);
 
           // Création de la réconciliation
           Reconcile newReconcile =
@@ -1114,7 +1114,7 @@ public class ReconcileServiceImpl implements ReconcileService {
           log.debug("Threshold respected");
 
           MoveLine creditAdjustMoveLine =
-              moveAdjustementService.createAdjustmentMove(debitMoveLine, debitAccount, true);
+              moveAdjustementService.createAdjustmentMove(debitMoveLine, debitAccount);
 
           // Création de la réconciliation
           Reconcile newReconcile =
