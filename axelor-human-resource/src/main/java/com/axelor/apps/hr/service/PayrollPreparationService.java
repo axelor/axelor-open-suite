d2fddf53d82c2fcee367aee6d2a907fc30da0862
mah-axelor <162461637+mah-axelor@users.noreply.github.com>
2024-06-07 16:01:43 +0200

    dev-80003 HR: Update lists on PayrollPreparation when employee is cha… (#12732)
    * dev-80003 HR: Update lists on PayrollPreparation when employee is changed
    
    (cherry picked from commit 1489a6a82461842729b3a724b2fcd3d3f56aa5ee)

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/PayrollPreparationService.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/PayrollPreparationService.java
@@ -65,6 +65,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.apache.commons.collections.CollectionUtils;
 
 public class PayrollPreparationService {
 
@@ -208,6 +209,9 @@ public class PayrollPreparationService {
     LocalDate fromDate = payrollPreparation.getPeriod().getFromDate();
     LocalDate toDate = payrollPreparation.getPeriod().getToDate();
     BigDecimal extraHoursNumber = BigDecimal.ZERO;
+    if (!CollectionUtils.isEmpty(payrollPreparation.getExtraHoursLineList())) {
+      payrollPreparation.getExtraHoursLineList().clear();
+    }
     for (ExtraHoursLine extraHoursLine :
         Beans.get(ExtraHoursLineRepository.class)
             .all()
@@ -226,6 +230,9 @@ public class PayrollPreparationService {
 
   public BigDecimal computeExpenseAmount(PayrollPreparation payrollPreparation) {
     BigDecimal expenseAmount = BigDecimal.ZERO;
+    if (!CollectionUtils.isEmpty(payrollPreparation.getExpenseList())) {
+      payrollPreparation.getExpenseList().clear();
+    }
     List<Expense> expenseList =
         Beans.get(ExpenseRepository.class)
             .all()
@@ -251,6 +258,9 @@ public class PayrollPreparationService {
 
   public BigDecimal computeLunchVoucherNumber(PayrollPreparation payrollPreparation) {
     BigDecimal lunchVoucherNumber = BigDecimal.ZERO;
+    if (!CollectionUtils.isEmpty(payrollPreparation.getLunchVoucherMgtLineList())) {
+      payrollPreparation.getLunchVoucherMgtLineList().clear();
+    }
     List<LunchVoucherMgtLine> lunchVoucherList =
         Beans.get(LunchVoucherMgtLineRepository.class)
             .all()
@@ -272,6 +282,9 @@ public class PayrollPreparationService {
 
   public BigDecimal computeEmployeeBonusAmount(PayrollPreparation payrollPreparation) {
     BigDecimal employeeBonusAmount = BigDecimal.ZERO;
+    if (!CollectionUtils.isEmpty(payrollPreparation.getEmployeeBonusMgtLineList())) {
+      payrollPreparation.getEmployeeBonusMgtLineList().clear();
+    }
     List<EmployeeBonusMgtLine> employeeBonusList =
         Beans.get(EmployeeBonusMgtLineRepository.class)
             .all()
