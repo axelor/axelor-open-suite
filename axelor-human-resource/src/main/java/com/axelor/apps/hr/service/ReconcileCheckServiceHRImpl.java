9ad9c1f8f512dd7e5b85fe9c9a5f662f039a0e46
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2024-06-10 10:46:37 +0200

    80153: Fixed expense ventilation (#12756)

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/ReconcileCheckServiceHRImpl.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/ReconcileCheckServiceHRImpl.java
@@ -1,15 +1,11 @@
 package com.axelor.apps.hr.service;
 
-import com.axelor.apps.account.db.Move;
-import com.axelor.apps.account.exception.AccountExceptionMessage;
+import com.axelor.apps.account.db.MoveLine;
 import com.axelor.apps.account.service.invoice.InvoiceTermPfpService;
 import com.axelor.apps.account.service.invoice.InvoiceTermToolService;
 import com.axelor.apps.account.service.moveline.MoveLineToolService;
 import com.axelor.apps.account.service.reconcile.ReconcileCheckServiceImpl;
-import com.axelor.apps.base.AxelorException;
-import com.axelor.apps.base.db.repo.TraceBackRepository;
 import com.axelor.apps.base.service.CurrencyScaleService;
-import com.axelor.common.ObjectUtils;
 import com.google.inject.Inject;
 
 public class ReconcileCheckServiceHRImpl extends ReconcileCheckServiceImpl {
@@ -24,19 +20,7 @@ public class ReconcileCheckServiceHRImpl extends ReconcileCheckServiceImpl {
   }
 
   @Override
-  protected void taxLinePrecondition(Move move) throws AxelorException {
-    // Checking also if move expense is null
-    if (move.getMoveLineList().stream()
-        .anyMatch(
-            it ->
-                (move.getExpense() == null && ObjectUtils.isEmpty(it.getTaxLineSet()))
-                    && ObjectUtils.isEmpty(it.getTaxLineSet())
-                    && moveLineToolService.isMoveLineTaxAccount(it)
-                    && it.getAccount().getIsTaxAuthorizedOnMoveLine())) {
-      throw new AxelorException(
-          TraceBackRepository.CATEGORY_MISSING_FIELD,
-          AccountExceptionMessage.RECONCILE_MISSING_TAX,
-          move.getReference());
-    }
+  protected boolean isMissingTax(MoveLine it) {
+    return it.getMove().getExpense() == null && super.isMissingTax(it);
   }
 }
