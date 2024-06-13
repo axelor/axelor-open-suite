62e4a6524f3e2fab58e3efc96e9d76d364744c28
dar-axelor <132374601+dar-axelor@users.noreply.github.com>
2024-06-13 14:22:00 +0200

    RM#79552 [Customer Park] - Indicator select on equipment is not set when contract is updated rebase after contract refacto merge

--- axelor-intervention/src/main/java/com/axelor/apps/intervention/service/ContractInterventionServiceImpl.java
+++ axelor-intervention/src/main/java/com/axelor/apps/intervention/service/ContractInterventionServiceImpl.java
@@ -1,28 +1,19 @@
 package com.axelor.apps.intervention.service;
 
 import com.axelor.apps.account.db.Invoice;
-import com.axelor.apps.account.db.repo.InvoiceRepository;
-import com.axelor.apps.account.service.invoice.InvoiceService;
 import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.service.DurationService;
-import com.axelor.apps.base.service.ProductCompanyService;
 import com.axelor.apps.base.service.administration.SequenceService;
 import com.axelor.apps.base.service.app.AppBaseService;
-import com.axelor.apps.base.service.tax.FiscalPositionService;
-import com.axelor.apps.base.service.tax.TaxService;
 import com.axelor.apps.contract.db.Contract;
 import com.axelor.apps.contract.db.repo.ContractLineRepository;
 import com.axelor.apps.contract.db.repo.ContractRepository;
 import com.axelor.apps.contract.db.repo.ContractVersionRepository;
-import com.axelor.apps.contract.service.AccountManagementContractService;
 import com.axelor.apps.contract.service.ContractLineService;
 import com.axelor.apps.contract.service.ContractServiceImpl;
 import com.axelor.apps.contract.service.ContractVersionService;
-import com.axelor.apps.contract.service.ContractYearEndBonusService;
-import com.axelor.apps.crm.db.repo.OpportunityRepository;
 import com.axelor.apps.intervention.db.Equipment;
 import com.axelor.apps.intervention.repo.EquipmentRepository;
-import com.axelor.apps.supplychain.service.AnalyticLineModelService;
 import com.axelor.apps.supplychain.service.PartnerLinkSupplychainService;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
@@ -44,15 +35,6 @@ public class ContractInterventionServiceImpl extends ContractServiceImpl {
       DurationService durationService,
       ContractLineRepository contractLineRepo,
       ContractRepository contractRepository,
-      TaxService taxService,
-      InvoiceRepository invoiceRepository,
-      InvoiceService invoiceService,
-      AnalyticLineModelService analyticLineModelService,
-      ContractYearEndBonusService contractYearEndBonusService,
-      OpportunityRepository opportunityRepository,
-      ProductCompanyService productCompanyService,
-      AccountManagementContractService accountManagementContractService,
-      FiscalPositionService fiscalPositionService,
       PartnerLinkSupplychainService partnerLinkSupplychainService,
       EquipmentRepository equipmentRepository) {
     super(
@@ -65,15 +47,6 @@ public class ContractInterventionServiceImpl extends ContractServiceImpl {
         durationService,
         contractLineRepo,
         contractRepository,
-        taxService,
-        invoiceRepository,
-        invoiceService,
-        analyticLineModelService,
-        contractYearEndBonusService,
-        opportunityRepository,
-        productCompanyService,
-        accountManagementContractService,
-        fiscalPositionService,
         partnerLinkSupplychainService);
     this.equipmentRepository = equipmentRepository;
   }
