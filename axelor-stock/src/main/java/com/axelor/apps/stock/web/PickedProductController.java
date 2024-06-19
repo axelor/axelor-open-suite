b114e58bf158014805a6b6d7d51a79566b285d9d
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2024-06-19 16:30:09 +0200

    RM #80074: Stored product domains and view

--- axelor-stock/src/main/java/com/axelor/apps/stock/web/PickedProductController.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/web/PickedProductController.java
@@ -6,6 +6,7 @@ import com.axelor.apps.stock.db.PickedProduct;
 import com.axelor.apps.stock.db.repo.PickedProductRepository;
 import com.axelor.apps.stock.service.massstockmove.MassStockMovableProductService;
 import com.axelor.apps.stock.service.massstockmove.PickedProductAttrsService;
+import com.axelor.apps.stock.service.massstockmove.MassStockMovableProductAttrsService;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
@@ -14,15 +15,14 @@ import java.util.Optional;
 public class PickedProductController {
 
   public void setFromStockLocationDomain(ActionRequest request, ActionResponse response) {
-    var pickedProduct = request.getContext().asType(PickedProduct.class);
     var massStockMove = request.getContext().getParent().asType(MassStockMove.class);
 
     if (massStockMove != null) {
       response.setAttr(
           "fromStockLocation",
           "domain",
-          Beans.get(PickedProductAttrsService.class)
-              .getStockLocationDomain(pickedProduct, massStockMove));
+          Beans.get(MassStockMovableProductAttrsService.class)
+              .getStockLocationDomain(massStockMove));
     }
   }
 
