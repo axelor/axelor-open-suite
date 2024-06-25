7666cf45f230c11a5a6cadb3710158180e3fdfea
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-06-25 17:44:58 +0200

    77189 refacto financial discount tests

--- axelor-account/src/test/java/com/axelor/apps/account/service/financialdiscount/TestInvoiceFinancialDiscountService.java
+++ axelor-account/src/test/java/com/axelor/apps/account/service/financialdiscount/TestInvoiceFinancialDiscountService.java
@@ -2,7 +2,6 @@ package com.axelor.apps.account.service.financialdiscount;
 
 import com.axelor.apps.account.db.FinancialDiscount;
 import com.axelor.apps.account.db.Invoice;
-import com.axelor.apps.account.db.InvoiceTerm;
 import com.axelor.apps.account.db.repo.FinancialDiscountRepository;
 import com.axelor.apps.account.db.repo.InvoiceRepository;
 import com.axelor.apps.account.db.repo.PaymentConditionRepository;
@@ -12,9 +11,7 @@ import com.axelor.apps.account.service.invoice.InvoiceTermService;
 import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.repo.CompanyRepository;
 import com.axelor.apps.base.db.repo.PartnerRepository;
-import com.axelor.common.ObjectUtils;
 import com.axelor.inject.Beans;
-import com.axelor.meta.loader.LoaderHelper;
 import com.google.inject.Inject;
 import com.google.inject.servlet.RequestScoper;
 import com.google.inject.servlet.ServletScopes;
@@ -22,14 +19,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
-class TestInvoiceFinancialDiscountService extends AccountTest {
+public abstract class TestInvoiceFinancialDiscountService extends AccountTest {
 
   protected final InvoiceRepository invoiceRepository;
   protected final FinancialDiscountRepository financialDiscountRepository;
@@ -38,7 +29,9 @@ class TestInvoiceFinancialDiscountService extends AccountTest {
   protected final CompanyRepository companyRepository;
   protected final InvoiceFinancialDiscountService invoiceFinancialDiscountService;
   protected final InvoiceTermService invoiceTermService;
-  protected static final LoaderHelper loaderHelper = Beans.get(LoaderHelper.class);
+  protected Invoice invoice;
+  protected BigDecimal rate;
+  protected FinancialDiscount financialDiscount;
 
   @Inject
   public TestInvoiceFinancialDiscountService(
@@ -59,12 +52,7 @@ class TestInvoiceFinancialDiscountService extends AccountTest {
     }
   }
 
-  @BeforeAll
-  static void setUp() {
-    loaderHelper.importCsv("data/account-config-input.xml");
-  }
-
-  protected Invoice prepareInvoice(Long paymentConditionId) throws AxelorException {
+  protected void givenInvoice(Long paymentConditionId) throws AxelorException {
     Invoice invoice = new Invoice();
     invoice.setInvoiceDate(LocalDate.of(2024, 1, 1));
     invoice.setDueDate(LocalDate.of(2024, 1, 15));
@@ -79,72 +67,23 @@ class TestInvoiceFinancialDiscountService extends AccountTest {
             .filter("self.importId = :importId")
             .bind("importId", paymentConditionId)
             .fetchOne());
-    /*invoice.setCompany(companyRepository.all()
-    .filter("self.importId = :importId")
-    .bind("importId", 1L)
-    .fetchOne());*/
     invoice.setStatusSelect(InvoiceRepository.STATUS_DRAFT);
     invoice.setInvoiceTermList(new ArrayList<>());
-    invoice = invoiceTermService.computeInvoiceTerms(invoice);
-    return invoice;
+    this.invoice = invoiceTermService.computeInvoiceTerms(invoice);
   }
 
-  @Test
-  void testSetFinancialDiscountInformation() throws AxelorException {
-    Invoice invoiceMonoInvoiceTerm = prepareInvoice(1L);
-    Invoice invoiceMultiInvoiceTerm = prepareInvoice(1000L);
-
-    testSetFinancialDiscountInformationOnInvoice(invoiceMonoInvoiceTerm);
-    testSetFinancialDiscountInformationOnInvoice(invoiceMultiInvoiceTerm);
-  }
-
-  protected void testSetFinancialDiscountInformationOnInvoice(Invoice invoice) {
-    verifyFinancialDiscountValuesOnInvoice(invoice, BigDecimal.ZERO, null);
-
-    // Verify values for WT financial discounts
-    verifyFinancialDiscountValuesOnInvoice(invoice, BigDecimal.valueOf(0.02), 1L);
-    verifyFinancialDiscountValuesOnInvoice(invoice, BigDecimal.valueOf(0.03), 2L);
-
-    // Verify values for ATI financial discounts
-    verifyFinancialDiscountValuesOnInvoice(invoice, BigDecimal.valueOf(0.02), 100L);
-    verifyFinancialDiscountValuesOnInvoice(invoice, BigDecimal.valueOf(0.03), 101L);
-
-    // Verify values without financial discounts
-    verifyFinancialDiscountValuesOnInvoice(invoice, BigDecimal.ZERO, null);
-  }
-
-  protected void verifyFinancialDiscountValuesOnInvoice(
-      Invoice invoice, BigDecimal rate, Long financialDiscountId) {
-    invoice = computeFinancialDiscount(invoice, financialDiscountId);
-
-    Assertions.assertEquals(
-        rate.multiply(new BigDecimal(100)).intValue(),
-        invoice.getFinancialDiscountRate().intValue(),
-        "Financial discount rate");
-    Assertions.assertEquals(
-        invoice.getExTaxTotal().multiply(rate).intValue(),
-        invoice.getFinancialDiscountTotalAmount().intValue(),
-        "Financial discount total amount");
-
-    if (rate.signum() == 0) {
-      Assertions.assertEquals(
-          0,
-          invoice.getRemainingAmountAfterFinDiscount().intValue(),
-          "Remaining amount after fin discount");
-      Assertions.assertNull(invoice.getLegalNotice(), "Legal notice");
-    } else {
-      Assertions.assertEquals(
-          invoice.getExTaxTotal().multiply(BigDecimal.ONE.subtract(rate)).intValue(),
-          invoice.getRemainingAmountAfterFinDiscount().intValue(),
-          "Remaining amount after fin discount");
-      Assertions.assertNotNull(invoice.getLegalNotice(), "Legal notice");
-    }
+  protected void givenComputeFinancialDiscount(
+      BigDecimal rate, Long financialDiscountImportId, Long paymentConditionId)
+      throws AxelorException {
+    givenInvoice(paymentConditionId);
+    this.rate = rate;
+    invoice = computeFinancialDiscount(invoice, financialDiscountImportId);
   }
 
   protected Invoice computeFinancialDiscount(Invoice invoice, Long financialDiscountId) {
-    FinancialDiscount financialDiscount = null;
+    this.financialDiscount = null;
     if (financialDiscountId != null) {
-      financialDiscount =
+      this.financialDiscount =
           financialDiscountRepository
               .all()
               .filter("self.importId = :importId")
@@ -154,111 +93,5 @@ class TestInvoiceFinancialDiscountService extends AccountTest {
     invoice.setFinancialDiscount(financialDiscount);
     invoiceFinancialDiscountService.setFinancialDiscountInformations(invoice);
     return invoice;
-  }
-
-  @Test
-  void testUpdateFinancialDiscount() throws AxelorException {
-    Invoice invoiceMonoInvoiceTerm = prepareInvoice(1L);
-    Invoice invoiceMultiInvoiceTerm = prepareInvoice(1000L);
-
-    testUpdateFinancialDiscountOnInvoiceTermList(invoiceMonoInvoiceTerm);
-    testUpdateFinancialDiscountOnInvoiceTermList(invoiceMultiInvoiceTerm);
-  }
-
-  protected void testUpdateFinancialDiscountOnInvoiceTermList(Invoice invoice) {
-    verifyFinancialDiscountValuesOnInvoiceTerm(invoice, BigDecimal.ZERO, null);
-
-    // Verify values for WT financial discounts
-    verifyFinancialDiscountValuesOnInvoiceTerm(invoice, BigDecimal.valueOf(0.02), 1L);
-    verifyFinancialDiscountValuesOnInvoiceTerm(invoice, BigDecimal.valueOf(0.03), 2L);
-
-    // Verify values for ATI financial discounts
-    verifyFinancialDiscountValuesOnInvoiceTerm(invoice, BigDecimal.valueOf(0.02), 100L);
-    verifyFinancialDiscountValuesOnInvoiceTerm(invoice, BigDecimal.valueOf(0.03), 101L);
-
-    // Verify values without financial discounts
-    verifyFinancialDiscountValuesOnInvoiceTerm(invoice, BigDecimal.ZERO, null);
-  }
-
-  protected void verifyFinancialDiscountValuesOnInvoiceTerm(
-      Invoice invoice, BigDecimal rate, Long financialDiscountId) {
-    invoice = computeFinancialDiscount(invoice, financialDiscountId);
-
-    Map<String, BigDecimal> amountByField = updateFinancialDiscount(invoice, financialDiscountId);
-
-    Assertions.assertEquals(
-        amountByField.get("financialDiscountAmount").intValue(),
-        invoice.getExTaxTotal().multiply(rate).intValue(),
-        "Financial discount amount");
-
-    if (rate.signum() == 0) {
-      Assertions.assertEquals(
-          0,
-          amountByField.get("remainingAmountAfterFinDiscount").intValue(),
-          "Remaining amount after fin discount");
-      Assertions.assertEquals(
-          0,
-          amountByField.get("amountRemainingAfterFinDiscount").intValue(),
-          "Amount remaining after fin discount");
-      Assertions.assertTrue(
-          amountByField.get("applyFinancialDiscount").signum() == 0, "Apply Financial discount");
-    } else {
-      Assertions.assertEquals(
-          invoice.getExTaxTotal().multiply(BigDecimal.ONE.subtract(rate)).intValue(),
-          amountByField.get("remainingAmountAfterFinDiscount").intValue(),
-          "Remaining amount after fin discount");
-      Assertions.assertEquals(
-          invoice.getExTaxTotal().multiply(BigDecimal.ONE.subtract(rate)).intValue(),
-          amountByField.get("amountRemainingAfterFinDiscount").intValue(),
-          "Amount remaining after fin discount");
-      Assertions.assertTrue(
-          amountByField.get("applyFinancialDiscount").signum() > 0, "Apply Financial discount");
-    }
-  }
-
-  protected Map<String, BigDecimal> updateFinancialDiscount(
-      Invoice invoice, Long financialDiscountId) {
-    FinancialDiscount financialDiscount = null;
-    if (financialDiscountId != null) {
-      financialDiscount =
-          financialDiscountRepository
-              .all()
-              .filter("self.importId = :importId")
-              .bind("importId", financialDiscountId)
-              .fetchOne();
-    }
-    invoice.setFinancialDiscount(financialDiscount);
-    List<InvoiceTerm> invoiceTermList =
-        invoiceFinancialDiscountService.updateFinancialDiscount(invoice);
-
-    Map<String, BigDecimal> amountByField = new HashMap<>();
-    amountByField.put("applyFinancialDiscount", BigDecimal.ONE);
-    amountByField.put("financialDiscountAmount", BigDecimal.ZERO);
-    amountByField.put("remainingAmountAfterFinDiscount", BigDecimal.ZERO);
-    amountByField.put("amountRemainingAfterFinDiscount", BigDecimal.ZERO);
-    if (!ObjectUtils.isEmpty(invoiceTermList)) {
-      for (InvoiceTerm invoiceTerm : invoiceTermList) {
-        if (!invoiceTerm.getApplyFinancialDiscount()) {
-          amountByField.replace("applyFinancialDiscount", BigDecimal.ZERO);
-        }
-        amountByField.replace(
-            "financialDiscountAmount",
-            amountByField
-                .get("financialDiscountAmount")
-                .add(invoiceTerm.getFinancialDiscountAmount()));
-        amountByField.replace(
-            "remainingAmountAfterFinDiscount",
-            amountByField
-                .get("remainingAmountAfterFinDiscount")
-                .add(invoiceTerm.getRemainingAmountAfterFinDiscount()));
-        amountByField.replace(
-            "amountRemainingAfterFinDiscount",
-            amountByField
-                .get("amountRemainingAfterFinDiscount")
-                .add(invoiceTerm.getAmountRemainingAfterFinDiscount()));
-      }
-    }
-
-    return amountByField;
   }
 }
