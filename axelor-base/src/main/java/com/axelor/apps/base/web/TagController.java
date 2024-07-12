dac6ca1d40c17ab7df8bfc25901f2d7d9ca19091
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2024-07-12 14:50:28 +0200

    80635 - Set default model

--- axelor-base/src/main/java/com/axelor/apps/base/web/TagController.java
+++ axelor-base/src/main/java/com/axelor/apps/base/web/TagController.java
@@ -2,8 +2,12 @@ package com.axelor.apps.base.web;
 
 import com.axelor.apps.base.db.Tag;
 import com.axelor.apps.base.service.exception.ErrorException;
+import com.axelor.inject.Beans;
+import com.axelor.meta.db.MetaModel;
+import com.axelor.meta.db.repo.MetaModelRepository;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import com.axelor.rpc.Context;
 
 public class TagController {
 
@@ -18,5 +22,27 @@ public class TagController {
 
     response.setAttr(
         "concernedModelSet", "domain", "self.packageName like '%" + packageName + "%'");
+  }
+
+  public void setDefaultConcernedModel(ActionRequest request, ActionResponse response) {
+
+    Tag tag = request.getContext().asType(Tag.class);
+    Context parentContext = request.getContext().getParent();
+
+    if (tag == null) {
+      return;
+    }
+
+    if (parentContext != null && parentContext.get("_model") != null) {
+      String fullNameModel = parentContext.get("_model").toString();
+      MetaModel metaModel =
+          Beans.get(MetaModelRepository.class)
+              .all()
+              .filter("self.fullName = ?", fullNameModel)
+              .fetchOne();
+
+      tag.addConcernedModelSetItem(metaModel);
+      response.setValue("concernedModelSet", tag.getConcernedModelSet());
+    }
   }
 }
