5caff6293f713a615ffb161bb77244da298cdfe2
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2024-07-24 18:08:13 +0200

    fixup! 81307 Create a button in project form to convert a project into busin… (#12998)

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/BusinessProjectService.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/BusinessProjectService.java
@@ -4,7 +4,6 @@ import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.project.db.Project;
-import com.google.inject.persist.Transactional;
 
 public interface BusinessProjectService {
 
