a8c51a090513759a01fe213bc1561db0de5ac2b8
dar-axelor <132374601+dar-axelor@users.noreply.github.com>
2024-07-25 18:19:50 +0200

    RM#82442 ProjectTask : "Compute project totals" ConstraintViolation on percentageOfProgress and percentageOfConsumption.

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/translation/ITranslation.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/translation/ITranslation.java
@@ -28,4 +28,6 @@ public interface ITranslation {
 
   public static final String EXPENSE_LINE_CREATION_WITH_PROJECT = /*$$(*/
       "Expense line successfully created. The project is configured as billable, so the charge has been marked as billable by default."; /*)*/
+  public static final String PROJECT_TASK_FOLLOW_UP_PERCENTAGE_ABOVE_1000 = /*$$(*/
+      "These following tasks have follow-up percentages above 1000%% %s. <br>" /*)*/;
 }
