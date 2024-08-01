5d7e1894eaeaa6b48c54bbcb231656c96a860d0e
Majda EL ABDI <m.elabdi@axelor.com>
2024-08-01 16:31:26 +0200

    RM-82414 Add the domain verification of the product

--- axelor-sale/src/main/java/com/axelor/apps/sale/service/saleorder/SaleOrderLineGeneratorServiceImpl.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/service/saleorder/SaleOrderLineGeneratorServiceImpl.java
@@ -20,38 +20,49 @@ package com.axelor.apps.sale.service.saleorder;
 
 import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.Product;
+import com.axelor.apps.base.db.repo.ProductRepository;
+import com.axelor.apps.base.db.repo.TraceBackRepository;
+import com.axelor.apps.base.service.app.AppBaseService;
 import com.axelor.apps.sale.db.SaleOrder;
 import com.axelor.apps.sale.db.SaleOrderLine;
 import com.axelor.apps.sale.db.repo.SaleOrderLineRepository;
 import com.axelor.apps.sale.db.repo.SaleOrderRepository;
+import com.axelor.apps.sale.exception.SaleExceptionMessage;
+import com.axelor.i18n.I18n;
 import com.axelor.apps.sale.service.saleorderline.SaleOrderLineInitValueService;
 import com.axelor.apps.sale.service.saleorderline.SaleOrderLineProductService;
 import com.axelor.inject.Beans;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
-import java.math.BigDecimal;
-import java.util.Map;
 
 public class SaleOrderLineGeneratorServiceImpl implements SaleOrderLineGeneratorService {
   protected SaleOrderLineInitValueService saleOrderLineInitValueService;
   protected SaleOrderLineProductService saleOrderLineProductService;
   protected SaleOrderLineRepository saleOrderLineRepository;
+  protected ProductRepository productRepository;
   protected SaleOrderRepository saleOrderRepository;
-  protected  SaleOrderLineComputeService saleOrderLineComputeService;
+  protected SaleOrderLineComputeService saleOrderLineComputeService;
   protected SaleOrderComputeService saleOrderComputeService;
+  protected SaleOrderLineDomainService saleOrderLineDomainService;
+
   @Inject
   public SaleOrderLineGeneratorServiceImpl(
       SaleOrderLineInitValueService saleOrderLineInitValueService,
       SaleOrderLineProductService saleOrderLineProductService,
       SaleOrderRepository saleOrderRepository,
-      SaleOrderLineRepository saleOrderLineRepository, SaleOrderLineComputeService saleOrderLineComputeService,
- SaleOrderComputeService saleOrderComputeService) {
+      SaleOrderLineRepository saleOrderLineRepository,
+      SaleOrderLineComputeService saleOrderLineComputeService,
+      SaleOrderComputeService saleOrderComputeService,
+      SaleOrderLineDomainService saleOrderLineDomainService,
+      ProductRepository productRepository) {
     this.saleOrderLineInitValueService = saleOrderLineInitValueService;
     this.saleOrderLineProductService = saleOrderLineProductService;
     this.saleOrderLineRepository = saleOrderLineRepository;
     this.saleOrderRepository = saleOrderRepository;
-    this.saleOrderLineComputeService =saleOrderLineComputeService;
-    this.saleOrderComputeService =saleOrderComputeService;
+    this.saleOrderLineComputeService = saleOrderLineComputeService;
+    this.saleOrderComputeService = saleOrderComputeService;
+    this.saleOrderLineDomainService = saleOrderLineDomainService;
+    this.productRepository = productRepository;
   }
 
   @Transactional(rollbackOn = {Exception.class})
@@ -60,19 +71,25 @@ public class SaleOrderLineGeneratorServiceImpl implements SaleOrderLineGenerator
       throws AxelorException {
     SaleOrderLine saleOrderLine = new SaleOrderLine();
     saleOrderLineInitValueService.onNewInitValues(saleOrder, saleOrderLine);
+    checkProduct(saleOrder, saleOrderLine, product);
     saleOrderLine.setProduct(product);
     saleOrderLineProductService.computeProductInformation(saleOrderLine, saleOrder);
     saleOrderLineComputeService.computeValues(saleOrder, saleOrderLine);
     saleOrderLineRepository.save(saleOrderLine);
     saleOrder.addSaleOrderLineListItem(saleOrderLine);
-     saleOrderComputeService.computeSaleOrder(saleOrder);
+    saleOrderComputeService.computeSaleOrder(saleOrder);
     saleOrderRepository.save(saleOrder);
     return saleOrderLine;
   }
 
-  private void setSaleOrdervalues(
-      Map<String, Object> saleOrderLineMap, SaleOrderLine saleOrderLine) {
-    saleOrderLine.setPrice((BigDecimal) saleOrderLineMap.get("price"));
-    saleOrderLine.setExTaxTotal((BigDecimal) saleOrderLineMap.get("exTotal"));
+  protected void checkProduct(SaleOrder saleOrder, SaleOrderLine saleOrderLine, Product product)
+      throws AxelorException {
+    String domain = saleOrderLineDomainService.computeProductDomain(saleOrderLine, saleOrder);
+    if (!productRepository.all().filter(domain).bind("__date__", Beans.get(AppBaseService.class).getTodayDate(saleOrder.getCompany())).fetch().contains(product)) {
+      throw new AxelorException(
+          TraceBackRepository.CATEGORY_INCONSISTENCY,
+          I18n.get(SaleExceptionMessage.PRODUCT_DOES_NOT_RESPECT_DOMAIN_RESTRICTIONS),
+          product.getName());
+    }
   }
 }
