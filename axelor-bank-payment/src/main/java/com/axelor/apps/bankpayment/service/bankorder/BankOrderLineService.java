e6a209804475796a2b48d7bfce32b1058c994307
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-08-02 14:38:01 +0200

    82529 BankOrder : fixed manual multi currency bank order's move generation (#13028)

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankorder/BankOrderLineService.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankorder/BankOrderLineService.java
@@ -408,7 +408,7 @@ public class BankOrderLineService {
 
     Currency bankOrderCurrency = bankOrder.getBankOrderCurrency();
 
-    if (BankOrderToolService.isMultiCurrency(bankOrder)) {
+    if (bankOrder.getIsMultiCurrency()) {
       bankOrderCurrency = bankOrderLine.getBankOrderCurrency();
     }
 
