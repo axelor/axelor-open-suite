8d599896f62b92c0171e23a1410a193e06f5f2ed
mea-axelor <132366817+mea-axelor@users.noreply.github.com>
2024-08-02 10:22:59 +0200

    82410 Create a sale order using APIs  (#13031)

--- axelor-sale/src/main/java/com/axelor/apps/sale/module/SaleModule.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/module/SaleModule.java
@@ -50,6 +50,8 @@ import com.axelor.apps.sale.service.PartnerSaleServiceImpl;
 import com.axelor.apps.sale.service.PricingGroupSaleServiceImpl;
 import com.axelor.apps.sale.service.ProductCategorySaleService;
 import com.axelor.apps.sale.service.ProductCategoryServiceSaleImpl;
+import com.axelor.apps.sale.service.SaleOrderGeneratorService;
+import com.axelor.apps.sale.service.SaleOrderGeneratorServiceImpl;
 import com.axelor.apps.sale.service.app.AppSaleService;
 import com.axelor.apps.sale.service.app.AppSaleServiceImpl;
 import com.axelor.apps.sale.service.config.SaleConfigService;
@@ -255,5 +257,6 @@ public class SaleModule extends AxelorModule {
     bind(LoyaltyAccountService.class).to(LoyaltyAccountServiceImpl.class);
     bind(LoyaltyAccountPointsManagementService.class)
         .to(LoyaltyAccountPointsManagementServiceImpl.class);
+    bind(SaleOrderGeneratorService.class).to(SaleOrderGeneratorServiceImpl.class);
   }
 }
