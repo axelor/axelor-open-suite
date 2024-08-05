73905a477d07566edbf34818ac602c955682ba96
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-08-05 16:06:06 +0200

    82636 add global config for progress on task status (#13035)

--- axelor-business-support/src/main/java/com/axelor/apps/businesssupport/service/ProjectTaskBusinessSupportServiceImpl.java
+++ axelor-business-support/src/main/java/com/axelor/apps/businesssupport/service/ProjectTaskBusinessSupportServiceImpl.java
@@ -33,6 +33,7 @@ import com.axelor.apps.project.db.ProjectTask;
 import com.axelor.apps.project.db.TaskTemplate;
 import com.axelor.apps.project.db.repo.ProjectRepository;
 import com.axelor.apps.project.db.repo.ProjectTaskRepository;
+import com.axelor.apps.project.service.app.AppProjectService;
 import com.google.inject.Inject;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -47,6 +48,7 @@ public class ProjectTaskBusinessSupportServiceImpl extends ProjectTaskBusinessPr
       FrequencyService frequencyService,
       AppBaseService appBaseService,
       ProjectRepository projectRepository,
+      AppProjectService appProjectService,
       PriceListLineRepository priceListLineRepo,
       PriceListService priceListService,
       PartnerPriceListService partnerPriceListService,
@@ -59,6 +61,7 @@ public class ProjectTaskBusinessSupportServiceImpl extends ProjectTaskBusinessPr
         frequencyService,
         appBaseService,
         projectRepository,
+        appProjectService,
         priceListLineRepo,
         priceListService,
         partnerPriceListService,
