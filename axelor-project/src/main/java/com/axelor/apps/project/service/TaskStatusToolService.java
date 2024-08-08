80c4afd83e7a17106d9fb04cd83667e643997f77
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2024-08-08 18:08:14 +0200

    82869 New TaskStatus management on projectTask - By category

---
+++ axelor-project/src/main/java/com/axelor/apps/project/service/TaskStatusToolService.java
@@ -0,0 +1,14 @@
+package com.axelor.apps.project.service;
+
+import com.axelor.apps.project.db.Project;
+import com.axelor.apps.project.db.ProjectTask;
+import com.axelor.apps.project.db.TaskStatus;
+import java.util.Set;
+
+public interface TaskStatusToolService {
+  TaskStatus getCompletedTaskStatus(Project project, ProjectTask projectTask);
+
+  Set<TaskStatus> getTaskStatusSet(Project project, ProjectTask projectTask);
+
+  String checkCompletedTaskStatus(Project project, ProjectTask projectTask);
+}
