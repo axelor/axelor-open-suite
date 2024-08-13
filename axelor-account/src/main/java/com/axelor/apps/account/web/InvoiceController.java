ee8d044dd1038bb2cd6a25c3eb7e40e7b3f553b0
vva-axelor <v.vaghela@axelor.com>
2024-08-13 15:57:45 +0530

    Anomaly RM#82398:[Invoice] Unable to display invoice lines (grid view) when one or more invoices are selected

--- axelor-account/src/main/java/com/axelor/apps/account/web/InvoiceController.java
+++ axelor-account/src/main/java/com/axelor/apps/account/web/InvoiceController.java
@@ -82,6 +82,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.commons.collections.CollectionUtils;
@@ -1031,8 +1032,7 @@ public class InvoiceController {
 
   public void showCustomerInvoiceLines(ActionRequest request, ActionResponse response) {
     try {
-      String idList =
-          StringHelper.getIdListString(request.getCriteria().createQuery(Invoice.class).fetch());
+      String idList = getIdListString(request);
       response.setView(
           ActionView.define(I18n.get("Customer Invoice Line"))
               .model(InvoiceLine.class.getName())
@@ -1047,6 +1047,16 @@ public class InvoiceController {
     } catch (Exception e) {
       TraceBackService.trace(response, e);
     }
+  }
+
+  @SuppressWarnings("unchecked")
+  protected String getIdListString(ActionRequest request) {
+    return Optional.ofNullable((List<Integer>) request.getContext().get("_ids"))
+        .map(idList -> idList.stream().map(String::valueOf).collect(Collectors.joining(",")))
+        .orElseGet(
+            () ->
+                StringHelper.getIdListString(
+                    request.getCriteria().createQuery(Invoice.class).fetch()));
   }
 
   public void checkInvoiceLinesAnalyticDistribution(
@@ -1095,8 +1105,7 @@ public class InvoiceController {
 
   public void showSupplierInvoiceLines(ActionRequest request, ActionResponse response) {
     try {
-      String idList =
-          StringHelper.getIdListString(request.getCriteria().createQuery(Invoice.class).fetch());
+      String idList = getIdListString(request);
       response.setView(
           ActionView.define(I18n.get("Supplier Invoice Line"))
               .model(InvoiceLine.class.getName())
