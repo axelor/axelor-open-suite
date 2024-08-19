b0122bd3ee1d6b548c94365ff0d0826b227503be
Majda EL ABDI <m.elabdi@axelor.com>
2024-08-19 17:17:50 +0200

    RM-83140 add parametrs in sale order creation

--- axelor-sale/src/main/java/com/axelor/apps/sale/rest/dto/SaleOrderPostRequest.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/rest/dto/SaleOrderPostRequest.java
@@ -1,22 +1,53 @@
 package com.axelor.apps.sale.rest.dto;
 
+import com.axelor.apps.base.db.Company;
+import com.axelor.apps.base.db.Currency;
 import com.axelor.apps.base.db.Partner;
 import com.axelor.utils.api.ObjectFinder;
 import com.axelor.utils.api.RequestPostStructure;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+
+
 public class SaleOrderPostRequest extends RequestPostStructure {
 
   @NotNull
   @Min(0)
   private Long clientPartnerId;
+  private Long companyId;
+  private Long contactId;
+  private Long currencyId;
+
+  public Long getCompanyId() {
+    return companyId;
+  }
+
+  public Long getContactId() {
+    return contactId;
+  }
+
+  public Long getCurrencyId() {
+    return currencyId;
+  }
 
   public Long getClientPartnerId() {
     return clientPartnerId;
   }
 
-  public void setClientPartner(Long clientPartnerId) {
+  public void setCompanyId(Long companyId) {
+    this.companyId = companyId;
+  }
+
+  public void setContactId(Long contactId) {
+    this.contactId = contactId;
+  }
+
+  public void setCurrencyId(Long currencyId) {
+    this.currencyId = currencyId;
+  }
+
+  public void setClientPartnerId(Long clientPartnerId) {
     this.clientPartnerId = clientPartnerId;
   }
 
@@ -26,4 +57,25 @@ public class SaleOrderPostRequest extends RequestPostStructure {
     }
     return ObjectFinder.find(Partner.class, clientPartnerId, ObjectFinder.NO_VERSION);
   }
+
+  public Partner fetchContact() {
+    if (contactId == null || contactId == 0L) {
+      return null;
+    }
+    return ObjectFinder.find(Partner.class, contactId, ObjectFinder.NO_VERSION);
+  }
+
+  public Company fetchCompany() {
+    if (companyId == null || companyId == 0L) {
+      return null;
+    }
+    return ObjectFinder.find(Company.class, companyId, ObjectFinder.NO_VERSION);
+  }
+  public Currency fetchCurrency() {
+    if (clientPartnerId == null || clientPartnerId == 0L) {
+      return null;
+    }
+    return ObjectFinder.find(Currency.class, currencyId, ObjectFinder.NO_VERSION);
+  }
+
 }
