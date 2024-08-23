e1dde248bbc26e44246a3392b06420fbbdc2ccb9
Majda EL ABDI <m.elabdi@axelor.com>
2024-08-23 14:25:55 +0200

    RM-83169 check inAti value

--- axelor-sale/src/main/java/com/axelor/apps/sale/rest/SaleOrderRestController.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/rest/SaleOrderRestController.java
@@ -51,8 +51,7 @@ public class SaleOrderRestController {
       throws AxelorException, JsonProcessingException {
     RequestValidator.validateBody(requestBody);
     new SecurityCheck().createAccess(SaleOrder.class).check();
-
-    Boolean inAti = Boolean.getBoolean(requestBody.getInAti());
+    Boolean inAti = getInAti(requestBody.getInAti());
 
     SaleOrder saleOrder =
         Beans.get(SaleOrderGeneratorService.class)
@@ -64,5 +63,11 @@ public class SaleOrderRestController {
                 inAti);
 
     return ResponseConstructor.buildCreateResponse(saleOrder, new SaleOrderResponse(saleOrder));
+  }
+
+  private static Boolean getInAti(String inAti) {
+    if (inAti.equals("true")) return true;
+    if (inAti.equals("false")) return false;
+    return null;
   }
 }
