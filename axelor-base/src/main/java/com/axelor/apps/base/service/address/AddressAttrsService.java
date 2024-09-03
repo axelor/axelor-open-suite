9c05b534fb6f3cd4aadc510bb62d14af4d03fe49
tyu-axelor <148809551+tyu-axelor@users.noreply.github.com>
2024-09-03 16:25:23 -0400

    manage townName and city

--- axelor-base/src/main/java/com/axelor/apps/base/service/address/AddressAttrsService.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/address/AddressAttrsService.java
@@ -19,9 +19,12 @@
 package com.axelor.apps.base.service.address;
 
 import com.axelor.apps.base.db.Address;
+import com.axelor.apps.base.db.Country;
 import java.util.Map;
 
 public interface AddressAttrsService {
 
   Map<String, Map<String, Object>> getCountryAddressMetaFieldOnChangeAttrsMap(Address address);
+
+  boolean ifCountryHasCities(Country country);
 }
