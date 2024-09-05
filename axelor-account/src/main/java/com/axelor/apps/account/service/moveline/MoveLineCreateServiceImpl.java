50d8165c70902ff8c27df77c6d83f51d2264c268
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2024-09-05 15:36:14 +0200

    fixup! Invoice: fixed move generation with end of pack line

--- axelor-account/src/main/java/com/axelor/apps/account/service/moveline/MoveLineCreateServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/moveline/MoveLineCreateServiceImpl.java
@@ -71,6 +71,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
