baa8e87f88c92c9daf025b7a7d3444e59bd022c3
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2024-09-12 12:09:12 +0200

    Part 3

--- axelor-account/src/main/java/com/axelor/apps/account/service/reconcile/foreignexchange/ForeignExchangeGapToolServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/reconcile/foreignexchange/ForeignExchangeGapToolServiceImpl.java
@@ -2,6 +2,7 @@ package com.axelor.apps.account.service.reconcile.foreignexchange;
 
 import com.axelor.apps.account.db.MoveLine;
 import com.axelor.apps.account.db.Reconcile;
+import com.axelor.apps.account.db.repo.InvoicePaymentRepository;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,36 +10,52 @@ public class ForeignExchangeGapToolServiceImpl implements ForeignExchangeGapTool
 
   @Override
   public List<Integer> getForeignExchangeTypes() {
-    return new ArrayList<>();
+    return new ArrayList<>(
+        List.of(
+            InvoicePaymentRepository.TYPE_FOREIGN_EXCHANGE_GAIN,
+            InvoicePaymentRepository.TYPE_FOREIGN_EXCHANGE_LOSS));
   }
 
   @Override
   public boolean isGain(MoveLine creditMoveLine, MoveLine debitMoveLine) {
-    return false;
+    return this.isGain(creditMoveLine, debitMoveLine, this.isDebit(creditMoveLine, debitMoveLine));
   }
 
   protected boolean isGain(MoveLine creditMoveLine, MoveLine debitMoveLine, boolean isDebit) {
-    return false;
+    return isDebit
+        ? creditMoveLine.getCurrencyRate().compareTo(debitMoveLine.getCurrencyRate()) > 0
+        : debitMoveLine.getCurrencyRate().compareTo(creditMoveLine.getCurrencyRate()) < 0;
   }
 
   @Override
   public boolean isDebit(MoveLine creditMoveLine, MoveLine debitMoveLine) {
-    return false;
+    return debitMoveLine.getDate().isAfter(creditMoveLine.getDate());
   }
 
   @Override
   public int getInvoicePaymentType(Reconcile reconcile) {
-    return 0;
+    return this.isGain(reconcile.getCreditMoveLine(), reconcile.getDebitMoveLine())
+        ? InvoicePaymentRepository.TYPE_FOREIGN_EXCHANGE_GAIN
+        : InvoicePaymentRepository.TYPE_FOREIGN_EXCHANGE_LOSS;
   }
 
   @Override
   public boolean checkCurrencies(MoveLine creditMoveLine, MoveLine debitMoveLine) {
-    return false;
+    return debitMoveLine != null
+        && creditMoveLine != null
+        && !creditMoveLine.getCurrency().equals(creditMoveLine.getCompanyCurrency())
+        && !debitMoveLine.getCurrency().equals(debitMoveLine.getCompanyCurrency())
+        && creditMoveLine.getCurrency().equals(debitMoveLine.getCurrency());
   }
 
   @Override
   public boolean checkIsTotalPayment(
       Reconcile reconcile, MoveLine creditMoveLine, MoveLine debitMoveLine) {
-    return false;
+    boolean paymentIsDebit = this.isDebit(creditMoveLine, debitMoveLine);
+
+    return reconcile
+            .getAmount()
+            .compareTo(paymentIsDebit ? creditMoveLine.getCredit() : debitMoveLine.getDebit())
+        == 0;
   }
 }
