7647fd782cd47d7a648816970c15230b1352920f
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2024-09-12 11:43:08 +0200

    Part 2

---
+++ axelor-account/src/main/java/com/axelor/apps/account/service/reconcile/foreignexchange/ForeignExchangeGapToolService.java
@@ -0,0 +1,20 @@
+package com.axelor.apps.account.service.reconcile.foreignexchange;
+
+import com.axelor.apps.account.db.MoveLine;
+import com.axelor.apps.account.db.Reconcile;
+import java.util.List;
+
+public interface ForeignExchangeGapToolService {
+
+  List<Integer> getForeignExchangeTypes();
+
+  boolean isGain(MoveLine creditMoveLine, MoveLine debitMoveLine);
+
+  boolean isDebit(MoveLine creditMoveLine, MoveLine debitMoveLine);
+
+  int getInvoicePaymentType(Reconcile reconcile);
+
+  boolean checkCurrencies(MoveLine creditMoveLine, MoveLine debitMoveLine);
+
+  boolean checkIsTotalPayment(Reconcile reconcile, MoveLine creditMoveLine, MoveLine debitMoveLine);
+}
