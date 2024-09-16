8952f4fc7f05bbad6751c6d423e1aa361b2f4a80
Majda EL ABDI <m.elabdi@axelor.com>
2024-09-16 17:10:15 +0200

    RM-84209 Fix the extend and put the cart ID in URL

--- axelor-sale/src/main/java/com/axelor/apps/sale/rest/CartRestController.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/rest/CartRestController.java
@@ -3,15 +3,14 @@ package com.axelor.apps.sale.rest;
 import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.sale.db.Cart;
 import com.axelor.apps.sale.db.SaleOrder;
-import com.axelor.apps.sale.db.repo.CartRepository;
-import com.axelor.apps.sale.rest.dto.CartPutRequest;
 import com.axelor.apps.sale.rest.dto.SaleOrderResponse;
 import com.axelor.apps.sale.service.cart.CartResetService;
 import com.axelor.apps.sale.service.cart.CartSaleOrderGeneratorService;
 import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
 import com.axelor.utils.api.HttpExceptionHandler;
-import com.axelor.utils.api.RequestValidator;
+import com.axelor.utils.api.ObjectFinder;
+import com.axelor.utils.api.RequestStructure;
 import com.axelor.utils.api.ResponseConstructor;
 import com.axelor.utils.api.SecurityCheck;
 import com.axelor.web.ITranslation;
@@ -33,14 +32,13 @@ public class CartRestController {
   @Operation(
       summary = "Empty cart",
       tags = {"Cart"})
-  @Path("/empty")
+  @Path("/empty/{cartId}")
   @PUT
   @HttpExceptionHandler
-  public Response emptyCart(CartPutRequest requestBody) {
-    RequestValidator.validateBody(requestBody);
-    new SecurityCheck().writeAccess(Cart.class, requestBody.getCartId()).check();
-    Beans.get(CartResetService.class).emptyCart(requestBody.fetchCart());
-
+  public Response emptyCart(@PathParam("cartId") Long cartId, RequestStructure requestBody) {
+    new SecurityCheck().writeAccess(Cart.class, cartId).check();
+    Cart cart = ObjectFinder.find(Cart.class, cartId, requestBody.getVersion());
+    Beans.get(CartResetService.class).emptyCart(cart);
     return ResponseConstructor.build(Response.Status.OK, I18n.get(ITranslation.EMPTY_CART));
   }
 
@@ -50,10 +48,10 @@ public class CartRestController {
   @Path("/validate/{cartId}")
   @PUT
   @HttpExceptionHandler
-  public Response createSaleOrder(@PathParam("cartId") Long cartId)
+  public Response createSaleOrder(@PathParam("cartId") Long cartId, RequestStructure requestBody)
       throws AxelorException, JsonProcessingException {
     new SecurityCheck().readAccess(Cart.class, cartId).createAccess(SaleOrder.class).check();
-    Cart cart = Beans.get(CartRepository.class).find(cartId);
+    Cart cart = ObjectFinder.find(Cart.class, cartId, requestBody.getVersion());
     SaleOrder saleOrder = Beans.get(CartSaleOrderGeneratorService.class).createSaleOrder(cart);
     return ResponseConstructor.buildCreateResponse(saleOrder, new SaleOrderResponse(saleOrder));
   }
