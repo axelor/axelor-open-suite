6b9d99e0a1073ef9e05e7fa6992b8aabc676ac4e
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2024-09-17 13:52:56 +0200

    Fix scale in grid and create a readonly grid

--- axelor-production/src/main/java/com/axelor/apps/production/module/ProductionModule.java
+++ axelor-production/src/main/java/com/axelor/apps/production/module/ProductionModule.java
@@ -38,6 +38,8 @@ import com.axelor.apps.production.db.repo.ProductionBatchManagementRepository;
 import com.axelor.apps.production.db.repo.ProductionBatchRepository;
 import com.axelor.apps.production.db.repo.RawMaterialRequirementProductionRepository;
 import com.axelor.apps.production.db.repo.RawMaterialRequirementRepository;
+import com.axelor.apps.production.db.repo.SaleOrderLineDetailsManagementRepository;
+import com.axelor.apps.production.db.repo.SaleOrderLineDetailsRepository;
 import com.axelor.apps.production.db.repo.StockMoveLineProductionRepository;
 import com.axelor.apps.production.db.repo.StockMoveProductionRepository;
 import com.axelor.apps.production.db.repo.UnitCostCalculationManagementRepository;
@@ -336,5 +338,6 @@ public class ProductionModule extends AxelorModule {
     bind(ProdProductAttrsService.class).to(ProdProductAttrsServiceImpl.class);
     bind(ProdProductService.class).to(ProdProductServiceImpl.class);
     bind(SaleOrderLineDetailsService.class).to(SaleOrderLineDetailsServiceImpl.class);
+    bind(SaleOrderLineDetailsRepository.class).to(SaleOrderLineDetailsManagementRepository.class);
   }
 }
