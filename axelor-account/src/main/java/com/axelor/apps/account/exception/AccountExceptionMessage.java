f9344ab17e4a42e952b72de29f0ec159fd174511
cce-axelor <c.chea@axelor.com>
2024-09-24 15:51:33 +0200

    RM-84352: remove wrongly added exception message

--- axelor-account/src/main/java/com/axelor/apps/account/exception/AccountExceptionMessage.java
+++ axelor-account/src/main/java/com/axelor/apps/account/exception/AccountExceptionMessage.java
@@ -1771,9 +1771,6 @@ public final class AccountExceptionMessage {
   public static final String LATE_PAYMENT_INTEREST_BELOW_THRESHOLD = /*$$(*/
       "The amount remaining is below the threshold defined in configuration." /*)*/;
 
-  public static final String LATE_PAYMENT_INTEREST_NO_PAYMENT_MODE = /*$$(*/
-      "There is no payment mode in invoice term." /*)*/;
-
   public static final String LATE_PAYMENT_INTEREST_NO_PAYMENT_MODE_RATE = /*$$(*/
       "There is no interest rate defined in invoice term payment mode." /*)*/;
 }
