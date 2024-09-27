94b849be8577d1ba642ec0f19659e6406479f430
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2024-09-27 16:38:46 +0200

    improve javadoc

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/order/OrderInvoiceService.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/order/OrderInvoiceService.java
@@ -13,8 +13,8 @@ public interface OrderInvoiceService {
 
   /**
    * Warning: it is not the same as {@link SaleOrder#getAmountInvoiced()}. This method is also
-   * including invoice that are not ventilated. The purpose is to prevent the user from creating too
-   * many invoices, but until all invoices are ventilated the two amounts are different.
+   * including invoices that are not ventilated. Unless all invoices are ventilated the two amounts
+   * are different.
    *
    * @param saleOrder a saved sale order
    * @return the sum of amount of all non canceled invoices related to the sale order
@@ -23,8 +23,8 @@ public interface OrderInvoiceService {
 
   /**
    * Warning: it is not the same as {@link PurchaseOrder#getAmountInvoiced()}. This method is also
-   * including invoice that are not ventilated. The purpose is to prevent the user from creating too
-   * many invoices, but until all invoices are ventilated the two amounts are different.
+   * including invoices that are not ventilated. Unless all invoices are ventilated the two amounts
+   * are different.
    *
    * @param purchaseOrder a saved purchase order
    * @return the sum of amount of all non canceled invoices related to the purchase order
