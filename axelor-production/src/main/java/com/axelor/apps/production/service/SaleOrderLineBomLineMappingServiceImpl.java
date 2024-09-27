2a85f248bb72a0cd17d93d17ee0aa6926e8805de
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2024-09-27 13:52:28 +0200

    83918: Synchronize BomLines and saleorderLine (#13293)

--- axelor-production/src/main/java/com/axelor/apps/production/service/SaleOrderLineBomLineMappingServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/SaleOrderLineBomLineMappingServiceImpl.java
@@ -8,6 +8,7 @@ import com.axelor.apps.sale.db.SaleOrderLine;
 import com.axelor.apps.sale.service.saleorderline.product.SaleOrderLineOnProductChangeService;
 import com.google.inject.Inject;
 import java.util.Objects;
+import java.util.Optional;
 
 public class SaleOrderLineBomLineMappingServiceImpl implements SaleOrderLineBomLineMappingService {
 
@@ -32,9 +33,37 @@ public class SaleOrderLineBomLineMappingServiceImpl implements SaleOrderLineBomL
       SaleOrderLine saleOrderLine = new SaleOrderLine();
       saleOrderLine.setProduct(billOfMaterialLine.getProduct());
       saleOrderLine.setQty(billOfMaterialLine.getQty());
+      saleOrderLine.setBillOfMaterialLine(billOfMaterialLine);
+      // computing the line will generate sub lines.
       saleOrderLineOnProductChangeService.computeLineFromProduct(saleOrder, saleOrderLine);
+
       return saleOrderLine;
     }
     return null;
+  }
+
+  @Override
+  public boolean isBomLineEqualsSol(
+      BillOfMaterialLine billOfMaterialLine, SaleOrderLine saleOrderLine) {
+
+    return billOfMaterialLine.getQty().equals(saleOrderLine.getQty())
+        && billOfMaterialLine.getProduct().equals(saleOrderLine.getProduct())
+        && billOfMaterialLine.getUnit().equals(saleOrderLine.getUnit())
+        && Optional.ofNullable(saleOrderLine.getBillOfMaterial())
+            .map(solBom -> solBom.equals(billOfMaterialLine.getBillOfMaterial()))
+            .or(() -> Optional.of(billOfMaterialLine.getBillOfMaterial() == null))
+            .orElse(false);
+  }
+
+  @Override
+  public boolean isSyncWithBomLine(SaleOrderLine saleOrderLine) {
+    Objects.requireNonNull(saleOrderLine);
+
+    // No BOM => Not synchronize
+    if (saleOrderLine.getBillOfMaterialLine() == null) {
+      return false;
+    }
+
+    return this.isBomLineEqualsSol(saleOrderLine.getBillOfMaterialLine(), saleOrderLine);
   }
 }
