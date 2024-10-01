497dce3fd6ebd77b4568dd8d22a6fa441ab57b1a
cce-axelor <113428800+cce-axelor@users.noreply.github.com>
2024-10-01 15:18:09 +0200

    RM-84355 : add interest rate history (#13312)

--- axelor-account/src/main/java/com/axelor/apps/account/module/AccountModule.java
+++ axelor-account/src/main/java/com/axelor/apps/account/module/AccountModule.java
@@ -351,6 +351,8 @@ import com.axelor.apps.account.service.moveline.massentry.MoveLineMassEntryServi
 import com.axelor.apps.account.service.moveline.massentry.MoveLineMassEntryServiceImpl;
 import com.axelor.apps.account.service.notebills.NoteBillsCreateService;
 import com.axelor.apps.account.service.notebills.NoteBillsCreateServiceImpl;
+import com.axelor.apps.account.service.payment.PaymentModeInterestRateService;
+import com.axelor.apps.account.service.payment.PaymentModeInterestRateServiceImpl;
 import com.axelor.apps.account.service.payment.PaymentModeService;
 import com.axelor.apps.account.service.payment.PaymentModeServiceImpl;
 import com.axelor.apps.account.service.payment.PaymentService;
@@ -958,5 +960,6 @@ public class AccountModule extends AxelorModule {
     bind(ForeignExchangeGapToolService.class).to(ForeignExchangeGapToolServiceImpl.class);
 
     bind(LatePaymentInterestInvoiceService.class).to(LatePaymentInterestInvoiceServiceImpl.class);
+    bind(PaymentModeInterestRateService.class).to(PaymentModeInterestRateServiceImpl.class);
   }
 }
