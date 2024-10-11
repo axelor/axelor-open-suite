36937b626a1f1be02591b164a4baea3cb45b80ca
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2024-10-11 10:00:25 +0200

    RM#84373 Compute parent SOL unit price with SOL details

---
+++ axelor-production/src/main/java/com/axelor/apps/production/service/SaleOrderComputeServiceProductionImpl.java
@@ -0,0 +1,70 @@
+package com.axelor.apps.production.service;
+
+import com.axelor.apps.base.AxelorException;
+import com.axelor.apps.production.db.SaleOrderLineDetails;
+import com.axelor.apps.sale.db.SaleOrder;
+import com.axelor.apps.sale.db.SaleOrderLine;
+import com.axelor.apps.sale.service.saleorderline.SaleOrderLineComputeService;
+import com.axelor.apps.sale.service.saleorderline.pack.SaleOrderLinePackService;
+import com.axelor.apps.sale.service.saleorderline.subline.SubSaleOrderLineComputeService;
+import com.axelor.apps.sale.service.saleorderline.tax.SaleOrderLineCreateTaxLineService;
+import com.axelor.apps.supplychain.service.invoice.AdvancePaymentRefundService;
+import com.axelor.apps.supplychain.service.saleorder.SaleOrderComputeServiceSupplychainImpl;
+import com.google.inject.Inject;
+import java.math.BigDecimal;
+import java.util.List;
+import org.apache.commons.collections.CollectionUtils;
+
+public class SaleOrderComputeServiceProductionImpl extends SaleOrderComputeServiceSupplychainImpl {
+
+  @Inject
+  public SaleOrderComputeServiceProductionImpl(
+      SaleOrderLineCreateTaxLineService saleOrderLineCreateTaxLineService,
+      SaleOrderLineComputeService saleOrderLineComputeService,
+      SaleOrderLinePackService saleOrderLinePackService,
+      SubSaleOrderLineComputeService subSaleOrderLineComputeService,
+      AdvancePaymentRefundService refundService) {
+    super(
+        saleOrderLineCreateTaxLineService,
+        saleOrderLineComputeService,
+        saleOrderLinePackService,
+        subSaleOrderLineComputeService,
+        refundService);
+  }
+
+  @Override
+  public SaleOrder _computeSaleOrderLineList(SaleOrder saleOrder) throws AxelorException {
+
+    List<SaleOrderLine> saleOrderLineList = saleOrder.getSaleOrderLineList();
+
+    if (CollectionUtils.isEmpty(saleOrderLineList)) {
+      return saleOrder;
+    }
+
+    for (SaleOrderLine saleOrderLine : saleOrderLineList) {
+      List<SaleOrderLine> subSaleOrderLineList = saleOrderLine.getSubSaleOrderLineList();
+      List<SaleOrderLineDetails> saleOrderLineDetailsList =
+          saleOrderLine.getSaleOrderLineDetailsList();
+      BigDecimal totalPrice = BigDecimal.ZERO;
+      BigDecimal subDetailsTotal;
+      if (CollectionUtils.isNotEmpty(saleOrderLineDetailsList)) {
+        subDetailsTotal =
+            saleOrderLineDetailsList.stream()
+                .map(SaleOrderLineDetails::getTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        totalPrice = totalPrice.add(subDetailsTotal);
+        saleOrderLine.setPrice(totalPrice);
+      }
+      if (CollectionUtils.isNotEmpty(subSaleOrderLineList)) {
+        totalPrice =
+            totalPrice.add(
+                subSaleOrderLineComputeService.computeSumSubLineList(
+                    subSaleOrderLineList, saleOrder));
+        saleOrderLine.setPrice(totalPrice);
+      }
+      saleOrderLineComputeService.computeValues(saleOrder, saleOrderLine);
+    }
+
+    return saleOrder;
+  }
+}
