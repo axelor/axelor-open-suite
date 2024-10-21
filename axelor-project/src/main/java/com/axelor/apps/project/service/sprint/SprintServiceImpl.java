777c5ed0b9f47ad1cd065a797a15dd700c2426c7
p.shah <p.shah@axelor.com>
2024-10-21 11:40:08 +0530

    [Feature #85813] [Sprint] Add generator for sprint and periods

--- axelor-project/src/main/java/com/axelor/apps/project/service/sprint/SprintServiceImpl.java
+++ axelor-project/src/main/java/com/axelor/apps/project/service/sprint/SprintServiceImpl.java
@@ -18,13 +18,19 @@
  */
 package com.axelor.apps.project.service.sprint;
 
+import com.axelor.apps.base.db.Company;
 import com.axelor.apps.project.db.AllocationPeriod;
+import com.axelor.apps.project.db.Project;
 import com.axelor.apps.project.db.ProjectTask;
 import com.axelor.apps.project.db.Sprint;
 import com.axelor.apps.project.db.repo.ProjectTaskRepository;
+import com.axelor.apps.project.db.repo.SprintRepository;
+import com.axelor.i18n.I18n;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -32,12 +38,21 @@ import org.apache.commons.collections.CollectionUtils;
 
 public class SprintServiceImpl implements SprintService {
 
+  protected static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("MMM d");
+
   protected ProjectTaskRepository projectTaskRepo;
+  protected SprintRepository sprintRepo;
+  protected AllocationPeriodService allocationPeriodService;
 
   @Inject
-  public SprintServiceImpl(ProjectTaskRepository projectTaskRepo) {
+  public SprintServiceImpl(
+      ProjectTaskRepository projectTaskRepo,
+      SprintRepository sprintRepo,
+      AllocationPeriodService allocationPeriodService) {
 
     this.projectTaskRepo = projectTaskRepo;
+    this.sprintRepo = sprintRepo;
+    this.allocationPeriodService = allocationPeriodService;
   }
 
   @Override
@@ -82,5 +97,45 @@ public class SprintServiceImpl implements SprintService {
               task.setSprint(sprint);
               projectTaskRepo.save(task);
             });
+  }
+
+  @Override
+  public Set<Sprint> generateSprints(
+      Company company, Set<Project> projectSet, LocalDate fromDate, LocalDate toDate) {
+
+    Set<Sprint> sprintSet = new HashSet<>();
+
+    if (CollectionUtils.isNotEmpty(projectSet)) {
+      Set<AllocationPeriod> allocationPeriodSet =
+          allocationPeriodService.generateAllocationPeriods(company, fromDate, toDate, 1);
+
+      for (Project project : projectSet) {
+        sprintSet.add(createSprint(project, allocationPeriodSet, fromDate, toDate));
+      }
+    }
+
+    return sprintSet;
+  }
+
+  @Transactional(rollbackOn = Exception.class)
+  protected Sprint createSprint(
+      Project project,
+      Set<AllocationPeriod> allocationPeriodSet,
+      LocalDate fromDate,
+      LocalDate toDate) {
+
+    Sprint sprint = new Sprint();
+    sprint.setProject(project);
+    sprint.setAllocationPeriodSet(allocationPeriodSet);
+    sprint.setFromDate(fromDate);
+    sprint.setToDate(toDate);
+    sprint.setName(
+        I18n.get("Sprint")
+            + " "
+            + DATE_FORMATTER.format(fromDate)
+            + " - "
+            + DATE_FORMATTER.format(toDate));
+
+    return sprintRepo.save(sprint);
   }
 }
