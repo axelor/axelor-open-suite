d070843faf230af15842ad40f3e1f2da328c8d0f
bsu-axelor <b.suvagiya@axelor.com>
2024-10-28 18:01:54 +0530

    Anomaly RM#85304: PARTNER / ACCOUNTCONFIG _ 3 accounts are systematically created when partnerAccountGenerationModeSelect is 'prefix based' regardless of the partner's type
    (cherry picked from commit 60bf319ccf7524c61f0b712b51f460314a82a35c)

--- axelor-account/src/main/java/com/axelor/apps/account/service/AccountingSituationInitServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/AccountingSituationInitServiceImpl.java
@@ -152,7 +152,7 @@ public class AccountingSituationInitServiceImpl implements AccountingSituationIn
       AccountConfig accountConfig, AccountingSituation situation, int creationMode)
       throws AxelorException {
     Partner partner = situation.getPartner();
-    if (partner.getIsCustomer() == Boolean.FALSE || situation.getCustomerAccount() != null) return;
+    if (!partner.getIsCustomer() || situation.getCustomerAccount() != null) return;
 
     if (accountConfig.getCustomerAccount() == null) {
       throw new AxelorException(
@@ -208,7 +208,7 @@ public class AccountingSituationInitServiceImpl implements AccountingSituationIn
       AccountConfig accountConfig, AccountingSituation situation, int creationMode)
       throws AxelorException {
     Partner partner = situation.getPartner();
-    if (partner.getIsSupplier() == Boolean.FALSE || situation.getSupplierAccount() != null) return;
+    if (!partner.getIsSupplier() || situation.getSupplierAccount() != null) return;
 
     if (accountConfig.getSupplierAccount() == null) {
       throw new AxelorException(
@@ -265,7 +265,7 @@ public class AccountingSituationInitServiceImpl implements AccountingSituationIn
       AccountConfig accountConfig, AccountingSituation situation, int creationMode)
       throws AxelorException {
     Partner partner = situation.getPartner();
-    if (partner.getIsEmployee() == Boolean.FALSE || situation.getEmployeeAccount() != null) return;
+    if (!partner.getIsEmployee() || situation.getEmployeeAccount() != null) return;
 
     if (accountConfig.getEmployeeAccount() == null) {
       throw new AxelorException(
