ffe3497003f85c96ac1d81ee024eec7b964bee57
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2024-10-31 11:17:29 +0100

    spotless apply

--- axelor-account/src/main/java/com/axelor/apps/account/web/InvoiceController.java
+++ axelor-account/src/main/java/com/axelor/apps/account/web/InvoiceController.java
@@ -1055,11 +1055,7 @@ public class InvoiceController {
             .map(idList -> idList.stream().map(String::valueOf).collect(Collectors.joining(",")))
             .orElseGet(
                 () ->
-                    request
-                        .getCriteria()
-                        .createQuery(Invoice.class)
-                        .select("id")
-                        .fetch(0, 0)
+                    request.getCriteria().createQuery(Invoice.class).select("id").fetch(0, 0)
                         .stream()
                         .map(m -> (Long) m.get("id"))
                         .map(String::valueOf)
