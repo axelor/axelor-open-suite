4bff960ef8d7539e05701922537e88fe3ac7dff9
vva-axelor <v.vaghela@axelor.com>
2024-11-19 15:49:02 +0530

    Anomaly RM#86205:Stock Move: inline split should take into account the tracking number

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineServiceImpl.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineServiceImpl.java
@@ -1878,7 +1878,6 @@ public class StockMoveLineServiceImpl implements StockMoveLineService {
     BigDecimal realQty = fulfilledStockMoveLine.getRealQty();
     unfulfilledStockMoveLine.setQty(fulfilledStockMoveLine.getQty().subtract(realQty));
     fulfilledStockMoveLine.setQty(realQty);
-    unfulfilledStockMoveLine.setTrackingNumber(null);
     unfulfilledStockMoveLine.setTotalNetMass(BigDecimal.ZERO);
     unfulfilledStockMoveLine.setNetMass(BigDecimal.ZERO);
     unfulfilledStockMoveLine.setRealQty(BigDecimal.ZERO);
