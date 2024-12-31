98c3368541211250f0c0edecba8971c3f7622dba
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2024-12-31 13:27:50 +0100

    85930 - Tag : Take into account company and tradingName

--- axelor-base/src/main/java/com/axelor/apps/base/service/TagServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/TagServiceImpl.java
@@ -18,22 +18,30 @@
  */
 package com.axelor.apps.base.service;
 
+import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.Tag;
+import com.axelor.apps.base.db.TradingName;
+import com.axelor.apps.base.service.app.AppBaseService;
+import com.axelor.common.ObjectUtils;
 import com.axelor.common.StringUtils;
 import com.axelor.meta.MetaStore;
 import com.axelor.meta.db.MetaModel;
 import com.axelor.meta.db.repo.MetaModelRepository;
 import com.google.inject.Inject;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class TagServiceImpl implements TagService {
 
   protected MetaModelRepository metaModelRepository;
+  protected AppBaseService appBaseService;
 
   @Inject
-  public TagServiceImpl(MetaModelRepository metaModelRepository) {
+  public TagServiceImpl(MetaModelRepository metaModelRepository, AppBaseService appBaseService) {
     this.metaModelRepository = metaModelRepository;
+    this.appBaseService = appBaseService;
   }
 
   @Override
@@ -64,5 +72,62 @@ public class TagServiceImpl implements TagService {
     valuesMap.put("concernedModelSet", tag.getConcernedModelSet());
     valuesMap.put("color", tag.getColor());
     return valuesMap;
+  }
+
+  @Override
+  public String getTagDomain(String fullNameModel, Company company, TradingName tradingName) {
+    String domain = this.getConcernedModelTagDomain(fullNameModel);
+    Set<Company> companySet = new HashSet<>();
+    if (company != null) {
+      companySet.add(company);
+    }
+
+    domain = this.getCompanyTagDomain(domain, companySet, tradingName);
+    // this.getTradingNameTagDomain(domain, tradingName);
+
+    return domain;
+  }
+
+  @Override
+  public String getTagDomain(
+      String fullNameModel, Set<Company> companySet, TradingName tradingName) {
+    String domain = this.getConcernedModelTagDomain(fullNameModel);
+    domain = this.getCompanyTagDomain(domain, companySet, tradingName);
+    // this.getTradingNameTagDomain(domain, tradingName);
+
+    return domain;
+  }
+
+  protected String getConcernedModelTagDomain(String fullNameModel) {
+    return String.format(
+        "(self.concernedModelSet IS EMPTY OR %s member of self.concernedModelSet)",
+        metaModelRepository.findByName(fullNameModel).getId());
+  }
+
+  protected String getCompanyTagDomain(
+      String domain, Set<Company> companySet, TradingName tradingName) {
+    if (ObjectUtils.notEmpty(companySet)) {
+      domain = domain.concat(" AND (self.companySet IS EMPTY");
+      for (Company company : companySet) {
+        domain = domain.concat(String.format(" OR (%s member of self.companySet", company.getId()));
+        domain = this.getTradingNameTagDomain(domain, tradingName).concat(")");
+      }
+      domain = domain.concat(")");
+    } else {
+      domain = getTradingNameTagDomain(domain, tradingName);
+    }
+
+    return domain;
+  }
+
+  protected String getTradingNameTagDomain(String domain, TradingName tradingName) {
+    if (appBaseService.getAppBase().getEnableTradingNamesManagement() && tradingName != null) {
+      domain =
+          domain.concat(
+              String.format(
+                  " AND (self.tradingNameSet IS EMPTY OR %s member of self.tradingNameSet)",
+                  tradingName.getId()));
+    }
+    return domain;
   }
 }
