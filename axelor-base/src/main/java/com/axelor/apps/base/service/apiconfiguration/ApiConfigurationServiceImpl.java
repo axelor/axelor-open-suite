2dfc2ba01338f34d07d96fb851727ddd60fefe51
nbu-axelor <132373223+nbu-axelor@users.noreply.github.com>
2025-01-02 13:29:10 +0100

    Added error gestion for api response

--- axelor-base/src/main/java/com/axelor/apps/base/service/apiconfiguration/ApiConfigurationServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/apiconfiguration/ApiConfigurationServiceImpl.java
@@ -3,7 +3,9 @@ package com.axelor.apps.base.service.apiconfiguration;
 import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.ApiConfiguration;
 import com.axelor.apps.base.db.repo.TraceBackRepository;
+import com.axelor.apps.base.exceptions.BaseExceptionMessage;
 import com.axelor.common.StringUtils;
+import com.axelor.i18n.I18n;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -24,7 +26,21 @@ public class ApiConfigurationServiceImpl implements ApiConfigurationService {
     if (apiConfiguration == null || StringUtils.isEmpty(siretNumber)) {
       return StringUtil.EMPTY_STRING;
     }
+    siretNumber = cleanAndValidateSiret(siretNumber);
+    if (siretNumber == null) {
+      return I18n.get(BaseExceptionMessage.API_INVALID_SIRET_NUMBER);
+    }
     return getData(apiConfiguration, siretNumber);
+  }
+
+  protected String cleanAndValidateSiret(String siretNumber) {
+    siretNumber = siretNumber.replaceAll("\\s", "");
+
+    if (!siretNumber.matches("\\d{14}")) {
+      return null;
+    }
+
+    return siretNumber;
   }
 
   public String getData(ApiConfiguration apiConfiguration, String siretNumber)
@@ -45,21 +61,34 @@ public class ApiConfigurationServiceImpl implements ApiConfigurationService {
               .build();
 
       HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+      return treatResponse(response, siretNumber);
 
-      int statusCode = response.statusCode();
-      if (statusCode != 200) {
-        return "Cannot get information with siret: " + siretNumber + ".";
-      }
-      return new JSONObject(response.body()).get("etablissement").toString();
     } catch (URISyntaxException | IOException | JSONException e) {
-      throw new AxelorException(e, TraceBackRepository.CATEGORY_NO_VALUE);
+      throw new AxelorException(e, TraceBackRepository.CATEGORY_CONFIGURATION_ERROR);
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new AxelorException(e, TraceBackRepository.CATEGORY_INCONSISTENCY);
     }
   }
 
-  private String getUrl(ApiConfiguration apiConfiguration, String siretNumber) {
+  protected String getUrl(ApiConfiguration apiConfiguration, String siretNumber) {
     return apiConfiguration.getApiUrl() + "/siret/" + siretNumber;
+  }
+
+  protected String treatResponse(HttpResponse<String> response, String siretNumber)
+      throws JSONException {
+    int statusCode = response.statusCode();
+
+    switch (statusCode) {
+      case 200:
+        return new JSONObject(response.body()).get("etablissement").toString();
+      case 400:
+      case 404:
+        return I18n.get(BaseExceptionMessage.API_BAD_REQUEST);
+      case 401:
+        return I18n.get(BaseExceptionMessage.API_WRONG_CREDENTIALS);
+      default:
+        return String.format(I18n.get(BaseExceptionMessage.API_WRONG_SIRET_NUMBER), siretNumber);
+    }
   }
 }
