4a22105f11c23c665366bdd32318e13ffdb7269a
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2025-01-08 11:19:35 +0100

    Revert "RM#87173 Fill fiscal position for PO generated by MRP (#13740)"
    This reverts commit 5e912e159f9b983b0e255887abac24f989fc27b9.

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/MrpLineServiceImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/MrpLineServiceImpl.java
@@ -190,8 +190,7 @@ public class MrpLineServiceImpl implements MrpLineService {
                   Beans.get(PartnerPriceListService.class)
                       .getDefaultPriceList(supplierPartner, PriceListRepository.TYPE_PURCHASE),
                   supplierPartner,
-                  null,
-                  supplierPartner.getFiscalPosition()));
+                  null));
       if (isProposalsPerSupplier) {
         if (purchaseOrdersPerSupplier != null) {
           purchaseOrdersPerSupplier.put(supplierPartner, purchaseOrder);
