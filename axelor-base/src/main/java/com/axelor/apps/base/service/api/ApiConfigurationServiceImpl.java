264fac97b9587f455b638772524979d9f8a0236d
nbu-axelor <132373223+nbu-axelor@users.noreply.github.com>
2025-01-08 16:05:26 +0100

    Added some field filling

--- axelor-base/src/main/java/com/axelor/apps/base/service/api/ApiConfigurationServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/api/ApiConfigurationServiceImpl.java
@@ -1,8 +1,16 @@
 package com.axelor.apps.base.service.apiconfiguration;
 
 import com.axelor.apps.base.AxelorException;
+import com.axelor.apps.base.db.Address;
 import com.axelor.apps.base.db.ApiConfiguration;
+import com.axelor.apps.base.db.City;
+import com.axelor.apps.base.db.Country;
 import com.axelor.apps.base.db.Partner;
+import com.axelor.apps.base.db.PartnerAddress;
+import com.axelor.apps.base.db.PartnerCategory;
+import com.axelor.apps.base.db.repo.CityRepository;
+import com.axelor.apps.base.db.repo.CountryRepository;
+import com.axelor.apps.base.db.repo.PartnerCategoryRepository;
 import com.axelor.apps.base.db.repo.PartnerRepository;
 import com.axelor.apps.base.db.repo.TraceBackRepository;
 import com.axelor.common.StringUtils;
@@ -15,6 +23,8 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Objects;
+import java.util.function.Consumer;
 import javax.ws.rs.core.MediaType;
 import org.apache.http.HttpHeaders;
 import org.eclipse.birt.report.model.api.util.StringUtil;
@@ -24,19 +34,105 @@ import wslite.json.JSONObject;
 public class ApiConfigurationServiceImpl implements ApiConfigurationService {
 
   protected final PartnerRepository partnerRepository;
+  protected final PartnerCategoryRepository partnerCategoryRepository;
+  protected final CountryRepository countryRepository;
+  protected final CityRepository cityRepository;
 
   @Inject
-  public ApiConfigurationServiceImpl(PartnerRepository partnerRepository) {
+  public ApiConfigurationServiceImpl(
+          PartnerRepository partnerRepository,
+          PartnerCategoryRepository partnerCategoryRepository, CountryRepository countryRepository,
+          CityRepository cityRepository) {
     this.partnerRepository = partnerRepository;
+    this.partnerCategoryRepository = partnerCategoryRepository;
+    this.countryRepository = countryRepository;
+    this.cityRepository = cityRepository;
   }
 
   @Transactional
   @Override
   public void setData(Partner partner, JSONObject resutlJson) throws JSONException {
-    partner.setRegistrationCode(resutlJson.get("siren").toString());
-    partner.setName(
-        resutlJson.getJSONObject("uniteLegale").get("denominationUniteLegale").toString());
+    String registrationCode = getSafeString(resutlJson, "siret");
+    safeSet(partner::setRegistrationCode, registrationCode);
+    JSONObject jsonUniteLegal = resutlJson.optJSONObject("uniteLegale");
+
+    if (jsonUniteLegal != null) {
+      String partnerCategoryCode = getSafeString(jsonUniteLegal, "categorieEntreprise");
+      PartnerCategory partnerCategory = partnerCategoryRepository.findByCode(partnerCategoryCode);
+      if(partnerCategory != null){
+        partner.setPartnerCategory(partnerCategory);
+      }
+      String categorieJuridique = getSafeString(jsonUniteLegal, "categorieJuridiqueUniteLegale");
+      if (categorieJuridique != null && Integer.parseInt(categorieJuridique) == 1000) {
+        partner.setPartnerTypeSelect(PartnerRepository.PARTNER_TYPE_INDIVIDUAL);
+        String name = getSafeString(jsonUniteLegal, "nomUniteLegale");
+        safeSet(partner::setName, name);
+        String firstName = getSafeString(jsonUniteLegal, "prenom1UniteLegale");
+        safeSet(partner::setFirstName, firstName);
+        String sexUniteLegale = getSafeString(jsonUniteLegal, "sexUniteLegale");
+        if(Objects.equals(sexUniteLegale, "F")){
+          partner.setTitleSelect(PartnerRepository.PARTNER_TITLE_MS);
+        } else if (Objects.equals(sexUniteLegale, "M")) {
+          partner.setTitleSelect(PartnerRepository.PARTNER_TITLE_M);
+        }
+      } else {
+        partner.setPartnerTypeSelect(PartnerRepository.PARTNER_TYPE_COMPANY);
+        String name = getSafeString(jsonUniteLegal, "denominationUniteLegale");
+        safeSet(partner::setName, name);
+      }
+    }
+
+    JSONObject jsonAddresseEtablissement = resutlJson.optJSONObject("adresseEtablissement");
+    if (jsonAddresseEtablissement != null) {
+      Address address = new Address();
+      String zip = getSafeString(jsonAddresseEtablissement, "codePostalEtablissement");
+      safeSet(address::setZip, zip);
+      String floor = getSafeString(jsonAddresseEtablissement, "complementAdresseEtablissement");
+      safeSet(address::setFloor, floor);
+      String postBox =
+          getSafeString(jsonAddresseEtablissement, "distributionSpecialeEtablissement");
+      safeSet(address::setPostBox, postBox);
+      String departement = getSafeString(jsonAddresseEtablissement, "enseigne1Etablissement");
+      safeSet(address::setDepartment, departement);
+      Country currentCountry = countryRepository.findByName("FRANCE");
+      if (currentCountry != null) {
+        address.setCountry(currentCountry);
+      }
+      String cityName = getSafeString(jsonAddresseEtablissement, "libelleCommuneEtablissement");
+      City currentCity = cityRepository.findByName(cityName);
+      if (currentCity != null) {
+        address.setCity(currentCity);
+      }
+      String streetName = getSafeString(jsonAddresseEtablissement, "libelleVoieEtablissement");
+      safeSet(address::setStreetName, streetName);
+      safeSet(address::setFullName, streetName + zip + cityName);
+      PartnerAddress partnerAddress = new PartnerAddress();
+      if(address.getStreetName() != null && address.getCity() != null && address.getZip() != null){
+        partnerAddress.setPartner(partner);
+        partnerAddress.setAddress(address);
+      }
+      if(partnerAddress.getAddress() != null){
+        partner.addPartnerAddressListItem(partnerAddress);
+      }
+    }
     partnerRepository.save(partner);
+  }
+
+  private void safeSet(Consumer<String> setter, String value) {
+    if (value != null) {
+      setter.accept(value);
+    }
+  }
+
+  private String getSafeString(JSONObject jsonObject, String key) {
+    try {
+      if (jsonObject.has(key) && !jsonObject.isNull(key)) {
+        return jsonObject.getString(key);
+      }
+    } catch (JSONException e) {
+      System.err.println("Error retrieving key '" + key + "': " + e.getMessage());
+    }
+    return null;
   }
 
   @Override
