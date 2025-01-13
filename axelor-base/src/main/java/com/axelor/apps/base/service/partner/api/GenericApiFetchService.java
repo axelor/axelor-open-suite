8c39123919433ec5993e5f4ed4acd18517a68d35
nbu-axelor <132373223+nbu-axelor@users.noreply.github.com>
2025-01-13 16:57:06 +0100

    Remove useless method implementation

--- axelor-base/src/main/java/com/axelor/apps/base/service/partner/api/GenericApiFetchService.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/partner/api/GenericApiFetchService.java
@@ -3,8 +3,6 @@ package com.axelor.apps.base.service.partner.api;
 import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.PartnerApiConfiguration;
 import com.axelor.apps.base.db.repo.TraceBackRepository;
-import com.axelor.apps.base.exceptions.BaseExceptionMessage;
-import com.axelor.i18n.I18n;
 import com.google.common.net.HttpHeaders;
 import java.io.IOException;
 import java.net.URI;
@@ -16,34 +14,18 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.ws.rs.core.MediaType;
 import wslite.json.JSONException;
-import wslite.json.JSONObject;
 
 public abstract class GenericApiFetchService {
 
-  public abstract String fetch(PartnerApiConfiguration configuration, String identifier)
+  protected abstract String fetch(PartnerApiConfiguration configuration, String identifier)
       throws AxelorException;
 
   protected String getUrl(PartnerApiConfiguration configuration, String identifier) {
     return configuration.getApiUrl() + "/" + identifier;
   }
 
-  protected String treatResponse(HttpResponse<String> response, String identifier)
-      throws JSONException {
-    int statusCode = response.statusCode();
-
-    switch (statusCode) {
-      case 200:
-        return new JSONObject(response.body()).toString();
-      case 400:
-        case 404:
-          return I18n.get(BaseExceptionMessage.API_BAD_REQUEST);
-      case 401:
-        return I18n.get(BaseExceptionMessage.API_WRONG_CREDENTIALS);
-      default:
-        return String.format(
-            I18n.get(BaseExceptionMessage.API_ERROR_IN_REQUEST_FOR_IDENTIFIER), identifier);
-    }
-  }
+  protected abstract String treatResponse(HttpResponse<String> response, String identifier)
+      throws JSONException;
 
   protected Map<String, String> getHeaders(PartnerApiConfiguration configuration) {
     Map<String, String> headers = new HashMap<>();
