19524e70c4c7c50eb7e21f4e792c7f11d8e6ff83
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2025-01-14 14:18:15 +0100

    RM#86243 API Request to compute leave date for a specific date

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/translation/ITranslation.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/translation/ITranslation.java
@@ -109,4 +109,7 @@ public interface ITranslation {
       "Leave request(s) have been correctly created. Some requests were ignored as they were not correctly configured." /*)*/;
 
   String API_LEAVE_REQUEST_COMPUTE_DURATION = /*$$(*/ "Duration computed." /*)*/;
+
+  String API_LEAVE_REQUEST_LEAVE_DAYS_TO_DATE_COMPUTATION = /*$$(*/
+      "Available leave days computed." /*)*/;
 }
