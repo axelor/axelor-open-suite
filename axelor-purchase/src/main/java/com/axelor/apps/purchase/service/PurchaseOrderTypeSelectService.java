e940add8395079ac39aeb1d9eda66fdc7ab3f46b
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2025-01-22 15:00:08 +0100

    88552: Add new Type for purchase order for standard and subcontract (#13787)
    * 88552: Add new Type for purchase order for standard and subcontract

---
+++ axelor-purchase/src/main/java/com/axelor/apps/purchase/service/PurchaseOrderTypeSelectService.java
@@ -0,0 +1,8 @@
+package com.axelor.apps.purchase.service;
+
+import com.axelor.apps.purchase.db.PurchaseOrder;
+
+public interface PurchaseOrderTypeSelectService {
+
+  void setTypeSelect(PurchaseOrder purchaseOrder);
+}
