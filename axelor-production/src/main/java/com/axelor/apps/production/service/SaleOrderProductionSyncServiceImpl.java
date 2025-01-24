12c23b6b498d24f3c2df284e556c6862522e9746
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2025-01-24 17:54:11 +0100

    Spotless

--- axelor-production/src/main/java/com/axelor/apps/production/service/SaleOrderProductionSyncServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/SaleOrderProductionSyncServiceImpl.java
@@ -19,7 +19,6 @@
 package com.axelor.apps.production.service;
 
 import com.axelor.apps.base.AxelorException;
-import com.axelor.apps.production.db.BillOfMaterial;
 import com.axelor.apps.production.service.app.AppProductionService;
 import com.axelor.apps.sale.db.SaleOrder;
 import com.axelor.apps.sale.db.SaleOrderLine;
