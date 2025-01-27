47e1326d1824728d97275f384420b17ccb69db96
vva-axelor <v.vaghela@axelor.com>
2025-01-27 15:50:10 +0530

    Feature RM#90093:Sale order: Add check on delivery address in command lines before invoicing

--- axelor-sale/src/main/java/com/axelor/apps/sale/service/saleorder/SaleOrderDeliveryAddressService.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/service/saleorder/SaleOrderDeliveryAddressService.java
@@ -1,5 +1,6 @@
 package com.axelor.apps.sale.service.saleorder;
 
+import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.Address;
 import com.axelor.apps.sale.db.SaleOrder;
 import com.axelor.apps.sale.db.SaleOrderLine;
@@ -10,4 +11,7 @@ public interface SaleOrderDeliveryAddressService {
   List<SaleOrderLine> updateSaleOrderLinesDeliveryAddress(SaleOrder saleOrder);
 
   Address getDeliveryAddress(SaleOrder saleOrder, List<SaleOrderLine> saleOrderLineList);
+
+  void checkSaleOrderLinesDeliveryAddress(List<SaleOrderLine> saleOrderLineList)
+      throws AxelorException;
 }
