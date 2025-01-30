362acf7ffb531e2892c6f239c89d293dce3aa7fd
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2025-01-30 14:48:32 +0100

    87215 - Remove useless code

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceLineServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceLineServiceImpl.java
@@ -785,31 +785,4 @@ public class InvoiceLineServiceImpl implements InvoiceLineService {
     valuesMap.put("taxEquiv", taxEquiv);
     return valuesMap;
   }
-
-  public void checkTaxLinesNotOnlyNonDeductibleTaxes(List<InvoiceLine> invoiceLineList)
-      throws AxelorException {
-    if (CollectionUtils.isEmpty(invoiceLineList)) {
-      return;
-    }
-
-    // split in for loop, catch the exception, and throw another exception with the specific account
-    taxAccountService.checkTaxLinesNotOnlyNonDeductibleTaxes(
-        invoiceLineList.stream()
-            .map(InvoiceLine::getTaxLineSet)
-            .flatMap(Set::stream)
-            .collect(Collectors.toSet()));
-  }
-
-  public void checkSumOfNonDeductibleTaxes(List<InvoiceLine> invoiceLineList)
-      throws AxelorException {
-    if (CollectionUtils.isEmpty(invoiceLineList)) {
-      return;
-    }
-
-    taxAccountService.checkSumOfNonDeductibleTaxes(
-        invoiceLineList.stream()
-            .map(InvoiceLine::getTaxLineSet)
-            .flatMap(Set::stream)
-            .collect(Collectors.toSet()));
-  }
 }
