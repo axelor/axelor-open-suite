8f8e753724b964d290d1580118ae3af96ee69dcb
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2025-02-05 17:27:40 +0100

    84583 Assistant button to delete status from TaskCategories

--- axelor-project/src/main/java/com/axelor/apps/project/service/batch/ProjectBatchInitService.java
+++ axelor-project/src/main/java/com/axelor/apps/project/service/batch/ProjectBatchInitService.java
@@ -2,10 +2,16 @@ package com.axelor.apps.project.service.batch;
 
 import com.axelor.apps.project.db.Project;
 import com.axelor.apps.project.db.ProjectBatch;
+import com.axelor.apps.project.db.ProjectTaskCategory;
 import com.axelor.apps.project.db.TaskStatus;
 import java.util.Set;
 
 public interface ProjectBatchInitService {
-  ProjectBatch initializeProjectBatch(
+  ProjectBatch initializeProjectBatchWithProjects(
       Integer actionSelect, Set<Project> projectSet, Set<TaskStatus> taskStatusSet);
+
+  ProjectBatch initializeProjectBatchWithCategories(
+      Integer actionSelect,
+      Set<ProjectTaskCategory> projectTaskCategorySet,
+      Set<TaskStatus> taskStatusSet);
 }
