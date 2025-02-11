c21d7ac3a9496c194c2bbf535c49a622cc511dbe
vva-axelor <v.vaghela@axelor.com>
2025-02-11 12:19:37 +0530

    Anomaly RM#90818:SIRET duplicate check : Add partner full name on the error

--- axelor-base/src/main/java/com/axelor/apps/base/web/PartnerController.java
+++ axelor-base/src/main/java/com/axelor/apps/base/web/PartnerController.java
@@ -47,6 +47,7 @@ import com.axelor.apps.base.service.user.UserService;
 import com.axelor.auth.AuthUtils;
 import com.axelor.auth.db.User;
 import com.axelor.common.ObjectUtils;
+import com.axelor.common.StringUtils;
 import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
 import com.axelor.message.db.Message;
@@ -480,10 +481,10 @@ public class PartnerController {
 
   public void checkIfRegistrationCodeExists(ActionRequest request, ActionResponse response) {
     Partner partner = request.getContext().asType(Partner.class);
-    if (!Beans.get(PartnerService.class).checkIfRegistrationCodeExists(partner)) {
+    String message = Beans.get(PartnerService.class).checkIfRegistrationCodeExists(partner);
+    if (StringUtils.isEmpty(message)) {
       return;
     }
-    String message = I18n.get(BaseExceptionMessage.PARTNER_REGISTRATION_CODE_ALREADY_EXISTS);
     if (Beans.get(AppBaseService.class).getAppBase().getIsRegistrationCodeCheckBlocking()) {
       response.setError(message);
     } else {
