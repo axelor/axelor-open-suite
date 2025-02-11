a6e82c1e27c1abe7b1451fde5206a6b08444e227
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2025-02-11 16:22:02 +0100

    RM#88925 Improve service readability

--- axelor-production/src/main/java/com/axelor/apps/production/service/SaleOrderLineDummyProductionServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/SaleOrderLineDummyProductionServiceImpl.java
@@ -5,6 +5,7 @@ import com.axelor.apps.base.service.ProductMultipleQtyService;
 import com.axelor.apps.base.service.app.AppBaseService;
 import com.axelor.apps.production.db.ManufOrder;
 import com.axelor.apps.production.db.repo.ManufOrderRepository;
+import com.axelor.apps.sale.db.ProductionStatusSelect;
 import com.axelor.apps.sale.db.SaleOrder;
 import com.axelor.apps.sale.db.SaleOrderLine;
 import com.axelor.apps.sale.service.app.AppSaleService;
@@ -23,13 +24,7 @@ import org.apache.commons.collections.CollectionUtils;
 public class SaleOrderLineDummyProductionServiceImpl
     extends SaleOrderLineDummySupplychainServiceImpl {
 
-  protected final String DUMMY_PRODUCTION_STATUS = "$productionStatus";
-  protected final String PRODUCTION_STATUS_FINISHED = "FINISHED";
-  protected final String PRODUCTION_STATUS_STANDBY = "STANDBY";
-  protected final String PRODUCTION_STATUS_IN_PROGRESS = "IN_PROGRESS";
-  protected final String PRODUCTION_STATUS_PLANNED = "PLANNED";
-  protected final String PRODUCTION_STATUS_CANCELED = "CANCELED";
-  protected final String PRODUCTION_STATUS_DRAFT = "DRAFT";
+  protected static final String DUMMY_PRODUCTION_STATUS = "$productionStatus";
 
   @Inject
   public SaleOrderLineDummyProductionServiceImpl(
@@ -67,55 +62,133 @@ public class SaleOrderLineDummyProductionServiceImpl
     }
     List<Integer> statusSelectList =
         manufOrderList.stream().map(ManufOrder::getStatusSelect).collect(Collectors.toList());
+
+    dummyFields.putAll(getCanceledStatusSelect(statusSelectList));
+    if (dummyFields.get(DUMMY_PRODUCTION_STATUS) != null) {
+      return dummyFields;
+    }
+
+    dummyFields.putAll(getDraftStatusSelect(statusSelectList));
+    if (dummyFields.get(DUMMY_PRODUCTION_STATUS) != null) {
+      return dummyFields;
+    }
+
+    dummyFields.putAll(getAllStandByStatusSelect(statusSelectList));
+    if (dummyFields.get(DUMMY_PRODUCTION_STATUS) != null) {
+      return dummyFields;
+    }
+
+    dummyFields.putAll(getFinishedStatusSelect(statusSelectList));
+    if (dummyFields.get(DUMMY_PRODUCTION_STATUS) != null) {
+      return dummyFields;
+    }
+
+    dummyFields.putAll(getInProgressStatusSelect(statusSelectList));
+    if (dummyFields.get(DUMMY_PRODUCTION_STATUS) != null) {
+      return dummyFields;
+    }
+
+    dummyFields.putAll(getPlannedStatusSelect(statusSelectList));
+    if (dummyFields.get(DUMMY_PRODUCTION_STATUS) != null) {
+      return dummyFields;
+    }
+
+    dummyFields.putAll(getAnyStandByStatusSelect(statusSelectList));
+    if (dummyFields.get(DUMMY_PRODUCTION_STATUS) != null) {
+      return dummyFields;
+    }
+
+    dummyFields.putAll(getAnyDraftStatusSelect(statusSelectList));
+    if (dummyFields.get(DUMMY_PRODUCTION_STATUS) != null) {
+      return dummyFields;
+    }
+
+    return dummyFields;
+  }
+
+  protected Map<String, Object> getCanceledStatusSelect(List<Integer> statusSelectList) {
+    Map<String, Object> dummyFields = new HashMap<>();
     if (statusSelectList.stream()
         .allMatch(status -> status == ManufOrderRepository.STATUS_CANCELED)) {
-      dummyFields.put(DUMMY_PRODUCTION_STATUS, PRODUCTION_STATUS_CANCELED);
-      return dummyFields;
+      dummyFields.put(
+          DUMMY_PRODUCTION_STATUS, ProductionStatusSelect.PRODUCTION_STATUS_CANCELED.getValue());
     }
+    return dummyFields;
+  }
+
+  protected Map<String, Object> getDraftStatusSelect(List<Integer> statusSelectList) {
+    Map<String, Object> dummyFields = new HashMap<>();
     if (statusSelectList.stream().allMatch(status -> status == ManufOrderRepository.STATUS_DRAFT)) {
-      dummyFields.put(DUMMY_PRODUCTION_STATUS, PRODUCTION_STATUS_DRAFT);
-      return dummyFields;
+      dummyFields.put(
+          DUMMY_PRODUCTION_STATUS, ProductionStatusSelect.PRODUCTION_STATUS_DRAFT.getValue());
     }
+    return dummyFields;
+  }
+
+  protected Map<String, Object> getAllStandByStatusSelect(List<Integer> statusSelectList) {
+    Map<String, Object> dummyFields = new HashMap<>();
     if (statusSelectList.stream()
         .allMatch(status -> status == ManufOrderRepository.STATUS_STANDBY)) {
-      dummyFields.put(DUMMY_PRODUCTION_STATUS, PRODUCTION_STATUS_STANDBY);
-      return dummyFields;
+      dummyFields.put(
+          DUMMY_PRODUCTION_STATUS, ProductionStatusSelect.PRODUCTION_STATUS_CANCELED.getValue());
     }
+    return dummyFields;
+  }
+
+  protected Map<String, Object> getFinishedStatusSelect(List<Integer> statusSelectList) {
+    Map<String, Object> dummyFields = new HashMap<>();
     if (statusSelectList.stream()
         .allMatch(
             status ->
                 status == ManufOrderRepository.STATUS_FINISHED
                     || status == ManufOrderRepository.STATUS_CANCELED
                     || status == ManufOrderRepository.STATUS_MERGED)) {
-      dummyFields.put(DUMMY_PRODUCTION_STATUS, PRODUCTION_STATUS_FINISHED);
-      return dummyFields;
+      dummyFields.put(
+          DUMMY_PRODUCTION_STATUS, ProductionStatusSelect.PRODUCTION_STATUS_FINISHED.getValue());
     }
+    return dummyFields;
+  }
 
+  protected Map<String, Object> getInProgressStatusSelect(List<Integer> statusSelectList) {
+    Map<String, Object> dummyFields = new HashMap<>();
     if (statusSelectList.stream()
             .anyMatch(status -> status == ManufOrderRepository.STATUS_IN_PROGRESS)
         || statusSelectList.stream()
             .anyMatch(status -> status == ManufOrderRepository.STATUS_FINISHED)
         || statusSelectList.stream()
             .anyMatch(status -> status == ManufOrderRepository.STATUS_STANDBY)) {
-      dummyFields.put(DUMMY_PRODUCTION_STATUS, PRODUCTION_STATUS_IN_PROGRESS);
-      return dummyFields;
+      dummyFields.put(
+          DUMMY_PRODUCTION_STATUS, ProductionStatusSelect.PRODUCTION_STATUS_IN_PROGRESS.getValue());
     }
+    return dummyFields;
+  }
 
+  protected Map<String, Object> getPlannedStatusSelect(List<Integer> statusSelectList) {
+    Map<String, Object> dummyFields = new HashMap<>();
     if (statusSelectList.stream()
         .anyMatch(status -> status == ManufOrderRepository.STATUS_PLANNED)) {
-      dummyFields.put(DUMMY_PRODUCTION_STATUS, PRODUCTION_STATUS_PLANNED);
-      return dummyFields;
+      dummyFields.put(
+          DUMMY_PRODUCTION_STATUS, ProductionStatusSelect.PRODUCTION_STATUS_PLANNED.getValue());
     }
+    return dummyFields;
+  }
+
+  protected Map<String, Object> getAnyStandByStatusSelect(List<Integer> statusSelectList) {
+    Map<String, Object> dummyFields = new HashMap<>();
     if (statusSelectList.stream()
         .anyMatch(status -> status == ManufOrderRepository.STATUS_STANDBY)) {
-      dummyFields.put(DUMMY_PRODUCTION_STATUS, PRODUCTION_STATUS_STANDBY);
-      return dummyFields;
+      dummyFields.put(
+          DUMMY_PRODUCTION_STATUS, ProductionStatusSelect.PRODUCTION_STATUS_STANDBY.getValue());
     }
-    if (statusSelectList.stream().anyMatch(status -> status == ManufOrderRepository.STATUS_DRAFT)) {
-      dummyFields.put(DUMMY_PRODUCTION_STATUS, PRODUCTION_STATUS_DRAFT);
-      return dummyFields;
-    }
+    return dummyFields;
+  }
 
+  protected Map<String, Object> getAnyDraftStatusSelect(List<Integer> statusSelectList) {
+    Map<String, Object> dummyFields = new HashMap<>();
+    if (statusSelectList.stream().anyMatch(status -> status == ManufOrderRepository.STATUS_DRAFT)) {
+      dummyFields.put(
+          DUMMY_PRODUCTION_STATUS, ProductionStatusSelect.PRODUCTION_STATUS_DRAFT.getValue());
+    }
     return dummyFields;
   }
 }
