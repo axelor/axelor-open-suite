7ddd2246f7c874f5a54ac88233a2077e47431e23
Majda EL ABDI <m.elabdi@axelor.com>
2025-02-17 18:18:03 +0100

    RM-90648 compute allocation time

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/allocation/AllocationLineComputeServiceImpl.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/allocation/AllocationLineComputeServiceImpl.java
@@ -30,6 +30,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class AllocationLineComputeServiceImpl implements AllocationLineComputeService {
 
