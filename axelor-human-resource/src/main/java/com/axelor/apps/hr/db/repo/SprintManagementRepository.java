d791a72d910e793d0f7f5a146eaabedd34e96ba6
Majda EL ABDI <m.elabdi@axelor.com>
2025-02-17 18:50:58 +0100

    RM-90648 improve code

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/db/repo/SprintManagementRepository.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/db/repo/SprintManagementRepository.java
@@ -16,12 +16,15 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package com.axelor.apps.project.db.repo;
+package com.axelor.apps.hr.db.repo;
 
 import com.axelor.apps.base.service.exception.TraceBackService;
+import com.axelor.apps.hr.service.allocation.AllocationLineComputeService;
 import com.axelor.apps.project.db.Project;
 import com.axelor.apps.project.db.ProjectTask;
 import com.axelor.apps.project.db.Sprint;
+import com.axelor.apps.project.db.repo.ProjectRepository;
+import com.axelor.apps.project.db.repo.SprintRepository;
 import com.axelor.apps.project.service.dashboard.ProjectManagementDashboardService;
 import com.axelor.inject.Beans;
 import com.google.inject.Inject;
@@ -32,24 +35,31 @@ public class SprintManagementRepository extends SprintRepository {
 
   ProjectRepository projectRepository;
   ProjectManagementDashboardService projectManagementDashboardService;
+  AllocationLineComputeService allocationLineComputeService;
 
   @Inject
   public SprintManagementRepository(
       ProjectRepository projectRepository,
-      ProjectManagementDashboardService projectManagementDashboardService) {
+      ProjectManagementDashboardService projectManagementDashboardService,
+      AllocationLineComputeService allocationLineComputeService) {
     this.projectRepository = projectRepository;
     this.projectManagementDashboardService = projectManagementDashboardService;
+    this.allocationLineComputeService = allocationLineComputeService;
   }
 
   @Override
   public Map<String, Object> populate(Map<String, Object> json, Map<String, Object> context) {
     try {
-      Long projectId = Long.valueOf(((Map) context.get("importFile")).get("id").toString());
+      Long projectId = Long.valueOf(((Map) context.get("project")).get("id").toString());
      Project project= projectRepository.find(projectId);
       Long sprintId = (Long) json.get("id");
       Sprint sprint = Beans.get(SprintRepository.class).find(sprintId);
+
       final String totalEstimatedTime = "$totalEstimatedTime";
+      final String totalAllocatedTime = "$totalAllocatedTime";
+      BigDecimal allocatedTime = allocationLineComputeService.getAllocatedTime(project, sprint);
       BigDecimal budgetedTime = getBudgetedTime(sprint);
+   json.put(totalAllocatedTime, allocatedTime);
       json.put(totalEstimatedTime, budgetedTime);
 
     } catch (Exception e) {
