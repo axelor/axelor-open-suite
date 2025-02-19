0d49d42a6261cd22d6a8068dc518f888b7d3465f
Majda EL ABDI <m.elabdi@axelor.com>
2025-02-19 17:22:29 +0100

    RM-90648 Use Allocation line employee

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/allocation/AllocationLineComputeServiceImpl.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/allocation/AllocationLineComputeServiceImpl.java
@@ -279,7 +279,9 @@ public class AllocationLineComputeServiceImpl implements AllocationLineComputeSe
 
   @Override
   public BigDecimal getAllocatedTime(Project project, Sprint sprint) {
-    List<AllocationLine> allocationLineList = allocationLineRepo.findByProject(project).fetch();
+
+    List<AllocationLine> allocationLineList =
+        allocationLineRepo.findByProject(project).fetch();
     BigDecimal allocatedTime = BigDecimal.ZERO;
     if (ObjectUtils.isEmpty(allocationLineList)) {
       return allocatedTime;
@@ -309,7 +311,7 @@ public class AllocationLineComputeServiceImpl implements AllocationLineComputeSe
       BigDecimal sprintPeriod = BigDecimal.ZERO;
       if (sprintFromDate.isAfter(allocationLineFromDate)
           && sprintToDate.isBefore(allocationLineToDate)) {
-        sprintPeriod = getWorkingDays(sprintFromDate, sprintToDate, employee);
+        sprintPeriod = getWorkingDays(sprintFromDate, sprintToDate, allocationLine.getEmployee());
       }
 
       if (!sprintFromDate.isAfter(allocationLineFromDate)
