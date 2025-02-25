275b07db74938e6d1e2560058fbec8ca5c4fdb44
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2025-02-25 16:08:40 +0100

    fixup! 91006: Check configurator is activated (#14008)

--- axelor-sale/src/main/java/com/axelor/apps/sale/service/configurator/ConfiguratorCheckServiceImpl.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/service/configurator/ConfiguratorCheckServiceImpl.java
@@ -59,9 +59,9 @@ public class ConfiguratorCheckServiceImpl implements ConfiguratorCheckService {
   @Override
   public void checkConfiguratorActivated(Configurator configurator) throws AxelorException {
     if (configurator != null && !configurator.getConfiguratorCreator().getIsActive()) {
-        throw new AxelorException(
-            TraceBackRepository.CATEGORY_INCONSISTENCY,
-            I18n.get(SaleExceptionMessage.CONFIGURATOR_IS_NOT_ACTIVATED));
-      }
+      throw new AxelorException(
+          TraceBackRepository.CATEGORY_INCONSISTENCY,
+          I18n.get(SaleExceptionMessage.CONFIGURATOR_IS_NOT_ACTIVATED));
+    }
   }
 }
