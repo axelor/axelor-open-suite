6e17194fe12f7d733af2d95a83bd661ec4f0492c
vva-axelor <v.vaghela@axelor.com>
2025-03-10 19:05:46 +0530

    Anomaly RM#91664:PROJECT / INVOICE : Fiscal position not set on invoices generated from Business Project
    (cherry picked from commit 3f4ff47e8202fa5c49b6242eaa278491d2de1de5)

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/ProjectGenerateInvoiceServiceImpl.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/ProjectGenerateInvoiceServiceImpl.java
@@ -200,6 +200,7 @@ public class ProjectGenerateInvoiceServiceImpl implements ProjectGenerateInvoice
         Invoice invoice = super.createInvoiceHeader();
         invoice.setProject(project);
         invoice.setPriceList(project.getPriceList());
+        invoice.setFiscalPosition(invoicedPartner.getFiscalPosition());
         return invoice;
       }
     };
