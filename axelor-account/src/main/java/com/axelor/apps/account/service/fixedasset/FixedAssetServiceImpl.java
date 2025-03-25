b88ba460b3c4f48dc347c2abf2c4e948602c29d1
vva-axelor <v.vaghela@axelor.com>
2025-03-25 11:17:18 +0530

    Anomaly RM#91748:FIXEDASSET : null value on moveline for ongoing cession
    (cherry picked from commit 738422e2ac4214c3db7a9eaa08c9cf0466272098)

--- axelor-account/src/main/java/com/axelor/apps/account/service/fixedasset/FixedAssetServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/fixedasset/FixedAssetServiceImpl.java
@@ -178,7 +178,7 @@ public class FixedAssetServiceImpl implements FixedAssetService {
       }
     }
 
-    setDisposalFields(fixedAsset, disposalDate, BigDecimal.ZERO, transferredReason);
+    setDisposalFields(fixedAsset, disposalDate, disposalAmount, transferredReason);
     fixedAssetRepo.save(fixedAsset);
     return depreciationFixedAssetLine;
   }
