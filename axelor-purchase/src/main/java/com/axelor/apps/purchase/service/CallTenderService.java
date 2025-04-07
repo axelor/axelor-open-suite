372ce1e1b24c02d34b016b6c93a674b1ef207315
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2025-04-07 18:05:47 +0200

    92972: save

--- axelor-purchase/src/main/java/com/axelor/apps/purchase/service/CallTenderService.java
+++ axelor-purchase/src/main/java/com/axelor/apps/purchase/service/CallTenderService.java
@@ -1,8 +1,13 @@
 package com.axelor.apps.purchase.service;
 
+import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.purchase.db.CallTender;
+
+import java.io.IOException;
 
 public interface CallTenderService {
 
   void generateCallTenderOffers(CallTender callTender);
+
+  void sendCallTenderOffers(CallTender callTender) throws AxelorException, IOException;
 }
