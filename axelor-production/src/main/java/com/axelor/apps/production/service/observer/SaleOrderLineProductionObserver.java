2ccfe4bf2e96151cf93164b5c3a48ef5cc4258f7
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2025-04-10 16:05:53 +0200

    Sale order: fixed NPE when opening sale order line form view
    RM93658

--- axelor-production/src/main/java/com/axelor/apps/production/service/observer/SaleOrderLineProductionObserver.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/observer/SaleOrderLineProductionObserver.java
@@ -43,7 +43,7 @@ public class SaleOrderLineProductionObserver {
     Map<String, Map<String, Object>> saleOrderLineMap = event.getSaleOrderLineMap();
     saleOrderLineMap.putAll(
         Beans.get(SaleOrderLineViewProductionService.class)
-            .getProductionOnLoadAttrs(saleOrderLine, saleOrderLine.getSaleOrder()));
+            .getProductionOnLoadAttrs(saleOrderLine, event.getSaleOrder()));
   }
 
   void onSaleOrderLineProductOnChange(@Observes SaleOrderLineProductOnChange event)
