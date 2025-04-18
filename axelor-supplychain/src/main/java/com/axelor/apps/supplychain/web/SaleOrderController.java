2c4bfedc7095e85710667b000fe18f863d299de7
Pierre-Arnaud Almeida <p.almeida@axelor.com>
2025-04-18 16:49:31 +0200

    91726 - Replace xml action by java

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/web/SaleOrderController.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/web/SaleOrderController.java
@@ -54,7 +54,6 @@ import com.axelor.apps.supplychain.service.saleorder.SaleOrderShipmentService;
 import com.axelor.apps.supplychain.service.saleorder.SaleOrderStockLocationService;
 import com.axelor.apps.supplychain.service.saleorder.SaleOrderStockService;
 import com.axelor.apps.supplychain.service.saleorder.SaleOrderSupplychainService;
-import com.axelor.apps.supplychain.service.saleorderline.SaleOrderLineAnalyticService;
 import com.axelor.apps.supplychain.service.saleorderline.SaleOrderLineServiceSupplyChain;
 import com.axelor.db.JPA;
 import com.axelor.db.Model;
@@ -847,15 +846,6 @@ public class SaleOrderController {
                 freightCarrierModeList, Long.valueOf(context.get("_id").toString()));
         response.setCanClose(true);
       }
-    } catch (Exception e) {
-      TraceBackService.trace(response, e);
-    }
-  }
-
-  public void checkAnalyticAxis(ActionRequest request, ActionResponse response) {
-    SaleOrder saleOrder = request.getContext().asType(SaleOrder.class);
-    try {
-      Beans.get(SaleOrderLineAnalyticService.class).checkAnalyticAxisByCompany(saleOrder);
     } catch (Exception e) {
       TraceBackService.trace(response, e);
     }
