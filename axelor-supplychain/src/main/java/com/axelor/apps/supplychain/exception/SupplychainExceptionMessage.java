3d750e7b55a553d0d967ca097b3282851bec9614
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2025-04-30 14:00:28 +0200

    RM#93067 Add limit to how many stock location line we can add in the … (#14279)
    * RM#93067 Add limit to how many stock location line we can add in the cart at once

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/exception/SupplychainExceptionMessage.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/exception/SupplychainExceptionMessage.java
@@ -411,4 +411,7 @@ public final class SupplychainExceptionMessage {
   public static final String
       SALE_ORDER_LINE_PRODUCT_WITH_NON_DEDUCTIBLE_TAX_NOT_AUTHORIZED = /*$$(*/
           "Non-deductible tax not authorized for sale orders. Please remove the non-deductible tax on product." /*)*/;
+
+  public static final String STOCK_LOCATION_TO_CART_LIMIT_EXCEEDED = /*$$(*/
+      "There is too many stock location lines to add to the cart. The limit configured is %s." /*)*/;
 }
