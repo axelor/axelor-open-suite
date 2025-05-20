54d7f9dc7285e2baadcd7fd41a127eae160f58bd
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2025-05-20 16:15:39 +0200

    93972 add budget dates management on move (#14247)

--- axelor-budget/src/main/java/com/axelor/apps/budget/web/BudgetDateController.java
+++ axelor-budget/src/main/java/com/axelor/apps/budget/web/BudgetDateController.java
@@ -2,15 +2,22 @@ package com.axelor.apps.budget.web;
 
 import com.axelor.apps.account.db.Invoice;
 import com.axelor.apps.account.db.InvoiceLine;
+import com.axelor.apps.account.db.Move;
+import com.axelor.apps.account.db.MoveLine;
 import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.service.exception.ErrorException;
 import com.axelor.apps.budget.db.Budget;
 import com.axelor.apps.budget.db.BudgetDistribution;
 import com.axelor.apps.budget.service.date.BudgetDateService;
+import com.axelor.apps.purchase.db.PurchaseOrder;
+import com.axelor.apps.purchase.db.PurchaseOrderLine;
+import com.axelor.apps.sale.db.SaleOrder;
+import com.axelor.apps.sale.db.SaleOrderLine;
 import com.axelor.common.StringUtils;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import com.axelor.rpc.Context;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +38,24 @@ public class BudgetDateController {
       toDate = invoiceLine.getBudgetToDate();
       budget = invoiceLine.getBudget();
       budgetDistributionList = invoiceLine.getBudgetDistributionList();
+    } else if (MoveLine.class.equals(request.getContext().getContextClass())) {
+      MoveLine moveLine = request.getContext().asType(MoveLine.class);
+      fromDate = moveLine.getBudgetFromDate();
+      toDate = moveLine.getBudgetToDate();
+      budget = moveLine.getBudget();
+      budgetDistributionList = moveLine.getBudgetDistributionList();
+    } else if (PurchaseOrderLine.class.equals(request.getContext().getContextClass())) {
+      PurchaseOrderLine purchaseOrderLine = request.getContext().asType(PurchaseOrderLine.class);
+      fromDate = purchaseOrderLine.getBudgetFromDate();
+      toDate = purchaseOrderLine.getBudgetToDate();
+      budget = purchaseOrderLine.getBudget();
+      budgetDistributionList = purchaseOrderLine.getBudgetDistributionList();
+    } else if (SaleOrderLine.class.equals(request.getContext().getContextClass())) {
+      SaleOrderLine saleOrderLine = request.getContext().asType(SaleOrderLine.class);
+      fromDate = saleOrderLine.getBudgetFromDate();
+      toDate = saleOrderLine.getBudgetToDate();
+      budget = saleOrderLine.getBudget();
+      budgetDistributionList = saleOrderLine.getBudgetDistributionList();
     }
 
     String labelError =
@@ -48,10 +73,18 @@ public class BudgetDateController {
   public void checkBudgetDates(ActionRequest request, ActionResponse response)
       throws AxelorException {
     String labelError = "";
-    if (Invoice.class.equals(request.getContext().getContextClass())) {
-      labelError =
-          Beans.get(BudgetDateService.class)
-              .checkBudgetDates(request.getContext().asType(Invoice.class));
+    BudgetDateService budgetDateService = Beans.get(BudgetDateService.class);
+    Context currentContext = request.getContext();
+    Class<?> currentClass = currentContext.getContextClass();
+
+    if (Invoice.class.equals(currentClass)) {
+      labelError = budgetDateService.checkBudgetDates(currentContext.asType(Invoice.class));
+    } else if (Move.class.equals(currentClass)) {
+      labelError = budgetDateService.checkBudgetDates(currentContext.asType(Move.class));
+    } else if (PurchaseOrder.class.equals(currentClass)) {
+      labelError = budgetDateService.checkBudgetDates(currentContext.asType(PurchaseOrder.class));
+    } else if (SaleOrder.class.equals(currentClass)) {
+      labelError = budgetDateService.checkBudgetDates(currentContext.asType(SaleOrder.class));
     } else {
       return;
     }
