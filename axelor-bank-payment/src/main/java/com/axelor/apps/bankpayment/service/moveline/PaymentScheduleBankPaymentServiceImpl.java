e2b909e61ae5897c535aa7f101aeb01406e50c27
nbu-axelor <132373223+nbu-axelor@users.noreply.github.com>
2025-05-26 16:56:57 +0200

    Feature RM#95648 UMR/PARTNER/BANKDETAILS : Get bankDetails with active UMR on PaymentSchedule/PaymentSession

---
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/moveline/PaymentScheduleBankPaymentServiceImpl.java
@@ -0,0 +1,55 @@
+package com.axelor.apps.bankpayment.service.moveline;
+
+import com.axelor.apps.account.db.Invoice;
+import com.axelor.apps.account.db.PaymentMode;
+import com.axelor.apps.account.db.PaymentSchedule;
+import com.axelor.apps.account.db.repo.PaymentScheduleLineRepository;
+import com.axelor.apps.account.db.repo.PaymentScheduleRepository;
+import com.axelor.apps.account.service.PaymentScheduleLineService;
+import com.axelor.apps.account.service.PaymentScheduleServiceImpl;
+import com.axelor.apps.account.service.app.AppAccountService;
+import com.axelor.apps.bankpayment.service.bankdetails.BankDetailsBankPaymentService;
+import com.axelor.apps.base.AxelorException;
+import com.axelor.apps.base.db.BankDetails;
+import com.axelor.apps.base.db.Company;
+import com.axelor.apps.base.db.Partner;
+import com.axelor.apps.base.service.PartnerService;
+import com.axelor.apps.base.service.administration.SequenceService;
+import com.google.inject.Inject;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+public class PaymentScheduleBankPaymentServiceImpl extends PaymentScheduleServiceImpl {
+    protected BankDetailsBankPaymentService bankDetailsBankPaymentService;
+
+    @Inject
+    public PaymentScheduleBankPaymentServiceImpl(AppAccountService appAccountService, PaymentScheduleLineService paymentScheduleLineService, PaymentScheduleLineRepository paymentScheduleLineRepo, SequenceService sequenceService, PaymentScheduleRepository paymentScheduleRepo, PartnerService partnerService, BankDetailsBankPaymentService bankDetailsBankPaymentService) {
+        super(appAccountService, paymentScheduleLineService, paymentScheduleLineRepo, sequenceService, paymentScheduleRepo, partnerService);
+        this.bankDetailsBankPaymentService = bankDetailsBankPaymentService;
+    }
+
+    @Override
+    public PaymentSchedule createPaymentSchedule(
+            Partner partner,
+            Invoice invoice,
+            Company company,
+            LocalDate date,
+            LocalDate startDate,
+            int nbrTerm,
+            BankDetails bankDetails,
+            PaymentMode paymentMode)
+            throws AxelorException {
+
+        PaymentSchedule paymentSchedule = super.createPaymentSchedule(partner, invoice, company, date, startDate, nbrTerm, bankDetails, paymentMode);
+
+        bankDetailsBankPaymentService
+                .getBankDetailsLinkedToActiveUmr(
+                        paymentMode, partner, company)
+                .stream()
+                .findAny()
+                .ifPresent(paymentSchedule::setBankDetails);
+
+        return paymentSchedule;
+    }
+}
