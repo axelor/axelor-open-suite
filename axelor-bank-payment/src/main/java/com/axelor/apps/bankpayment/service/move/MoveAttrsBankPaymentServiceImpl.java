f8c363b2e8fd869709ba3614889b66718f394562
nbu-axelor <132373223+nbu-axelor@users.noreply.github.com>
2025-05-26 14:12:42 +0200

    Added partnerBankDetails domain on move

---
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/move/MoveAttrsBankPaymentServiceImpl.java
@@ -0,0 +1,84 @@
+package com.axelor.apps.bankpayment.service.move;
+
+import com.axelor.apps.account.db.Move;
+import com.axelor.apps.account.db.PaymentMode;
+import com.axelor.apps.account.db.repo.JournalRepository;
+import com.axelor.apps.account.service.analytic.AnalyticAttrsService;
+import com.axelor.apps.account.service.analytic.AnalyticToolService;
+import com.axelor.apps.account.service.app.AppAccountService;
+import com.axelor.apps.account.service.config.AccountConfigService;
+import com.axelor.apps.account.service.move.MoveInvoiceTermService;
+import com.axelor.apps.account.service.move.MovePfpService;
+import com.axelor.apps.account.service.move.MoveViewHelperService;
+import com.axelor.apps.account.service.move.attributes.MoveAttrsServiceImpl;
+import com.axelor.apps.bankpayment.service.bankdetails.BankDetailsBankPaymentService;
+import com.axelor.apps.base.db.BankDetails;
+import com.axelor.apps.base.db.Company;
+import com.axelor.apps.base.db.Partner;
+import com.axelor.apps.base.db.repo.CompanyRepository;
+import com.google.inject.Inject;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.commons.collections.CollectionUtils;
+
+public class MoveAttrsBankPaymentServiceImpl extends MoveAttrsServiceImpl {
+  protected BankDetailsBankPaymentService bankDetailsBankPaymentService;
+
+  @Inject
+  public MoveAttrsBankPaymentServiceImpl(
+      AccountConfigService accountConfigService,
+      AppAccountService appAccountService,
+      MoveInvoiceTermService moveInvoiceTermService,
+      MoveViewHelperService moveViewHelperService,
+      MovePfpService movePfpService,
+      AnalyticToolService analyticToolService,
+      AnalyticAttrsService analyticAttrsService,
+      CompanyRepository companyRepository,
+      JournalRepository journalRepository,
+      BankDetailsBankPaymentService bankDetailsBankPaymentService) {
+    super(
+        accountConfigService,
+        appAccountService,
+        moveInvoiceTermService,
+        moveViewHelperService,
+        movePfpService,
+        analyticToolService,
+        analyticAttrsService,
+        companyRepository,
+        journalRepository);
+    this.bankDetailsBankPaymentService = bankDetailsBankPaymentService;
+  }
+
+  @Override
+  public void addPartnerBankDetailsDomain(Move move, Map<String, Map<String, Object>> attrsMap) {
+    Partner partner = move.getPartner();
+    String domain;
+
+    if (partner == null || CollectionUtils.isEmpty(partner.getBankDetailsList())) {
+      domain = "self = NULL";
+    } else {
+      PaymentMode paymentMode = move.getPaymentMode();
+      Company company = move.getCompany();
+      List<BankDetails> bankDetailsList =
+          bankDetailsBankPaymentService.getBankDetailsLinkedToActiveUmr(
+              paymentMode, partner, company);
+      if (bankDetailsList.isEmpty()) {
+        bankDetailsList =
+            partner.getBankDetailsList().stream()
+                .filter(BankDetails::getActive)
+                .collect(Collectors.toList());
+      }
+
+      String bankDetailsIds =
+          bankDetailsList.stream()
+              .map(BankDetails::getId)
+              .map(Object::toString)
+              .collect(Collectors.joining(","));
+
+      domain = String.format("self.id IN (%s)", bankDetailsIds);
+    }
+
+    this.addAttr("partnerBankDetails", "domain", domain, attrsMap);
+  }
+}
