6c6b438ed0be36aceba4e4f364aa51655f17c15c
vva-axelor <v.vaghela@axelor.com>
2025-05-29 14:40:46 +0530

    Feature RM#95815:DATA BACKUP : export APP

--- axelor-base/src/main/java/com/axelor/apps/base/service/DataBackupServiceImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/DataBackupServiceImpl.java
@@ -65,7 +65,7 @@ public class DataBackupServiceImpl implements DataBackupService {
   public void createBackUp(DataBackup dataBackup) {
     DataBackup obj = setStatus(dataBackup);
     if (dataBackup.getUpdateImportId()) {
-      updateImportId();
+      updateImportId(dataBackup.getIsExportApp());
     }
     executor.submit(
         new TenantAware(
@@ -172,12 +172,18 @@ public class DataBackupServiceImpl implements DataBackupService {
   }
 
   @Transactional
-  public void updateImportId() {
+  public void updateImportId(boolean isExportApp) {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyHHmm");
-    String filterStr =
-        "self.packageName NOT LIKE '%meta%' AND (self.packageName != 'com.axelor.studio.db' OR self.name LIKE 'App%') AND self.name!='DataBackup' AND self.tableName IS NOT NULL";
+    StringBuilder filter =
+        new StringBuilder(
+            "self.packageName NOT LIKE '%meta%' AND self.name != 'DataBackup' AND self.tableName IS NOT NULL");
+    filter.append(" AND (self.packageName != 'com.axelor.studio.db'");
+    if (isExportApp) {
+      filter.append(" OR self.name LIKE 'App%'");
+    }
+    filter.append(")");
 
-    List<MetaModel> metaModelList = metaModelRepo.all().filter(filterStr).fetch();
+    List<MetaModel> metaModelList = metaModelRepo.all().filter(filter.toString()).fetch();
     metaModelList.add(metaModelRepo.findByName(MetaFile.class.getSimpleName()));
     metaModelList.add(metaModelRepo.findByName(MetaJsonField.class.getSimpleName()));
 
