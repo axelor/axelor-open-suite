d9436a9a9189a62acbea4264b3e50ba11df1e3cf
nbu-axelor <132373223+nbu-axelor@users.noreply.github.com>
2025-06-03 16:38:09 +0200

    Added method to check if bankDetails is linked to an active umr

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankdetails/BankDetailsBankPaymentService.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankdetails/BankDetailsBankPaymentService.java
@@ -29,4 +29,7 @@ public interface BankDetailsBankPaymentService {
 
   List<BankDetails> getBankDetailsLinkedToActiveUmr(
       PaymentMode paymentMode, Partner partner, Company company);
+
+  boolean isBankDetailsNotLinkedToActiveUmr(
+      PaymentMode paymentMode, Partner partner, Company company, BankDetails bankDetails);
 }
