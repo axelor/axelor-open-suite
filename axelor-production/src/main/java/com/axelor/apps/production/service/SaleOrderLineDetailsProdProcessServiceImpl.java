d26e7a72b893f02c5c50d5e1d660ea9d6ccf330e
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2025-06-11 10:50:04 +0200

    RM#90504 Config to enable or not sol details prod process lines generation

--- axelor-production/src/main/java/com/axelor/apps/production/service/SaleOrderLineDetailsProdProcessServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/SaleOrderLineDetailsProdProcessServiceImpl.java
@@ -5,6 +5,7 @@ import com.axelor.apps.production.db.ProdProcess;
 import com.axelor.apps.production.db.ProdProcessLine;
 import com.axelor.apps.production.db.SaleOrderLineDetails;
 import com.axelor.apps.production.db.repo.SaleOrderLineDetailsRepository;
+import com.axelor.apps.production.service.app.AppProductionService;
 import com.axelor.apps.sale.db.SaleOrder;
 import com.axelor.apps.sale.db.SaleOrderLine;
 import com.google.inject.Inject;
@@ -18,17 +19,23 @@ public class SaleOrderLineDetailsProdProcessServiceImpl
     implements SaleOrderLineDetailsProdProcessService {
 
   protected final SolDetailsProdProcessLineMappingService solDetailsProdProcessLineMappingService;
+  protected final AppProductionService appProductionService;
 
   @Inject
   public SaleOrderLineDetailsProdProcessServiceImpl(
-      SolDetailsProdProcessLineMappingService solDetailsProdProcessLineMappingService) {
+      SolDetailsProdProcessLineMappingService solDetailsProdProcessLineMappingService,
+      AppProductionService appProductionService) {
     this.solDetailsProdProcessLineMappingService = solDetailsProdProcessLineMappingService;
+    this.appProductionService = appProductionService;
   }
 
   @Override
   public List<SaleOrderLineDetails> createSaleOrderLineDetailsFromProdProcess(
       ProdProcess prodProcess, SaleOrder saleOrder, SaleOrderLine saleOrderLine)
       throws AxelorException {
+    if (appProductionService.getAppProduction().getIsProdProcessLineGenerationInSODisabled()) {
+      return List.of();
+    }
     Objects.requireNonNull(prodProcess);
     List<SaleOrderLineDetails> originSaleOrderLineDetailsList =
         saleOrderLine.getSaleOrderLineDetailsList();
