af9ce744d9a87b3034021caa900afeb72d43a9eb
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2025-06-16 11:08:41 +0200

    Upgrade studio dependency to 3.5.0
    RM96536

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/app/AppBusinessProjectServiceImpl.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/app/AppBusinessProjectServiceImpl.java
@@ -31,9 +31,8 @@ import com.axelor.apps.project.service.app.AppProjectService;
 import com.axelor.i18n.I18n;
 import com.axelor.meta.MetaFiles;
 import com.axelor.meta.db.repo.MetaFileRepository;
-import com.axelor.meta.db.repo.MetaModelRepository;
 import com.axelor.meta.db.repo.MetaModuleRepository;
-import com.axelor.studio.app.service.AppVersionService;
+import com.axelor.meta.loader.AppVersionService;
 import com.axelor.studio.db.AppBusinessProject;
 import com.axelor.studio.db.repo.AppBusinessProjectRepository;
 import com.axelor.studio.db.repo.AppRepository;
@@ -57,7 +56,6 @@ public class AppBusinessProjectServiceImpl extends AppBaseServiceImpl
       AppRepository appRepo,
       MetaFiles metaFiles,
       AppVersionService appVersionService,
-      MetaModelRepository metaModelRepo,
       AppSettingsStudioService appSettingsService,
       MetaModuleRepository metaModuleRepo,
       MetaFileRepository metaFileRepo,
@@ -65,14 +63,7 @@ public class AppBusinessProjectServiceImpl extends AppBaseServiceImpl
       BusinessProjectConfigRepository businessProjectConfigRepository,
       CompanyRepository companyRepository,
       AppProjectService appProjectService) {
-    super(
-        appRepo,
-        metaFiles,
-        appVersionService,
-        metaModelRepo,
-        appSettingsService,
-        metaModuleRepo,
-        metaFileRepo);
+    super(appRepo, metaFiles, appVersionService, appSettingsService, metaModuleRepo, metaFileRepo);
     this.appBusinessProjectRepo = appBusinessProjectRepo;
     this.businessProjectConfigRepository = businessProjectConfigRepository;
     this.companyRepository = companyRepository;
