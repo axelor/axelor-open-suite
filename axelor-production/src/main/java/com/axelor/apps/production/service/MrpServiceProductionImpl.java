76164dbaff765fde51d217f9ffe1063ede850ee5
kra-axelor <84926674+kra-axelor@users.noreply.github.com>
2025-07-11 11:37:28 +0200

    96912: fixed not stock managed bom line (#14648)

--- axelor-production/src/main/java/com/axelor/apps/production/service/MrpServiceProductionImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/MrpServiceProductionImpl.java
@@ -543,7 +543,7 @@ public class MrpServiceProductionImpl extends MrpServiceImpl {
       } else {
         for (BillOfMaterialLine billOfMaterialLine : billOfMaterial.getBillOfMaterialLineList()) {
           Product subProduct = billOfMaterialLine.getProduct();
-          if (this.isMrpProduct(subProduct)) {
+          if (this.isMrpProduct(subProduct) && !billOfMaterialLine.getHasNoManageStock()) {
             MrpLine subProductMrpLine =
                 super.createProposalMrpLine(
                     mrp,
@@ -771,7 +771,7 @@ public class MrpServiceProductionImpl extends MrpServiceImpl {
 
         Product subProduct = billOfMaterialLine.getProduct();
 
-        if (this.isMrpProduct(subProduct)) {
+        if (this.isMrpProduct(subProduct) && !billOfMaterialLine.getHasNoManageStock()) {
           this.assignProductLevel(billOfMaterialLine, level);
 
           Company company = mrp.getStockLocation().getCompany();
