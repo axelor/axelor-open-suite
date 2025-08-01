d4fde5a043d08a4910196faa260ed778889bea81
cce-axelor <113428800+cce-axelor@users.noreply.github.com>
2025-08-01 12:06:04 +0200

    RM-98082: add new API to update project check list item update status (#14719)
    * RM-98082: add new API to update project check list item update status
    
    * RM-98082: add update parent item

---
+++ axelor-project/src/main/java/com/axelor/apps/project/rest/service/ProjectCheckListItemUpdateAPIServiceImpl.java
@@ -0,0 +1,60 @@
+package com.axelor.apps.project.rest.service;
+
+import com.axelor.apps.project.db.ProjectCheckListItem;
+import com.axelor.apps.project.db.repo.ProjectCheckListItemRepository;
+import com.axelor.apps.project.rest.dto.ProjectCheckListItemPutStructure;
+import com.google.inject.Inject;
+import com.google.inject.persist.Transactional;
+import java.util.List;
+
+public class ProjectCheckListItemUpdateAPIServiceImpl
+    implements ProjectCheckListItemUpdateAPIService {
+
+  protected ProjectCheckListItemRepository projectCheckListItemRepository;
+
+  @Inject
+  public ProjectCheckListItemUpdateAPIServiceImpl(
+      ProjectCheckListItemRepository projectCheckListItemRepository) {
+    this.projectCheckListItemRepository = projectCheckListItemRepository;
+  }
+
+  @Transactional(rollbackOn = Exception.class)
+  @Override
+  public void updateCompleteStatus(
+      ProjectCheckListItem projectCheckListItem,
+      ProjectCheckListItemPutStructure projectCheckListItemPutStructure) {
+
+    boolean isComplete = projectCheckListItemPutStructure.isComplete();
+    projectCheckListItem.setCompleted(isComplete);
+
+    updateChildren(projectCheckListItem, isComplete);
+    updateParentItem(projectCheckListItem, isComplete);
+
+    projectCheckListItemRepository.save(projectCheckListItem);
+  }
+
+  protected void updateChildren(ProjectCheckListItem projectCheckListItem, boolean isComplete) {
+    List<ProjectCheckListItem> childItems = projectCheckListItem.getProjectCheckListItemList();
+    if (childItems != null) {
+      childItems.forEach(childItem -> childItem.setCompleted(isComplete));
+    }
+  }
+
+  protected void updateParentItem(ProjectCheckListItem projectCheckListItem, boolean isComplete) {
+    ProjectCheckListItem parentItem = projectCheckListItem.getParentItem();
+
+    if (parentItem == null) {
+      return;
+    }
+    List<ProjectCheckListItem> siblings = parentItem.getProjectCheckListItemList();
+
+    if (isComplete) {
+      boolean allSiblingsCompleted = siblings.stream().allMatch(ProjectCheckListItem::getCompleted);
+      if (allSiblingsCompleted) {
+        parentItem.setCompleted(true);
+      }
+    } else {
+      parentItem.setCompleted(false);
+    }
+  }
+}
