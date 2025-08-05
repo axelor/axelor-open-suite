ddc6bbd751cd8ddb00abf7ffe4f5d74771ae8201
bsu-axelor <86819435+bsu-axelor@users.noreply.github.com>
2025-08-05 12:25:15 +0530

    Anomaly RM#96614: Add priorities on observer events (#14604)
    * Anomaly RM#98759: [SALE ORDER] Add priorities in SaleOrder events for 8.4 features
    
    * Delete changelogs/unreleased/98759.yml
    
    ---------
    
    Co-authored-by: alo-axelor <100591458+alo-axelor@users.noreply.github.com>

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/observer/SaleOrderSupplychainObserver.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/observer/SaleOrderSupplychainObserver.java
@@ -37,7 +37,7 @@ public class SaleOrderSupplychainObserver {
         Beans.get(SaleOrderConfirmSupplychainService.class).confirmProcess(saleOrder));
   }
 
-  public void copySaleOrder(@Observes SaleOrderCopy event) {
+  public void copySaleOrder(@Observes @Priority(value = 20) SaleOrderCopy event) {
     SaleOrder saleOrder = event.getSaleOrder();
     Beans.get(SaleOrderCopySupplychainService.class).copySaleOrderSupplychainProcess(saleOrder);
   }
