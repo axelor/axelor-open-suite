5d342d9f85ea67decdc4ded1b38c0d195b03d443
cma-axelor <c.malaviya@axelor.com>
2025-08-21 11:32:02 +0530

    Anomaly RM#99687: Manufacturing order: avoid requesting outsource virtual stock location when not needed

--- axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderCreatePurchaseOrderServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderCreatePurchaseOrderServiceImpl.java
@@ -47,6 +47,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import org.apache.commons.collections.CollectionUtils;
 
 public class ManufOrderCreatePurchaseOrderServiceImpl
     implements ManufOrderCreatePurchaseOrderService {
@@ -85,6 +86,9 @@ public class ManufOrderCreatePurchaseOrderServiceImpl
   public void createPurchaseOrders(ManufOrder manufOrder) throws AxelorException {
 
     List<Partner> outsourcePartners = getOutsourcePartnersForGenerationPO(manufOrder);
+    if (CollectionUtils.isEmpty(outsourcePartners)) {
+      return;
+    }
 
     List<PurchaseOrder> generatedPurchaseOrders = new ArrayList<>();
     Company company = manufOrder.getCompany();
