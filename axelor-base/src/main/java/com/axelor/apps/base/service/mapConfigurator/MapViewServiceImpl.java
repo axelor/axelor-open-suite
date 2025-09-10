83b0c24b975a1bccb0a7da9243022fa637e66c2d
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2025-09-10 17:48:17 +0200

    feat: add action view creation on map view save

---
+++ axelor-base/src/main/java/com/axelor/apps/base/service/mapConfigurator/MapViewServiceImpl.java
@@ -0,0 +1,52 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2025 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.axelor.apps.base.service.mapConfigurator;
+
+import com.axelor.apps.base.db.MapView;
+import com.axelor.meta.schema.actions.ActionView;
+import com.axelor.utils.helpers.MetaActionHelper;
+import com.google.inject.persist.Transactional;
+import javax.ws.rs.core.UriBuilder;
+
+public class MapViewServiceImpl implements MapViewService {
+
+  @Override
+  @Transactional(rollbackOn = {Exception.class})
+  public void computeMapActionView(MapView mapView) {
+
+    String actionName = String.format("menu.map.view.%d", mapView.getId());
+
+    ActionView actionView =
+        ActionView.define(mapView.getName())
+            .name(actionName)
+            .add(
+                "html",
+                UriBuilder.fromUri("base/map-viewer")
+                    .queryParam("mapId", String.valueOf(mapView.getId()))
+                    .build()
+                    .toString())
+            .get();
+
+    mapView
+        .getMetaMenu()
+        .setAction(
+            MetaActionHelper.actionToMetaAction(
+                actionView, actionName, "action-view", "axelor-base"));
+  }
+}
---
+++ axelor-base/src/main/java/com/axelor/apps/base/web/MapViewController.java
@@ -0,0 +1,43 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2025 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.axelor.apps.base.web;
+
+import com.axelor.apps.base.db.MapView;
+import com.axelor.apps.base.db.repo.MapViewRepository;
+import com.axelor.apps.base.service.mapConfigurator.MapViewService;
+import com.axelor.inject.Beans;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.google.inject.Singleton;
+import java.util.Objects;
+
+@Singleton
+public class MapViewController {
+
+  public void computeActionView(ActionRequest request, ActionResponse response) {
+    MapView _mapView = request.getContext().asType(MapView.class);
+
+    MapView mapView = Beans.get(MapViewRepository.class).find(_mapView.getId());
+
+    if (!Objects.isNull(mapView.getMetaMenu())
+        && Objects.isNull(mapView.getMetaMenu().getAction())) {
+      Beans.get(MapViewService.class).computeMapActionView(mapView);
+    }
+  }
+}
