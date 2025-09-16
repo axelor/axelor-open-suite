4771208bb74c228a780d1313052752bbd127384b
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2025-09-16 15:12:16 +0200

    RM#100800 Fix sourcesJar task for bankpayment (#14926)

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankstatementline/camt53/BankStatementLineCreationCAMT53Service.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankstatementline/camt53/BankStatementLineCreationCAMT53Service.java
@@ -1,8 +1,8 @@
 package com.axelor.apps.bankpayment.service.bankstatementline.camt53;
 
 import com.axelor.apps.bankpayment.db.BankStatement;
-import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.CashBalance3;
-import com.axelor.apps.bankpayment.xsd.bankstatement.camt_053_001_02.ReportEntry2;
+import com.axelor.apps.bankpayment.xsd.sepa.camt_053_001_02.CashBalance3;
+import com.axelor.apps.bankpayment.xsd.sepa.camt_053_001_02.ReportEntry2;
 import com.axelor.apps.base.db.BankDetails;
 
 public interface BankStatementLineCreationCAMT53Service {
