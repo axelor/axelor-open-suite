eab932680fba6790c26a0dbb8cfb5556a60f5e92
bsu-axelor <b.suvagiya@axelor.com>
2025-09-17 17:38:42 +0530

    Anomaly RM#98801: BUSINESSPROJECT/PROJECTTASK : budgetedTime modification should not update the soldTime

--- axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/projecttask/ProjectTaskComputeBusinessProjectService.java
+++ axelor-business-project/src/main/java/com/axelor/apps/businessproject/service/projecttask/ProjectTaskComputeBusinessProjectService.java
@@ -19,17 +19,10 @@
 package com.axelor.apps.businessproject.service.projecttask;
 
 import com.axelor.apps.base.AxelorException;
+import com.axelor.apps.base.db.Unit;
 import com.axelor.apps.project.db.ProjectTask;
-import java.util.Map;
 
-public interface ProjectTaskGroupBusinessService {
+public interface ProjectTaskComputeBusinessProjectService {
 
-  Map<String, Object> updateUpdatedTime(ProjectTask projectTask) throws AxelorException;
-
-  Map<String, Object> updateQuantity(ProjectTask projectTask) throws AxelorException;
-
-  Map<String, Object> updateFinancialDatas(ProjectTask projectTask) throws AxelorException;
-
-  Map<String, Object> updateFinancialDatasWithoutPriceUnits(ProjectTask projectTask)
-      throws AxelorException;
+  void computeSoldTime(ProjectTask projectTask, Unit oldTimeUnit) throws AxelorException;
 }
