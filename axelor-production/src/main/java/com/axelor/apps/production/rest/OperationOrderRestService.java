79791e325f10d28bf1a9adc3d76787c63cf3846b
bsu-axelor <b.suvagiya@axleor.com>
2025-09-19 14:04:18 +0530

    Feature RM#99972: Production API : add new request to create a new consumed product on a operation order

--- axelor-production/src/main/java/com/axelor/apps/production/rest/OperationOrderRestService.java
+++ axelor-production/src/main/java/com/axelor/apps/production/rest/OperationOrderRestService.java
@@ -19,11 +19,19 @@
 package com.axelor.apps.production.rest;
 
 import com.axelor.apps.base.AxelorException;
+import com.axelor.apps.base.db.Product;
 import com.axelor.apps.production.db.OperationOrder;
+import com.axelor.apps.stock.db.StockMoveLine;
+import com.axelor.apps.stock.db.TrackingNumber;
+import java.math.BigDecimal;
 import javax.ws.rs.core.Response;
 
 public interface OperationOrderRestService {
 
   Response updateStatusOfOperationOrder(OperationOrder operationOrder, Integer targetStatus)
+      throws AxelorException;
+
+  StockMoveLine addOperationOrderProduct(
+      Product product, BigDecimal qty, TrackingNumber trackingNumber, OperationOrder operationOrder)
       throws AxelorException;
 }
