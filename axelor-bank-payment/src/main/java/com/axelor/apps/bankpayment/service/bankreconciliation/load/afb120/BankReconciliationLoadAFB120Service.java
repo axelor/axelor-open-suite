69acca23655cbd15847def4b625dbe4b27cf6758
nbu-axelor <132373223+nbu-axelor@users.noreply.github.com>
2025-10-14 17:22:23 +0200

    Anomaly Rm#101900 BANKRECONCILIATION / BANKRECONCILIATIONLINE / BANKSTATEMENT : includeOtherBankStatements option shall only retrieve the unreconciled statement lines in the past

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankreconciliation/load/afb120/BankReconciliationLoadAFB120Service.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankreconciliation/load/afb120/BankReconciliationLoadAFB120Service.java
@@ -125,6 +125,12 @@ public class BankReconciliationLoadAFB120Service extends BankReconciliationLoadS
             .bind("lineTypeSelect", BankStatementLineAFB120Repository.LINE_TYPE_MOVEMENT)
             .order("valueDate")
             .order("sequence");
+
+    if (bankReconciliation.getIncludeOtherBankStatements()) {
+      queryFilter += " AND self.operationDate <= (:bankReconciliationToDate)";
+      bankStatementLinesQuery.bind("bankReconciliationToDate", bankReconciliation.getToDate());
+    }
+
     List<Long> existingBankStatementLineIds = getExistingBankStatementLines(bankReconciliation);
     if (!CollectionUtils.isEmpty(existingBankStatementLineIds)) {
       queryFilter += " AND self.id NOT IN (:existingBankStatementLines)";
