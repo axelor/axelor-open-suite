01ce7c27d1db8b980186318851b5c781cfde2a72
jak-axelor <j.kukadiya@axelor.com>
2025-10-20 15:16:50 +0530

    Anomaly RM#102292:MOVE : wrong value set in pfpValidateStatusSelect (#15081)

--- axelor-account/src/main/java/com/axelor/apps/account/service/move/record/MoveGroupServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/move/record/MoveGroupServiceImpl.java
@@ -198,7 +198,7 @@ public class MoveGroupServiceImpl implements MoveGroupService {
 
     if (pfpService.isManagePassedForPayment(move.getCompany())) {
       moveRecordSetService.setPfpStatus(move);
-      valuesMap.put("pfpValidateStatusSelect", move.getOriginDate());
+      valuesMap.put("pfpValidateStatusSelect", move.getPfpValidateStatusSelect());
     }
 
     if (isMassEntry) {
