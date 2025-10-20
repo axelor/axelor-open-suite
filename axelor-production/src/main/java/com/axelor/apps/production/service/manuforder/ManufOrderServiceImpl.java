95f70c839f5cb662587d2e2869786802346dd7d8
bsu-axelor <86819435+bsu-axelor@users.noreply.github.com>
2025-10-20 11:20:56 +0300

    Anomaly RM#96950: [Child MO] No link between parent and child MO (#15062)

--- axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderServiceImpl.java
@@ -1268,6 +1268,7 @@ public class ManufOrderServiceImpl implements ManufOrderService {
   }
 
   @Override
+  @Transactional
   public List<Long> planSelectedOrdersAndDiscardOthers(List<Map<String, Object>> manufOrders)
       throws AxelorException {
     List<Long> ids = new ArrayList<>();
