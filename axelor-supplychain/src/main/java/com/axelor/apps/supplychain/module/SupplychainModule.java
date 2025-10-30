fc838984ec4ffeee7ea62d27dd4bbe5575467b2d
cma-axelor <44425727+cma-axelor@users.noreply.github.com>
2025-10-30 17:35:14 +0530

    Anomaly RM#100148:SALEORDER/INVOICE/MOVE :Add a consistency check on … (#15151)
    * Anomaly RM#100148:SALEORDER/INVOICE/MOVE :Add a consistency check on fiscal position
    
    * Update 100148.yml
    
    ---------
    
    Co-authored-by: alo-axelor <100591458+alo-axelor@users.noreply.github.com>

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/module/SupplychainModule.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/module/SupplychainModule.java
@@ -296,6 +296,7 @@ import com.axelor.apps.supplychain.service.saleorder.SaleOrderVersionSupplyChain
 import com.axelor.apps.supplychain.service.saleorder.merge.SaleOrderMergingServiceSupplyChain;
 import com.axelor.apps.supplychain.service.saleorder.merge.SaleOrderMergingServiceSupplyChainImpl;
 import com.axelor.apps.supplychain.service.saleorder.merge.SaleOrderMergingViewServiceSupplyChainImpl;
+import com.axelor.apps.supplychain.service.saleorder.onchange.SaleOrderOnChangeSupplychainService;
 import com.axelor.apps.supplychain.service.saleorder.onchange.SaleOrderOnChangeSupplychainServiceImpl;
 import com.axelor.apps.supplychain.service.saleorder.onchange.SaleOrderOnLineChangeSupplyChainServiceImpl;
 import com.axelor.apps.supplychain.service.saleorder.opportunity.OpportunitySaleOrderSupplychainServiceImpl;
@@ -483,6 +484,8 @@ public class SupplychainModule extends AxelorModule {
         .to(SaleOrderLineOnChangeSupplychainServiceImpl.class);
     bind(SaleOrderInitValueServiceImpl.class).to(SaleOrderInitValueSupplychainServiceImpl.class);
     bind(SaleOrderViewServiceImpl.class).to(SaleOrderViewSupplychainServiceImpl.class);
+    bind(SaleOrderOnChangeSupplychainService.class)
+        .to(SaleOrderOnChangeSupplychainServiceImpl.class);
     bind(SaleOrderOnChangeServiceImpl.class).to(SaleOrderOnChangeSupplychainServiceImpl.class);
     bind(SaleOrderIntercoService.class).to(SaleOrderIntercoServiceImpl.class);
     bind(SaleOrderProductPrintingService.class).to(SaleOrderProductPrintingServiceImpl.class);
