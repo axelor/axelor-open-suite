ca8a29e5cb50b15f437cd05754b83ecac1890ae5
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2025-11-03 18:36:00 +0100

    RM#103096 Update EmployeeController with Java map

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/web/EmployeeController.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/web/EmployeeController.java
@@ -49,8 +49,6 @@ import java.util.Map;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import wslite.json.JSONException;
-import wslite.json.JSONObject;
 
 @Singleton
 public class EmployeeController {
@@ -58,12 +56,12 @@ public class EmployeeController {
   private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   public void showAnnualReport(ActionRequest request, ActionResponse response)
-      throws JSONException, NumberFormatException, AxelorException {
+      throws NumberFormatException, AxelorException {
 
     String employeeId = request.getContext().get("_id").toString();
-    String year = request.getContext().get("year").toString();
-    int yearId = new JSONObject(year).getInt("id");
-    String yearName = new JSONObject(year).getString("name");
+    Map<String, Object> year = (Map<String, Object>) request.getContext().get("year");
+    int yearId = (int) year.get("id");
+    String yearName = (String) year.get("name");
     User user = AuthUtils.getUser();
 
     String name =
