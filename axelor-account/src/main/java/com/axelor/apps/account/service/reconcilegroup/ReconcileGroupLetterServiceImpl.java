8af728608f74965f6d8766e366f96372ab538347
vva-axelor <v.vaghela@axelor.com>
2025-11-11 11:49:12 +0530

    Anomaly RM#103273:RECONCILE GROUP : company check

--- axelor-account/src/main/java/com/axelor/apps/account/service/reconcilegroup/ReconcileGroupLetterServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/reconcilegroup/ReconcileGroupLetterServiceImpl.java
@@ -18,13 +18,20 @@
  */
 package com.axelor.apps.account.service.reconcilegroup;
 
+import com.axelor.apps.account.db.Move;
 import com.axelor.apps.account.db.MoveLine;
 import com.axelor.apps.account.db.ReconcileGroup;
 import com.axelor.apps.account.db.repo.MoveLineRepository;
+import com.axelor.apps.account.exception.AccountExceptionMessage;
 import com.axelor.apps.account.service.moveline.MoveLineService;
 import com.axelor.apps.base.AxelorException;
+import com.axelor.apps.base.db.Company;
+import com.axelor.apps.base.db.repo.TraceBackRepository;
+import com.axelor.i18n.I18n;
 import com.google.inject.Inject;
 import java.util.List;
+import java.util.Optional;
+import org.apache.commons.collections.CollectionUtils;
 
 public class ReconcileGroupLetterServiceImpl implements ReconcileGroupLetterService {
 
@@ -40,13 +47,27 @@ public class ReconcileGroupLetterServiceImpl implements ReconcileGroupLetterServ
 
   @Override
   public void letter(ReconcileGroup reconcileGroup) throws AxelorException {
-
+    Company company = reconcileGroup.getCompany();
     List<MoveLine> moveLines =
         moveLineRepository
             .all()
             .filter("self.reconcileGroup = :reconcileGroup")
             .bind("reconcileGroup", reconcileGroup)
             .fetch();
+    if (CollectionUtils.isEmpty(moveLines)) {
+      return;
+    }
+    for (MoveLine moveLine : moveLines) {
+      Company moveCompany =
+          Optional.ofNullable(moveLine).map(MoveLine::getMove).map(Move::getCompany).orElse(null);
+      if (company != null && !company.equals(moveCompany)) {
+        throw new AxelorException(
+            TraceBackRepository.CATEGORY_INCONSISTENCY,
+            I18n.get(AccountExceptionMessage.RECONCILE_GROUP_WRONG_MOVE_LINE_COMPANY),
+            moveLine.getName(),
+            reconcileGroup.getCode());
+      }
+    }
     moveLineService.reconcileMoveLines(moveLines);
   }
 }
