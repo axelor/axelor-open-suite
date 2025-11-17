d3fe1562421f157a599857511a0c49d49e6c6818
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2025-11-17 15:25:32 +0100

    Update spotless license header

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/batch/BatchSeniorityLeaveManagement.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/batch/BatchSeniorityLeaveManagement.java
@@ -16,21 +16,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-/**
- * Axelor Business Solutions
- *
- * <p>Copyright (C) 2016 Axelor (<http://axelor.com>).
- *
- * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
- * GNU Affero General Public License, version 3, as published by the Free Software Foundation.
- *
- * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * <p>You should have received a copy of the GNU Affero General Public License along with this
- * program. If not, see <http://www.gnu.org/licenses/>.
- */
 package com.axelor.apps.hr.service.batch;
 
 import com.axelor.app.internal.AppFilter;
