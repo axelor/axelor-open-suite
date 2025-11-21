2999b4859d768e3ce9fa191aa8485d61ab7d7690
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2025-11-21 17:53:26 +0100

    101017 Reconcile : added an error when trying to pay a holdback invoi… (#15275)

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceTermServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceTermServiceImpl.java
@@ -1301,6 +1301,8 @@ public class InvoiceTermServiceImpl implements InvoiceTermService {
       throws AxelorException {
     List<InvoiceTermPayment> invoiceTermPaymentList = new ArrayList<>();
     if (invoiceTermList != null) {
+      invoiceTermToolService.checkHoldbackBeforeReconcile(invoiceTermList);
+
       BigDecimal currencyAmount =
           invoicePayment != null
               ? currencyService.getAmountCurrencyConvertedAtDate(
