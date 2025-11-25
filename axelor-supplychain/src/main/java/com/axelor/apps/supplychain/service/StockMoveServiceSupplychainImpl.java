b402709598cea9f2207aefa09e68e5f4521d076f
vva-axelor <88328290+vva-axelor@users.noreply.github.com>
2025-11-25 15:17:54 +0530

    Anomaly RM#99868:STOCKMOVE/RETURNS : The returned qty should be automatically unreserved (#15286)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockMoveServiceSupplychainImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockMoveServiceSupplychainImpl.java
@@ -582,6 +582,7 @@ public class StockMoveServiceSupplychainImpl extends StockMoveServiceImpl
     if (stockMoveLineList != null && !stockMoveLineList.isEmpty()) {
       for (StockMoveLine stockMoveLine : stockMoveLineList) {
         stockMoveLine.setQtyInvoiced(BigDecimal.ZERO);
+        stockMoveLine.setRequestedReservedQty(BigDecimal.ZERO);
       }
     }
     return newStockMove;
