3f5fd594b6b5c6d8cf72658f3f40273a931245fb
cma-axelor <c.malaviya@axelor.com>
2025-12-09 11:27:55 +0530

    Anomaly RM#104750: SALEORDER/MANUFORDER : Fix incorrect quantity on ManufOrder generated from SaleOrder

--- axelor-production/src/main/java/com/axelor/apps/production/service/productionorder/manuforder/SaleOrderLineMOGenerationSingleLineServiceImpl.java
+++ axelor-production/src/main/java/com/axelor/apps/production/service/productionorder/manuforder/SaleOrderLineMOGenerationSingleLineServiceImpl.java
@@ -84,8 +84,7 @@ public class SaleOrderLineMOGenerationSingleLineServiceImpl
       throws AxelorException {
 
     Map<BillOfMaterial, BigDecimal> subBomMapWithLineQty = new HashMap<>();
-    // One for the parent BOM (It will be multiplied by qtyRequested anyway)
-    subBomMapWithLineQty.put(billOfMaterial, BigDecimal.ONE);
+    subBomMapWithLineQty.put(billOfMaterial, billOfMaterial.getQty());
 
     Map<BillOfMaterial, ManufOrder> subBomManufOrderParentMap = new HashMap<>();
     // prevent infinite loop
