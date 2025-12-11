cce6aeb3cf144f05b22be0be142d6ffda3ebb244
Tiza Talla <tiza.talla@kouete.com>
2025-12-11 18:59:26 +0100

    fix: restrict project field editability on task creation (#116)
    - Make project field readonly for existing tasks
    - Make project field readonly when project exists in context (current or parent)
    - Allow project field editing only for new tasks without project in context (can't be prefilled)
    - Add domain filtering for projects on task creation based on user groups

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/timesheet/TimesheetLineService.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/timesheet/TimesheetLineService.java
@@ -101,5 +101,6 @@ public interface TimesheetLineService {
   void splitTimesheetLine(TimesheetLine line);
 
   void validateLine(TimesheetLine line);
+
   void cancelTimesheetLineValidation(TimesheetLine line);
 }
