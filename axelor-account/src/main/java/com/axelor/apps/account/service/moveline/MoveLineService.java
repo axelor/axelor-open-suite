52342ab45fcd4155afc972475d183d2241f297ce
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2026-01-14 13:25:28 +0100

    Anomaly 105254 MassReconcile/MoveLine : added an info message when er… (#15435)
    * Anomaly 105254 MassReconcile/MoveLine : added an info message when errors were encountered during process

--- axelor-account/src/main/java/com/axelor/apps/account/service/moveline/MoveLineService.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/moveline/MoveLineService.java
@@ -36,7 +36,7 @@ public interface MoveLineService {
 
   public void usherProcess(MoveLine moveLine);
 
-  public void reconcileMoveLinesWithCacheManagement(List<MoveLine> moveLineList)
+  public int reconcileMoveLinesWithCacheManagement(List<MoveLine> moveLineList)
       throws AxelorException;
 
   public void reconcileMoveLines(List<MoveLine> moveLineList);
