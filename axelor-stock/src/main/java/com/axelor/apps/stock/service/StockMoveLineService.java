de70a3f84f72682ea3172ca7527c4a6d477a95e2
cma-axelor <44425727+cma-axelor@users.noreply.github.com>
2026-02-16 14:01:53 +0530

    Anomaly RM#107913: STOCKMOVE : Fix available qty unit conversion (#15672)

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineService.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StockMoveLineService.java
@@ -254,7 +254,8 @@ public interface StockMoveLineService {
   public String createDomainForProduct(StockMoveLine stockMoveLine, StockMove stockMove)
       throws AxelorException;
 
-  public Map<String, Object> setAvailableStatus(StockMoveLine stockMoveLine) throws AxelorException;
+  public Map<String, Object> setAvailableStatus(StockMoveLine stockMoveLine, StockMove stockMove)
+      throws AxelorException;
 
   public List<TrackingNumber> getAvailableTrackingNumbers(StockMoveLine stockMoveLine);
 
