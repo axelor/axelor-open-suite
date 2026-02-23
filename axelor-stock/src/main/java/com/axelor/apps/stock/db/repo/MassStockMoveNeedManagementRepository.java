7a87f95719e169e6780214c83e10b8176da3d3e6
bsu-axelor <b.suvagiya@axelor.com>
2026-02-23 12:56:19 +0530

    Anomaly RM#108357: STOCK : qty fields doesn't use nbDecimalDigitForQty

--- axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/MassStockMoveNeedManagementRepository.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/MassStockMoveNeedManagementRepository.java
@@ -19,35 +19,14 @@
 package com.axelor.apps.stock.db.repo;
 
 import com.axelor.apps.base.service.app.AppBaseService;
-import com.axelor.apps.stock.db.InventoryLine;
-import com.axelor.apps.stock.service.InventoryLineService;
 import com.axelor.inject.Beans;
 import java.util.Map;
 
-public class InventoryLineManagementRepository extends InventoryLineRepository {
+public class MassStockMoveNeedManagementRepository extends MassStockMoveNeedRepository {
 
   @Override
   public Map<String, Object> populate(Map<String, Object> json, Map<String, Object> context) {
-
-    if (context.get("_model") != null
-        && context.get("_model").toString().equals(InventoryLine.class.getName())) {
-
-      Long id = (Long) json.get("id");
-      if (id != null) {
-        InventoryLine inventoryLine = find(id);
-        json.put(
-            "isPresentInStockLocation",
-            inventoryLine != null
-                && Beans.get(InventoryLineService.class).isPresentInStockLocation(inventoryLine));
-      } else {
-        json.put("isPresentInStockLocation", false);
-      }
-
-      json.put(
-          "nbDecimalDigitForUnitPrice",
-          Beans.get(AppBaseService.class).getNbDecimalDigitForUnitPrice());
-      json.put("$nbDecimalDigitForQty", Beans.get(AppBaseService.class).getNbDecimalDigitForQty());
-    }
+    json.put("$nbDecimalDigitForQty", Beans.get(AppBaseService.class).getNbDecimalDigitForQty());
     return super.populate(json, context);
   }
 }
