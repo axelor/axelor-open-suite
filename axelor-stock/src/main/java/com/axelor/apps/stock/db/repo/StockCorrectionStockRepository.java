6e1bd181cddd2be741653da01a18144f3d933b84
bsu-axelor <b.suvagiya@axelor.com>
2026-02-23 13:06:51 +0530

    Anomaly RM#108357: STOCK : qty fields doesn't use nbDecimalDigitForQty

--- axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/StockCorrectionStockRepository.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/db/repo/StockCorrectionStockRepository.java
@@ -18,7 +18,10 @@
  */
 package com.axelor.apps.stock.db.repo;
 
+import com.axelor.apps.base.service.app.AppBaseService;
 import com.axelor.apps.stock.db.StockCorrection;
+import com.axelor.inject.Beans;
+import java.util.Map;
 
 public class StockCorrectionStockRepository extends StockCorrectionRepository {
 
@@ -27,5 +30,11 @@ public class StockCorrectionStockRepository extends StockCorrectionRepository {
     StockCorrection copy = super.copy(entity, deep);
     copy.setStatusSelect(STATUS_DRAFT);
     return copy;
+  }
+
+  @Override
+  public Map<String, Object> populate(Map<String, Object> json, Map<String, Object> context) {
+    json.put("$nbDecimalDigitForQty", Beans.get(AppBaseService.class).getNbDecimalDigitForQty());
+    return super.populate(json, context);
   }
 }
