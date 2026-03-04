60b50a5902466d09dbcf99d1c7cb20b4c4098928
alo-axelor <100591458+alo-axelor@users.noreply.github.com>
2026-03-04 16:57:47 +0100

    RM#108616 Fix multi level planning with multi level bom (#15778)

---
+++ axelor-production/src/main/java/com/axelor/apps/production/service/manuforder/ManufOrderQueryService.java
@@ -0,0 +1,34 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2026 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.axelor.apps.production.service.manuforder;
+
+import com.axelor.apps.production.db.ManufOrder;
+import java.util.List;
+
+public interface ManufOrderQueryService {
+
+  /**
+   * Recursively collects the IDs of all ManufOrders descending from the given ManufOrder (children,
+   * grandchildren, etc.) via the parentMO relationship.
+   *
+   * @param manufOrder the root manufacturing order
+   * @return list of all descendant ManufOrder IDs
+   */
+  List<Long> getAllDescendantManufOrderIds(ManufOrder manufOrder);
+}
