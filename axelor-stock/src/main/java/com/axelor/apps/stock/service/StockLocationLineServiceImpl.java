d5f721a478396e0f9d51bb652e699d4e1fa874e4
bsu-axelor <86819435+bsu-axelor@users.noreply.github.com>
2026-03-16 15:25:20 +0530

    Anomaly RM#101952: Future qty compute method is based on the wrong field (#15877)

--- axelor-stock/src/main/java/com/axelor/apps/stock/service/StockLocationLineServiceImpl.java
+++ axelor-stock/src/main/java/com/axelor/apps/stock/service/StockLocationLineServiceImpl.java
@@ -733,8 +733,8 @@ public class StockLocationLineServiceImpl implements StockLocationLineService {
           unitConversionService.convert(
               incomingStockMoveLine.getUnit(),
               stockLocationLine.getUnit(),
-              incomingStockMoveLine.getRealQty(),
-              incomingStockMoveLine.getRealQty().scale(),
+              incomingStockMoveLine.getQty(),
+              incomingStockMoveLine.getQty().scale(),
               product);
       futureQty = futureQty.add(qtyToAdd);
     }
@@ -744,8 +744,8 @@ public class StockLocationLineServiceImpl implements StockLocationLineService {
           unitConversionService.convert(
               outgoingStockMoveLine.getUnit(),
               stockLocationLine.getUnit(),
-              outgoingStockMoveLine.getRealQty(),
-              outgoingStockMoveLine.getRealQty().scale(),
+              outgoingStockMoveLine.getQty(),
+              outgoingStockMoveLine.getQty().scale(),
               product);
       futureQty = futureQty.subtract(qtyToSubtract);
     }
