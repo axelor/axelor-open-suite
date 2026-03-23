816e3dcc4b1386b0f8949624e5fc2048253c6c4b
vva-axelor <88328290+vva-axelor@users.noreply.github.com>
2026-03-23 19:26:13 +0530

    Anomaly RM#109393:BANKRECONCILIATION / MOVE : we souldn't be able to update a moveline value linked to a validated bank reconciliation (#15906)

---
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/move/MoveAttrsBankPaymentServiceImpl.java
@@ -0,0 +1,98 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2026 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.axelor.apps.bankpayment.service.move;
+
+import com.axelor.apps.account.db.Move;
+import com.axelor.apps.account.db.MoveLine;
+import com.axelor.apps.account.db.repo.JournalRepository;
+import com.axelor.apps.account.service.analytic.AnalyticAttrsService;
+import com.axelor.apps.account.service.analytic.AnalyticToolService;
+import com.axelor.apps.account.service.app.AppAccountService;
+import com.axelor.apps.account.service.config.AccountConfigService;
+import com.axelor.apps.account.service.move.MoveInvoiceTermService;
+import com.axelor.apps.account.service.move.MovePfpService;
+import com.axelor.apps.account.service.move.MoveViewHelperService;
+import com.axelor.apps.account.service.move.attributes.MoveAttrsServiceImpl;
+import com.axelor.apps.bankpayment.db.repo.BankReconciliationLineRepository;
+import com.axelor.apps.bankpayment.db.repo.BankReconciliationRepository;
+import com.axelor.apps.bankpayment.service.app.AppBankPaymentService;
+import com.axelor.apps.bankpayment.service.bankdetails.BankDetailsBankPaymentService;
+import com.axelor.apps.base.db.repo.CompanyRepository;
+import com.google.inject.Inject;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.collections.CollectionUtils;
+
+public class MoveAttrsBankPaymentServiceImpl extends MoveAttrsServiceImpl {
+
+  protected BankDetailsBankPaymentService bankDetailsBankPaymentService;
+  protected AppBankPaymentService appBankPaymentService;
+  protected BankReconciliationLineRepository bankReconciliationLineRepository;
+
+  @Inject
+  public MoveAttrsBankPaymentServiceImpl(
+      AccountConfigService accountConfigService,
+      AppAccountService appAccountService,
+      MoveInvoiceTermService moveInvoiceTermService,
+      MoveViewHelperService moveViewHelperService,
+      MovePfpService movePfpService,
+      AnalyticToolService analyticToolService,
+      AnalyticAttrsService analyticAttrsService,
+      CompanyRepository companyRepository,
+      JournalRepository journalRepository,
+      BankDetailsBankPaymentService bankDetailsBankPaymentService,
+      AppBankPaymentService appBankPaymentService,
+      BankReconciliationLineRepository bankReconciliationLineRepository) {
+    super(
+        accountConfigService,
+        appAccountService,
+        moveInvoiceTermService,
+        moveViewHelperService,
+        movePfpService,
+        analyticToolService,
+        analyticAttrsService,
+        companyRepository,
+        journalRepository);
+    this.bankDetailsBankPaymentService = bankDetailsBankPaymentService;
+    this.appBankPaymentService = appBankPaymentService;
+    this.bankReconciliationLineRepository = bankReconciliationLineRepository;
+  }
+
+  @Override
+  public void addMoveLineListViewerHidden(Move move, Map<String, Map<String, Object>> attrsMap) {
+    super.addMoveLineListViewerHidden(move, attrsMap);
+
+    List<MoveLine> moveLineList = move.getMoveLineList();
+    boolean isLinkedToValidatedBankReconciliation = false;
+
+    if (!CollectionUtils.isEmpty(moveLineList)) {
+      isLinkedToValidatedBankReconciliation =
+          bankReconciliationLineRepository
+                  .all()
+                  .filter(
+                      "self.moveLine IN :moveLineList AND self.bankReconciliation.statusSelect = :statusSelect")
+                  .bind("moveLineList", moveLineList)
+                  .bind("statusSelect", BankReconciliationRepository.STATUS_VALIDATED)
+                  .count()
+              > 0;
+    }
+    this.addAttr(
+        "$bankReconciliationTag", "hidden", !isLinkedToValidatedBankReconciliation, attrsMap);
+  }
+}
