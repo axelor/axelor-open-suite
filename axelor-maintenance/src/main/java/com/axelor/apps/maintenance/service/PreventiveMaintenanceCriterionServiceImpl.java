bfa7eb7a414bdcd4267708591d4dbcb3e7ee2dca
Majda EL ABDI <m.elabdi@axelor.com>
2026-04-07 17:31:33 +0200

    RM-108679 Apply spotless

--- axelor-maintenance/src/main/java/com/axelor/apps/maintenance/service/PreventiveMaintenanceCriterionServiceImpl.java
+++ axelor-maintenance/src/main/java/com/axelor/apps/maintenance/service/PreventiveMaintenanceCriterionServiceImpl.java
@@ -1,3 +1,21 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2026 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.axelor.apps.maintenance.service;
 
 import com.axelor.apps.base.AxelorException;
@@ -29,8 +47,7 @@ public class PreventiveMaintenanceCriterionServiceImpl
   }
 
   @Override
-  public Boolean evaluateCalendarCriterion(EquipementMaintenance equipment)
-      throws AxelorException {
+  public Boolean evaluateCalendarCriterion(EquipementMaintenance equipment) throws AxelorException {
     if (equipment.getMtnEachDay() <= 0) {
       return null;
     }
@@ -98,8 +115,7 @@ public class PreventiveMaintenanceCriterionServiceImpl
     return configuredResults.stream().anyMatch(Boolean::booleanValue);
   }
 
-  protected MaintenanceRequest findLastCompletedPreventiveRequest(
-      EquipementMaintenance equipment) {
+  protected MaintenanceRequest findLastCompletedPreventiveRequest(EquipementMaintenance equipment) {
     return maintenanceRequestRepository
         .all()
         .filter(
