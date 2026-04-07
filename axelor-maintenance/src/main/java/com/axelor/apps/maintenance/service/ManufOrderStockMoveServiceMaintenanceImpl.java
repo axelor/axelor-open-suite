d86598e00dcb963ee0eeaa2508ba9b9b23387896
Majda EL ABDI <m.elabdi@axelor.com>
2026-04-07 18:24:05 +0200

    RM-96909 fix inject import

--- axelor-maintenance/src/main/java/com/axelor/apps/maintenance/service/ManufOrderStockMoveServiceMaintenanceImpl.java
+++ axelor-maintenance/src/main/java/com/axelor/apps/maintenance/service/ManufOrderStockMoveServiceMaintenanceImpl.java
@@ -44,7 +44,7 @@ import com.axelor.apps.stock.service.StockMoveToolService;
 import com.axelor.apps.stock.utils.JpaModelHelper;
 import com.axelor.apps.supplychain.service.config.SupplyChainConfigService;
 import com.axelor.i18n.I18n;
-import com.google.inject.Inject;
+import jakarta.inject.Inject;
 import org.apache.commons.collections.CollectionUtils;
 
 public class ManufOrderStockMoveServiceMaintenanceImpl extends ManufOrderStockMoveServiceImpl {
