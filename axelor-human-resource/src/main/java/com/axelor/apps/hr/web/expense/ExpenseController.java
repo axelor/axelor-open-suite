d9ab00145e949151f6399c773167856c2c738a79
cma-axelor <44425727+cma-axelor@users.noreply.github.com>
2026-04-21 18:31:11 +0530

    Feature RM#111107: HR : Add button to show accounting moves linked to expense (#16094)

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/web/expense/ExpenseController.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/web/expense/ExpenseController.java
@@ -72,6 +72,7 @@ import com.axelor.apps.hr.service.expense.ExpenseRefusalService;
 import com.axelor.apps.hr.service.expense.ExpenseToolService;
 import com.axelor.apps.hr.service.expense.ExpenseValidateService;
 import com.axelor.apps.hr.service.expense.ExpenseVentilateService;
+import com.axelor.apps.hr.service.expense.ExpenseViewService;
 import com.axelor.apps.hr.service.expense.ExpenseWorkflowService;
 import com.axelor.apps.hr.service.user.UserHrService;
 import com.axelor.auth.AuthUtils;
@@ -797,5 +798,14 @@ public class ExpenseController {
     Beans.get(ExpenseLineUpdateService.class).updateCurrencyOnLines(expense);
     response.setValue("generalExpenseLineList", expense.getGeneralExpenseLineList());
     response.setValue("kilometricExpenseLineList", expense.getKilometricExpenseLineList());
+  }
+
+  public void showRelatedMoves(ActionRequest request, ActionResponse response) {
+    try {
+      Expense expense = request.getContext().asType(Expense.class);
+      response.setView(Beans.get(ExpenseViewService.class).showMoves(expense.getId()));
+    } catch (Exception e) {
+      TraceBackService.trace(response, e, ResponseMessageType.WARNING);
+    }
   }
 }
