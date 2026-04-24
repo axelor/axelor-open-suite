8f41fba39c8710e7a3d7f4651456a9711c7e0a70
cma-axelor <44425727+cma-axelor@users.noreply.github.com>
2026-04-24 19:00:12 +0530

    Anomaly RM#111288: SUPPLYCHAIN : Make MrpServiceImpl.isMrpProduct null-safe on productTypeSelect and excludeFromMrp (#16111)

--- axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/MrpServiceImpl.java
+++ axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/MrpServiceImpl.java
@@ -1438,8 +1438,8 @@ public class MrpServiceImpl implements MrpService {
 
   public boolean isMrpProduct(Product product) {
     return product != null
-        && !product.getExcludeFromMrp()
-        && product.getProductTypeSelect().equals(ProductRepository.PRODUCT_TYPE_STORABLE);
+        && Boolean.FALSE.equals(product.getExcludeFromMrp())
+        && ProductRepository.PRODUCT_TYPE_STORABLE.equals(product.getProductTypeSelect());
   }
 
   protected void assignProductAndLevel(Set<Product> productList) throws AxelorException {
