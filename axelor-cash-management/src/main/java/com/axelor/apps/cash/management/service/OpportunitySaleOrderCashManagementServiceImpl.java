5c949ca117671698434d2fb92b00fdeb4337ca93
rth-axelor <82039632+rth-axelor@users.noreply.github.com>
2026-04-28 14:51:22 +0200

    Fix after Ticket #111299 (#16108)

--- axelor-cash-management/src/main/java/com/axelor/apps/cash/management/service/OpportunitySaleOrderCashManagementServiceImpl.java
+++ axelor-cash-management/src/main/java/com/axelor/apps/cash/management/service/OpportunitySaleOrderCashManagementServiceImpl.java
@@ -27,8 +27,8 @@ import com.axelor.apps.sale.db.repo.SaleOrderRepository;
 import com.axelor.apps.sale.service.saleorder.SaleOrderCreateService;
 import com.axelor.apps.sale.service.saleorder.status.SaleOrderWorkflowService;
 import com.axelor.apps.supplychain.service.saleorder.opportunity.OpportunitySaleOrderSupplychainServiceImpl;
+import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
-import jakarta.inject.Inject;
 
 public class OpportunitySaleOrderCashManagementServiceImpl
     extends OpportunitySaleOrderSupplychainServiceImpl {
