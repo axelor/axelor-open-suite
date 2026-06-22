e7f794ab0a37c24bf3d937ab5d2e3e0fb8e59abe
vva-axelor <88328290+vva-axelor@users.noreply.github.com>
2026-06-22 13:19:47 +0530

    Anomaly RM#114110: INVOICE : mass ventilation / anomaly message management (#16367)

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoice/InvoiceServiceImpl.java
@@ -1001,8 +1001,13 @@ public class InvoiceServiceImpl extends InvoiceRepository implements InvoiceServ
               @Override
               public void accept(Invoice invoice) throws Exception {
                 if (invoice.getStatusSelect() == statusSelect) {
-                  consumer.accept(invoice);
-                  doneCounter.increment();
+                  try {
+                    consumer.accept(invoice);
+                    doneCounter.increment();
+                  } catch (Exception e) {
+                    TraceBackService.trace(e);
+                    throw e;
+                  }
                 }
               }
             });
