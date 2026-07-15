ed0dc79e435916e1d8071fbe3c9c120ac7277aef
bsu-axelor <86819435+bsu-axelor@users.noreply.github.com>
2026-07-15 21:32:06 +0530

    Anomaly RM#115306: CRM/LEAD : Lead conversion (Partner+ Contact) does not carry over 'companyDepartment' field to the created Contact (#16495)

--- axelor-crm/src/main/java/com/axelor/apps/crm/web/ConvertLeadWizardController.java
+++ axelor-crm/src/main/java/com/axelor/apps/crm/web/ConvertLeadWizardController.java
@@ -257,6 +257,8 @@ public class ConvertLeadWizardController {
 
     contactMap.put("jobTitleFunction", lead.getJobTitleFunction());
 
+    contactMap.put("companyDepartment", lead.getCompanyDepartment());
+
     return contactMap;
   }
 
