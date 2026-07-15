da5932d2366e319f1d2b4a9d1fced6096e66c0ac
bsu-axelor <86819435+bsu-axelor@users.noreply.github.com>
2026-07-15 20:27:29 +0530

    Anomaly RM#115346: EXPENSE : NPE when refreshing an expense after adding an kilometricExpenseLine (#16496)

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/service/expense/ExpenseKilometricServiceImpl.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/service/expense/ExpenseKilometricServiceImpl.java
@@ -51,6 +51,9 @@ public class ExpenseKilometricServiceImpl implements ExpenseKilometricService {
 
   @Override
   public Product getKilometricExpenseProduct(Expense expense) throws AxelorException {
+    if (expense.getCompany() == null) {
+      return null;
+    }
     HRConfig hrConfig = hrConfigService.getHRConfig(expense.getCompany());
     return hrConfigService.getKilometricExpenseProduct(hrConfig);
   }
