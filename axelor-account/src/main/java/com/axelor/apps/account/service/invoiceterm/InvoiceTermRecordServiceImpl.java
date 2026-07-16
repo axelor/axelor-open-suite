d9e2a6f3f055f6798eea1a3e262136da003fb898
vva-axelor <88328290+vva-axelor@users.noreply.github.com>
2026-07-16 15:19:33 +0530

    Anomaly RM#114644:Invoice: changing the due date wrongly flags invoice terms as isCustomized and blocks editing a draft invoice (#16463)

--- axelor-account/src/main/java/com/axelor/apps/account/service/invoiceterm/InvoiceTermRecordServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/invoiceterm/InvoiceTermRecordServiceImpl.java
@@ -19,17 +19,8 @@
 package com.axelor.apps.account.service.invoiceterm;
 
 import com.axelor.apps.account.db.InvoiceTerm;
-import com.axelor.apps.account.service.invoice.InvoiceTermService;
-import com.google.inject.Inject;
 
 public class InvoiceTermRecordServiceImpl implements InvoiceTermRecordService {
-
-  protected InvoiceTermService invoiceTermService;
-
-  @Inject
-  public InvoiceTermRecordServiceImpl(InvoiceTermService invoiceTermService) {
-    this.invoiceTermService = invoiceTermService;
-  }
 
   @Override
   public boolean computeIsCustomized(InvoiceTerm invoiceTerm) {
