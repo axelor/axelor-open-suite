3c06f635198e79316f1570ac026ded4b1132bf74
cma-axelor <44425727+cma-axelor@users.noreply.github.com>
2026-07-24 15:17:01 +0530

    Anomaly RM#116036: ACCOUNTINGCUTOFF : Correctly reverse the sign of the tax line on the FNP reverse move (#16534)

--- axelor-account/src/main/java/com/axelor/apps/account/service/AccountingCutOffServiceImpl.java
+++ axelor-account/src/main/java/com/axelor/apps/account/service/AccountingCutOffServiceImpl.java
@@ -554,7 +554,7 @@ public class AccountingCutOffServiceImpl implements AccountingCutOffService {
                 move,
                 move.getPartner(),
                 taxAccount,
-                currencyTaxAmount,
+                currencyTaxAmount.abs(),
                 isDebit,
                 move.getDate(),
                 ++counter,
