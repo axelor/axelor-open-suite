e2d9512ed416b31963bc458c64b90f9485e9e270
cma-axelor <44425727+cma-axelor@users.noreply.github.com>
2026-08-26 21:33:42 +0530

    Anomaly RM#117043: BASE : Remove inappropriate query-cache usages (#16646)

--- axelor-base/src/main/java/com/axelor/apps/base/service/message/MessageServiceBaseImpl.java
+++ axelor-base/src/main/java/com/axelor/apps/base/service/message/MessageServiceBaseImpl.java
@@ -173,7 +173,6 @@ public class MessageServiceBaseImpl extends MessageServiceImpl implements Messag
               JPA.all(klass)
                   .filter(String.format("self.%s = :email", modelEmailLink.getEmailField()))
                   .bind("email", fromEmailAddress.getAddress())
-                  .cacheable()
                   .fetch());
         }
 
@@ -183,7 +182,6 @@ public class MessageServiceBaseImpl extends MessageServiceImpl implements Messag
               JPA.all(klass)
                   .filter(String.format("self.%s IN :emails", modelEmailLink.getEmailField()))
                   .bind("emails", emailAddresses)
-                  .cacheable()
                   .fetch());
         }
 
