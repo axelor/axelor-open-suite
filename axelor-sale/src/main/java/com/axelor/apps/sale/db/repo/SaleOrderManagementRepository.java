ea309f41644047238c5cd5f4cb8e1bc4bc474ebc
vva-axelor <88328290+vva-axelor@users.noreply.github.com>
2026-09-11 21:08:01 +0530

    Anomaly RM#98201:Archiving an order does not also archive its orderLines (#16681)

--- axelor-sale/src/main/java/com/axelor/apps/sale/db/repo/SaleOrderManagementRepository.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/db/repo/SaleOrderManagementRepository.java
@@ -42,6 +42,7 @@ import com.google.common.base.Strings;
 import jakarta.inject.Inject;
 import jakarta.persistence.PersistenceException;
 import java.util.List;
+import org.apache.commons.collections.CollectionUtils;
 
 public class SaleOrderManagementRepository extends SaleOrderRepository {
 
@@ -90,6 +91,8 @@ public class SaleOrderManagementRepository extends SaleOrderRepository {
       }
       Beans.get(SaleOrderLineComputeService.class)
           .computeLevels(saleOrder.getSaleOrderLineList(), null);
+      archiveSaleOrderLines(
+          saleOrder.getSaleOrderLineList(), Boolean.TRUE.equals(saleOrder.getArchived()));
       return super.save(saleOrder);
     } catch (Exception e) {
       TraceBackService.traceExceptionFromSaveMethod(e);
@@ -166,5 +169,17 @@ public class SaleOrderManagementRepository extends SaleOrderRepository {
       throw new PersistenceException(e.getMessage(), e);
     }
     super.remove(saleOrder);
+  }
+
+  protected void archiveSaleOrderLines(List<SaleOrderLine> saleOrderLineList, boolean archived) {
+    if (CollectionUtils.isEmpty(saleOrderLineList)) {
+      return;
+    }
+    for (SaleOrderLine saleOrderLine : saleOrderLineList) {
+      if (Boolean.TRUE.equals(saleOrderLine.getArchived()) != archived) {
+        saleOrderLine.setArchived(archived);
+      }
+      archiveSaleOrderLines(saleOrderLine.getSubSaleOrderLineList(), archived);
+    }
   }
 }
