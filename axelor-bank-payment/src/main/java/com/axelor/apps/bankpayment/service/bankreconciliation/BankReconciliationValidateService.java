91d8eb3f74e0bd0af9c6e3c5909f41eec6ae259a
cma-axelor <44425727+cma-axelor@users.noreply.github.com>
2026-09-11 21:18:11 +0530

    Anomaly RM#117989: BANKRECONCILE : Write reconciliation label and a discriminant reference in generated move origin (#16686)

--- axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankreconciliation/BankReconciliationValidateService.java
+++ axelor-bank-payment/src/main/java/com/axelor/apps/bankpayment/service/bankreconciliation/BankReconciliationValidateService.java
@@ -38,6 +38,7 @@ import com.axelor.apps.base.service.CurrencyScaleService;
 import com.axelor.apps.base.service.app.AppBaseService;
 import com.axelor.auth.AuthUtils;
 import com.axelor.inject.Beans;
+import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 import java.math.BigDecimal;
@@ -157,7 +158,6 @@ public class BankReconciliationValidateService {
 
     Partner partner = bankReconciliationLine.getPartner();
 
-    String reference = bankReconciliationLine.getReference();
     String description = bankReconciliationLine.getName();
     if (description != null && description.length() > 255) {
       description = description.substring(0, 255);
@@ -166,7 +166,7 @@ public class BankReconciliationValidateService {
     BigDecimal amount =
         currencyScaleService.getScaledValue(bankReconciliationLine, debit.add(credit));
 
-    String origin = bankReconciliation.getName() + reference != null ? " - " + reference : "";
+    String origin = computeOrigin(bankReconciliationLine);
 
     boolean isDebit = debit.compareTo(BigDecimal.ZERO) > 0;
 
@@ -225,6 +225,20 @@ public class BankReconciliationValidateService {
     bankReconciliationLine.setIsPosted(true);
 
     bankReconciliationLineService.updateBankReconciledAmounts(bankReconciliationLine);
+  }
+
+  protected String computeOrigin(BankReconciliationLine bankReconciliationLine) {
+    BankReconciliation bankReconciliation = bankReconciliationLine.getBankReconciliation();
+    BankStatementLine bankStatementLine = bankReconciliationLine.getBankStatementLine();
+    String origin = Strings.nullToEmpty(bankReconciliation.getName());
+    String reference = bankReconciliationLine.getReference();
+    if (Strings.isNullOrEmpty(reference) && bankStatementLine != null) {
+      reference = bankStatementLine.getOrigin();
+    }
+    if (!Strings.isNullOrEmpty(reference)) {
+      origin = origin + " - " + reference;
+    }
+    return origin;
   }
 
   @Transactional
